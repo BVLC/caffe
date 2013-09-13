c18d41f432c3fb519fbfaa4428c4ff4155ed1a54
Yangqing Jia <jiayq84@gmail.com>
2013-09-12 21:46:19 -0700

    converted to glog

--- src/caffeine/common.hpp
+++ src/caffeine/common.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #include <boost/shared_ptr.hpp>
+#include <glog/logging.h>
 
 #include "driver_types.h"
 
@@ -13,43 +14,7 @@ namespace caffeine {
 
 static std::ostream nullout(0);
 
-// TODO(Yangqing): make a better logging scheme
-#define LOG_IF(condition) \
-    !(condition) ? nullout : std::cout
-
-#define CHECK(condition) \
-    LOG_IF(condition) << "Check failed: " #condition " "
-
-#ifndef NDEBUG
-
-#define DCHECK(condition) CHECK(condition)
-
-#else
-
-#define DCHECK(condition)
-
-#endif  // NDEBUG
-
-
 #define CUDA_CHECK(condition) \
-    CUDA_LOG_IF(condition) << "Check failed: " #condition " "
-
-
-// TODO(Yangqing): make a better logging scheme
-#define CUDA_LOG_IF(condition) \
-    ((condition) == cudaSuccess) ? nullout : std::cout
-
-#define CUDA_CHECK(condition) \
-    CUDA_LOG_IF(condition) << "Check failed: " #condition " "
-
-#ifndef NDEBUG
-
-#define CUDA_DCHECK(condition) CUDA_CHECK(condition)
-
-#else
-
-#define CUDA_DCHECK(condition)
-
-#endif  // NDEBUG
+    CHECK((condition) == cudaSuccess)
 
 #endif  // CAFFEINE_COMMON_HPP_
