92652b373272e80c02205a8a9f194bc440ca2ce2
Yangqing Jia <jiayq84@gmail.com>
2013-09-18 23:39:16 -0700

    misc update

--- src/caffeine/test/test_gradient_check_util.cpp
+++ src/caffeine/test/test_gradient_check_util.cpp
@@ -27,10 +27,10 @@ void GradientChecker<Dtype>::CheckGradient(Layer<Dtype>& layer,
     blobs_to_check.push_back(bottom[check_bottom]);
   }
   // go through the blobs
-  LOG(ERROR) << "Checking " << blobs_to_check.size() << " blobs.";
+  //LOG(ERROR) << "Checking " << blobs_to_check.size() << " blobs.";
   for (int blobid = 0; blobid < blobs_to_check.size(); ++blobid) {
     Blob<Dtype>* current_blob = blobs_to_check[blobid];
-    LOG(ERROR) << "Blob " << blobid << ": checking " << current_blob->count()
+    //LOG(ERROR) << "Blob " << blobid << ": checking " << current_blob->count()
         << " parameters.";
     // go through the values
     for (int feat_id = 0; feat_id < current_blob->count(); ++feat_id) {
