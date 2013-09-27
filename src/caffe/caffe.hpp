4a50c39e00e08fc72e069df67b6565b3903e4915
Yangqing Jia <jiayq84@gmail.com>
2013-09-27 11:12:47 -0700

    make caffe.hpp the only header one need

---
+++ src/caffe/caffe.hpp
@@ -0,0 +1,16 @@
+// Copyright Yangqing Jia 2013
+
+#ifndef CAFFE_CAFFE_HPP_
+#define CAFFE_CAFFE_HPP_
+
+#include "caffe/common.hpp"
+#include "caffe/blob.hpp"
+#include "caffe/filler.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/layer_factory.hpp"
+#include "caffe/net.hpp"
+#include "caffe/vision_layers.hpp"
+
+#include "caffe/proto/caffe.pb.h"
+
+#endif  // CAFFE_CAFFE_HPP_
