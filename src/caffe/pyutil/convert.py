9711bb999df787edc04fbe488368a9dbaf241311
Yangqing Jia <jiayq84@gmail.com>
2013-09-29 17:58:03 -0700

    convert scripts

--- src/caffe/pyutil/convert.py
+++ src/caffe/pyutil/convert.py
@@ -12,7 +12,17 @@ def blobproto_to_array(blob):
 def array_to_blobproto(arr):
   if arr.ndim != 4:
     raise ValueError('Incorrect array shape.')
-  blob = caffe_pb2.Blob()
+  blob = caffe_pb2.BlobProto()
   blob.num, blob.channels, blob.height, blob.width = arr.shape;
   blob.data.extend(arr.flat)
   return blob
+
+def array_to_datum(arr):
+  if arr.ndim != 3:
+    raise ValueError('Incorrect array shape.')
+  if arr.dtype != np.uint8:
+    raise TypeError('Input array has to be of type uint8.')
+  datum = caffe_pb2.Datum()
+  datum.channels, datum.height, datum.width = arr.shape
+  datum.data = arr.tostring()
+  return datum
