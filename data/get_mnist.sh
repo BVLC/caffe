524e0956ef58d0f1fac0bf302de341c9eff882e0
Yangqing Jia <jiayq84@gmail.com>
2013-12-02 15:19:37 -0800

    get_mnist.sh: changed the script to generate leveldb as well.

--- data/get_mnist.sh
+++ data/get_mnist.sh
@@ -15,4 +15,9 @@ gunzip train-labels-idx1-ubyte.gz
 gunzip t10k-images-idx3-ubyte.gz
 gunzip t10k-labels-idx1-ubyte.gz
 
+echo "Creating leveldb..."
+
+../examples/convert_mnist_data.bin train-images-idx3-ubyte train-labels-idx1-ubyte mnist-train-leveldb
+../examples/convert_mnist_data.bin t10k-images-idx3-ubyte t10k-labels-idx1-ubyte mnist-test-leveldb
+
 echo "Done."
