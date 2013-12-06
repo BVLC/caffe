b0e7d645154fd98cebe972d5557c02553ec95f90
Sergey Karayev <sergeykarayev@gmail.com>
2013-12-05 16:25:05 -0800

    now using dataframes to pass data around and storing windows

--- python/caffe/imagenet/wrapper2.py
+++ python/caffe/imagenet/wrapper2.py
@@ -1,13 +1,17 @@
 """
-wrapper2.py classifies a number of images at once, optionally using
-selective search crops.
+Classify a number of images at once, optionally using the selective
+search window proposal method.
+
+This implementation follows
+  Ross Girshick, Jeff Donahue, Trevor Darrell, Jitendra Malik.
+  Rich feature hierarchies for accurate object detection and semantic
+  segmentation.
+  http://arxiv.org/abs/1311.2524
 
 The selective_search_ijcv_with_python code is available at
-https://github.com/sergeyk/selective_search_ijcv_with_python
+  https://github.com/sergeyk/selective_search_ijcv_with_python
 
 TODO:
-- [ ] make sure all batches are the same size
-- [ ] store the window coordinates with the image filenames.
 - [ ] batch up image filenames as well: don't want to load all of them into memory
 """
 import numpy as np
@@ -16,8 +20,8 @@ import sys
 import gflags
 import pandas as pd
 import time
-from skimage import io
-from skimage import transform
+import skimage.io
+import skimage.transform
 import selective_search_ijcv_with_python as selective_search
 import caffe
 
@@ -38,10 +42,11 @@ def load_image(filename):
   """
   Input:
     filename: string
+
   Output:
     image: an image of size (256 x 256 x 3) of type uint8.
   """
-  img = io.imread(filename)
+  img = skimage.io.imread(filename)
   if img.ndim == 2:
     img = np.tile(img[:, :, np.newaxis], (1, 1, 3))
   elif img.shape[2] == 4:
@@ -57,6 +62,10 @@ def format_image(image, window=None, dim=IMAGE_DIM):
       (y, x, h, w) coordinates
     dim: int
       Final width of the square image.
+
+  Output:
+    image: (3 x H x W) ndarray
+      Resized to (dim, dim).
   """
   # Crop a subimage if window is provided.
   if window is not None:
@@ -66,17 +75,27 @@ def format_image(image, window=None, dim=IMAGE_DIM):
     ]
 
   # Resize to ImageNet size, convert to BGR, subtract mean.
-  image = (transform.resize(image, (IMAGE_DIM, IMAGE_DIM)) * 255)[:, :, ::-1]
+  image = (skimage.transform.resize(image, (IMAGE_DIM, IMAGE_DIM)) * 255)[:, :, ::-1]
   image -= IMAGENET_MEAN
 
   # Resize further if needed.
   if not dim == IMAGE_DIM:
-    image = transform.resize(image, (dim, dim))
+    image = skimage.transform.resize(image, (dim, dim))
   image = image.swapaxes(1, 2).swapaxes(0, 1)
   return image
 
 
 def _assemble_images_center_only(image_fnames):
+  """
+  For each image, square the image and crop its center.
+
+  Input:
+    image_fnames: list
+
+  Output:
+    images_df: pandas.DataFrame
+      With 'image', 'filename' columns.
+  """
   all_images = []
   for image_filename in image_fnames:
     image = format_image(load_image(image_filename))
@@ -86,12 +105,27 @@ def _assemble_images_center_only(image_fnames):
               IMAGE_CENTER:IMAGE_CENTER + CROPPED_DIM],
         dtype=np.float32
     ))
-  return all_images, image_fnames
+
+  images_df = pd.DataFrame({
+    'image': all_images,
+    'filename': image_fnames
+  })
+  return images_df
 
 
 def _assemble_images_corners(image_fnames):
+  """
+  For each image, square the image and crop its center, four corners,
+  and mirrored version of the above.
+
+  Input:
+    image_fnames: list
+
+  Output:
+    images_df: pandas.DataFrame
+      With 'image', 'filename' columns.
+  """
   all_images = []
-  fnames = []
   for image_filename in image_fnames:
     image = format_image(load_image(image_filename))
     indices = [0, IMAGE_DIM - CROPPED_DIM]
@@ -110,44 +144,61 @@ def _assemble_images_corners(image_fnames):
     images[5:] = images[:5, :, :, ::-1]  # flipped versions
 
     all_images.append(images)
-    fnames.append(image_filename)
-  return all_images, fnames
+
+  images_df = pd.DataFrame({
+    'image': [row for row in images for images in all_images],
+    'filename': np.repeat(image_fnames, 10)
+  })
+  return images_df
 
 
 def _assemble_images_selective_search(image_fnames):
-  # Get window proposals.
+  """
+  Run Selective Search window proposals on all images, then for each
+  image-window pair, extract a square crop.
+
+  Input:
+    image_fnames: list
+
+  Output:
+    images_df: pandas.DataFrame
+      With 'image', 'filename' columns.
+  """
   windows_list = selective_search.get_windows(image_fnames)
 
-  #
+  data = []
   for image_fname, windows in zip(image_fnames, windows_list):
     image = load_image(image_fname)
-    for i, window in enumerate(windows):
+    for window in windows:
       data.append({
-        'image': format_image(image, window, CROPPED_DIM),
+        'image': format_image(image, window, CROPPED_DIM)[np.newaxis, :],
         'window': window,
         'filename': image_fname
       })
+
   images_df = pd.DataFrame(data)
   return images_df
 
 
-def assemble_batches(
-  image_fnames, crop_mode='center_only'):
+def assemble_batches(image_fnames, crop_mode='center_only', batch_size=256):
   """
-  Assemble list of batches, each one of at most max_batch_size subimage
-  objects.
+  Assemble DataFrame of image crops for feature computation.
 
   Input:
-      image_fnames: list of string
-      mode: string
-        'center_only': the CROPPED_DIM middle of the image is taken as is
-        'corners': take CROPPED_DIM-sized boxes at 4 corners and center of
-          the image, as well as their flipped versions: a total of 10 images
-        'selective_search': run Selective Search region proposal on the
-          image, and take each enclosing subwindow.
+    image_fnames: list of string
+    mode: string
+      'center_only': the CROPPED_DIM middle of the image is taken as is
+      'corners': take CROPPED_DIM-sized boxes at 4 corners and center of
+        the image, as well as their flipped versions: a total of 10.
+      'selective_search': run Selective Search region proposal on the
+        image, and take each enclosing subwindow.
 
   Output:
-      : list of (X x 3 x 227 x 227) ndarray, where X <= max_batch_size
+    df_batches: list of DataFrames, each one of BATCH_SIZE rows.
+      Each row has 'image', 'filename', and 'window' info.
+      Column 'image' contains (X x 3 x 227 x 227) ndarrays.
+      Column 'filename' contains source filenames.
+      If 'filename' is None, then the row is just for padding.
   """
   if crop_mode == 'center_only':
     images_df = _assemble_images_center_only(image_fnames)
@@ -163,34 +214,43 @@ def assemble_batches(
 
   # Make sure the DataFrame has a multiple of BATCH_SIZE rows:
   # just fill the extra rows with NaN filenames and all-zero images.
-  pass
+  N = images_df.shape[0]
+  remainder = N % BATCH_SIZE
+  if remainder > 0:
+    zero_image = np.zeros_like(images_df['image'].iloc[0])
+    remainder_df = pd.DataFrame([{
+      'filename': None,
+      'image': zero_image,
+      'window': [0, 0, 0, 0]
+    }] * (BATCH_SIZE - remainder))
+    images_df = images_df.append(remainder_df)
+    N = images_df.shape[0]
 
-  # then split to (N/max_batch_size, 3, CROPPED_DIM, CROPPED_DIM) chunks
-  all_fnames = np.repeat(fnames, [len(images) for images in all_images])
-  all_images = np.concatenate(all_images, 0)
-  assert(all_images.shape[0] == all_fnames.shape[0])
-
-  num_batches = 1 + int(all_images.shape[0] / BATCH_SIZE)
-  image_batches = np.array_split(all_images, num_batches, axis=0)
-  fname_batches = np.array_split(all_fnames, num_batches, axis=0)
-  return image_batches, fname_batches
+  # Split into batches of BATCH_SIZE.
+  ind = np.arange(N) / BATCH_SIZE
+  df_batches = [images_df[ind == i] for i in range(N / BATCH_SIZE)]
+  return df_batches
 
 
-def compute_feats(batch, layer='imagenet'):
+def compute_feats(images_df, layer='imagenet'):
   if layer == 'imagenet':
     num_output = 1000
   else:
     raise ValueError("Unknown layer requested: {}".format(layer))
 
-  num = batch.shape[0]
-  input_blobs = [batch]
+  num = images_df.shape[0]
+  input_blobs = [np.concatenate(images_df['image'].values)]
   output_blobs = [np.empty((num, num_output, 1, 1), dtype=np.float32)]
   print(len(input_blobs), len(output_blobs))
   print(input_blobs[0].shape, output_blobs[0].shape)
-  caffenet.Forward(input_blobs, output_blobs)
+
+  #caffenet.Forward(input_blobs, output_blobs)
   feats = [output_blobs[0][i].flatten() for i in range(len(output_blobs[0]))]
 
-  return feats
+  # Add the features and delete the images.
+  del images_df['image']
+  images_df['feat'] = feats
+  return images_df
 
 
 if __name__ == "__main__":
@@ -206,43 +266,50 @@ if __name__ == "__main__":
   gflags.DEFINE_string(
     "images_file", "", "File that contains image filenames.")
   gflags.DEFINE_string(
+    "batch_size", 256, "Number of image crops to let through in one go")
+  gflags.DEFINE_string(
     "output", "", "The output DataFrame HDF5 filename.")
   gflags.DEFINE_string(
     "layer", "imagenet", "Layer to output.")
   FLAGS = gflags.FLAGS
   FLAGS(sys.argv)
 
-  ## Initialize network by loading model definition and weights.
-  caffenet = caffe.CaffeNet(FLAGS.model_def, FLAGS.pretrained_model)
-  caffenet.set_phase_test()
-  if FLAGS.gpu:
-    caffenet.set_mode_gpu()
-
   ## Load list of image filenames and assemble into batches.
   t = time.time()
   print('Assembling batches...')
   with open(FLAGS.images_file) as f:
     image_fnames = [_.strip() for _ in f.readlines()]
-  image_batches, fname_batches = assemble_batches(
-    image_fnames, FLAGS.crop_mode)
-  print('{} batches assembled in {:.3f} s'.format(time.time() - t))
+  image_batches = assemble_batches(
+    image_fnames, FLAGS.crop_mode, FLAGS.batch_size)
+  print('{} batches assembled in {:.3f} s'.format(
+    len(image_batches), time.time() - t))
+
+  # Initialize network by loading model definition and weights.
+  t = time.time()
+  print("Loading Caffe model.")
+  caffenet = caffe.CaffeNet(FLAGS.model_def, FLAGS.pretrained_model)
+  caffenet.set_phase_test()
+  if FLAGS.gpu:
+    caffenet.set_mode_gpu()
+  print("Caffe model loaded in {:.3f} s".format(time.time() - t))
 
   # Process the batches.
   t = time.time()
-  print 'Running on {} files in {} batches'.format(
+  print 'Processing {} files in {} batches'.format(
     len(image_fnames), len(image_batches))
-  all_feats = []
+  dfs_with_feats = []
   for i in range(len(image_batches)):
     if i % 10 == 0:
-      print('Batch {}/{}, elapsed {:.3f} s'.format(
+      print('Batch {}/{}, elapsed time: {:.3f} s'.format(
         i, len(image_batches), time.time() - t))
-    all_feats.append(compute_feats(image_batches[i]))
+    dfs_with_feats.append(compute_feats(image_batches[i]))
 
-  all_feats = np.concatenate(all_feats, 0)
-  all_fnames = np.concatenate(fname_batches, 0)
+  # Concatenate, droppping the padding rows.
+  df = pd.concat(dfs_with_feats).dropna(subset=['filename'])
+  print("Processing complete after {:.3f} s.".format(time.time() - t))
 
-  df = pd.DataFrame({'feat': all_feats, 'filename': all_fnames})
-
-  # Finally, write the results.
+  # Write our the results.
+  t = time.time()
   df.to_hdf(FLAGS.output, 'df', mode='w')
-  print 'Done. Saved to %s.' % FLAGS.output
+  print("Done. Saving to {} took {:.3f} s.".format(
+    FLAGS.output, time.time() - t))
