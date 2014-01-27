0cbbe390f9c5bea47f15ac67f3916ca5ddffb682
forresti <fiandola@gmail.com>
2014-01-27 14:20:08 -0800

    fixed demo

--- src/stitch_pyramid/build/demo.sh
+++ src/stitch_pyramid/build/demo.sh
@@ -1,7 +1,7 @@
 #./stitch_pyramid ../../../images_640x480/carsgraz_001.image.jpg
 #./stitch_pyramid --padding 8 ../../images_640x480/carsgraz_001.image.jpg
 
-./test_stitch_pyramid --padding 8 --output-stitched-dir ./stitched_results ../../imagenet/pascal_009959.jpg
+./test_stitch_pyramid --padding 8 --output-stitched-dir ./stitched_results ../../../python/caffe/imagenet/pascal_009959.jpg
 
 #for paper figs:
 #./test_stitch_pyramid --padding 16 --output-stitched-dir ~/paper-writing/ICML14_dense_convnet/figures/featpyramid_figs/stitched_img ~/paper-writing/ICML14_dense_convnet/figures/bicycle.jpg
