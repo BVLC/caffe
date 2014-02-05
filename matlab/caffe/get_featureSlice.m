e0aafd0cd0f230bf1b7ac3bfc7ac9b92735472f3
forresti <fiandola@gmail.com>
2014-02-04 19:09:37 -0800

    looks like there's systematic shift of bboxes slightly left from where they should be. (when rounding)

--- matlab/caffe/get_featureSlice.m
+++ matlab/caffe/get_featureSlice.m
@@ -4,10 +4,12 @@
 % @param bbox.{x1 x2 y1 y2} -- in image coordinates. -- must be round numbers.
 % @param templateSize = desired shape of output in feature descriptor units (e.g. [6 10] root filter)
 % (could pass in image_size ... though we currently don't use it, and imsize is typically in pyra)
-function [featureSlice, scaleIdx] = get_featureSlice(pyra, bbox, templateSize)
+function [featureSlice, scaleIdx, roundedBox_in_px] = get_featureSlice(pyra, bbox, templateSize)
 
 %experiment:
 %pyra.sbin = 8;
+%pyra.padx=4;
+%pyra.pady=4;
 
   %1. select a scale in the pyramid
 
@@ -35,15 +37,30 @@ function [featureSlice, scaleIdx] = get_featureSlice(pyra, bbox, templateSize)
     %scaleIdx
 
     featureSlice = pyra.feat{scaleIdx}(bbox_to_use.y1 : bbox_to_use.y2, bbox_to_use.x1 : bbox_to_use.x2, :); 
+
+    roundedBox_in_px = bbox_mult(bbox_to_use, (pyra.sbin / scale_to_use)); %project rounded box back to img space (approx)
+    %roundedBox_in_px = bbox_add(roundedBox_in_px, -pyra.padx*pyra.sbin/scale_to_use); %assume padx==pady
+    roundedBox_in_px.x1 = roundedBox_in_px.x1 - pyra.padx*pyra.sbin/scale_to_use;
+    roundedBox_in_px.x2 = roundedBox_in_px.x2 - pyra.padx*pyra.sbin/scale_to_use;
+    roundedBox_in_px.y1 = roundedBox_in_px.y1 - pyra.pady*pyra.sbin/scale_to_use;
+    roundedBox_in_px.y2 = roundedBox_in_px.y2 - pyra.pady*pyra.sbin/scale_to_use;
 end
 
-% divide a bbox.{x1 x2 y1 y2} by some value.
+% multiply a bbox.{x1 x2 y1 y2} by some value.
 function bbox_out = bbox_mult(bbox, multiplier)
     
     bbox_out.x1 = bbox.x1 * multiplier;
     bbox_out.x2 = bbox.x2 * multiplier;
     bbox_out.y1 = bbox.y1 * multiplier;
     bbox_out.y2 = bbox.y2 * multiplier;
+end
+
+% add some value to a bbox.{x1 x2 y1 y2}.
+function bbox_out = bbox_add(bbox, num_to_add)
+    bbox_out.x1 = bbox.x1 + num_to_add;
+    bbox_out.x2 = bbox.x2 + num_to_add;
+    bbox_out.y1 = bbox.y1 + num_to_add;
+    bbox_out.y2 = bbox.y2 + num_to_add;
 end
 
 % @param scale_to_use = a scale that maps bbox to templateSize
