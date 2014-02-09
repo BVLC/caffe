2c806504155d4a0d61c94febe1523b5903fca71e
sguada <sguada@gmail.com>
2014-02-09 14:01:14 -0800

    Extract Iteration from log instead of computing it from parameters

--- scripts/parselog.sh
+++ scripts/parselog.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# Usage parselog.sh caffe.log [init_iter=0] [test_interval=1000]
+# Usage parselog.sh caffe.log 
 # It creates two files one caffe.log.test that contains the loss and test accuracy of the test and
 # another one caffe.log.loss that contains the loss computed during the training
-# Use init_iter when the log start from a previous snapshot
-# Use test_interval when the testing interval is different than 1000
+
 if [ "$#" -lt 1 ]
 then
-echo "Usage parselog.sh /path_to/caffe.log [init_iter=0] [test_interval=1000]"
+echo "Usage parselog.sh /path_to/caffe.log"
 fi
 if [ "$#" -gt 1 ]
 then
@@ -21,8 +20,10 @@ else
  INC=1000
 fi
 LOG=`basename $1`
-grep 'Test score #0' $1 | awk -v init=$INIT -v inc=$INC '{print (NR*inc+init), $8}' > aux0.txt
-grep 'Test score #1' $1 | awk '{print $8}' > aux1.txt
+grep -B 2 'Test ' $1 > aux.txt
+grep 'Iteration ' aux.txt | awk -F ',| ' '{print $7}' > aux0.txt
+grep 'Test score #0' aux.txt | awk '{print $8}' > aux1.txt
+grep 'Test score #1' aux.txt | awk '{print $8}' > aux2.txt
 grep ' loss =' $1 | awk '{print $6,$9}' | sed 's/,//g' | column -t > $LOG.loss 
-paste aux0.txt aux1.txt | column -t > $LOG.test
-rm aux0.txt aux1.txt
+paste aux0.txt aux1.txt aux2.txt | column -t > $LOG.test
+rm aux0.txt aux1.txt aux2.txt
