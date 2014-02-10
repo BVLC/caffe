843cd9d48787940e9b33e3fc5aa12b9932244a35
Kai Li <kaili_kloud@163.com>
2014-02-10 15:42:37 +0800

    Extract elapsed seconds since the start of solving from training log

--- scripts/parselog.sh
+++ scripts/parselog.sh
@@ -9,18 +9,24 @@ echo "Usage parselog.sh /path/to/your.log"
 exit
 fi
 LOG=`basename $1`
-grep -B 2 'Test ' $1 > aux.txt
+# For extraction of time since this line constains the start time
+grep '] Solving ' $1 > aux.txt
+grep -B 2 'Test ' $1 >> aux.txt
 grep 'Iteration ' aux.txt | sed  's/.*Iteration \([[:digit:]]*\).*/\1/g' > aux0.txt
 grep 'Test score #0' aux.txt | awk '{print $8}' > aux1.txt
 grep 'Test score #1' aux.txt | awk '{print $8}' > aux2.txt
-echo '# Iters TestAccuracy TestLoss'> $LOG.test
-paste aux0.txt aux1.txt aux2.txt | column -t >> $LOG.test
-rm aux.txt aux0.txt aux1.txt aux2.txt
+./extract_seconds.py aux.txt aux3.txt
+echo '# Iters Seconds TestAccuracy TestLoss'> $LOG.test
+paste aux0.txt aux3.txt aux1.txt aux2.txt | column -t >> $LOG.test
+rm aux.txt aux0.txt aux1.txt aux2.txt aux3.txt
 
-grep ', loss = ' $1 > aux.txt
+# For extraction of time since this line constains the start time
+grep '] Solving ' $1 > aux.txt
+grep ', loss = ' $1 >> aux.txt
 grep 'Iteration ' aux.txt | sed  's/.*Iteration \([[:digit:]]*\).*/\1/g' > aux0.txt
 grep ', loss = ' $1 | awk '{print $9}' > aux1.txt
 grep ', lr = ' $1 | awk '{print $9}' > aux2.txt
-echo '# Iters TrainingLoss LearningRate'> $LOG.train
-paste aux0.txt aux1.txt aux2.txt | column -t >> $LOG.train
-rm aux.txt aux0.txt aux1.txt aux2.txt
+./extract_seconds.py aux.txt aux3.txt
+echo '# Iters Seconds TrainingLoss LearningRate'> $LOG.train
+paste aux0.txt aux3.txt aux1.txt aux2.txt | column -t >> $LOG.train
+rm aux.txt aux0.txt aux1.txt aux2.txt  aux3.txt
