995351d0b678fb5d33faad393d6cfc0f8c4227f8
linmin <mavenlin@gmail.com>
2014-02-12 12:13:51 +0800

    implemented padding aware im2col and col2im functions

--- include/caffe/util/im2col.hpp
+++ include/caffe/util/im2col.hpp
@@ -25,6 +25,26 @@ void col2im_gpu(const Dtype* data_col, const int channels,
     const int height, const int width, const int psize, const int stride,
     Dtype* data_im);
 
+template <typename Dtype>
+void padded_im2col_cpu(const Dtype* data_im, const int channels,
+    const int height, const int width, const int ksize, const int pad, const int stride,
+    Dtype* data_col);
+
+template <typename Dtype>
+void padded_col2im_cpu(const Dtype* data_col, const int channels,
+    const int height, const int width, const int psize, const int pad, const int stride,
+    Dtype* data_im);
+
+template <typename Dtype>
+void padded_im2col_gpu(const Dtype* data_im, const int channels,
+    const int height, const int width, const int ksize, const int pad, const int stride,
+    Dtype* data_col);
+
+template <typename Dtype>
+void padded_col2im_gpu(const Dtype* data_col, const int channels,
+    const int height, const int width, const int psize, const int pad, const int stride,
+    Dtype* data_im);
+
 }  // namespace caffe
 
 #endif  // CAFFE_UTIL_IM2COL_HPP_
