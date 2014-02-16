72686ab8c17abb4880c903734db23a7ff3585664
Jeff Donahue <jeff.donahue@gmail.com>
2014-02-16 03:18:39 -0800

    get_split_blob_name returns a string to remove some verbosity

--- include/caffe/util/insert_splits.hpp
+++ include/caffe/util/insert_splits.hpp
@@ -16,8 +16,7 @@ void insert_splits(const NetParameter& param, NetParameter* param_split);
 void configure_split_layer(const string& blob_name,
     const int split_count, LayerConnection* split_layer_connection);
 
-void get_split_blob_name(const string& blob_name, const int split_index,
-    string* split_blob_name);
+string get_split_blob_name(const string& blob_name, const int split_index);
 
 }  // namespace caffe
 
