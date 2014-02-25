342a9111c0acae020f1647e0570b729cb293cacf
Kai Li <kaili_kloud@163.com>
2014-02-26 00:00:59 +0800

    Add state machine, boost::posix_time based cpu timer & tests for Timer

--- include/caffe/util/benchmark.hpp
+++ include/caffe/util/benchmark.hpp
@@ -3,6 +3,7 @@
 #ifndef CAFFE_UTIL_BENCHMARK_H_
 #define CAFFE_UTIL_BENCHMARK_H_
 
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <cuda_runtime.h>
 
 namespace caffe {
@@ -13,13 +14,24 @@ class Timer {
   virtual ~Timer();
   void Start();
   void Stop();
-  float ElapsedSeconds();
+  float MilliSeconds();
+  float Seconds();
+
+  inline bool initted() { return initted_; }
+  inline bool running() { return running_; }
+  inline bool has_run_at_least_once() { return has_run_at_least_once_; }
 
  protected:
+  void Init();
+
+  bool initted_;
+  bool running_;
+  bool has_run_at_least_once_;
   cudaEvent_t start_gpu_;
   cudaEvent_t stop_gpu_;
-  clock_t start_cpu_;
-  clock_t stop_cpu_;
+  boost::posix_time::ptime start_cpu_;
+  boost::posix_time::ptime stop_cpu_;
+  float elapsed_milliseconds_;
 };
 
 }  // namespace caffe
