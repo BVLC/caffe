b794f540b7bbfeff0e62823ee2fbf97cd6673daa
Jeff Donahue <jeff.donahue@gmail.com>
2014-02-25 12:47:25 -0800

    add hpp to valid cpplint extensions

--- scripts/cpplint.py
+++ scripts/cpplint.py
@@ -450,7 +450,7 @@ _line_length = 80
 
 # The allowed extensions for file names
 # This is set by --extensions flag.
-_valid_extensions = set(['cc', 'h', 'cpp', 'cu', 'cuh'])
+_valid_extensions = set(['cc', 'h', 'cpp', 'hpp', 'cu', 'cuh'])
 
 def ParseNolintSuppressions(filename, raw_line, linenum, error):
   """Updates the global list of error-suppressions.
