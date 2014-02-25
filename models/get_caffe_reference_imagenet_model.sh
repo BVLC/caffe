142dfabd2522f281159f66948e4ce7fd1ff4d22f
Evan Shelhamer <shelhamer@imaginarynumber.net>
2014-02-25 01:57:19 -0800

    fetch caffe_reference_imagenet_model

---
+++ models/get_caffe_reference_imagenet_model.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# This scripts downloads the caffe reference imagenet model
+# for ilsvrc image classification and deep feature extraction
+
+echo "Downloading..."
+
+wget -q https://www.dropbox.com/s/n3jups0gr7uj0dv/caffe_reference_imagenet_model
+
+echo "Done."
