4b273e35cc2b3c29f291d4fa96139a0260ffcde7
linmin <mavenlin@gmail.com>
2014-02-25 09:59:30 +0800

    remove the pad=0 case in conv_layer and im2col_layer

--- src/caffe/layers/im2col_layer.cpp
+++ src/caffe/layers/im2col_layer.cpp
@@ -30,13 +30,8 @@ void Im2colLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* bottom_data = bottom[0]->cpu_data();
   Dtype* top_data = (*top)[0]->mutable_cpu_data();
   for (int n = 0; n < bottom[0]->num(); ++n) {
-    if (PAD_ == 0) {
-      im2col_cpu(bottom_data + bottom[0]->offset(n), CHANNELS_, HEIGHT_,
-        WIDTH_, KSIZE_, STRIDE_, top_data + (*top)[0]->offset(n));
-    } else {
-      padded_im2col_cpu(bottom_data + bottom[0]->offset(n), CHANNELS_, HEIGHT_,
-	WIDTH_, KSIZE_, PAD_, STRIDE_, top_data + (*top)[0]->offset(n));
-    }
+    padded_im2col_cpu(bottom_data + bottom[0]->offset(n), CHANNELS_, HEIGHT_,
+                      WIDTH_, KSIZE_, PAD_, STRIDE_, top_data + (*top)[0]->offset(n));
   }
 }
 
@@ -46,13 +41,8 @@ void Im2colLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* bottom_data = bottom[0]->gpu_data();
   Dtype* top_data = (*top)[0]->mutable_gpu_data();
   for (int n = 0; n < bottom[0]->num(); ++n) {
-    if (PAD_ == 0) {
-      im2col_gpu(bottom_data + bottom[0]->offset(n), CHANNELS_, HEIGHT_,
-        WIDTH_, KSIZE_, STRIDE_, top_data + (*top)[0]->offset(n));
-    } else {
-      padded_im2col_gpu(bottom_data + bottom[0]->offset(n), CHANNELS_, HEIGHT_,
-	WIDTH_, KSIZE_, PAD_, STRIDE_, top_data + (*top)[0]->offset(n));
-    }
+    padded_im2col_gpu(bottom_data + bottom[0]->offset(n), CHANNELS_, HEIGHT_,
+                      WIDTH_, KSIZE_, PAD_, STRIDE_, top_data + (*top)[0]->offset(n));
   }
 }
 
@@ -62,13 +52,8 @@ Dtype Im2colLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
   const Dtype* top_diff = top[0]->cpu_diff();
   Dtype* bottom_diff = (*bottom)[0]->mutable_cpu_diff();
   for (int n = 0; n < top[0]->num(); ++n) {
-    if (PAD_ == 0) {
-      col2im_cpu(top_diff + top[0]->offset(n), CHANNELS_, HEIGHT_,
-        WIDTH_, KSIZE_, STRIDE_, bottom_diff + (*bottom)[0]->offset(n));
-    } else {
-      padded_col2im_cpu(top_diff + top[0]->offset(n), CHANNELS_, HEIGHT_,
-	WIDTH_, KSIZE_, PAD_, STRIDE_, bottom_diff + (*bottom)[0]->offset(n));
-    }
+    padded_col2im_cpu(top_diff + top[0]->offset(n), CHANNELS_, HEIGHT_,
+                      WIDTH_, KSIZE_, PAD_, STRIDE_, bottom_diff + (*bottom)[0]->offset(n));
   }
   return Dtype(0.);
 }
@@ -80,13 +65,8 @@ Dtype Im2colLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
   const Dtype* top_diff = top[0]->gpu_diff();
   Dtype* bottom_diff = (*bottom)[0]->mutable_gpu_diff();
   for (int n = 0; n < top[0]->num(); ++n) {
-    if (PAD_ == 0) {
-      col2im_gpu(top_diff + top[0]->offset(n), CHANNELS_, HEIGHT_,
-        WIDTH_, KSIZE_, STRIDE_, bottom_diff + (*bottom)[0]->offset(n));
-    } else {
-      padded_col2im_gpu(top_diff + top[0]->offset(n), CHANNELS_, HEIGHT_,
-	WIDTH_, KSIZE_, PAD_, STRIDE_, bottom_diff + (*bottom)[0]->offset(n));
-    }
+    padded_col2im_gpu(top_diff + top[0]->offset(n), CHANNELS_, HEIGHT_,
+                      WIDTH_, KSIZE_, PAD_, STRIDE_, bottom_diff + (*bottom)[0]->offset(n));
   }
   return Dtype(0.);
 }
