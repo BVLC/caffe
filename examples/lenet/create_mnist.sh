37cebe64496a52ebf052fdfe5e973f3255b013e7
Evan Shelhamer <shelhamer@imaginarynumber.net>
2014-02-25 16:00:50 -0800

    fix mnist, add deploy net example

--- examples/lenet/create_mnist.sh
+++ examples/lenet/create_mnist.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 # This script converts the mnist data into leveldb format.
 
-cd ../data
+$EXAMPLES=../../build/examples/lenet
+$DATA=../../data/mnist
 
 echo "Creating leveldb..."
 
 rm -rf mnist-train-leveldb
 rm -rf mnist-test-leveldb
 
-../build/examples/convert_mnist_data.bin train-images-idx3-ubyte train-labels-idx1-ubyte mnist-train-leveldb
-../build/examples/convert_mnist_data.bin t10k-images-idx3-ubyte t10k-labels-idx1-ubyte mnist-test-leveldb
+$EXAMPLES/convert_mnist_data.bin $DATA/train-images-idx3-ubyte train-labels-idx1-ubyte mnist-train-leveldb
+$EXAMPLES/convert_mnist_data.bin $DATA/t10k-images-idx3-ubyte t10k-labels-idx1-ubyte mnist-test-leveldb
 
 echo "Done."
