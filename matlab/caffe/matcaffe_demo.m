4e4518e87bc46885c2daaf1030518204ced112b3
sguada <sguada@gmail.com>
2014-02-27 00:06:34 -0800

    Merged conflicts in matcaffe_demo.m

--- matlab/caffe/matcaffe_demo.m
+++ matlab/caffe/matcaffe_demo.m
@@ -1,4 +1,4 @@
-function scores = matcaffe_demo(im, use_gpu)
+function [scores, weights] = matcaffe_demo(im, use_gpu)
 % scores = matcaffe_demo(im, use_gpu)
 %
 % Demo of the matlab wrapper using the ILSVRC network.
@@ -11,21 +11,60 @@ function scores = matcaffe_demo(im, use_gpu)
 %   scores   1000-dimensional ILSVRC score vector
 %
 % You may need to do the following before you start matlab:
-%  $ export LD_LIBRARY_PATH=/opt/intel/mkl/lib/intel64:/usr/local/cuda/lib64
-%  $ export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+%  $ export LD_LIBRARY_PATH=/opt/intel/mkl/lib/intel64:/usr/local/cuda-5.5/lib64
 % Or the equivalent based on where things are installed on your system
 %
 % Usage:
 %  im = imread('../../examples/cat.jpg');
 %  scores = matcaffe_demo(im, 1);
 %  [score, class] = max(scores);
+% Five things to be aware of:
+%   caffe uses row-major order
+%   matlab uses column-major order
+%   caffe uses BGR color channel order
+%   matlab uses RGB color channel order
+%   images need to have the data mean subtracted
 
-model_def_file = '../../models/imagenet.prototxt';
-% NOTE: you'll have to get the pre-trained ILSVRC network
-model_file = '../../models/caffe_reference_imagenet_model';
+% Data coming in from matlab needs to be in the order 
+%   [width, height, channels, images]
+% where width is the fastest dimension.
+% Data coming out from matcaffe is 
+%  [height, width, channels, images]
+% Here is the rough matlab for putting image data into the correct
+% format:
+%   % convert from uint8 to single
+%   im = single(im);
+%   % reshape to a fixed size (e.g., 227x227)
+%   im = imresize(im, [IMAGE_DIM IMAGE_DIM], 'bilinear');
+%   % permute from RGB to BGR and subtract the data mean (already in BGR)
+%   im = im(:,:,[3 2 1]) - data_mean;
+%   % flip width and height to make width the fastest dimension
+%   im = permute(im, [2 1 3]);
+
+% If you have multiple images, cat them with cat(4, ...)
+
+% The actual forward function. It takes in a cell array of 4-D arrays as
+% input and outputs a cell array. 
+
 
 % init caffe network (spews logging info)
-caffe('init', model_def_file, model_file);
+if nargin < 1
+  % For demo purposes we will use the peppers image
+  im = imread('peppers.png');
+end
+if caffe('is_initialized') == 0
+  model_def_file = '../../models/imagenet.prototxt';
+  model_file = '../../models/caffe_reference_imagenet_model';
+  if exist(model_file, 'file') == 0
+    % NOTE: you'll have to get the pre-trained ILSVRC network
+    error('You need a network model file');
+  end
+  if ~exist(model_def_file,'file')
+    % NOTE: you'll have to get network definition
+    error('You need the network prototxt definition');
+  end
+  caffe('init', model_def_file, model_file);
+end
 
 % set to use GPU or CPU
 if exist('use_gpu', 'var') && use_gpu
@@ -38,19 +77,27 @@ end
 caffe('set_phase_test');
 
 % prepare oversampled input
+% input_data is Height x Width x Channel x Num
 tic;
 input_data = {prepare_image(im)};
 toc;
 
 % do forward pass to get scores
+% scores are now Width x Height x Channels x Num
 tic;
 scores = caffe('forward', input_data);
 toc;
 
-% average output scores
-scores = reshape(scores{1}, [1000 10]);
-scores = mean(scores, 2);
+% One can permute back Width and Height.
+scores = scores{1};
+size(scores)
+scores = squeeze(scores);
+scores = mean(squeeze,2);
 
+% you can also get network weights by calling
+if nargout == 2
+  weights = caffe('get_weights');
+end
 
 % ------------------------------------------------------------------------
 function images = prepare_image(im)
