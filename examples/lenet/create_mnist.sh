93806dd5cee290c4944c87e0327bc6ae79e8b570
Evan Shelhamer <shelhamer@imaginarynumber.net>
2014-02-26 20:06:21 -0800

    fix path for mnist leveldb creation

--- examples/lenet/create_mnist.sh
+++ examples/lenet/create_mnist.sh
@@ -9,7 +9,7 @@ echo "Creating leveldb..."
 rm -rf mnist-train-leveldb
 rm -rf mnist-test-leveldb
 
-$EXAMPLES/convert_mnist_data.bin $DATA/train-images-idx3-ubyte train-labels-idx1-ubyte mnist-train-leveldb
-$EXAMPLES/convert_mnist_data.bin $DATA/t10k-images-idx3-ubyte t10k-labels-idx1-ubyte mnist-test-leveldb
+$EXAMPLES/convert_mnist_data.bin $DATA/train-images-idx3-ubyte $DATA/train-labels-idx1-ubyte mnist-train-leveldb
+$EXAMPLES/convert_mnist_data.bin $DATA/t10k-images-idx3-ubyte $DATA/t10k-labels-idx1-ubyte mnist-test-leveldb
 
 echo "Done."
