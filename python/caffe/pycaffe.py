0755e5bceebc585a890ab374c6ba4583ddd9ba80
Jonathan L Long <jonlong@cs.berkeley.edu>
2014-03-10 20:33:31 -0700

    Use an OrderedDict for caffe.Net.blobs

--- python/caffe/pycaffe.py
+++ python/caffe/pycaffe.py
@@ -1,4 +1,25 @@
+"""
+Wrap the internal caffe C++ module (_caffe.so) with a clean, Pythonic
+interface.
+"""
+
 from ._caffe import CaffeNet
+from collections import OrderedDict
 
 class Net(CaffeNet):
-    pass
+    """
+    The direct Python interface to caffe, exposing Forward and Backward
+    passes, data, gradients, and layer parameters
+    """
+    def __init__(self, param_file, pretrained_param_file):
+        super(Net, self).__init__(param_file, pretrained_param_file)
+        self._blobs = OrderedDict([(bl.name, bl)
+                                   for bl in super(Net, self).blobs])
+
+    @property
+    def blobs(self):
+        """
+        An OrderedDict (bottom to top, i.e., input to output) of network
+        blobs indexed by name
+        """
+        return self._blobs
