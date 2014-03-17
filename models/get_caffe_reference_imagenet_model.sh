9b9eb7af01af5c8683d51f5383b2eed63953a778
Kai Li <kaili_kloud@163.com>
2014-03-17 14:33:13 +0800

    Avoid repeatedly downloading caffe reference imagenet model

--- models/get_caffe_reference_imagenet_model.sh
+++ models/get_caffe_reference_imagenet_model.sh
@@ -2,8 +2,22 @@
 # This scripts downloads the caffe reference imagenet model
 # for ilsvrc image classification and deep feature extraction
 
+MODEL=caffe_reference_imagenet_model
+CHECKSUM=bf44bac4a59aa7792b296962fe483f2b
+
+if [ -f $MODEL ]; then
+  echo "Model already exists. Checking md5..."
+  checksum=`md5sum $MODEL | awk '{ print $1 }'`
+  if [ "$checksum" = "$CHECKSUM" ]; then
+    echo "Model checksum is correct. No need to download."
+    exit 0
+  else
+    echo "Model checksum is incorrect. Need to download again."
+  fi
+fi
+
 echo "Downloading..."
 
-wget -q https://www.dropbox.com/s/n3jups0gr7uj0dv/caffe_reference_imagenet_model
+wget -q https://www.dropbox.com/s/n3jups0gr7uj0dv/$MODEL
 
-echo "Done. Please check that the checksum = bf44bac4a59aa7792b296962fe483f2b."
+echo "Done. Please check that the checksum = $CHECKSUM."
