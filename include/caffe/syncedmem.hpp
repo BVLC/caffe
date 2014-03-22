f42e347eb4aa6611949b79af133c643348f5473d
Kai Li <kaili_kloud@163.com>
2014-03-22 17:50:56 +0800

    Implement and test SyncedMem::resize and reserve

--- include/caffe/syncedmem.hpp
+++ include/caffe/syncedmem.hpp
@@ -58,6 +58,8 @@ class SyncedMemory {
   thrust::host_vector<uint8_t> cpu_vector_;
   thrust::device_vector<uint8_t> gpu_vector_;
   size_t size_;
+  uint8_t resize_default_value_;
+  size_t capacity_;
   SyncedHead head_;
   bool own_cpu_data_;
 
