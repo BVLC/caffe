20e6271afe0863918b07ce1f906f5dea50ce59af
Jeff Donahue <jeff.donahue@gmail.com>
2014-03-29 00:51:49 -0700

    add cifar example using LRN_MAP (just like the cuda-convnet layers-18pct architecture) instead of LRN

---
+++ examples/cifar10/train_full_lrn_map.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+TOOLS=../../build/tools
+
+GLOG_logtostderr=1 $TOOLS/train_net.bin \
+    cifar10_full_lrn_map_solver.prototxt \
+    cifar10_full_lrn_map_iter_60000.solverstate
+
+#reduce learning rate by factor of 10
+GLOG_logtostderr=1 $TOOLS/train_net.bin \
+    cifar10_full_lrn_map_solver_lr1.prototxt \
+    cifar10_full_lrn_map_iter_60000.solverstate
+
+#reduce learning rate by factor of 10
+GLOG_logtostderr=1 $TOOLS/train_net.bin \
+    cifar10_full_lrn_map_solver_lr2.prototxt \
+    cifar10_full_lrn_map_iter_65000.solverstate
