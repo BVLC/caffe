2662e7ba757e8f2478014773fb46755d6619b4af
Kai Li <kaili_kloud@163.com>
2014-03-31 22:23:22 +0800

    Add caffe::cublasGetErrorString and redefine CUBLAS_CHECK with it

--- src/caffe/common.cpp
+++ src/caffe/common.cpp
@@ -147,4 +147,26 @@ const void* Caffe::RNG::generator() const {
   return &generator_->rng;
 }
 
+const char* cublasGetErrorString(cublasStatus_t& error) {
+  switch (error) {
+  case CUBLAS_STATUS_SUCCESS:
+    return "CUBLAS_STATUS_SUCCESS";
+  case CUBLAS_STATUS_NOT_INITIALIZED:
+    return "CUBLAS_STATUS_NOT_INITIALIZED";
+  case CUBLAS_STATUS_ALLOC_FAILED:
+    return "CUBLAS_STATUS_ALLOC_FAILED";
+  case CUBLAS_STATUS_INVALID_VALUE:
+    return "CUBLAS_STATUS_INVALID_VALUE";
+  case CUBLAS_STATUS_ARCH_MISMATCH:
+    return "CUBLAS_STATUS_ARCH_MISMATCH";
+  case CUBLAS_STATUS_MAPPING_ERROR:
+    return "CUBLAS_STATUS_MAPPING_ERROR";
+  case CUBLAS_STATUS_EXECUTION_FAILED:
+    return "CUBLAS_STATUS_EXECUTION_FAILED";
+  case CUBLAS_STATUS_INTERNAL_ERROR:
+    return "CUBLAS_STATUS_INTERNAL_ERROR";
+  }
+  return "Unknown cublas status";
+}
+
 }  // namespace caffe
