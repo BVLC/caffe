eeaa38fb4dcbcaf9eb0424d790b18da9b1fd110c
Kai Li <kaili_kloud@163.com>
2014-03-31 21:56:05 +0800

    Log error string rather than enum value in CUDA_CHECK

--- include/caffe/common.hpp
+++ include/caffe/common.hpp
@@ -26,7 +26,12 @@ private:\
 #define NOT_IMPLEMENTED LOG(FATAL) << "Not Implemented Yet"
 
 // CUDA: various checks for different function calls.
-#define CUDA_CHECK(condition) CHECK_EQ((condition), cudaSuccess)
+#define CUDA_CHECK(condition) \
+  /* Code block avoids redefinition of cudaError_t error */ \
+  do { \
+    cudaError_t error = condition; \
+    CHECK_EQ(error, cudaSuccess) << cudaGetErrorString(error); \
+  } while(0)
 #define CUBLAS_CHECK(condition) CHECK_EQ((condition), CUBLAS_STATUS_SUCCESS)
 #define CURAND_CHECK(condition) CHECK_EQ((condition), CURAND_STATUS_SUCCESS)
 
