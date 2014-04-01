56ed1f84a2921f9a8df8049fb745d202b5b1c7cf
Kai Li <kaili_kloud@163.com>
2014-04-01 10:30:40 +0800

    Add a space before the error string

--- include/caffe/common.hpp
+++ include/caffe/common.hpp
@@ -30,20 +30,20 @@ private:\
   /* Code block avoids redefinition of cudaError_t error */ \
   do { \
     cudaError_t error = condition; \
-    CHECK_EQ(error, cudaSuccess) << cudaGetErrorString(error); \
+    CHECK_EQ(error, cudaSuccess) << " " << cudaGetErrorString(error); \
   } while (0)
 
 #define CUBLAS_CHECK(condition) \
   do { \
     cublasStatus_t status = condition; \
-    CHECK_EQ(status, CUBLAS_STATUS_SUCCESS) \
+    CHECK_EQ(status, CUBLAS_STATUS_SUCCESS) << " " \
       << caffe::cublasGetErrorString(status); \
   } while (0)
 
 #define CURAND_CHECK(condition) \
   do { \
     curandStatus_t status = condition; \
-    CHECK_EQ(status, CURAND_STATUS_SUCCESS) \
+    CHECK_EQ(status, CURAND_STATUS_SUCCESS) << " " \
       << caffe::curandGetErrorString(status); \
   } while (0)
 
