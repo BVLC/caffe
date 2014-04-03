f2318d2927ac7f5161ff64873f158e5ec578d505
Kai Li <kaili_kloud@163.com>
2014-04-03 13:53:45 +0800

    Check image data param has source before joining or creating pthread

--- src/caffe/layers/image_data_layer.cu
+++ src/caffe/layers/image_data_layer.cu
@@ -12,14 +12,18 @@ template <typename Dtype>
 Dtype ImageDataLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       vector<Blob<Dtype>*>* top) {
   // First, join the thread
-  JoinPrefetchThread();
+  if (this->layer_param_.image_data_param().has_source()) {
+    JoinPrefetchThread();
+  }
   // Copy the data
   caffe_copy(prefetch_data_.count(), prefetch_data_.cpu_data(),
       (*top)[0]->mutable_gpu_data());
   caffe_copy(prefetch_label_.count(), prefetch_label_.cpu_data(),
       (*top)[1]->mutable_gpu_data());
-  // Start a new prefetch thread
-  CreatePrefetchThread();
+  if (this->layer_param_.image_data_param().has_source()) {
+    // Start a new prefetch thread
+    CreatePrefetchThread();
+  }
   return Dtype(0.);
 }
 
