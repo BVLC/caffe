4e31fa071d9d2476d36bdf504108f3613a2cce5b
Jonathan L Long <jonlong@cs.berkeley.edu>
2014-04-03 22:21:13 -0700

    switch from inheritance to directly overriding methods for caffe.Net
    Also, internally drop the "Caffe" prefix for classes exported by the
    internal _caffe module. This change means that SGDSolver will
    automatically return a Net with the "nice" Python interface rather than
    the internal one, (and avoids the extra layer of method wrapping that
    results from switching to composition).

--- python/caffe/pycaffe.py
+++ python/caffe/pycaffe.py
@@ -3,26 +3,31 @@ Wrap the internal caffe C++ module (_caffe.so) with a clean, Pythonic
 interface.
 """
 
-from ._caffe import CaffeNet, SGDSolver
+from ._caffe import Net, SGDSolver
 from collections import OrderedDict
 
-class Net(CaffeNet):
-    """
-    The direct Python interface to caffe, exposing Forward and Backward
-    passes, data, gradients, and layer parameters
-    """
-    def __init__(self, param_file, pretrained_param_file):
-        super(Net, self).__init__(param_file, pretrained_param_file)
-        self._blobs = OrderedDict([(bl.name, bl)
-                                   for bl in super(Net, self).blobs])
-        self.params = OrderedDict([(lr.name, lr.blobs)
-                                   for lr in super(Net, self).layers
-                                   if len(lr.blobs) > 0])
+# we directly update methods from Net here (rather than using composition or
+# inheritance) so that nets created by caffe (e.g., by SGDSolver) will
+# automatically have the improved interface
 
-    @property
-    def blobs(self):
-        """
-        An OrderedDict (bottom to top, i.e., input to output) of network
-        blobs indexed by name
-        """
-        return self._blobs
+@property
+def _Net_blobs(self):
+    """
+    An OrderedDict (bottom to top, i.e., input to output) of network
+    blobs indexed by name
+    """
+    return OrderedDict([(bl.name, bl) for bl in self._blobs])
+
+Net.blobs = _Net_blobs
+
+@property
+def _Net_params(self):
+    """
+    An OrderedDict (bottom to top, i.e., input to output) of network
+    parameters indexed by name; each is a list of multiple blobs (e.g.,
+    weights and biases)
+    """
+    return OrderedDict([(lr.name, lr.blobs) for lr in self.layers
+                                            if len(lr.blobs) > 0])
+
+Net.params = _Net_params
