7fb71deeb80bb538134e445d298517522823b90c
Jeff Donahue <jeff.donahue@gmail.com>
2014-04-11 18:07:08 -0700

    add Adopt{Data,Diff} methods to blobs to enable "virtual copying"

--- src/caffe/layers/split_layer.cu
+++ src/caffe/layers/split_layer.cu
@@ -11,13 +11,8 @@ namespace caffe {
 template <typename Dtype>
 Dtype SplitLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       vector<Blob<Dtype>*>* top) {
-  const Dtype* bottom_data = bottom[0]->gpu_data();
   for (int i = 0; i < top->size(); ++i) {
-    if (i == 0 && (*top)[i] == bottom[0]) {
-      continue;
-    }
-    Dtype* top_data = (*top)[i]->mutable_gpu_data();
-    caffe_gpu_copy(count_, bottom_data, top_data);
+    (*top)[i]->AdoptData(*bottom[0]);
   }
   return Dtype(0.);
 }
@@ -26,17 +21,11 @@ template <typename Dtype>
 void SplitLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
       const bool propagate_down, vector<Blob<Dtype>*>* bottom) {
   if (propagate_down) {
-    const Dtype* top_diff = top[0]->gpu_diff();
-    Dtype* bottom_diff = (*bottom)[0]->mutable_gpu_diff();
-    // Initialize by copying first top blob diff to our diff, unless we're
-    // doing in-place computation for the first blob, in which case the diff is
-    // already initialized.
-    if (top[0] != (*bottom)[0]) {
-      caffe_gpu_copy(count_, top_diff, bottom_diff);
-    }
+    (*bottom)[0]->AdoptDiff(*top[0]);
     // Add remaining top blob diffs.
+    Dtype* bottom_diff = (*bottom)[0]->mutable_gpu_diff();
     for (int i = 1; i < top.size(); ++i) {
-      top_diff = top[i]->gpu_diff();
+      const Dtype* top_diff = top[i]->gpu_diff();
       caffe_gpu_axpy(count_, Dtype(1.), top_diff, bottom_diff);
     }
   }
