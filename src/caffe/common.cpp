46e203681786f88539635c9d20807db26a21bbcd
Jeff Donahue <jeff.donahue@gmail.com>
2014-04-28 21:50:00 -0700

    rollback 8368818, does not build

--- src/caffe/common.cpp
+++ src/caffe/common.cpp
@@ -155,8 +155,6 @@ const char* cublasGetErrorString(cublasStatus_t error) {
     return "CUBLAS_STATUS_EXECUTION_FAILED";
   case CUBLAS_STATUS_INTERNAL_ERROR:
     return "CUBLAS_STATUS_INTERNAL_ERROR";
-  case CUBLAS_STATUS_NOT_SUPPORTED:
-    return "CUBLAS_STATUS_NOT_SUPPORTED";
   }
   return "Unknown cublas status";
 }
