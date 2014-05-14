4f121dfcdf2581a7e99eff24ee57af85cc5ca485
Kai Li <kaili_kloud@163.com>
2014-05-14 16:18:26 +0800

    Wrap the CPU and GPU math functions in math backend classes

---
+++ src/caffe/util/math_backends.cpp
@@ -0,0 +1,29 @@
+// Copyright 2014 BVLC and contributors.
+
+#include "caffe/common.hpp"
+#include "caffe/util/math_backends.hpp"
+
+namespace caffe {
+
+template<typename Dtype>
+MathBackend<Dtype>* MathBackendFactory<Dtype>::GetInstance() {
+	switch (Caffe::mode()) {
+	case Caffe::CPU:
+		return cpu_math_backend_;
+	case Caffe::GPU:
+		return gpu_math_backend_;
+	default:
+		LOG(FATAL) << "Unknown caffe mode.";
+		return static_cast<MathBackend<Dtype>*>(NULL);
+	}
+}
+template<typename Dtype>
+MathBackend<Dtype>* MathBackendFactory<Dtype>::cpu_math_backend_ =
+		new CPUMathBackend<Dtype>();
+template<typename Dtype>
+MathBackend<Dtype>* MathBackendFactory<Dtype>::gpu_math_backend_ =
+		new GPUMathBackend<Dtype>();
+
+INSTANTIATE_CLASS(MathBackendFactory);
+
+}  // namespace caffe
