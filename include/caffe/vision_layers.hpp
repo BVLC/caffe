0e65f7f1b959f1e584653278993da733c33051da
Kai Li <kaili_kloud@163.com>
2014-05-15 18:24:13 +0800

    Directly implement device neutral Forward and Backward in ConcatLayer

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -74,21 +74,6 @@ class ConcatLayer : public Layer<Dtype> {
   virtual inline int ExactNumTopBlobs() const { return 1; }
 
  protected:
-  virtual Dtype Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      vector<Blob<Dtype>*>* top);
-  virtual Dtype Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      vector<Blob<Dtype>*>* top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, vector<Blob<Dtype>*>* bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, vector<Blob<Dtype>*>* bottom);
-
-  virtual Dtype Forward_xpu(const vector<Blob<Dtype>*>& bottom,
-                            vector<Blob<Dtype>*>* top);
-  virtual void Backward_xpu(const vector<Blob<Dtype>*>& top,
-                            const bool propagate_down,
-                            vector<Blob<Dtype>*>* bottom);
-
   Blob<Dtype> col_bob_;
   int count_;
   int num_;
