d2042435b459b9128c2ccb1bf0c445aae818f88a
Adam Kosiorek <a.kosiorek@samsung.com>
2014-05-21 10:10:15 +0200

    Enabled choosing test device at configuration time *Set via cmake -DCUDA_TEST_DEVICE= option *Still possible to specify a test device at test execution *CMakes generates a header file with the test device number

--- src/caffe/test/test_caffe_main.cpp
+++ src/caffe/test/test_caffe_main.cpp
@@ -5,6 +5,8 @@
 
 #include "test_caffe_main.hpp"
 
+#include "cuda_test_device.h"
+
 namespace caffe {
   cudaDeviceProp CAFFE_TEST_CUDA_PROP;
 }
@@ -15,15 +17,26 @@ int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   ::google::InitGoogleLogging(argv[0]);
   // Before starting testing, let's first print out a few cuda defice info.
-  int device;
-  cudaGetDeviceCount(&device);
-  cout << "Cuda number of devices: " << device << endl;
+  int deviceCount;
+  cudaGetDeviceCount(&deviceCount);
+  cout << "Cuda number of devices: " << deviceCount << endl;
+
+  // Use a default in case of no external configuration
+  int device = 0;
   if (argc > 1) {
+
     // Use the given device
     device = atoi(argv[1]);
-    cudaSetDevice(device);
-    cout << "Setting to use device " << device << endl;
+
+  } else if (CUDA_TEST_DEVICE >= 0) {
+
+	  // Use the device assigned in build configuration; but with a lower priority
+	  device = CUDA_TEST_DEVICE;
   }
+
+  cudaSetDevice(device);
+  cout << "Setting to use device " << device << endl;
+
   cudaGetDevice(&device);
   cout << "Current device id: " << device << endl;
   cudaGetDeviceProperties(&CAFFE_TEST_CUDA_PROP, device);
