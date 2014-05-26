740820b2fa86b0516da9ef9a53c2733565baf194
Kai Li <kaili_kloud@163.com>
2014-05-26 09:37:18 +0800

    Add im2col and col2im to wrap im2col_{cpu, gpu} and col2im_{cpu, gpu}

--- include/caffe/util/im2col.hpp
+++ include/caffe/util/im2col.hpp
@@ -1,6 +1,8 @@
 #ifndef _CAFFE_UTIL_IM2COL_HPP_
 #define _CAFFE_UTIL_IM2COL_HPP_
 
+#include "caffe/common.hpp"
+
 namespace caffe {
 
 template <typename Dtype>
@@ -26,6 +28,38 @@ void col2im_gpu(const Dtype* data_col, const int channels,
     const int height, const int width, const int patch_h, const int patch_w,
     const int pad_h, const int pad_w, const int stride_h,
     const int stride_w, Dtype* data_im);
+
+template <typename Dtype>
+inline void im2col(const Dtype* data_im, const int channels,
+    const int height, const int width, const int ksize, const int pad,
+    const int stride, Dtype* data_col) {
+  switch (Caffe::mode()) {
+    case Caffe::CPU:
+      im2col_cpu(data_im, channels, height, width, ksize, pad, stride,
+                 data_col);
+    case Caffe::GPU:
+      im2col_gpu(data_im, channels, height, width, ksize, pad, stride,
+                 data_col);
+    default:
+      LOG(FATAL) << "Unknown caffe mode.";
+  }
+}
+
+template <typename Dtype>
+inline void col2im(const Dtype* data_col, const int channels,
+    const int height, const int width, const int psize, const int pad,
+    const int stride, Dtype* data_im) {
+  switch (Caffe::mode()) {
+    case Caffe::CPU:
+      im2col_cpu(data_col, channels, height, width, psize, pad, stride,
+                 data_im);
+    case Caffe::GPU:
+      im2col_gpu(data_col, channels, height, width, psize, pad, stride,
+                 data_im);
+    default:
+      LOG(FATAL) << "Unknown caffe mode.";
+  }
+}
 
 }  // namespace caffe
 
