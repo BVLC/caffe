e9e63398875dfc997c57e9ced7f4c8e55f535f6b
Jeff Donahue <jeff.donahue@gmail.com>
2014-06-05 17:20:30 -0700

    Make TanH cleaner, more efficient, and possible to use in-place

--- src/caffe/layers/tanh_layer.cpp
+++ src/caffe/layers/tanh_layer.cpp
@@ -18,8 +18,8 @@ Dtype TanHLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   Dtype exp2x;
   const int count = bottom[0]->count();
   for (int i = 0; i < count; ++i) {
-    exp2x = exp(2*bottom_data[i]);
-    top_data[i] = (exp2x - Dtype(1))/(exp2x + Dtype(1));
+    exp2x = exp(2 * bottom_data[i]);
+    top_data[i] = (exp2x - Dtype(1)) / (exp2x + Dtype(1));
   }
   return Dtype(0);
 }
@@ -29,16 +29,15 @@ void TanHLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const bool propagate_down,
     vector<Blob<Dtype>*>* bottom) {
   if (propagate_down) {
-    const Dtype* bottom_data = (*bottom)[0]->cpu_data();
+    const Dtype* top_data = top[0]->cpu_data();
     const Dtype* top_diff = top[0]->cpu_diff();
     Dtype* bottom_diff = (*bottom)[0]->mutable_cpu_diff();
     const int count = (*bottom)[0]->count();
     Dtype exp2x;
     Dtype tanhx;
     for (int i = 0; i < count; ++i) {
-      exp2x = exp(2*bottom_data[i]);
-      tanhx = (exp2x - Dtype(1))/(exp2x + Dtype(1));
-      bottom_diff[i] = top_diff[i] * (1 - tanhx*tanhx);
+      tanhx = top_data[i];
+      bottom_diff[i] = top_diff[i] * (1 - tanhx * tanhx);
     }
   }
 }
