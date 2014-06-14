a5235c3a1f6189b1125f3b8097bbf790730e7ed3
Sergio <sguada@gmail.com>
2014-06-14 08:39:35 -0700

    Added test to Dropout to check gradients during Test phase

--- src/caffe/layers/dropout_layer.cu
+++ src/caffe/layers/dropout_layer.cu
@@ -66,7 +66,8 @@ void DropoutLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
           static_cast<const unsigned int*>(rand_vec_->gpu_data());
       const int count = (*bottom)[0]->count();
       // NOLINT_NEXT_LINE(whitespace/operators)
-      DropoutBackward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+      DropoutBackward<Dtype><<<CAFFE_GET_BLOCKS(count),
+        CAFFE_CUDA_NUM_THREADS>>>(
           count, top_diff, mask, uint_thres_, scale_, bottom_diff);
       CUDA_POST_KERNEL_CHECK;
     } else {
