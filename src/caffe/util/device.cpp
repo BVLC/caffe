821c74d7b0d8a9364e9c2d5bf3c4e3fa1837fdd2
Kai Li <kaili_kloud@163.com>
2014-06-18 11:49:25 +0800

    DeviceFactory opts out OpenCLDevice by default, users can opt in

--- src/caffe/util/device.cpp
+++ src/caffe/util/device.cpp
@@ -2,22 +2,26 @@
 
 #include "caffe/common.hpp"
 #include "caffe/util/device.hpp"
+#ifdef USE_OPENCL
+#include "caffe/util/opencl_device.hpp"
+#endif
 
 namespace caffe {
 
 template<typename Dtype>
-Device<Dtype>*
-DeviceFactory<Dtype>::GetDevice() {
+Device<Dtype>* DeviceFactory<Dtype>::GetDevice() {
   switch (Caffe::mode()) {
-    case Caffe::CPU:
-      return cpu_device_;
-    case Caffe::GPU:
-      return gpu_device_;
-    case Caffe::OPENCL:
-      return opencl_device_;
-    default:
-      LOG(FATAL) << "Unknown caffe mode.";
-      return static_cast<Device<Dtype>*>(NULL);
+  case Caffe::CPU:
+    return cpu_device_;
+  case Caffe::GPU:
+    return gpu_device_;
+#ifdef USE_OPENCL
+  case Caffe::OPENCL:
+    return opencl_device_;
+#endif
+  default:
+    LOG(FATAL) << "Unknown caffe mode.";
+    return static_cast<Device<Dtype>*>(NULL);
   }
 }
 
@@ -26,6 +30,11 @@ Device<Dtype>* DeviceFactory<Dtype>::cpu_device_ = new CPUDevice<Dtype>();
 
 template<typename Dtype>
 Device<Dtype>* DeviceFactory<Dtype>::gpu_device_ = new GPUDevice<Dtype>();
+
+#ifdef USE_OPENCL
+template<typename Dtype>
+Device<Dtype>* DeviceFactory<Dtype>::opencl_device_ = new OpenCLDevice<Dtype>();
+#endif
 
 INSTANTIATE_CLASS(DeviceFactory);
 
