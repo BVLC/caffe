e50e6166b189297f9bd09adc5b80c7b633f387e7
Sergio <sguada@gmail.com>
2014-06-17 19:55:45 -0700

    Added this-> access attributes of Blob

--- src/caffe/virtualblob.cpp
+++ src/caffe/virtualblob.cpp
@@ -21,38 +21,38 @@ void VirtualBlob<Dtype>::Reshape(const int num, const int channels, const int he
   CHECK_GE(channels, 0);
   CHECK_GE(height, 0);
   CHECK_GE(width, 0);
-  num_ = num;
-  channels_ = channels;
-  height_ = height;
-  width_ = width;
-  count_ = num_ * channels_ * height_ * width_;
-  if (count_ == 0) {
+  this->num_ = num;
+  this->channels_ = channels;
+  this->height_ = height;
+  this->width_ = width;
+  this->count_ = num_ * channels_ * height_ * width_;
+  if (this->count_ == 0) {
     // If new size is zero then release data and diff pointers
-    data_.reset(reinterpret_cast<SyncedMemory*>(NULL));
-    diff_.reset(reinterpret_cast<SyncedMemory*>(NULL));
+    this->data_.reset(reinterpret_cast<SyncedMemory*>(NULL));
+    this->diff_.reset(reinterpret_cast<SyncedMemory*>(NULL));
   }
-  if (data_) {
+  if (this->data_) {
     // If it already has data then the new size has to fit in
-    CHECK_GE(data_.size(), count_ * sizeof(Dtype));
+    CHECK_GE(this->data_.size(), this->count_ * sizeof(Dtype));
   }
-  if (diff_) {
+  if (this->diff_) {
     // If it already has diff then the new size has to fit in
-    CHECK_GE(diff_.size(), count_ * sizeof(Dtype));
+    CHECK_GE(this->diff_.size(), this->count_ * sizeof(Dtype));
   }
 }
 
 template <typename Dtype>
 void VirtualBlob<Dtype>::ShareData(const Blob<Dtype>& other) {
   // An VirtualBlob can share data with another Blobs that is bigger
-  CHECK_LE(count_, other.count());
-  data_ = other.data();
+  CHECK_LE(this->count_, other.count());
+  this->data_ = other.data();
 }
 
 template <typename Dtype>
 void VirtualBlob<Dtype>::ShareDiff(const Blob<Dtype>& other) {
   // An VirtualBlob can share diff with another Blobs that is bigger
   CHECK_LE(count_, other.count());
-  diff_ = other.diff();
+  this->diff_ = other.diff();
 }
 
 INSTANTIATE_CLASS(VirtualBlob);
