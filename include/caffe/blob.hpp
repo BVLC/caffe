a012b99910032d43bb5b8778ce3c3f0d0e594564
Sergio <sguada@gmail.com>
2014-06-17 19:14:16 -0700

    Added VirtualBlob to share data or diff across multiple blobs of different sizes

--- include/caffe/blob.hpp
+++ include/caffe/blob.hpp
@@ -95,6 +95,21 @@ class Blob {
   DISABLE_COPY_AND_ASSIGN(Blob);
 };  // class Blob
 
+// It is meant to be use as a shared buffer for im2col and col2im col_buffer
+// When resizing it will only increase the memory size as needed, never decrease it
+template <typename Dtype>
+class VirtualBlob : public Blob<Dtype> {
+ public:
+  // A VirtualBlob doesn't reserve any memory, after creating it, 
+  // it needs to ShareData and ShareDiff with a Blob
+  virtual void Reshape(const int num, const int channels, const int height,
+    const int width);
+  virtual void ShareData(const Blob& other);
+  virtual void ShareDiff(const Blob& other);
+protected:
+
+};  // class VirtualBlob
+
 }  // namespace caffe
 
 #endif  // CAFFE_BLOB_HPP_
---
+++ src/caffe/virtualblob.cpp
@@ -0,0 +1,57 @@
+// Copyright 2014 BVLC and contributors.
+
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/syncedmem.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void VirtualBlob<Dtype>::Reshape(const int num, const int channels, const int height,
+    const int width) {
+  CHECK_GE(num, 0);
+  CHECK_GE(channels, 0);
+  CHECK_GE(height, 0);
+  CHECK_GE(width, 0);
+  num_ = num;
+  channels_ = channels;
+  height_ = height;
+  width_ = width;
+  count_ = num_ * channels_ * height_ * width_;
+  if (count_ == 0) {
+    // If new size is zero then release data and diff pointers
+    data_.reset(reinterpret_cast<SyncedMemory*>(NULL));
+    diff_.reset(reinterpret_cast<SyncedMemory*>(NULL));
+  }
+  if (data_) {
+    // If it already has data then the new size has to fit in
+    CHECK_GE(data_.size(), count_ * sizeof(Dtype));
+  }
+  if (diff_) {
+    // If it already has diff then the new size has to fit in
+    CHECK_GE(diff_.size(), count_ * sizeof(Dtype));
+  }
+}
+
+template <typename Dtype>
+void VirtualBlob<Dtype>::ShareData(const Blob& other) {
+  // An VirtualBlob can share data with another Blobs that is bigger
+  CHECK(other.data())
+  CHECK_LE(count_, other.count());
+  data_ = other.data();
+}
+
+template <typename Dtype>
+void VirtualBlob<Dtype>::ShareDiff(const Blob& other) {
+  // An VirtualBlob can share diff with another Blobs that is bigger
+  CHECK(other.diff())
+  CHECK_LE(count_, other.count());
+  diff_ = other.diff();
+}
+
+INSTANTIATE_CLASS(VirtualBlob);
+
+}  // namespace caffe
