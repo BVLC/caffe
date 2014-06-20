b67bdde0948ab2851a6f35165e52083c0c3c0777
Sergio <sguada@gmail.com>
2014-06-19 17:38:18 -0700

    Added index to set_label
    Added index to set_label missing in cifar10
    
    Conflicts:
    	src/caffe/test/test_net.cpp

--- examples/mnist/convert_mnist_data.cpp
+++ examples/mnist/convert_mnist_data.cpp
@@ -78,7 +78,7 @@ void convert_dataset(const char* image_filename, const char* label_filename,
     image_file.read(pixels, rows * cols);
     label_file.read(&label, 1);
     datum.set_data(pixels, rows*cols);
-    datum.set_label(label);
+    datum.set_label(0,label);
     datum.SerializeToString(&value);
     snprintf(key, kMaxKeyLength, "%08d", itemid);
     db->Put(leveldb::WriteOptions(), std::string(key), value);
