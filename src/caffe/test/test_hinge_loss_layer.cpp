7be676cfb743bd2dddb2b070da5bf5c52a9cd8d5
Sergio <sguada@gmail.com>
2014-06-20 18:34:24 -0700

    Fixed switch and test l2hingeloss
    Conflicts:
    	src/caffe/layers/loss_layer.cpp
    	src/caffe/test/test_hinge_loss_layer.cpp

--- src/caffe/test/test_hinge_loss_layer.cpp
+++ src/caffe/test/test_hinge_loss_layer.cpp
@@ -79,7 +79,7 @@ TYPED_TEST(HingeLossLayerTest, TestGradientL2CPU) {
   Caffe::set_mode(Caffe::CPU);
   HingeLossLayer<TypeParam> layer(layer_param);
   layer.SetUp(this->blob_bottom_vec_, &this->blob_top_vec_);
-  GradientChecker<TypeParam> checker(1e-2, 2e-3, 1701, 1, 0.01);
+  GradientChecker<TypeParam> checker(1e-2, 2e-3, 1701);
   checker.CheckGradientSingle(&layer, &(this->blob_bottom_vec_),
       &(this->blob_top_vec_), 0, -1, -1);
 }
@@ -93,7 +93,7 @@ TYPED_TEST(HingeLossLayerTest, TestGradientL2GPU) {
   Caffe::set_mode(Caffe::GPU);
   HingeLossLayer<TypeParam> layer(layer_param);
   layer.SetUp(this->blob_bottom_vec_, &this->blob_top_vec_);
-  GradientChecker<TypeParam> checker(1e-2, 2e-3, 1701, 1, 0.01);
+  GradientChecker<TypeParam> checker(1e-2, 2e-3, 1701);
   checker.CheckGradientSingle(&layer, &(this->blob_bottom_vec_),
       &(this->blob_top_vec_), 0, -1, -1);
 }
