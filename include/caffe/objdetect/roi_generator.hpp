ae3b0b6bdd7716d82b5f4adeda288eb621d5b311
Kai Li <kaili_kloud@163.com>
2014-07-05 21:18:26 +0800

    Add Rect::area, intersect and empty methods

--- include/caffe/objdetect/roi_generator.hpp
+++ include/caffe/objdetect/roi_generator.hpp
@@ -3,59 +3,18 @@
 #ifndef CAFFE_ROI_GENERATOR_HPP_
 #define CAFFE_ROI_GENERATOR_HPP_
 
-#include <sstream>
 #include <string>
 #include <vector>
 
 #include "caffe/blob.hpp"
 #include "caffe/common.hpp"
+#include "caffe/objdetect/rect.hpp"
 #include "caffe/proto/caffe.pb.h"
 
 namespace caffe {
 
 using std::string;
-using std::ostringstream;
 using std::vector;
-
-class Rect {
- public:
-  Rect(const int x1, const int y1, const int x2, const int y2) : x1_(x1),
-    y1_(y1), x2_(x2), y2_(y2) {
-  }
-
-  Rect(const Rect& other) : x1_(other.x1()),
-    y1_(other.y1()), x2_(other.x2()), y2_(other.y2()) {
-  }
-
-  Rect& operator = (const Rect& other) {
-    x1_ = other.x1();
-    y1_ = other.y1();
-    x2_ = other.x2();
-    y2_ = other.y2();
-    return *this;
-  }
-
-  bool operator == (const Rect& other) const {
-    return x1_ == other.x1() && y1_ == other.y1() && x2_ == other.x2() &&
-        y2_ == other.y2();
-  }
-
-  inline int x1() { return x1_; }
-  inline int y1() { return y1_; }
-  inline int x2() { return x2_; }
-  inline int y2() { return y2_; }
-  inline const int x1() const { return x1_; }
-  inline const int y1() const { return y1_; }
-  inline const int x2() const { return x2_; }
-  inline const int y2() const { return y2_; }
-
- protected:
-  // All are inclusive
-  int x1_;  // left upper corner horizontal
-  int y1_;  // left upper corner vertical
-  int x2_;  // right bottom corner horizontal
-  int y2_;  // right bottom corner vertical
-};
 
 template <typename Dtype>
 class ROIGenerator {
