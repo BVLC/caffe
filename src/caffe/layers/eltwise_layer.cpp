f8256c99fff3848efda073681ffddfb066f6904d
to3i <tobias.dencker@gmail.com>
2014-07-11 13:19:23 +0200

    Implemented elementwise max layer -> GPU test OK, CPU test fails

--- src/caffe/layers/eltwise_layer.cpp
+++ src/caffe/layers/eltwise_layer.cpp
@@ -1,6 +1,7 @@
 // Copyright 2014 BVLC and contributors.
 
 #include <vector>
+#include <cfloat>
 
 #include "caffe/layer.hpp"
 #include "caffe/vision_layers.hpp"
@@ -38,11 +39,20 @@ void EltwiseLayer<Dtype>::SetUp(const vector<Blob<Dtype>*>& bottom,
       coeffs_[i] = this->layer_param().eltwise_param().coeff(i);
     }
   }
+  // If max operation, we will initialize the vector index part.
+  if (this->layer_param_.eltwise_param().operation() ==
+		  EltwiseParameter_EltwiseOp_MAX && top->size() == 1) {
+    max_idx_.reset(new Blob<int>(bottom[0]->num(), channels,
+                                 height, width));
+  }
 }
 
 template <typename Dtype>
 Dtype EltwiseLayer<Dtype>::Forward_cpu(
     const vector<Blob<Dtype>*>& bottom, vector<Blob<Dtype>*>* top) {
+  int* mask = NULL;
+  const Dtype* bottom_data_a = NULL;
+  const Dtype* bottom_data_b = NULL;
   const int count = (*top)[0]->count();
   Dtype* top_data = (*top)[0]->mutable_cpu_data();
   switch (op_) {
@@ -59,6 +69,50 @@ Dtype EltwiseLayer<Dtype>::Forward_cpu(
       caffe_axpy(count, coeffs_[i], bottom[i]->cpu_data(), top_data);
     }
     break;
+  case EltwiseParameter_EltwiseOp_MAX:
+	// Initialize
+	mask = max_idx_->mutable_cpu_data();
+	caffe_set(count, -1, mask);
+	caffe_set(count, Dtype(-FLT_MAX), top_data);
+	// bottom 0 & 1
+	bottom_data_a = bottom[0]->gpu_data();
+	bottom_data_b = bottom[1]->gpu_data();
+	for (int idx = 0; idx < count; ++idx) {
+		if (bottom_data_a[idx] > bottom_data_b[idx]) {
+			top_data[idx] = bottom_data_a[idx]; // maxval
+			mask[idx] = 0; // maxid
+		} else {
+			top_data[idx] = bottom_data_b[idx]; // maxval
+			mask[idx] = 1; // maxid
+		}
+	}
+	// bottom 2++
+	bottom_data_a = top_data;
+	for (int blob_idx = 2; blob_idx < bottom.size(); ++blob_idx) {
+		bottom_data_b = bottom[blob_idx]->gpu_data();
+		for (int idx = 0; idx < count; ++idx) {
+			if (bottom_data_a[idx] < bottom_data_b[idx]) {
+				top_data[idx] = bottom_data_b[idx];	// maxval
+				mask[idx] = blob_idx;	// maxid
+			}
+		}
+	}
+	/*
+	NOT_IMPLEMENTED;
+	int channels = bottom[0]->channels();
+	int height = bottom[0]->height();
+	int width = bottom[0]->width();
+	// The main loop
+	for (int n = 0; n < bottom[0]->num(); ++n) {
+		for (int c = 0; c < channels; ++c) {
+			for (int h = 0; h < height; ++h) {
+				for (int w = 0; w < width; ++w) {
+				}
+			}
+		}
+	}
+	*/
+	break;
   default:
     LOG(FATAL) << "Unknown elementwise operation.";
   }
@@ -68,6 +122,7 @@ Dtype EltwiseLayer<Dtype>::Forward_cpu(
 template <typename Dtype>
 void EltwiseLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const vector<bool>& propagate_down, vector<Blob<Dtype>*>* bottom) {
+  const int* mask = NULL;
   const int count = top[0]->count();
   const Dtype* top_data = top[0]->cpu_data();
   const Dtype* top_diff = top[0]->cpu_diff();
@@ -87,6 +142,16 @@ void EltwiseLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
           caffe_cpu_scale(count, coeffs_[i], top_diff, bottom_diff);
         }
         break;
+      case EltwiseParameter_EltwiseOp_MAX:
+    	mask = max_idx_->cpu_data();
+    	for (int index = 0; index < count; ++index) {
+    		Dtype gradient = 0;
+    		if (mask[index] == i) {
+    			gradient += top_diff[index];
+    		}
+    		bottom_diff[index] = gradient;
+		}
+		break;
       default:
         LOG(FATAL) << "Unknown elementwise operation.";
       }
