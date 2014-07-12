4ddfeabcc5b85b3737f9d5196e97f07820fd843d
Sergey Karayev <sergeykarayev@gmail.com>
2014-07-11 17:19:35 -0700

    [docs] shelhamer's minor suggestions

--- scripts/gather_examples.sh
+++ scripts/gather_examples.sh
@@ -6,15 +6,16 @@ ROOT_DIR="$( cd "$(dirname "$0")"/.. ; pwd -P )"
 cd $ROOT_DIR
 
 # Gather docs from examples/**/readme.md
-rm -r docs/gathered
-mkdir docs/gathered
+GATHERED_DIR=docs/gathered
+rm -r $GATHERED_DIR
+mkdir $GATHERED_DIR
 for README_FILENAME in $(find examples -iname "readme.md"); do
     # Only use file if it is to be included in docs.
     if grep -Fxq "include_in_docs: true" $README_FILENAME; then
         # Make link to readme.md in docs/gathered/.
         # Since everything is called readme.md, rename it by its dirname.
         README_DIRNAME=`dirname $README_FILENAME`
-        DOCS_FILENAME=docs/gathered/$README_DIRNAME.md
+        DOCS_FILENAME=$GATHERED_DIR/$README_DIRNAME.md
         mkdir -p `dirname $DOCS_FILENAME`
         ln -s $ROOT_DIR/$README_FILENAME $DOCS_FILENAME
     fi
@@ -22,7 +23,7 @@ done
 
 # Gather docs from examples/*.ipynb and add YAML front-matter.
 for NOTEBOOK_FILENAME in $(find examples -d 1 -iname "*.ipynb"); do
-    DOCS_FILENAME=docs/gathered/$NOTEBOOK_FILENAME
+    DOCS_FILENAME=$GATHERED_DIR/$NOTEBOOK_FILENAME
     mkdir -p `dirname $DOCS_FILENAME`
     python scripts/copy_notebook.py $NOTEBOOK_FILENAME $DOCS_FILENAME
 done
