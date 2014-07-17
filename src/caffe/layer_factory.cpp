da89eda719bf64fd2e66742e87f22b23439b779c
Yangqing Jia <jiayq@google.com>
2014-07-17 13:19:57 -0700

    More namespace cleaning.

--- src/caffe/layer_factory.cpp
+++ src/caffe/layer_factory.cpp
@@ -10,9 +10,7 @@
 #include "caffe/proto/caffe.pb.h"
 
 namespace caffe {
-
-using std::string;
-
+  
 // A function to get a specific layer from the specification given in
 // LayerParameter. Ideally this would be replaced by a factory pattern,
 // but we will leave it this way for now.
