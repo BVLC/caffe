1849d6b34e0b0638506c535386e36db258c01a9b
OpenHero <zhao.kaiyong@gmail.com>
2014-07-18 20:06:43 +0800

    For Travis CI

--- src/caffe/util/math_functions.cpp
+++ src/caffe/util/math_functions.cpp
@@ -12,7 +12,7 @@
 #ifdef _MSC_VER
 #if _MSC_VER < 1800
 inline double round(double x) {
-	return (x > 0.0) ? floor(x + 0.5) : ceil(x - 0.5);
+  return (x > 0.0) ? floor(x + 0.5) : ceil(x - 0.5);
 }
 #endif
 #endif
