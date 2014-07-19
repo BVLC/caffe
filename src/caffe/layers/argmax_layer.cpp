799b8358b78d82bcdde51b53627867ad5a360d89
Kai Li <kaili_kloud@163.com>
2014-07-20 00:42:12 +0800

    Fix style issues in accuracy & argmax layer

--- src/caffe/layers/argmax_layer.cpp
+++ src/caffe/layers/argmax_layer.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cfloat>
+#include <functional>
 #include <queue>
 #include <utility>
 #include <vector>
@@ -29,12 +30,6 @@ void ArgMaxLayer<Dtype>::SetUp(const vector<Blob<Dtype>*>& bottom,
   }
 }
 
-template<typename Dtype>
-static bool int_Dtype_pair_greater(std::pair<int, Dtype> a,
-                            std::pair<int, Dtype> b) {
-  return a.second > b.second;
-}
-
 template <typename Dtype>
 Dtype ArgMaxLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     vector<Blob<Dtype>*>* top) {
@@ -43,22 +38,20 @@ Dtype ArgMaxLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   int num = bottom[0]->num();
   int dim = bottom[0]->count() / bottom[0]->num();
   for (int i = 0; i < num; ++i) {
-    std::vector<std::pair<int, Dtype> > bottom_data_vector;
+    std::vector<std::pair<Dtype, int> > bottom_data_vector;
     for (int j = 0; j < dim; ++j) {
       bottom_data_vector.push_back(
-          std::make_pair(j, bottom_data[i * dim + j]));
+          std::make_pair(bottom_data[i * dim + j], j));
     }
     std::partial_sort(
         bottom_data_vector.begin(), bottom_data_vector.begin() + top_k_,
-        bottom_data_vector.end(), int_Dtype_pair_greater<Dtype>);
+        bottom_data_vector.end(), std::greater<std::pair<Dtype, int> >());
+    for (int j = 0; j < top_k_; ++j) {
+      top_data[(*top)[0]->offset(i, 0, j)] = bottom_data_vector[j].second;
+    }
     if (out_max_val_) {
       for (int j = 0; j < top_k_; ++j) {
-        top_data[(*top)[0]->offset(i, 0, j)] = bottom_data_vector[j].first;
-        top_data[(*top)[0]->offset(i, 1, j)] = bottom_data_vector[j].second;
-      }
-    } else {
-      for (int j = 0; j < top_k_; ++j) {
-        top_data[(*top)[0]->offset(i, 0, j)] = bottom_data_vector[j].first;
+        top_data[(*top)[0]->offset(i, 1, j)] = bottom_data_vector[j].first;
       }
     }
   }
