0fe2cc3fc7e2013c697e1f0ccce7d12d78ca0e0e
Adam Kosiorek <a.kosiorek@samsung.com>
2014-07-23 09:12:44 +0200

    fixed lint issues

--- src/caffe/test/test_hdf5data_layer.cpp
+++ src/caffe/test/test_hdf5data_layer.cpp
@@ -17,7 +17,7 @@ using std::string;
 
 namespace caffe {
 
-template <typename TypeParam>
+template<typename TypeParam>
 class HDF5DataLayerTest : public MultiDeviceTest<TypeParam> {
   typedef typename TypeParam::Dtype Dtype;
 
@@ -25,14 +25,16 @@ class HDF5DataLayerTest : public MultiDeviceTest<TypeParam> {
   HDF5DataLayerTest()
       : filename(NULL),
         blob_top_data_(new Blob<Dtype>()),
-        blob_top_label_(new Blob<Dtype>()) {}
+        blob_top_label_(new Blob<Dtype>()) {
+  }
   virtual void SetUp() {
     blob_top_vec_.push_back(blob_top_data_);
     blob_top_vec_.push_back(blob_top_label_);
 
     // Check out generate_sample_data.py in the same directory.
-    filename = new string(CMAKE_SOURCE_DIR "caffe/test/test_data/sample_data_list.txt" CMAKE_EXT);
-    LOG(INFO) << "Using sample HDF5 data file " << filename;
+    filename = new string(
+    CMAKE_SOURCE_DIR "caffe/test/test_data/sample_data_list.txt" CMAKE_EXT);
+    LOG(INFO)<< "Using sample HDF5 data file " << filename;
   }
 
   virtual ~HDF5DataLayerTest() {
@@ -51,72 +53,72 @@ class HDF5DataLayerTest : public MultiDeviceTest<TypeParam> {
 TYPED_TEST_CASE(HDF5DataLayerTest, TestDtypesAndDevices);
 
 TYPED_TEST(HDF5DataLayerTest, TestRead) {
-  typedef typename TypeParam::Dtype Dtype;
-  // Create LayerParameter with the known parameters.
-  // The data file we are reading has 10 rows and 8 columns,
-  // with values from 0 to 10*8 reshaped in row-major order.
-  LayerParameter param;
-  HDF5DataParameter* hdf5_data_param = param.mutable_hdf5_data_param();
-  int batch_size = 5;
-  hdf5_data_param->set_batch_size(batch_size);
-  hdf5_data_param->set_source(*(this->filename));
-  int num_cols = 8;
-  int height = 5;
-  int width = 5;
+typedef typename TypeParam::Dtype Dtype;
+// Create LayerParameter with the known parameters.
+// The data file we are reading has 10 rows and 8 columns,
+// with values from 0 to 10*8 reshaped in row-major order.
+LayerParameter param;
+HDF5DataParameter* hdf5_data_param = param.mutable_hdf5_data_param();
+int batch_size = 5;
+hdf5_data_param->set_batch_size(batch_size);
+hdf5_data_param->set_source(*(this->filename));
+int num_cols = 8;
+int height = 5;
+int width = 5;
 
-  // Test that the layer setup got the correct parameters.
-  HDF5DataLayer<Dtype> layer(param);
-  layer.SetUp(this->blob_bottom_vec_, &this->blob_top_vec_);
-  EXPECT_EQ(this->blob_top_data_->num(), batch_size);
-  EXPECT_EQ(this->blob_top_data_->channels(), num_cols);
-  EXPECT_EQ(this->blob_top_data_->height(), height);
-  EXPECT_EQ(this->blob_top_data_->width(), width);
+// Test that the layer setup got the correct parameters.
+HDF5DataLayer<Dtype> layer(param);
+layer.SetUp(this->blob_bottom_vec_, &this->blob_top_vec_);
+EXPECT_EQ(this->blob_top_data_->num(), batch_size);
+EXPECT_EQ(this->blob_top_data_->channels(), num_cols);
+EXPECT_EQ(this->blob_top_data_->height(), height);
+EXPECT_EQ(this->blob_top_data_->width(), width);
 
-  EXPECT_EQ(this->blob_top_label_->num(), batch_size);
-  EXPECT_EQ(this->blob_top_label_->channels(), 1);
-  EXPECT_EQ(this->blob_top_label_->height(), 1);
-  EXPECT_EQ(this->blob_top_label_->width(), 1);
+EXPECT_EQ(this->blob_top_label_->num(), batch_size);
+EXPECT_EQ(this->blob_top_label_->channels(), 1);
+EXPECT_EQ(this->blob_top_label_->height(), 1);
+EXPECT_EQ(this->blob_top_label_->width(), 1);
 
-  layer.SetUp(this->blob_bottom_vec_, &this->blob_top_vec_);
+layer.SetUp(this->blob_bottom_vec_, &this->blob_top_vec_);
 
-  // Go through the data 10 times (5 batches).
-  const int data_size = num_cols * height * width;
-  for (int iter = 0; iter < 10; ++iter) {
-    layer.Forward(this->blob_bottom_vec_, &this->blob_top_vec_);
+// Go through the data 10 times (5 batches).
+const int data_size = num_cols * height * width;
+for (int iter = 0; iter < 10; ++iter) {
+  layer.Forward(this->blob_bottom_vec_, &this->blob_top_vec_);
 
-    // On even iterations, we're reading the first half of the data.
-    // On odd iterations, we're reading the second half of the data.
-    int label_offset = (iter % 2 == 0) ? 0 : batch_size;
-    int data_offset = (iter % 2 == 0) ? 0 : batch_size * data_size;
+  // On even iterations, we're reading the first half of the data.
+  // On odd iterations, we're reading the second half of the data.
+  int label_offset = (iter % 2 == 0) ? 0 : batch_size;
+  int data_offset = (iter % 2 == 0) ? 0 : batch_size * data_size;
 
-    // Every two iterations we are reading the second file,
-    // which has the same labels, but data is offset by total data size,
-    // which is 2000 (see generate_sample_data).
-    int file_offset = (iter % 4 < 2) ? 0 : 2000;
+  // Every two iterations we are reading the second file,
+  // which has the same labels, but data is offset by total data size,
+  // which is 2000 (see generate_sample_data).
+  int file_offset = (iter % 4 < 2) ? 0 : 2000;
 
-    for (int i = 0; i < batch_size; ++i) {
-      EXPECT_EQ(
+  for (int i = 0; i < batch_size; ++i) {
+    EXPECT_EQ(
         label_offset + i,
         this->blob_top_label_->cpu_data()[i]);
-    }
-    for (int i = 0; i < batch_size; ++i) {
-      for (int j = 0; j < num_cols; ++j) {
-        for (int h = 0; h < height; ++h) {
-          for (int w = 0; w < width; ++w) {
-            int idx = (
+  }
+  for (int i = 0; i < batch_size; ++i) {
+    for (int j = 0; j < num_cols; ++j) {
+      for (int h = 0; h < height; ++h) {
+        for (int w = 0; w < width; ++w) {
+          int idx = (
               i * num_cols * height * width +
               j * height * width +
               h * width + w);
-            EXPECT_EQ(
+          EXPECT_EQ(
               file_offset + data_offset + idx,
               this->blob_top_data_->cpu_data()[idx])
-              << "debug: i " << i << " j " << j
-              << " iter " << iter;
-          }
+          << "debug: i " << i << " j " << j
+          << " iter " << iter;
         }
       }
     }
   }
+}
 }
 
 }  // namespace caffe
