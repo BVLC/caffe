98da304ce2020b5adb2d7cf17039d4857a2e0854
Adam Kosiorek <a.kosiorek@samsung.com>
2014-07-23 09:12:44 +0200

    fixed lint issues

--- src/caffe/test/test_hdf5_output_layer.cpp
+++ src/caffe/test/test_hdf5_output_layer.cpp
@@ -23,7 +23,8 @@ class HDF5OutputLayerTest : public MultiDeviceTest<TypeParam> {
  protected:
   HDF5OutputLayerTest()
       : output_file_name_(tmpnam(NULL)),
-        input_file_name_(CMAKE_SOURCE_DIR  "caffe/test/test_data/sample_data.h5"),
+        input_file_name_(
+        CMAKE_SOURCE_DIR "caffe/test/test_data/sample_data.h5"),
         blob_data_(new Blob<Dtype>()),
         blob_label_(new Blob<Dtype>()),
         num_(5),
