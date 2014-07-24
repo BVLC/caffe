7de3382561bbbe235358f4259328093ecf048e2d
Jeff Donahue <jeff.donahue@gmail.com>
2014-07-23 19:22:11 -0700

    Add gradient checks for infogain loss layer, letting it take the infogain matrix as the third bottom blob.

--- src/caffe/layers/infogain_loss_layer.cpp
+++ src/caffe/layers/infogain_loss_layer.cpp
@@ -18,14 +18,24 @@ void InfogainLossLayer<Dtype>::FurtherSetUp(
   CHECK_EQ(bottom[1]->channels(), 1);
   CHECK_EQ(bottom[1]->height(), 1);
   CHECK_EQ(bottom[1]->width(), 1);
-
-  BlobProto blob_proto;
-  ReadProtoFromBinaryFile(
-    this->layer_param_.infogain_loss_param().source(), &blob_proto);
-  infogain_.FromProto(blob_proto);
-  CHECK_EQ(infogain_.num(), 1);
-  CHECK_EQ(infogain_.channels(), 1);
-  CHECK_EQ(infogain_.height(), infogain_.width());
+  Blob<Dtype>* infogain = NULL;
+  if (bottom.size() < 3) {
+    CHECK(this->layer_param_.infogain_loss_param().has_source())
+        << "Infogain matrix source must be specified.";
+    BlobProto blob_proto;
+    ReadProtoFromBinaryFile(
+      this->layer_param_.infogain_loss_param().source(), &blob_proto);
+    infogain_.FromProto(blob_proto);
+    infogain = &infogain_;
+  } else {
+    infogain = bottom[2];
+  }
+  const int num = bottom[0]->num();
+  const int dim = bottom[0]->count() / num;
+  CHECK_EQ(infogain->num(), 1);
+  CHECK_EQ(infogain->channels(), 1);
+  CHECK_EQ(infogain->height(), dim);
+  CHECK_EQ(infogain->width(), dim);
 }
 
 
@@ -34,10 +44,14 @@ Dtype InfogainLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     vector<Blob<Dtype>*>* top) {
   const Dtype* bottom_data = bottom[0]->cpu_data();
   const Dtype* bottom_label = bottom[1]->cpu_data();
-  const Dtype* infogain_mat = infogain_.cpu_data();
+  const Dtype* infogain_mat = NULL;
+  if (bottom.size() < 3) {
+    infogain_mat = infogain_.cpu_data();
+  } else {
+    infogain_mat = bottom[2]->cpu_data();
+  }
   int num = bottom[0]->num();
   int dim = bottom[0]->count() / bottom[0]->num();
-  CHECK_EQ(infogain_.height(), dim);
   Dtype loss = 0;
   for (int i = 0; i < num; ++i) {
     int label = static_cast<int>(bottom_label[i]);
@@ -46,10 +60,11 @@ Dtype InfogainLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       loss -= infogain_mat[label * dim + j] * log(prob);
     }
   }
+  loss /= num;
   if (top->size() == 1) {
-    (*top)[0]->mutable_cpu_data()[0] = loss / num;
+    (*top)[0]->mutable_cpu_data()[0] = loss;
   }
-  return loss / num;
+  return loss;
 }
 
 template <typename Dtype>
@@ -60,14 +75,22 @@ void InfogainLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     LOG(FATAL) << this->type_name()
                << " Layer cannot backpropagate to label inputs.";
   }
+  if (propagate_down.size() > 2 && propagate_down[2]) {
+    LOG(FATAL) << this->type_name()
+               << " Layer cannot backpropagate to infogain inputs.";
+  }
   if (propagate_down[0]) {
     const Dtype* bottom_data = (*bottom)[0]->cpu_data();
     const Dtype* bottom_label = (*bottom)[1]->cpu_data();
-    const Dtype* infogain_mat = infogain_.cpu_data();
+    const Dtype* infogain_mat = NULL;
+    if (bottom->size() < 3) {
+      infogain_mat = infogain_.cpu_data();
+    } else {
+      infogain_mat = (*bottom)[2]->cpu_data();
+    }
     Dtype* bottom_diff = (*bottom)[0]->mutable_cpu_diff();
     int num = (*bottom)[0]->num();
     int dim = (*bottom)[0]->count() / (*bottom)[0]->num();
-    CHECK_EQ(infogain_.height(), dim);
     for (int i = 0; i < num; ++i) {
       int label = static_cast<int>(bottom_label[i]);
       for (int j = 0; j < dim; ++j) {
