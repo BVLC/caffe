27b8932e9f4929f981b8199ba74c99e46ae9db52
Yangqing Jia <jiayq@google.com>
2014-07-25 09:02:37 -0700

    Adding new caffe binary that does everything in one binary; deprecating device_query, finetune_net, net_speed_benchmark, train_net

--- tools/finetune_net.cpp
+++ tools/finetune_net.cpp
@@ -1,31 +1,9 @@
 // Copyright 2014 BVLC and contributors.
-//
-// This is a simple script that allows one to quickly finetune a network.
-// Usage:
-//    finetune_net solver_proto_file pretrained_net
-
-#include <string>
 
 #include "caffe/caffe.hpp"
 
-using namespace caffe;  // NOLINT(build/namespaces)
-
 int main(int argc, char** argv) {
-  ::google::InitGoogleLogging(argv[0]);
-  if (argc != 3) {
-    LOG(ERROR) << "Usage: finetune_net solver_proto_file pretrained_net";
-    return 1;
-  }
-
-  SolverParameter solver_param;
-  ReadProtoFromTextFileOrDie(argv[1], &solver_param);
-
-  LOG(INFO) << "Starting Optimization";
-  SGDSolver<float> solver(solver_param);
-  LOG(INFO) << "Loading from " << argv[2];
-  solver.net()->CopyTrainedLayersFrom(string(argv[2]));
-  solver.Solve();
-  LOG(INFO) << "Optimization Done.";
-
+  LOG(ERROR) << "Deprecated. Use caffe.bin train --solver_proto_file=... "
+                "[pretrained_net_file=...] instead.";
   return 0;
 }
