41a56376816b8c59c4777c1ab2d3230f8a785241
yajiedesign <yajiedesign@gmail.com>
2014-07-26 00:01:15 +0800

    fix some namespace with std::signbit

--- src/caffe/util/math_functions.cpp
+++ src/caffe/util/math_functions.cpp
@@ -366,4 +366,16 @@ void caffe_cpu_scale<double>(const int n, const double alpha, const double *x,
   cblas_dscal(n, alpha, y, 1);
 }
 
+
+using std::signbit;
+bool caffe_signbit(float arg) {
+    return signbit(arg);
+}
+bool caffe_signbit(double arg) {
+    return signbit(arg);
+}
+bool caffe_signbit(long double arg) {
+    return signbit(arg);
+}
+
 }  // namespace caffe
