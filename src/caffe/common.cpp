add0c601048da287ad3cf943dfee3f01fd8770d6
qipeng <pengrobertqi@163.com>
2014-07-26 16:14:46 -0700

    using ::gflags seems incompatible with homebrew's gflags 2.0

--- src/caffe/common.cpp
+++ src/caffe/common.cpp
@@ -25,7 +25,7 @@ int64_t cluster_seedgen(void) {
 
 void GlobalInit(int* pargc, char*** pargv) {
   // Google flags.
-  ::gflags::ParseCommandLineFlags(pargc, pargv, true);
+  ::google::ParseCommandLineFlags(pargc, pargv, true);
   // Google logging.
   ::google::InitGoogleLogging(*(pargv)[0]);
 }
