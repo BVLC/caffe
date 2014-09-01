77d66618af116b6faddc9e8b6035ce1b0f099149
Jeff Donahue <jeff.donahue@gmail.com>
2014-09-01 14:51:59 -0700

    revert tools/train_net.cpp to previous, depecated version

--- tools/train_net.cpp
+++ tools/train_net.cpp
@@ -1,26 +1,7 @@
 #include "caffe/caffe.hpp"
 
-using namespace caffe;  // NOLINT(build/namespaces)
-
 int main(int argc, char** argv) {
-  ::google::InitGoogleLogging(argv[0]);
-  if (argc < 2 || argc > 3) {
-    LOG(ERROR) << "Usage: train_net solver_proto_file [resume_point_file]";
-    return 1;
-  }
-
-  SolverParameter solver_param;
-  ReadProtoFromTextFileOrDie(argv[1], &solver_param);
-
-  LOG(INFO) << "Starting Optimization";
-  shared_ptr<Solver<float> > solver(GetSolver<float>(solver_param));
-  if (argc == 3) {
-    LOG(INFO) << "Resuming from " << argv[2];
-    solver->Solve(argv[2]);
-  } else {
-    solver->Solve();
-  }
-  LOG(INFO) << "Optimization Done.";
-
+  LOG(FATAL) << "Deprecated. Use caffe train --solver=... "
+                "[--snapshot=...] instead.";
   return 0;
 }
