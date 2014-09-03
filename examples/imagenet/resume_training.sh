84917d6ccae60fc4063a5d1bb68b86e658e80545
Sergey Karayev <sergeykarayev@gmail.com>
2014-09-03 15:42:46 +0100

    removing unneeded scripts from imagenet example

--- examples/imagenet/resume_training.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-./build/tools/caffe train \
-    --solver=examples/imagenet/imagenet_solver.prototxt \
-    --snapshot=examples/imagenet/caffe_imagenet_10000.solverstate
-
-echo "Done."
