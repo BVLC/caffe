4ce6e43140819588d2e47c22ca13c05a8030f57d
Jeff Donahue <jeff.donahue@gmail.com>
2014-09-08 11:11:41 +0200

    restore "red X" build failures in Travis

--- scripts/travis/travis_build_and_test.sh
+++ scripts/travis/travis_build_and_test.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 # Script called by Travis to do a CPU-only build of and test Caffe.
 
+set -e
+MAKE="make --jobs=$NUM_THREADS --keep-going"
+
 if $WITH_CMAKE; then
   mkdir build
   cd build
   cmake -DBUILD_PYTHON=ON -DBUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Release -DCPU_ONLY=ON ..
-  make --keep-going
-  make runtest
-  make lint
-  make clean
+  $MAKE
+  if ! $WITH_CUDA; then
+    $MAKE runtest
+    $MAKE lint
+  fi
+  $MAKE clean
   cd -
 else
-  export CPU_ONLY=1
-  make --keep-going all test pycaffe warn lint
-  make runtest
-  make all
-  make test
-  make pycaffe
-  make warn
-  make lint
+  if ! $WITH_CUDA; then
+    export CPU_ONLY=1
+  fi
+  $MAKE all test pycaffe warn lint || true
+  if ! $WITH_CUDA; then
+    $MAKE runtest
+  fi
+  $MAKE all
+  $MAKE test
+  $MAKE pycaffe
+  $MAKE warn
+  if ! $WITH_CUDA; then
+    $MAKE lint
+  fi
 fi
