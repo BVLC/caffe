256209da4def9f54a6401c19802501bde5cbf1ba
Jonathan L Long <jonlong@cs.berkeley.edu>
2014-09-10 21:48:51 -0700

    split off Reshape for neuron layers

--- src/caffe/layers/neuron_layer.cpp
+++ src/caffe/layers/neuron_layer.cpp
@@ -6,13 +6,9 @@
 namespace caffe {
 
 template <typename Dtype>
-void NeuronLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+void NeuronLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       vector<Blob<Dtype>*>* top) {
-  // NeuronLayer allows in-place computations. If the computation is not
-  // in-place, we will need to initialize the top blob.
-  if ((*top)[0] != bottom[0]) {
-    (*top)[0]->ReshapeLike(*bottom[0]);
-  }
+  (*top)[0]->ReshapeLike(*bottom[0]);
 }
 
 INSTANTIATE_CLASS(NeuronLayer);
