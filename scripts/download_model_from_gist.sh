bfe763ec4a8863f16a45a1fd6724168d3e292383
Sergey Karayev <sergeykarayev@gmail.com>
2014-09-18 16:21:03 -0700

    bug fix

--- scripts/download_model_from_gist.sh
+++ scripts/download_model_from_gist.sh
@@ -4,12 +4,13 @@ GIST=$1
 DIRNAME=$2
 
 if [ -d "$DIRNAME/$GIST" ]; then
-    echo "$DIRNAME/$GIST already exists! Please make sure you're not overwriting anything."
+    echo "$DIRNAME/$GIST already exists! Please make sure you're not overwriting anything important!"
+    exit
 fi
 
 echo "Downloading Caffe model info to $DIRNAME/$GIST ..."
 mkdir -p $DIRNAME/$GIST
-wget https://gist.github.com/sergeyk/034c6ac3865563b69e60/download -O $DIRNAME/$GIST/gist.tar.gz
+wget https://gist.github.com/$GIST/download -O $DIRNAME/$GIST/gist.tar.gz
 tar xzf $DIRNAME/$GIST/gist.tar.gz --directory=$DIRNAME/$GIST --strip-components=1
 rm $DIRNAME/$GIST/gist.tar.gz
 echo "Done"
