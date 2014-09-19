8fd17a32e45f4e9b01ea58e87514acefcde0b6f4
Jeff Donahue <jeff.donahue@gmail.com>
2014-09-19 12:38:39 -0700

    fix cifar10 paths so they can be run from caffe root

--- examples/cifar10/train_quick.sh
+++ examples/cifar10/train_quick.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
-TOOLS=../../build/tools
+TOOLS=./build/tools
 
-$TOOLS/caffe.bin train --solver=cifar10_quick_solver.prototxt
+$TOOLS/caffe train \
+  --solver=examples/cifar10/cifar10_quick_solver.prototxt
 
-# reduce learning rate by fctor of 10 after 8 epochs
-$TOOLS/caffe.bin train \
-  --solver=cifar10_quick_solver_lr1.prototxt \
-  --snapshot=cifar10_quick_iter_4000.solverstate
+# reduce learning rate by factor of 10 after 8 epochs
+$TOOLS/caffe train \
+  --solver=examples/cifar10/cifar10_quick_solver_lr1.prototxt \
+  --snapshot=examples/cifar10/cifar10_quick_iter_4000.solverstate
