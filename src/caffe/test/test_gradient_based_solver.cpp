89fd7da77983d5bbbb045f3802821c8372722c5f
Evan Shelhamer <shelhamer@imaginarynumber.net>
2014-09-18 22:07:01 -0700

    relax precision of gradient-based solver tests

--- src/caffe/test/test_gradient_based_solver.cpp
+++ src/caffe/test/test_gradient_based_solver.cpp
@@ -234,7 +234,7 @@ class GradientBasedSolverTest : public MultiDeviceTest<TypeParam> {
     ASSERT_EQ(2, param_blobs.size());
     const Blob<Dtype>& solver_updated_weights = *param_blobs[0];
     ASSERT_EQ(D, solver_updated_weights.count());
-    const double kPrecision = 1e-3;
+    const double kPrecision = 1e-2;
     const double kMinPrecision = 1e-7;
     for (int i = 0; i < D; ++i) {
       const Dtype expected_updated_weight = updated_weights.cpu_data()[i];
