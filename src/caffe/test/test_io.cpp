4aa791443a939078d1f4810ca35a67b0970bfc8f
Sergio <sguada@gmail.com>
2014-09-24 13:41:11 -0700

    Added more tests to test_io for CVMatToDatum

--- src/caffe/test/test_io.cpp
+++ src/caffe/test/test_io.cpp
@@ -227,4 +227,52 @@ TEST_F(IOTest, TestReadImageToCVMatResizedGray) {
   EXPECT_EQ(cv_img.cols, 256);
 }
 
+TEST_F(IOTest, TestCVMatToDatum) {
+  string filename = EXAMPLES_SOURCE_DIR "images/cat.jpg";
+  cv::Mat cv_img = ReadImageToCVMat(filename);
+  Datum datum;
+  CVMatToDatum(cv_img, &datum);
+  EXPECT_EQ(datum.channels(), 3);
+  EXPECT_EQ(datum.height(), 360);
+  EXPECT_EQ(datum.width(), 480);
+}
+
+TEST_F(IOTest, TestCVMatToDatumContent) {
+  string filename = EXAMPLES_SOURCE_DIR "images/cat.jpg";
+  cv::Mat cv_img = ReadImageToCVMat(filename);
+  Datum datum;
+  CVMatToDatum(cv_img, &datum);
+  Datum datum_ref;
+  ReadImageToDatum(filename, 0, &datum_ref);
+  EXPECT_EQ(datum.channels(), datum_ref.channels());
+  EXPECT_EQ(datum.height(), datum_ref.height());
+  EXPECT_EQ(datum.width(), datum_ref.width());
+  EXPECT_EQ(datum.data().size(), datum_ref.data().size());
+
+  const string& data = datum.data();
+  const string& data_ref = datum_ref.data();
+  for (int i = 0; i < datum.data().size(); ++i) {
+    EXPECT_TRUE(data[i] == data_ref[i]);
+  }
+}
+
+TEST_F(IOTest, TestCVMatToDatumReference) {
+  string filename = EXAMPLES_SOURCE_DIR "images/cat.jpg";
+  cv::Mat cv_img = ReadImageToCVMat(filename);
+  Datum datum;
+  CVMatToDatum(cv_img, &datum);
+  Datum datum_ref;
+  ReadImageToDatumReference(filename, 0, 0, 0, true, &datum_ref);
+  EXPECT_EQ(datum.channels(), datum_ref.channels());
+  EXPECT_EQ(datum.height(), datum_ref.height());
+  EXPECT_EQ(datum.width(), datum_ref.width());
+  EXPECT_EQ(datum.data().size(), datum_ref.data().size());
+
+  const string& data = datum.data();
+  const string& data_ref = datum_ref.data();
+  for (int i = 0; i < datum.data().size(); ++i) {
+    EXPECT_TRUE(data[i] == data_ref[i]);
+  }
+}
+
 }  // namespace caffe
