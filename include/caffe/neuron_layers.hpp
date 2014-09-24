a157be0a88683d2402c9c5da8ce21cb73f100da6
Dmytro Mishkin <ducha.aiki@gmail.com>
2014-09-24 14:06:43 +0300

    Revert commit the gpu implementation - it is too slow.

--- include/caffe/neuron_layers.hpp
+++ include/caffe/neuron_layers.hpp
@@ -395,14 +395,10 @@ class TopKLayer : public NeuronLayer<Dtype> {
    *      the computed outputs.    */
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-     const vector<Blob<Dtype>*>& top);
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
-  Blob<uint> idxs_;
+//  Blob<uint> idxs_;
 
 
   ///
