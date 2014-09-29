2a1e929922218bc88ebb3b4e30675d35ddb53932
Chiyuan Zhang <pluskid@gmail.com>
2014-09-29 17:34:54 -0400

    fix style errors reported by lint.

--- src/caffe/layers/hdf5_data_layer.cpp
+++ src/caffe/layers/hdf5_data_layer.cpp
@@ -41,13 +41,14 @@ void HDF5DataLayer<Dtype>::LoadHDF5FileData(const char* filename) {
 
   for (int i = 0; i < top_size; ++i) {
       hdf_blobs_[i] = shared_ptr<Blob<Dtype> >(new Blob<Dtype>());
-      hdf5_load_nd_dataset(file_id, this->layer_param_.top(i).c_str(), 
+      hdf5_load_nd_dataset(file_id, this->layer_param_.top(i).c_str(),
         MIN_DATA_DIM, MAX_DATA_DIM, hdf_blobs_[i].get());
   }
 
   herr_t status = H5Fclose(file_id);
   CHECK_GE(status, 0) << "Failed to close HDF5 file " << filename;
-  CHECK_GE(hdf_blobs_.size(), 1) << "At least one 'top' must be specified for HDF5 data layer";
+  CHECK_GE(hdf_blobs_.size(), 1)
+      << "At least one 'top' must be specified for HDF5 data layer";
   int num = hdf_blobs_[0]->num();
   for (int i = 1; i < top_size; ++i) {
       CHECK_EQ(hdf_blobs_[i]->num(), num);
@@ -82,10 +83,10 @@ void HDF5DataLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
   const int batch_size = this->layer_param_.hdf5_data_param().batch_size();
   int top_size = this->layer_param_.top_size();
   for (int i = 0; i < top_size; ++i) {
-       (*top)[i]->Reshape(batch_size, hdf_blobs_[i]->channels(), 
+       (*top)[i]->Reshape(batch_size, hdf_blobs_[i]->channels(),
                           hdf_blobs_[i]->height(), hdf_blobs_[i]->width());
-       LOG(INFO) << "output '" << this->layer_param_.top(i) << "' size: " 
-           << (*top)[i]->num() << "," << (*top)[i]->channels() 
+       LOG(INFO) << "output '" << this->layer_param_.top(i) << "' size: "
+           << (*top)[i]->num() << "," << (*top)[i]->channels()
            << "," << (*top)[i]->height() << ","
            << (*top)[i]->width();
   }
@@ -110,8 +111,9 @@ void HDF5DataLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     }
     for (int j = 0; j < this->layer_param_.top_size(); ++j) {
         int data_count = (*top)[j]->count() / (*top)[j]->num();
-        caffe_copy(data_count, &hdf_blobs_[j]->cpu_data()[current_row_ * data_count],
-                   &(*top)[j]->mutable_cpu_data()[i * data_count]);
+        caffe_copy(data_count,
+                &hdf_blobs_[j]->cpu_data()[current_row_ * data_count],
+                &(*top)[j]->mutable_cpu_data()[i * data_count]);
     }
   }
 }
