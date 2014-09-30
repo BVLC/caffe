90b6b1db37c55ef77f601edac68fa9e8375681e8
Jeff Donahue <jeff.donahue@gmail.com>
2014-09-30 14:25:28 -0700

    Add Net::Reset and Layer::Reset methods.
    Net::Reset calls Layer::Reset on all its layers.
    Layer::Reset currently has a trivial implementation only; should be
    impelmented for data layers.

--- src/caffe/net.cpp
+++ src/caffe/net.cpp
@@ -685,6 +685,13 @@ void Net<Dtype>::Reshape() {
 }
 
 template <typename Dtype>
+void Net<Dtype>::Reset() {
+  for (int i = 0; i < layers_.size(); ++i) {
+    layers_[i]->Reset();
+  }
+}
+
+template <typename Dtype>
 void Net<Dtype>::CopyTrainedLayersFrom(const NetParameter& param) {
   int num_source_layers = param.layer_size();
   for (int i = 0; i < num_source_layers; ++i) {
