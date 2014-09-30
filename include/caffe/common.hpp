1b38527e6f88114e90650dfc808bfb81f8da6acf
Yangqing Jia <jiayq84@gmail.com>
2014-09-30 15:57:38 -0700

    remove a wrong space in common.hpp

--- include/caffe/common.hpp
+++ include/caffe/common.hpp
@@ -19,7 +19,7 @@
 
 // gflags 2.1 issue: namespace google was changed to gflags without warning.
 // Luckily we will be able to use GFLAGS_GFAGS_H_ to detect if it is version
-// 2.1. If yes , we will add a temporary solution to redirect the namespace.
+// 2.1. If yes, we will add a temporary solution to redirect the namespace.
 // TODO(Yangqing): Once gflags solves the problem in a more elegant way, let's
 // remove the following hack.
 #ifndef GFLAGS_GFLAGS_H_
