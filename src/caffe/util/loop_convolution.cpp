5c882375b5e0b39508981521a2599da37a81df8e
Takuya Narihira <dvbscb@gmail.com>
2014-10-09 20:05:46 -0700

    Refactoring

--- src/caffe/util/loop_convolution.cpp
+++ src/caffe/util/loop_convolution.cpp
@@ -2,44 +2,44 @@
 
 namespace caffe {
 
-/*
- * Convolution loop accesser function
- */
+// Helper function for convolutional data access
+// This function takes a function object `accumulator` which
+// is called inside convolution loop.
 template <typename ACCUMULATOR>
 static void conv_loop(
-    int channels, int in_h, int in_w,
-    int num_output, int kernel_h, int kernel_w,
+    int channels, int bottom_h, int bottom_w,
+    int num_output, int weight_h, int weight_w,
     int pad_h, int pad_w, int stride_h, int stride_w,
     ACCUMULATOR accumulator) {
 
-  int out_h = (in_h + 2 * pad_h - kernel_h) / stride_h + 1;
-  int out_w = (in_w + 2 * pad_w - kernel_w) / stride_w + 1;
+  int top_h = (bottom_h + 2 * pad_h - weight_h) / stride_h + 1;
+  int top_w = (bottom_w + 2 * pad_w - weight_w) / stride_w + 1;
 
   // Image
-  for (int oi = 0; oi < num_output * out_h * out_w; oi++) {
+  for (int ti = 0; ti < num_output * top_h * top_w; ti++) {
     // position on output
-    int c = oi / (out_h * out_w);
-    int y = (oi / out_w) % out_h;
-    int x = oi % out_w;
+    int t_c = ti / (top_h * top_w);
+    int t_y = (ti / top_w) % top_h;
+    int t_x = ti % top_w;
 
     // Filter begin
-    for (int ii = 0; ii < channels * kernel_h * kernel_w; ii++) {
-      // position on kernel
-      int cc = ii / (kernel_h * kernel_w);
-      int yy = (ii / kernel_w) % kernel_h;
-      int xx = ii % kernel_w;
+    for (int ki = 0; ki < channels * weight_h * weight_w; ki++) {
+      // position on weight
+      int k_c = ki / (weight_h * weight_w);
+      int k_y = (ki / weight_w) % weight_h;
+      int k_x = ki % weight_w;
       // position on input
-      int yyy = y * stride_h + yy - pad_h;
-      int xxx = x * stride_w + xx - pad_w;
-      if ((yyy < 0) || (yyy >= in_h) || (xxx < 0) || (xxx >= in_w)) {
+      int b_y = t_y * stride_h + k_y - pad_h;
+      int b_x = t_x * stride_w + k_x - pad_w;
+      if ((b_y < 0) || (b_y >= bottom_h) || (b_x < 0) || (b_x >= bottom_w)) {
         // skip if padded area
         continue;
       }
-      // accumulate
-      int ooo = (c * out_h+ y) * out_w + x;
-      int kkk = ((c * channels + cc) * kernel_h + yy) * kernel_w + xx;
-      int iii = (cc * in_h + yyy) * in_w + xxx;
-      accumulator(ooo, kkk, iii);
+      // Resolve linear memory accessing from spatial coordinate
+      int t = (t_c * top_h+ t_y) * top_w + t_x;
+      int k = ((t_c * channels + k_c) * weight_h + k_y) * weight_w + k_x;
+      int b = (k_c * bottom_h + b_y) * bottom_w + b_x;
+      accumulator(t, k, b);
     }
     // Filter end
   }
@@ -47,119 +47,118 @@ static void conv_loop(
 }
 
 
-// Accumulator
-
-// function object for conv_filter
+// Accumulator function object for `conv_top`
 template <typename Dtype>
-class ConvFilter {
+class ConvTop {
  public:
-  ConvFilter(const Dtype* in, const Dtype* kernel, Dtype* out) :
-      in_(in), kernel_(kernel), out_(out) {}
-  void operator()(int o, int k, int i) {
-    out_[o] += kernel_[k] * in_[i];
+  ConvTop(const Dtype* bottom, const Dtype* weight, Dtype* top) :
+      bottom_(bottom), weight_(weight), top_(top) {}
+  void operator()(int t, int k, int b) {
+    top_[t] += weight_[k] * bottom_[b];
   }
  protected:
-  const Dtype* in_;
-  const Dtype* kernel_;
-  Dtype* out_;
+  const Dtype* bottom_;
+  const Dtype* weight_;
+  Dtype* top_;
 };
 
-// function object for conv_filter
+// Accumulator function object for `conv_weight`
 template <typename Dtype>
-class ConvWeight {
+class ConvKernel {
  public:
-  ConvWeight(const Dtype* in, Dtype* kernel, const Dtype* out) :
-      in_(in), kernel_(kernel), out_(out) {}
-  void operator()(int o, int k, int i) {
-    kernel_[k] += out_[o] * in_[i];
+  ConvKernel(const Dtype* bottom, Dtype* weight, const Dtype* top) :
+      bottom_(bottom), weight_(weight), top_(top) {}
+  void operator()(int t, int k, int b) {
+    weight_[k] += top_[t] * bottom_[b];
   }
  protected:
-  const Dtype* in_;
-  Dtype* kernel_;
-  const Dtype* out_;
+  const Dtype* bottom_;
+  Dtype* weight_;
+  const Dtype* top_;
 };
 
-// function object for conv_weight
+// Accumulator function object for `conv_bottom`
 template <typename Dtype>
-class ConvImage {
+class ConvBottom {
  public:
-  ConvImage(Dtype* in, const Dtype* kernel, const Dtype* out) :
-      in_(in), kernel_(kernel), out_(out) {}
-  void operator()(int o, int k, int i) {
-    in_[i] += kernel_[k] * out_[o];
+  ConvBottom(Dtype* bottom, const Dtype* weight, const Dtype* top) :
+      bottom_(bottom), weight_(weight), top_(top) {}
+  void operator()(int t, int k, int b) {
+    bottom_[b] += weight_[k] * top_[t];
   }
  protected:
-  Dtype* in_;
-  const Dtype* kernel_;
-  const Dtype* out_;
+  Dtype* bottom_;
+  const Dtype* weight_;
+  const Dtype* top_;
 };
 
 
-//
+// Convolution
 template <typename Dtype>
-void conv_filter(
-    const Dtype* in, const Dtype* kernel, Dtype* out,
-    int channels, int in_h, int in_w,
-    int num_output, int kernel_h, int kernel_w,
+void conv_top(
+    const Dtype* bottom, const Dtype* weight, Dtype* top,
+    int channels, int bottom_h, int bottom_w,
+    int num_output, int weight_h, int weight_w,
     int pad_h, int pad_w, int stride_h, int stride_w) {
-  conv_loop<ConvFilter<Dtype> >(channels, in_h, in_w,
-            num_output, kernel_h, kernel_w, pad_h, pad_w, stride_h, stride_w,
-            ConvFilter<Dtype>(in, kernel, out));
+  conv_loop<ConvTop<Dtype> >(channels, bottom_h, bottom_w,
+            num_output, weight_h, weight_w, pad_h, pad_w, stride_h, stride_w,
+            ConvTop<Dtype>(bottom, weight, top));
 }
 
-// 
+// Backprop to weight
 template <typename Dtype>
 void conv_weight(
-    const Dtype* in, Dtype* kernel, const Dtype* out,
-    int channels, int in_h, int in_w,
-    int num_output, int kernel_h, int kernel_w,
+    const Dtype* bottom, Dtype* weight, const Dtype* top,
+    int channels, int bottom_h, int bottom_w,
+    int num_output, int weight_h, int weight_w,
     int pad_h, int pad_w, int stride_h, int stride_w) {
-  conv_loop<ConvWeight<Dtype> >(channels, in_h, in_w,
-            num_output, kernel_h, kernel_w, pad_h, pad_w, stride_h, stride_w,
-            ConvWeight<Dtype>(in, kernel, out));
+  conv_loop<ConvKernel<Dtype> >(channels, bottom_h, bottom_w,
+            num_output, weight_h, weight_w, pad_h, pad_w, stride_h, stride_w,
+            ConvKernel<Dtype>(bottom, weight, top));
 }
 
-//
+// Backprop to bottom
 template <typename Dtype>
-void conv_image(
-    Dtype* in, const Dtype* kernel, const Dtype* out,
-    int channels, int in_h, int in_w,
-    int num_output, int kernel_h, int kernel_w,
+void conv_bottom(
+    Dtype* bottom, const Dtype* weight, const Dtype* top,
+    int channels, int bottom_h, int bottom_w,
+    int num_output, int weight_h, int weight_w,
     int pad_h, int pad_w, int stride_h, int stride_w) {
-  conv_loop<ConvImage<Dtype> >(channels, in_h, in_w,
-            num_output, kernel_h, kernel_w, pad_h, pad_w, stride_h, stride_w,
-            ConvImage<Dtype>(in, kernel, out));
+  conv_loop<ConvBottom<Dtype> >(channels, bottom_h, bottom_w,
+            num_output, weight_h, weight_w, pad_h, pad_w, stride_h, stride_w,
+            ConvBottom<Dtype>(bottom, weight, top));
 }
 
-template void conv_filter(
-    const float* in, const float* kernel, float* out,
-    int channels, int in_h, int in_w,
-    int num_output, int kernel_h, int kernel_w,
+// Template instanciation
+template void conv_top(
+    const float* bottom, const float* weight, float* top,
+    int channels, int bottom_h, int bottom_w,
+    int num_output, int weight_h, int weight_w,
     int pad_h, int pad_w, int stride_h, int stride_w);
-template void conv_filter(
-    const double* in, const double* kernel, double* out,
-    int channels, int in_h, int in_w,
-    int num_output, int kernel_h, int kernel_w,
-    int pad_h, int pad_w, int stride_h, int stride_w);
-template void conv_weight(
-    const float* in, float* kernel, const float* out,
-    int channels, int in_h, int in_w,
-    int num_output, int kernel_h, int kernel_w,
+template void conv_top(
+    const double* bottom, const double* weight, double* top,
+    int channels, int bottom_h, int bottom_w,
+    int num_output, int weight_h, int weight_w,
     int pad_h, int pad_w, int stride_h, int stride_w);
 template void conv_weight(
-    const double* in, double* kernel, const double* out,
-    int channels, int in_h, int in_w,
-    int num_output, int kernel_h, int kernel_w,
+    const float* bottom, float* weight, const float* top,
+    int channels, int bottom_h, int bottom_w,
+    int num_output, int weight_h, int weight_w,
     int pad_h, int pad_w, int stride_h, int stride_w);
-template void conv_image(
-    float* in, const float* kernel, const float* out,
-    int channels, int in_h, int in_w,
-    int num_output, int kernel_h, int kernel_w,
+template void conv_weight(
+    const double* bottom, double* weight, const double* top,
+    int channels, int bottom_h, int bottom_w,
+    int num_output, int weight_h, int weight_w,
     int pad_h, int pad_w, int stride_h, int stride_w);
-template void conv_image(
-    double* in, const double* kernel, const double* out,
-    int channels, int in_h, int in_w,
-    int num_output, int kernel_h, int kernel_w,
+template void conv_bottom(
+    float* bottom, const float* weight, const float* top,
+    int channels, int bottom_h, int bottom_w,
+    int num_output, int weight_h, int weight_w,
+    int pad_h, int pad_w, int stride_h, int stride_w);
+template void conv_bottom(
+    double* bottom, const double* weight, const double* top,
+    int channels, int bottom_h, int bottom_w,
+    int num_output, int weight_h, int weight_w,
     int pad_h, int pad_w, int stride_h, int stride_w);
 
 // end of namespace
