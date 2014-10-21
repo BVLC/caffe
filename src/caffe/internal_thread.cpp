4e951dd793113c958e1dff10917eabf552152e08
Ryota Tomioka <tomioka@ttic.edu>
2014-10-21 10:52:38 -0500

    Forward declare boost::thread instead of including boost/thread.hpp in internal_thread.hpp.

--- src/caffe/internal_thread.cpp
+++ src/caffe/internal_thread.cpp
@@ -1,3 +1,4 @@
+#include <boost/thread.hpp>
 #include "caffe/internal_thread.hpp"
 
 namespace caffe {
@@ -5,6 +6,11 @@ namespace caffe {
 InternalThread::~InternalThread() {
   WaitForInternalThreadToExit();
 }
+
+bool InternalThread::is_started() const {
+  return thread_.get() != NULL && thread_->joinable();
+}
+
 
 bool InternalThread::StartInternalThread() {
   if (!WaitForInternalThreadToExit()) {
