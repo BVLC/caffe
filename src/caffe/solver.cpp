e97daabcedb95a57136298596f3e01d9b928037d
Ken Chatfield <ken.chatfield@gmail.com>
2014-10-24 22:09:42 +0100

    Added override parameter for network input count

--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -347,7 +347,7 @@ template <typename Dtype>
 void Solver<Dtype>::Restore(const char* state_file) {
   SolverState state;
   NetParameter net_param;
-  ReadProtoFromBinaryFile(state_file, &state);
+  caffe::ReadProtoFromBinaryFile(state_file, &state);
   if (state.has_learned_net()) {
     ReadProtoFromBinaryFile(state.learned_net().c_str(), &net_param);
     net_->CopyTrainedLayersFrom(net_param);
