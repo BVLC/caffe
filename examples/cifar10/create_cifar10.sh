26a4b5f35103678898809d39ccedbab2892612c7
Christos Nikolaou <christnn@auth.gr>
2014-11-01 04:49:25 +0200

    Update CIFAR-10: readme.md and create_cifar10.sh
    Fixed bugs in create_cidfar10.sh file, because lmdb is not
    supported for CIFAR-10. Also fixed paths in readme.md file.

--- examples/cifar10/create_cifar10.sh
+++ examples/cifar10/create_cifar10.sh
@@ -3,17 +3,22 @@
 
 EXAMPLE=examples/cifar10
 DATA=data/cifar10
-DBTYPE=lmdb
+BUILD=build/examples/cifar10
+TOOLS=build/tools
+
+# At this time, only leveldb is available for CIFAR-10
+DBTYPE=leveldb
 
 echo "Creating $DBTYPE..."
 
-rm -rf $EXAMPLE/cifar10_train_$DBTYPE $EXAMPLE/cifar10_test_$DBTYPE
+rm -rf $EXAMPLE/cifar10_train_$DBTYPE 
+rm -rf $EXAMPLE/cifar10_test_$DBTYPE
 
-./build/examples/cifar10/convert_cifar_data.bin $DATA $EXAMPLE $DBTYPE
+$BUILD/convert_cifar_data.bin $DATA $EXAMPLE $DTYPE
 
 echo "Computing image mean..."
 
-./build/tools/compute_image_mean -backend=$DBTYPE \
-  $EXAMPLE/cifar10_train_$DBTYPE $EXAMPLE/mean.binaryproto
+$TOOLS/compute_image_mean.bin  $EXAMPLE/cifar10_train_$DBTYPE \
+  $EXAMPLE/mean.binaryproto $DBTYPE
 
 echo "Done."
