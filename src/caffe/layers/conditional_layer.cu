71ceee7073e27bcb3a14b7e892547842c76dc032
manuele <manuele.tamburrano@gmail.com>
2014-11-11 20:08:28 +0100

    added conditional_layer.cu

---
+++ src/caffe/layers/conditional_layer.cu
@@ -0,0 +1,66 @@
+#include <vector>
+
+#include "caffe/layer.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/vision_layers.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void ConditionalLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  
+  const Dtype* bottom_data_IF = bottom[0]->gpu_data();
+  const Dtype* bottom_data_THEN = bottom[1]->gpu_data();
+  
+  vector<float> indices_to_keep;
+  int num_items = bottom[0]->num();
+  int num_channels = bottom[0]->channels();
+  //look through the batch to find who passes the conditional check
+  for (size_t item_id = 0; item_id < num_items; ++item_id) {
+    
+    int index_IF = item_id*num_channels;
+    const Dtype* tmp_data_IF = bottom_data_IF + index_IF;
+    int argmax = std::distance(tmp_data_IF,
+        std::max_element(tmp_data_IF, tmp_data_IF + num_channels));
+    
+    if (argmax == conditional_index_)
+      indices_to_keep.push_back(item_id);
+      
+  }
+  
+  //Only items that passed conditional check will be forwarded
+  int new_tops_num = indices_to_keep.size();
+  if (new_tops_num == 0)
+    new_tops_num = 1;
+
+  top[0]->Reshape(new_tops_num, 1, 1, 1);
+  top[1]->Reshape(new_tops_num, top[1]->channels(), top[1]->height(), top[1]->width());
+
+  Dtype* top_data_indices = top[0]->mutable_gpu_data();
+  Dtype* top_data = top[1]->mutable_gpu_data();
+  
+  size_t size_top = top[1]->count()/top[1]->num();
+  for (size_t n = 0; n<indices_to_keep.size(); n++)
+  {
+    int offset = indices_to_keep[n];
+    top_data_indices[n] = (float)offset;
+    int data_offset_top = size_top*n;
+    int data_offset_bottom = size_top*offset;
+    for(size_t s = 0; s<size_top; s++)
+    {
+      top_data[data_offset_top + s] = bottom_data_THEN[data_offset_bottom + s];
+    }
+
+  }
+
+}
+
+template <typename Dtype>
+void ConditionalLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+        NOT_IMPLEMENTED;
+}
+
+INSTANTIATE_LAYER_GPU_FUNCS(ConditionalLayer);
+}  // namespace caffe
