294e85727e60711b72415d82076ef539d894c62b
manuele <manuele.tamburrano@gmail.com>
2014-11-11 19:23:56 +0100

    implemented conditional layer
    Conflicts:
    	include/caffe/common_layers.hpp
    	src/caffe/layers/conditional_layer.cpp
    	src/caffe/proto/caffe.proto

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -151,6 +151,80 @@ class ConcatLayer : public Layer<Dtype> {
 };
 
 /**
+ * @brief Takes two Blob%s, computes the result of the priority layer,
+ *  and allow or block the successive forward pass according to the result
+ */
+template <typename Dtype>
+class ConditionalLayer : public Layer<Dtype> {
+ public:
+  explicit ConditionalLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline LayerParameter_LayerType type() const {
+    return LayerParameter_LayerType_CONDITIONAL;
+  }
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  /**
+   * @param bottom input Blob vector (length 2+)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x_1 @f$
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x_2 @f$
+   *   -# ...
+   *   - K @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x_K @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (KN \times C \times H \times W) @f$ if concat_dim == 0, or
+   *      @f$ (N \times KC \times H \times W) @f$ if concat_dim == 1:
+   *      the concatenated output @f$
+   *        y = [\begin{array}{cccc} x_1 & x_2 & ... & x_K \end{array}]
+   *      @f$
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  /**
+   * @brief Computes the error gradient w.r.t. the concatenate inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *        respect to the outputs
+   *   -# @f$ (KN \times C \times H \times W) @f$ if concat_dim == 0, or
+   *      @f$ (N \times KC \times H \times W) @f$ if concat_dim == 1:
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to concatenated outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length K), into which the top gradient
+   *        @f$ \frac{\partial E}{\partial y} @f$ is deconcatenated back to the
+   *        inputs @f$
+   *        \left[ \begin{array}{cccc}
+   *          \frac{\partial E}{\partial x_1} &
+   *          \frac{\partial E}{\partial x_2} &
+   *          ... &
+   *          \frac{\partial E}{\partial x_K}
+   *        \end{array} \right] =
+   *        \frac{\partial E}{\partial y}
+   *        @f$
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  Blob<Dtype> col_bob_;
+  int count_;
+  int num_;
+  int channels_;
+  int height_;
+  int width_;
+  int concat_dim_;
+};
+
+/**
  * @brief Compute elementwise operations, such as product and sum,
  *        along multiple input Blobs.
  *
---
+++ src/caffe/layers/conditional_layer.cpp
@@ -0,0 +1,91 @@
+#include <vector>
+
+#include "caffe/layer.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/vision_layers.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void ConditionalLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  conditional_index_ = this->layer_param_.conditional_param().conditional_index();
+  CHECK_LE(conditional_index_,bottom[0]->channels()-1 ) <<
+    "conditional_index_ should be <= bottom[0]->channels()-1";
+}
+
+template <typename Dtype>
+void ConditionalLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  //bottom[0] is the "IF_blob"
+  //bottom[1] is the "THEN_blob"
+  //top[0] is the vector of indices that passed the conditional check
+  //top[1] is the top to which will be forwarded bottom[1]
+  top[0]->Reshape(1, 1, 1, 1);
+  top[1]->Reshape(bottom[1]->num(), bottom[1]->channels(), bottom[1]->height(), bottom[1]->width());
+  
+}
+
+template <typename Dtype>
+void ConditionalLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  
+  const Dtype* bottom_data_IF = bottom[0]->cpu_data();
+  const Dtype* bottom_data_THEN = bottom[1]->cpu_data();
+  
+  vector<float> indices_to_keep;
+  int num_items = bottom[0]->num();
+  int num_channels = bottom[0]->channels();
+  //look through the batch to find who passes the conditional check
+  for (size_t item_id = 0; item_id < num_items; ++item_id) {
+    
+    int index_IF = item_id*num_channels;
+    const Dtype* tmp_data_IF = bottom_data_IF + index_IF;
+    int argmax = std::distance(tmp_data_IF,
+        std::max_element(tmp_data_IF, tmp_data_IF + num_channels));
+    
+    if (argmax == conditional_index_)
+      indices_to_keep.push_back(item_id);
+      
+  }
+  
+  //Only items that passed conditional check will be forwarded
+  int new_tops_num = indices_to_keep.size();
+  if (new_tops_num == 0)
+    new_tops_num = 1;
+
+  top[0]->Reshape(new_tops_num, 1, 1, 1);
+  top[1]->Reshape(new_tops_num, top[1]->channels(), top[1]->height(), top[1]->width());
+
+  Dtype* top_data_indices = top[0]->mutable_cpu_data();
+  Dtype* top_data = top[1]->mutable_cpu_data();
+  
+  size_t size_top = top[1]->count()/top[1]->num();
+  for (size_t n = 0; n<indices_to_keep.size(); n++)
+  {
+    int offset = indices_to_keep[n];
+    top_data_indices[n] = (float)offset;
+    int data_offset_top = size_top*n;
+    int data_offset_bottom = size_top*offset;
+    for(size_t s = 0; s<size_top; s++)
+    {
+      top_data[data_offset_top + s] = bottom_data_THEN[data_offset_bottom + s];
+    }
+
+  }
+
+}
+
+template <typename Dtype>
+void ConditionalLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+        NOT_IMPLEMENTED;
+}
+
+#ifdef CPU_ONLY
+STUB_GPU(ConditionalLayer);
+#endif
+
+INSTANTIATE_CLASS(ConditionalLayer);
+REGISTER_LAYER_CLASS(CONDITIONAL, ConditionalLayer);
+}  // namespace caffe
