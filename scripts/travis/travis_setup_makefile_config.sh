0c2a1bcf4feb84de91c7d56f231f100cf8ad0b4f
Jonathan L Long <jonlong@cs.berkeley.edu>
2014-11-20 20:40:56 -0800

    [travis] run pytest

--- scripts/travis/travis_setup_makefile_config.sh
+++ scripts/travis/travis_setup_makefile_config.sh
@@ -10,3 +10,13 @@ if $WITH_CUDA; then
   GENCODE="$GENCODE -gencode arch=compute_50,code=compute_50"
   echo "CUDA_ARCH := $GENCODE" >> Makefile.config
 fi
+
+cat << 'EOF' >> Makefile.config
+ANACONDA_HOME := $(HOME)/miniconda
+PYTHON_INCLUDE := $(ANACONDA_HOME)/include \
+		$(ANACONDA_HOME)/include/python2.7 \
+		$(ANACONDA_HOME)/lib/python2.7/site-packages/numpy/core/include
+PYTHON_LIB := $(ANACONDA_HOME)/lib
+INCLUDE_DIRS := $(PYTHON_INCLUDE) /usr/local/include
+LIBRARY_DIRS := $(PYTHON_LIB) /usr/local/lib /usr/lib
+EOF
