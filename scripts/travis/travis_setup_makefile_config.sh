a44cb8a9106ea1fbc5da87680a3a629bff892172
Jonathan L Long <jonlong@cs.berkeley.edu>
2014-11-25 11:07:52 -0800

    [travis] remove unneeded Makefile.config sed hacking
    Make allows variable redefinition, so sed brittleness can be avoided.

--- scripts/travis/travis_setup_makefile_config.sh
+++ scripts/travis/travis_setup_makefile_config.sh
@@ -5,9 +5,7 @@ set -e
 mv Makefile.config.example Makefile.config
 
 if $WITH_CUDA; then
-  # Remove default gencode set; only generate compute_50.
-  sed -i 's/-gencode arch=.*\\//' Makefile.config
-  sed -i 's/CUDA_ARCH :=//' Makefile.config
+  # Only generate compute_50.
   GENCODE="-gencode arch=compute_50,code=sm_50"
   GENCODE="$GENCODE -gencode arch=compute_50,code=compute_50"
   echo "CUDA_ARCH := $GENCODE" >> Makefile.config
