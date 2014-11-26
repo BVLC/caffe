1f0bffde13a4c90cb5d4d108801325c943473245
manuele <manuele.tamburrano@gmail.com>
2014-11-26 19:18:23 +0100

    Removed propagate labels check in loss layers

--- src/caffe/layers/sigmoid_cross_entropy_loss_layer.cpp
+++ src/caffe/layers/sigmoid_cross_entropy_loss_layer.cpp
@@ -52,10 +52,6 @@ template <typename Dtype>
 void SigmoidCrossEntropyLossLayer<Dtype>::Backward_cpu(
     const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down,
     const vector<Blob<Dtype>*>& bottom) {
-  if (propagate_down[1]) {
-    LOG(FATAL) << this->type_name()
-               << " Layer cannot backpropagate to label inputs.";
-  }
   if (propagate_down[0]) {
     // First, compute the diff
     const int count = bottom[0]->count();
--- src/caffe/layers/softmax_loss_layer.cpp
+++ src/caffe/layers/softmax_loss_layer.cpp
@@ -61,10 +61,6 @@ template <typename Dtype>
 void SoftmaxWithLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const vector<bool>& propagate_down,
     const vector<Blob<Dtype>*>& bottom) {
-  if (propagate_down[1]) {
-    LOG(FATAL) << this->type_name()
-               << " Layer cannot backpropagate to label inputs.";
-  }
   if (propagate_down[0]) {
     Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
     const Dtype* prob_data = prob_.cpu_data();
