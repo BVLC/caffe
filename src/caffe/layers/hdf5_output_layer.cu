9bc99ebf1a419d767f4a11ea2a0e5018ff5fe4cd
Sergio <sguada@gmail.com>
2014-11-26 03:31:28 -0800

    Add check for bottom names

--- src/caffe/layers/hdf5_output_layer.cu
+++ src/caffe/layers/hdf5_output_layer.cu
@@ -15,22 +15,6 @@ template <typename Dtype>
 void HDF5OutputLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   Forward_cpu(bottom, top);
-  // CHECK_GE(bottom.size(), 2);
-  // CHECK_EQ(bottom[0]->num(), bottom[1]->num());
-  // data_blob_.Reshape(bottom[0]->num(), bottom[0]->channels(),
-  //                    bottom[0]->height(), bottom[0]->width());
-  // label_blob_.Reshape(bottom[1]->num(), bottom[1]->channels(),
-  //                    bottom[1]->height(), bottom[1]->width());
-  // const int data_datum_dim = bottom[0]->count() / bottom[0]->num();
-  // const int label_datum_dim = bottom[1]->count() / bottom[1]->num();
-
-  // for (int i = 0; i < bottom[0]->num(); ++i) {
-  //   caffe_copy(data_datum_dim, &bottom[0]->gpu_data()[i * data_datum_dim],
-  //       &data_blob_.mutable_cpu_data()[i * data_datum_dim]);
-  //   caffe_copy(label_datum_dim, &bottom[1]->gpu_data()[i * label_datum_dim],
-  //       &label_blob_.mutable_cpu_data()[i * label_datum_dim]);
-  // }
-  // SaveBlobs();
 }
 
 template <typename Dtype>
