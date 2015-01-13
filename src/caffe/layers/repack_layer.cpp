fd405683f59c63092c0df0e03e054d253dbaf1ff
philkr <philkr@users.noreply.github.com>
2015-01-13 13:36:25 -0800

    Making lint happy

--- src/caffe/layers/repack_layer.cpp
+++ src/caffe/layers/repack_layer.cpp
@@ -1,11 +1,14 @@
-#include "caffe/vision_layers.hpp"
+#include <vector>
+
 #include "caffe/proto/caffe.pb.h"
+#include "caffe/vision_layers.hpp"
 
 namespace caffe {
 template<typename Dtype>
-void RepackLayer< Dtype >::LayerSetUp(const vector< Blob< Dtype >* >& bottom, const vector< Blob< Dtype >* >& top) {
+void RepackLayer< Dtype >::LayerSetUp(const vector< Blob< Dtype >* >& bottom,
+                                      const vector< Blob< Dtype >* >& top) {
   RepackParameter repack_param = this->layer_param_.repack_param();
-  
+
   if (!repack_param.has_stride_h()) {
     stride_h_ = stride_w_ = repack_param.stride();
   } else {
@@ -15,66 +18,77 @@ void RepackLayer< Dtype >::LayerSetUp(const vector< Blob< Dtype >* >& bottom, co
   operation_ = repack_param.operation();
 }
 template<typename Dtype>
-void RepackLayer< Dtype >::Reshape(const vector< Blob< Dtype >* >& bottom, const vector< Blob< Dtype >* >& top) {
-  if( operation_ == RepackParameter_Operation_PACK_IMAGE ) {
-    int nW = (bottom[0]->width ()-1)/stride_w_+1;
+void RepackLayer< Dtype >::Reshape(const vector< Blob< Dtype >* >& bottom,
+                                   const vector< Blob< Dtype >* >& top) {
+  if ( operation_ == RepackParameter_Operation_PACK_IMAGE ) {
+    int nW = (bottom[0]->width()-1)/stride_w_+1;
     int nH = (bottom[0]->height()-1)/stride_h_+1;
     int nN = stride_w_*stride_h_*bottom[0]->num();
-    top[0]->Reshape( nN, bottom[0]->channels(), nH, nW );
-  }
-  else if( operation_ == RepackParameter_Operation_UNPACK_IMAGE ) {
-    int nW = bottom[0]->width ()*stride_w_;
+    top[0]->Reshape(nN, bottom[0]->channels(), nH, nW);
+  } else if ( operation_ == RepackParameter_Operation_UNPACK_IMAGE ) {
+    int nW = bottom[0]->width()*stride_w_;
     int nH = bottom[0]->height()*stride_h_;
     int nN = bottom[0]->num()/(stride_w_*stride_h_);
-    top[0]->Reshape( nN, bottom[0]->channels(), nH, nW );
-  }
-  else
-    LOG(ERROR)<<"Unknown repacking operation!";
+    top[0]->Reshape(nN, bottom[0]->channels(), nH, nW);
+  } else { LOG(ERROR) << "Unknown repacking operation!"; }
 }
 template<typename Dtype>
 LayerParameter_LayerType RepackLayer< Dtype >::type() const {
   return LayerParameter_LayerType_REPACK;
 }
 template<typename Dtype>
-inline void pack_cpu(const Dtype* input, int in_n, int in_c, int in_h, int in_w, Dtype* output, int s_h, int s_w) {
+inline void pack_cpu(const Dtype* input, int in_n, int in_c, int in_h, int in_w,
+                     Dtype* output, int s_h, int s_w) {
   int out_n = s_h*s_w*in_n, out_w = (in_w-1)/s_w+1, out_h = (in_h-1)/s_h+1;
-  for( int y=0,o=0; y<s_h; y++ )
-    for( int x=0; x<s_w; x++,o++ )
-      for( int n=0; n<in_n; n++ )
-        for( int c=0; c<in_c; c++ )
-          for( int j=0; j<out_h; j++ )
-            for( int i=0; i<out_w; i++ )
+  for ( int y = 0, o = 0; y < s_h; y++ )
+    for ( int x = 0; x < s_w; x++ )
+      for ( int n = 0; n < in_n; n++ )
+        for ( int c = 0; c < in_c; c++ )
+          for ( int j = 0; j < out_h; j++ )
+            for ( int i = 0; i < out_w; i++, o++ )
               if (j*s_h+y < in_h && i*s_w+x < in_w )
-                output[((o*in_c+c)*out_h+j)*out_w+i] = input[((n*in_c+c)*in_h+j*s_h+y)*in_w+i*s_w+x];
+                output[o] = input[((n*in_c+c)*in_h+j*s_h+y)*in_w+i*s_w+x];
               else
-                output[((o*in_c+c)*out_h+j)*out_w+i] = 0;
+                output[o] = 0;
 }
 template<typename Dtype>
-inline void unpack_cpu(const Dtype* input, Dtype* output, int out_n, int out_c, int out_h, int out_w, int s_h, int s_w) {
+inline void unpack_cpu(const Dtype* input, Dtype* output, int out_n, int out_c,
+                       int out_h, int out_w, int s_h, int s_w) {
   int in_n = s_h*s_w*out_n, in_w = (out_w-1)/s_w+1, in_h = (out_h-1)/s_h+1;
-  for( int y=0,o=0; y<s_h; y++ )
-    for( int x=0; x<s_w; x++,o++ )
-      for( int n=0; n<out_n; n++ )
-        for( int c=0; c<out_c; c++ )
-          for( int j=0; j<in_h; j++ )
-            for( int i=0; i<in_w; i++ )
+  for ( int y = 0, o = 0; y < s_h; y++ )
+    for ( int x = 0; x < s_w; x++ )
+      for ( int n = 0; n < out_n; n++ )
+        for ( int c = 0; c < out_c; c++ )
+          for ( int j = 0; j < in_h; j++ )
+            for ( int i = 0; i < in_w; i++, o++ )
               if (j*s_h+y < out_h && i*s_w+x < out_w )
-                output[((n*out_c+c)*out_h+j*s_h+y)*out_w+i*s_w+x] = input[((o*out_c+c)*in_h+j)*in_w+i];
+                output[((n*out_c+c)*out_h+j*s_h+y)*out_w+i*s_w+x] = input[o];
 }
 template<typename Dtype>
-void RepackLayer< Dtype >::Forward_cpu(const vector< Blob< Dtype >* >& bottom, const vector< Blob< Dtype >* >& top) {
-  if( operation_ == RepackParameter_Operation_PACK_IMAGE )
-    pack_cpu(bottom[0]->cpu_data(),bottom[0]->num(),bottom[0]->channels(),bottom[0]->height(),bottom[0]->width(), top[0]->mutable_cpu_data(), stride_h_, stride_w_);
+void RepackLayer< Dtype >::Forward_cpu(const vector< Blob< Dtype >* >& bottom,
+                                       const vector< Blob< Dtype >* >& top) {
+  if ( operation_ == RepackParameter_Operation_PACK_IMAGE )
+    pack_cpu(bottom[0]->cpu_data(), bottom[0]->num(), bottom[0]->channels(),
+             bottom[0]->height(), bottom[0]->width(),
+             top[0]->mutable_cpu_data(), stride_h_, stride_w_);
   else
-    unpack_cpu(bottom[0]->cpu_data(),top[0]->mutable_cpu_data(),top[0]->num(),top[0]->channels(),top[0]->height(),top[0]->width(), stride_h_, stride_w_);
+    unpack_cpu(bottom[0]->cpu_data(), top[0]->mutable_cpu_data(), top[0]->num(),
+               top[0]->channels(), top[0]->height(), top[0]->width(),
+               stride_h_, stride_w_);
 }
 template<typename Dtype>
-void RepackLayer< Dtype >::Backward_cpu(const vector< Blob< Dtype >* >& top, const vector< bool >& propagate_down, const vector< Blob< Dtype >* >& bottom) {
+void RepackLayer< Dtype >::Backward_cpu(const vector< Blob<Dtype>* >& top,
+                                        const vector<bool>& propagate_down,
+                                        const vector< Blob<Dtype>* >& bottom) {
   if (!propagate_down[0]) return;
-  if( operation_ == RepackParameter_Operation_UNPACK_IMAGE )
-    pack_cpu(top[0]->cpu_diff(),top[0]->num(),top[0]->channels(),top[0]->height(),top[0]->width(), bottom[0]->mutable_cpu_diff(), stride_h_, stride_w_);
+  if ( operation_ == RepackParameter_Operation_UNPACK_IMAGE )
+    pack_cpu(top[0]->cpu_diff(), top[0]->num(), top[0]->channels(),
+             top[0]->height(), top[0]->width(),
+             bottom[0]->mutable_cpu_diff(), stride_h_, stride_w_);
   else
-    unpack_cpu(top[0]->cpu_diff(),bottom[0]->mutable_cpu_diff(),bottom[0]->num(),bottom[0]->channels(),bottom[0]->height(),bottom[0]->width(), stride_h_, stride_w_);
+    unpack_cpu(top[0]->cpu_diff(), bottom[0]->mutable_cpu_diff(),
+               bottom[0]->num(), bottom[0]->channels(), bottom[0]->height(),
+               bottom[0]->width(), stride_h_, stride_w_);
 }
 
 
@@ -85,4 +99,4 @@ STUB_GPU(RepackLayer);
 INSTANTIATE_CLASS(RepackLayer);
 REGISTER_LAYER_CLASS(REPACK, RepackLayer);
 
-} // End namespace caffe;
+}  // namespace caffe
--- src/caffe/layers/repack_layer.cu
+++ src/caffe/layers/repack_layer.cu
@@ -3,31 +3,34 @@
 
 namespace caffe {
 
-__device__ void computeCoord( int & x, int & y, int & i, int & j, int & c, int & n, int index, int u_n, int u_c, int p_h, int p_w, int s_w ) {
-    // Get the coordinates
-  x = index;
-  i = x % p_w; // width dimension
-  x /= p_w;
-  j = x % p_h; // height dimension
-  x /= p_h;
-  c = x % u_c; // channel dimension
-  x /= u_c;
-  n = x % u_n; // num dimension
-  x /= u_n;
-  
+__device__ void computeCoord(int * x, int * y, int * i, int * j, int * c,
+                             int * n, int index, int u_n, int u_c, int p_h,
+                             int p_w, int s_w) {
+  // Get the coordinates
+  *x = index;
+  *i = *x % p_w;  // width dimension
+  *x /= p_w;
+  *j = *x % p_h;  // height dimension
+  *x /= p_h;
+  *c = *x % u_c;  // channel dimension
+  *x /= u_c;
+  *n = *x % u_n;  // num dimension
+  *x /= u_n;
+
   // Get the packing indices
-  y = x % s_w;
-  x /= s_w;
+  *y = *x % s_w;
+  *x /= s_w;
 }
 
 template <typename Dtype>
-__global__ void pack_gpu_kernel(const Dtype* unpacked, int u_n, int u_c, int u_h, int u_w, Dtype* packed, int s_h, int s_w) {
+__global__ void pack_gpu_kernel(const Dtype* unpacked, int u_n, int u_c,
+                                int u_h, int u_w, Dtype* packed, int s_h,
+                                int s_w) {
   const int p_n = s_h*s_w*u_n, p_w = (u_w-1)/s_w+1, p_h = (u_h-1)/s_h+1;
   const int N = p_n*u_c*p_h*p_w;
   CUDA_KERNEL_LOOP(index, N) {
     int x, y, i, j, c, n;
-    computeCoord( x, y, i, j, c, n, index, u_n, u_c, p_h, p_w, s_w );
-    
+    computeCoord(&x, &y, &i, &j, &c, &n, index, u_n, u_c, p_h, p_w, s_w);
     if (j*s_h+y < u_h && i*s_w+x < u_w )
       packed[index] = unpacked[((n*u_c+c)*u_h+j*s_h+y)*u_w+i*s_w+x];
     else
@@ -36,22 +39,25 @@ __global__ void pack_gpu_kernel(const Dtype* unpacked, int u_n, int u_c, int u_h
 }
 
 template <typename Dtype>
-void pack_gpu(const Dtype* unpacked, int u_n, int u_c, int u_h, int u_w, Dtype* packed, int s_h, int s_w) {
+void pack_gpu(const Dtype* unpacked, int u_n, int u_c, int u_h, int u_w,
+              Dtype* packed, int s_h, int s_w) {
   const int p_n = s_h*s_w*u_n, p_w = (u_w-1)/s_w+1, p_h = (u_h-1)/s_h+1;
   const int N = p_n*u_c*p_h*p_w;
+  /* NOLINT_NEXT_LINE(whitespace/operators) */
   pack_gpu_kernel<Dtype><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(
-      unpacked, u_n, u_c, u_h, u_w, packed, s_h, s_w );
+      unpacked, u_n, u_c, u_h, u_w, packed, s_h, s_w);
   CUDA_POST_KERNEL_CHECK;
 }
 
 template <typename Dtype>
-__global__ void unpack_gpu_kernel(const Dtype* packed, Dtype* unpacked, int u_n, int u_c, int u_h, int u_w, int s_h, int s_w) {
+__global__ void unpack_gpu_kernel(const Dtype* packed, Dtype* unpacked,
+                                  int u_n, int u_c, int u_h, int u_w,
+                                  int s_h, int s_w) {
   const int p_n = s_h*s_w*u_n, p_w = (u_w-1)/s_w+1, p_h = (u_h-1)/s_h+1;
   const int N = p_n*u_c*p_h*p_w;
   CUDA_KERNEL_LOOP(index, N) {
     int x, y, i, j, c, n;
-    computeCoord( x, y, i, j, c, n, index, u_n, u_c, p_h, p_w, s_w );
-    
+    computeCoord(x, y, i, j, c, n, index, u_n, u_c, p_h, p_w, s_w);
     // TODO: This is not all that memory friendly!
     if (j*s_h+y < u_h && i*s_w+x < u_w )
       unpacked[((n*u_c+c)*u_h+j*s_h+y)*u_w+i*s_w+x] = packed[index];
@@ -59,30 +65,43 @@ __global__ void unpack_gpu_kernel(const Dtype* packed, Dtype* unpacked, int u_n,
 }
 
 template <typename Dtype>
-void unpack_gpu(const Dtype* packed, Dtype* unpacked, int u_n, int u_c, int u_h, int u_w, int s_h, int s_w) {
+void unpack_gpu(const Dtype* packed, Dtype* unpacked, int u_n, int u_c,
+                int u_h, int u_w, int s_h, int s_w) {
   int p_n = s_h*s_w*u_n, p_w = (u_w-1)/s_w+1, p_h = (u_h-1)/s_h+1;
   const int N = p_n*u_c*p_h*p_w;
+  /* NOLINT_NEXT_LINE(whitespace/operators) */
   unpack_gpu_kernel<Dtype><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(
-      packed, unpacked, u_n, u_c, u_h, u_w, s_h, s_w );
+      packed, unpacked, u_n, u_c, u_h, u_w, s_h, s_w);
   CUDA_POST_KERNEL_CHECK;
 }
 
 template<typename Dtype>
-void RepackLayer< Dtype >::Forward_gpu(const vector< Blob< Dtype >* >& bottom, const vector< Blob< Dtype >* >& top) {
-  if( operation_ == RepackParameter_Operation_PACK_IMAGE )
-    pack_gpu(bottom[0]->gpu_data(),bottom[0]->num(),bottom[0]->channels(),bottom[0]->height(),bottom[0]->width(), top[0]->mutable_gpu_data(), stride_h_, stride_w_);
+void RepackLayer< Dtype >::Forward_gpu(const vector< Blob< Dtype >* >& bottom,
+                                       const vector< Blob< Dtype >* >& top) {
+  if ( operation_ == RepackParameter_Operation_PACK_IMAGE )
+    pack_gpu(bottom[0]->gpu_data(), bottom[0]->num(), bottom[0]->channels(),
+             bottom[0]->height(), bottom[0]->width(),
+             top[0]->mutable_gpu_data(), stride_h_, stride_w_);
   else
-    unpack_gpu(bottom[0]->gpu_data(),top[0]->mutable_gpu_data(),top[0]->num(),top[0]->channels(),top[0]->height(),top[0]->width(), stride_h_, stride_w_);
+    unpack_gpu(bottom[0]->gpu_data(), top[0]->mutable_gpu_data(), top[0]->num(),
+               top[0]->channels(), top[0]->height(), top[0]->width(),
+               stride_h_, stride_w_);
 }
 template<typename Dtype>
-void RepackLayer< Dtype >::Backward_gpu(const vector< Blob< Dtype >* >& top, const vector< bool >& propagate_down, const vector< Blob< Dtype >* >& bottom) {
+void RepackLayer< Dtype >::Backward_gpu(const vector< Blob<Dtype>* >& top,
+                                        const vector<bool>& propagate_down,
+                                        const vector< Blob<Dtype>* >& bottom) {
   if (!propagate_down[0]) return;
-  if( operation_ == RepackParameter_Operation_UNPACK_IMAGE )
-    pack_gpu(top[0]->gpu_diff(),top[0]->num(),top[0]->channels(),top[0]->height(),top[0]->width(), bottom[0]->mutable_gpu_diff(), stride_h_, stride_w_);
+  if ( operation_ == RepackParameter_Operation_UNPACK_IMAGE )
+    pack_gpu(top[0]->gpu_diff(), top[0]->num(), top[0]->channels(),
+             top[0]->height(), top[0]->width(),
+             bottom[0]->mutable_gpu_diff(), stride_h_, stride_w_);
   else
-    unpack_gpu(top[0]->gpu_diff(),bottom[0]->mutable_gpu_diff(),bottom[0]->num(),bottom[0]->channels(),bottom[0]->height(),bottom[0]->width(), stride_h_, stride_w_);
+    unpack_gpu(top[0]->gpu_diff(), bottom[0]->mutable_gpu_diff(),
+               bottom[0]->num(), bottom[0]->channels(), bottom[0]->height(),
+               bottom[0]->width(), stride_h_, stride_w_);
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(RepackLayer);
 
-} // End namespace caffe;
+}  // namespace caffe
