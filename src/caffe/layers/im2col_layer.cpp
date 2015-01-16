2273406d498d75a41593be5212a4d779a2a73dc5
Jeff Donahue <jeff.donahue@gmail.com>
2015-01-15 19:50:42 -0800

    Add CHECK_EQ(4, ...)s to "vision layers" to enforce that the num/channnels/height/width indexing is valid.

--- src/caffe/layers/im2col_layer.cpp
+++ src/caffe/layers/im2col_layer.cpp
@@ -50,6 +50,8 @@ void Im2colLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
 template <typename Dtype>
 void Im2colLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
+  CHECK_EQ(4, bottom[0]->num_axes()) << "Input must have 4 axes, "
+      << "corresponding to (num, channels, height, width)";
   channels_ = bottom[0]->channels();
   height_ = bottom[0]->height();
   width_ = bottom[0]->width();
