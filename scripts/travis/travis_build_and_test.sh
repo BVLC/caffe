c0ae656c0e80e721fec89b63e2cdc390e440d625
Anatoly Baksheev <no@email>
2015-01-18 23:22:09 +0300

    [travis] proper cmake params

--- scripts/travis/travis_build_and_test.sh
+++ scripts/travis/travis_build_and_test.sh
@@ -7,7 +7,7 @@ MAKE="make --jobs=$NUM_THREADS --keep-going"
 if $WITH_CMAKE; then
   mkdir build
   cd build
-  cmake -DBUILD_PYTHON=ON -DBUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Release -DCPU_ONLY=ON ..
+  cmake -DBUILD_python=ON -DCMAKE_BUILD_TYPE=Release -DCPU_ONLY=ON ..
   $MAKE
   if ! $WITH_CUDA; then
     $MAKE runtest
