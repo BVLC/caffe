1ccda867ea1eacc96dfd9e036a4cd9cf94939efd
Anatoly Baksheev <no@email>
2015-01-18 22:55:26 +0300

    opencv 3.0 compilation (replacement of PR 1714)

--- cmake/lint.cmake
+++ cmake/lint.cmake
@@ -1,9 +1,11 @@
 
-set(CMAKE_SOURCE_DIR ../)
+set(CMAKE_SOURCE_DIR ..)
 set(LINT_COMMAND ${CMAKE_SOURCE_DIR}/scripts/cpp_lint.py)
 set(SRC_FILE_EXTENSIONS h hpp hu c cpp cu cc)
 set(EXCLUDE_FILE_EXTENSTIONS pb.h pb.cc)
 set(LINT_DIRS include src/caffe examples tools python matlab)
+
+cmake_policy(SET CMP0009 NEW)  # supress cmake warning
 
 # find all files of interest
 foreach(ext ${SRC_FILE_EXTENSIONS})
