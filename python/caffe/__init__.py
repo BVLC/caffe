984f4e2115a778eb331603194ff251f28d9cafda
Evan Shelhamer <shelhamer@imaginarynumber.net>
2015-01-25 20:37:52 -0800

    [pycaffe] de-dupe imports

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -1,6 +1,6 @@
 from .pycaffe import Net, SGDSolver
-from .pycaffe import set_mode_cpu, set_mode_gpu, set_phase_train, set_phase_test
-from .pycaffe import set_device
+from ._caffe import set_mode_cpu, set_mode_gpu, set_device, \
+    set_phase_train, set_phase_test
 from .classifier import Classifier
 from .detector import Detector
 import io
