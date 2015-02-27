b63db7fbdac9d5e63b644485291e493dcdf33322
Ryosuke Okuta <okuta@preferred.jp>
2015-02-27 13:59:54 +0900

    add check code

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -14,6 +14,11 @@ template<typename Dtype>
 DataTransformer<Dtype>::DataTransformer(const TransformationParameter& param,
     Phase phase)
     : param_(param), phase_(phase) {
+  CHECK(param_.crop_size() == 0 ||
+        param_.crop_h() == 0 && param_.crop_w() == 0)
+        << "Crop size is crop_size OR crop_h and crop_w; not both";
+  CHECK((param_.crop_h() != 0) == (param_.crop_w() != 0))
+        << "For non-square crops both crop_h and crop_w are required.";
   // check if we want to use mean_file
   if (param_.has_mean_file()) {
     CHECK_EQ(param_.mean_value_size(), 0) <<
