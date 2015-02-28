22978f2042c4092fe40ee3134700f887e53fc2df
Rohit Durvasula <durvasula.rohit@gmail.com>
2015-02-28 15:26:06 -0500

    Update make_imagenet_mean.sh

--- examples/imagenet/make_imagenet_mean.sh
+++ examples/imagenet/make_imagenet_mean.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# Compute the mean image from the imagenet training leveldb
+# Compute the mean image from the imagenet training lmdb
 # N.B. this is available in data/ilsvrc12
 
 ./build/tools/compute_image_mean examples/imagenet/ilsvrc12_train_lmdb \
