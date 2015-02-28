6930e883b25c6d24b8b845e961559daa527b0b92
Takuya Narihira <dvbscb@gmail.com>
2015-02-27 19:01:54 -0800

    Error in python can be shown
    Now we can keep the exception information on Python interpreter. This is
    usefull if you are working on Python interface. You can catch the right
    Exception class when error occurs in your `PythonLayer`. Now we don't
    use `PyErr_Print` that clears exceptions. We use `PyErr_Fetch` to get
    the information and `PyErr_Restore` to restore the exceptions back. And
    I find that `bp::import` or `bp::object::attr` breaks Python interpreter
    if a module or an attribute doesn't exist. We now use `bp::exec` and
    `bp::eval` to keep Python interpreter working even after errors occur. I
    got the hint from the following page.
    
    http://boost.2283326.n4.nabble.com/embedded-python-td2702335.html
    
    And I also modified to use `bp::object` for `self_` member.

--- include/caffe/python_layer.hpp
+++ include/caffe/python_layer.hpp
@@ -2,6 +2,8 @@
 #define CAFFE_PYTHON_LAYER_HPP_
 
 #include <boost/python.hpp>
+
+#include <string>
 #include <vector>
 
 #include "caffe/layer.hpp"
@@ -10,29 +12,41 @@ namespace bp = boost::python;
 
 namespace caffe {
 
+#define PYTHON_LAYER_ERROR() { \
+  PyObject *petype, *pevalue, *petrace; \
+  PyErr_Fetch(&petype, &pevalue, &petrace); \
+  bp::object etype(bp::handle<>(bp::borrowed(petype))); \
+  bp::object evalue(bp::handle<>(bp::borrowed(bp::allow_null(pevalue)))); \
+  bp::object etrace(bp::handle<>(bp::borrowed(bp::allow_null(petrace)))); \
+  bp::object sio(bp::import("StringIO").attr("StringIO")()); \
+  bp::import("traceback").attr("print_exception")( \
+    etype, evalue, etrace, bp::object(), sio); \
+  LOG(INFO) << bp::extract<string>(sio.attr("getvalue")())(); \
+  PyErr_Restore(petype, pevalue, petrace); \
+  throw; \
+}
+
 template <typename Dtype>
 class PythonLayer : public Layer<Dtype> {
  public:
   PythonLayer(PyObject* self, const LayerParameter& param)
-      : Layer<Dtype>(param), self_(self) { }
+      : Layer<Dtype>(param), self_(bp::handle<>(bp::borrowed(self))) { }
 
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
     try {
-      bp::call_method<bp::object>(self_, "setup", bottom, top);
+      self_.attr("setup")(bottom, top);
     } catch (bp::error_already_set) {
-      PyErr_Print();
-      throw;
+      PYTHON_LAYER_ERROR();
     }
   }
 
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
     try {
-      bp::call_method<bp::object>(self_, "reshape", bottom, top);
+      self_.attr("reshape")(bottom, top);
     } catch (bp::error_already_set) {
-      PyErr_Print();
-      throw;
+      PYTHON_LAYER_ERROR();
     }
   }
 
@@ -42,25 +56,22 @@ class PythonLayer : public Layer<Dtype> {
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
     try {
-      bp::call_method<bp::object>(self_, "forward", bottom, top);
+      self_.attr("forward")(bottom, top);
     } catch (bp::error_already_set) {
-      PyErr_Print();
-      throw;
+      PYTHON_LAYER_ERROR();
     }
   }
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
     try {
-      bp::call_method<bp::object>(self_, "backward", top, propagate_down,
-          bottom);
+      self_.attr("backward")(top, propagate_down, bottom);
     } catch (bp::error_already_set) {
-      PyErr_Print();
-      throw;
+      PYTHON_LAYER_ERROR();
     }
   }
 
  private:
-  PyObject* self_;
+  bp::object self_;
 };
 
 }  // namespace caffe
