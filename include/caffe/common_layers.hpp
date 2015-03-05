70ac3346bec60e101fc1a464226c23ba52477016
Jonathan L Long <jonlong@cs.berkeley.edu>
2015-03-05 00:20:27 -0800

    add parameter layer for learning any bottom

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -297,6 +297,34 @@ class MVNLayer : public Layer<Dtype> {
   Blob<Dtype> sum_multiplier_;
 };
 
+template <typename Dtype>
+class ParameterLayer : public Layer<Dtype> {
+ public:
+  explicit ParameterLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+    this->blobs_.resize(1);
+    this->blobs_[0].reset(new Blob<Dtype>());
+    this->blobs_[0]->Reshape(this->layer_param_.parameter_param().shape());
+    top[0]->Reshape(this->layer_param_.parameter_param().shape());
+    this->blobs_[0]->ShareData(*top[0]);
+    this->blobs_[0]->ShareDiff(*top[0]);
+  }
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) { }
+  virtual inline const char* type() const { return "Parameter"; }
+  virtual inline int ExactNumBottomBlobs() const { return 0; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) { }
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)
+  { }
+};
+
 /**
  * @brief Ignores bottom blobs while producing no top blobs. (This is useful
  *        to suppress outputs during testing.)
