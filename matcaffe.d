fd03bc66b5c4f747fe12a5d81c9dfb7bdf4709d5
Daniel Gordon <xkcd@cs.washington.edu>
2015-03-12 13:20:53 -0700

    everything builds

---
+++ matcaffe.d
@@ -0,0 +1,127 @@
+matcaffe.o: matlab/caffe/matcaffe.cpp \
+ /usr/local/MATLAB/R2013b/extern/include/mex.h \
+ /usr/local/MATLAB/R2013b/extern/include/matrix.h \
+ /usr/local/MATLAB/R2013b/extern/include/tmwtypes.h \
+ include/caffe/caffe.hpp include/caffe/blob.hpp include/caffe/common.hpp \
+ include/caffe/util/device_alternate.hpp \
+ /usr/local/cuda/include/cublas_v2.h /usr/local/cuda/include/cublas_api.h \
+ /usr/local/cuda/include/driver_types.h \
+ /usr/local/cuda/include/host_defines.h \
+ /usr/local/cuda/include/cuComplex.h \
+ /usr/local/cuda/include/vector_types.h \
+ /usr/local/cuda/include/builtin_types.h \
+ /usr/local/cuda/include/device_types.h \
+ /usr/local/cuda/include/surface_types.h \
+ /usr/local/cuda/include/texture_types.h /usr/local/cuda/include/cuda.h \
+ /usr/local/cuda/include/cuda_runtime.h \
+ /usr/local/cuda/include/host_config.h \
+ /usr/local/cuda/include/channel_descriptor.h \
+ /usr/local/cuda/include/cuda_runtime_api.h \
+ /usr/local/cuda/include/cuda_device_runtime_api.h \
+ /usr/local/cuda/include/driver_functions.h \
+ /usr/local/cuda/include/vector_functions.h \
+ /usr/local/cuda/include/curand.h /usr/local/cuda/include/driver_types.h \
+ include/caffe/util/cudnn.hpp .build_release/src/caffe/proto/caffe.pb.h \
+ include/caffe/syncedmem.hpp include/caffe/util/math_functions.hpp \
+ include/caffe/util/mkl_alternate.hpp include/caffe/filler.hpp \
+ include/caffe/layer.hpp include/caffe/layer_factory.hpp \
+ include/caffe/net.hpp include/caffe/solver.hpp \
+ include/caffe/util/benchmark.hpp include/caffe/util/io.hpp \
+ include/caffe/vision_layers.hpp include/caffe/common_layers.hpp \
+ include/caffe/data_layers.hpp include/caffe/data_transformer.hpp \
+ include/caffe/internal_thread.hpp include/caffe/util/db.hpp \
+ include/caffe/loss_layers.hpp include/caffe/neuron_layers.hpp
+
+/usr/local/MATLAB/R2013b/extern/include/mex.h:
+
+/usr/local/MATLAB/R2013b/extern/include/matrix.h:
+
+/usr/local/MATLAB/R2013b/extern/include/tmwtypes.h:
+
+include/caffe/caffe.hpp:
+
+include/caffe/blob.hpp:
+
+include/caffe/common.hpp:
+
+include/caffe/util/device_alternate.hpp:
+
+/usr/local/cuda/include/cublas_v2.h:
+
+/usr/local/cuda/include/cublas_api.h:
+
+/usr/local/cuda/include/driver_types.h:
+
+/usr/local/cuda/include/host_defines.h:
+
+/usr/local/cuda/include/cuComplex.h:
+
+/usr/local/cuda/include/vector_types.h:
+
+/usr/local/cuda/include/builtin_types.h:
+
+/usr/local/cuda/include/device_types.h:
+
+/usr/local/cuda/include/surface_types.h:
+
+/usr/local/cuda/include/texture_types.h:
+
+/usr/local/cuda/include/cuda.h:
+
+/usr/local/cuda/include/cuda_runtime.h:
+
+/usr/local/cuda/include/host_config.h:
+
+/usr/local/cuda/include/channel_descriptor.h:
+
+/usr/local/cuda/include/cuda_runtime_api.h:
+
+/usr/local/cuda/include/cuda_device_runtime_api.h:
+
+/usr/local/cuda/include/driver_functions.h:
+
+/usr/local/cuda/include/vector_functions.h:
+
+/usr/local/cuda/include/curand.h:
+
+/usr/local/cuda/include/driver_types.h:
+
+include/caffe/util/cudnn.hpp:
+
+.build_release/src/caffe/proto/caffe.pb.h:
+
+include/caffe/syncedmem.hpp:
+
+include/caffe/util/math_functions.hpp:
+
+include/caffe/util/mkl_alternate.hpp:
+
+include/caffe/filler.hpp:
+
+include/caffe/layer.hpp:
+
+include/caffe/layer_factory.hpp:
+
+include/caffe/net.hpp:
+
+include/caffe/solver.hpp:
+
+include/caffe/util/benchmark.hpp:
+
+include/caffe/util/io.hpp:
+
+include/caffe/vision_layers.hpp:
+
+include/caffe/common_layers.hpp:
+
+include/caffe/data_layers.hpp:
+
+include/caffe/data_transformer.hpp:
+
+include/caffe/internal_thread.hpp:
+
+include/caffe/util/db.hpp:
+
+include/caffe/loss_layers.hpp:
+
+include/caffe/neuron_layers.hpp:
