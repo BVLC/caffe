5b0f40f758b04d735aa05682a07be6e48522dc17
Jeff Donahue <jeff.donahue@gmail.com>
2015-03-26 16:45:49 -0700

    Add prototxt & script for training LRCN from a pretrained language model (and pretrained AlexNet)

---
+++ examples/coco_caption/train_lrcn.from_lm.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+GPU_ID=0
+WEIGHTS=\
+./models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel\
+,\
+./examples/coco_caption/lstm_lm_iter_110000.caffemodel
+DATA_DIR=./examples/coco_caption/h5_data/
+if [ ! -d $DATA_DIR ]; then
+    echo "Data directory not found: $DATA_DIR"
+    echo "First, download the COCO dataset (follow instructions in data/coco)"
+    echo "Then, run ./examples/coco_caption/coco_to_hdf5_data.py to create the Caffe input data"
+    exit 1
+fi
+
+./build/tools/caffe train \
+    -solver ./examples/coco_caption/lrcn_solver.from_lm.prototxt \
+    -weights $WEIGHTS \
+    -gpu $GPU_ID
