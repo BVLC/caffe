c8304b9e7cdd72b8326706df94c7f91c70bcdfaa
George Papandreou <gpapan@google.com>
2015-03-30 16:28:31 -0700

    merged mat read/write

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -624,6 +624,62 @@ class LRNLayer : public Layer<Dtype> {
   vector<Blob<Dtype>*> product_bottom_vec_;
 };
 
+/*
+  MatReadLayer
+*/
+template <typename Dtype>
+class MatReadLayer : public Layer<Dtype> {
+ public:
+  explicit MatReadLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual inline const char* type() const { return "MatRead"; }
+  virtual inline int ExactNumBottomBlobs() const { return 0; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  bool reshape_;
+  int batch_size_;
+  int iter_;
+  string prefix_;
+  vector<string> fnames_;
+};
+
+/*
+  MatWriteLayer
+*/
+template <typename Dtype>
+class MatWriteLayer : public Layer<Dtype> {
+ public:
+  explicit MatWriteLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual inline const char* type() const { return "MatWrite"; }
+  virtual inline int ExactNumTopBlobs() const { return 0; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  int iter_;
+  int period_;
+  string prefix_;
+  vector<string> fnames_;
+};
+
 /**
  * @brief Pads (if pad >= 0) or crops (if pad < 0) parts of the input.
  */
