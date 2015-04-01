d16a8a713f3227f33c081f9103f95347e2cdd212
ddetone <daniel.detone@gmail.com>
2015-04-01 14:32:24 -0400

    Add 3DConvolutionLayer and vol2col function

--- include/caffe/util/im2col.hpp
+++ include/caffe/util/im2col.hpp
@@ -27,6 +27,21 @@ void col2im_gpu(const Dtype* data_col, const int channels,
     const int pad_h, const int pad_w, const int stride_h,
     const int stride_w, Dtype* data_im);
 
+template <typename Dtype>
+void vol2col_cpu(const Dtype* data_im, const int channels,
+    const int height, const int width, const int depth, 
+    const int kernel_h, const int kernel_w, const int kernel_d,
+    const int pad_h, const int pad_w, const int pad_d,
+    const int stride_h, const int stride_w, const int stride_d,
+    Dtype* data_col);
+
+template <typename Dtype>
+void col2vol_cpu(const Dtype* data_col, const int channels,
+    const int height, const int width, const int depth, const int patch_h, 
+    const int patch_w, const int patch_d, const int pad_h, const int pad_w,
+    const int pad_d, const int stride_h, const int stride_w, const int stride_d,
+    Dtype* data_im);
+
 }  // namespace caffe
 
 #endif  // CAFFE_UTIL_IM2COL_HPP_
--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -451,6 +451,115 @@ class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
 };
 #endif
 
+/**
+ * @brief Abstract base class that factors out the BLAS code common to
+ *        ConvolutionLayer and DeconvolutionLayer.
+ */
+template <typename Dtype>
+class Convolution3DLayer : public Layer<Dtype> {
+ public:
+  explicit Convolution3DLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  inline int MinBottomBlobs() const { return 1; }
+  inline int MinTopBlobs() const { return 1; }
+  inline bool EqualNumBottomTopBlobs() const { return true; }
+
+  inline const char* type() const { return "Convolution3D"; }
+
+ protected:
+  // Helper functions that abstract away the column buffer and gemm arguments.
+  // The last argument in forward_cpu_gemm is so that we can skip the im2col if
+  // we just called weight_cpu_gemm with the same input.
+  void forward_cpu_gemm(const Dtype* input, const Dtype* weights,
+      Dtype* output, bool skip_im2col = false);
+  void forward_cpu_bias(Dtype* output, const Dtype* bias);
+  void backward_cpu_gemm(const Dtype* input, const Dtype* weights,
+      Dtype* output);
+  void weight_cpu_gemm(const Dtype* input, const Dtype* output, Dtype*
+      weights);
+  void backward_cpu_bias(Dtype* bias, const Dtype* input);
+
+// #ifndef CPU_ONLY
+//   void forward_gpu_gemm(const Dtype* col_input, const Dtype* weights,
+//       Dtype* output, bool skip_im2col = false);
+//   void forward_gpu_bias(Dtype* output, const Dtype* bias);
+//   void backward_gpu_gemm(const Dtype* input, const Dtype* weights,
+//       Dtype* col_output);
+//   void weight_gpu_gemm(const Dtype* col_input, const Dtype* output, Dtype*
+//       weights);
+//   void backward_gpu_bias(Dtype* bias, const Dtype* input);
+// #endif
+
+  void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  // reverse_dimensions should return true iff we are implementing deconv, so
+  // that conv helpers know which dimensions are which.
+  inline bool reverse_dimensions() { return false; }
+  // Compute height_out_ and width_out_ and depth_out_ from other parameters.
+  void compute_output_shape();
+
+  int kernel_h_, kernel_w_, kernel_d_;
+  int stride_h_, stride_w_, stride_d_;
+  int num_;
+  int channels_;
+  int pad_h_, pad_w_, pad_d_;
+  int height_, width_, depth_;
+  int group_;
+  int num_output_;
+  int height_out_, width_out_, depth_out_;
+  bool bias_term_;
+  bool is_1x1_;
+
+ private:
+  // wrap im2col/col2im so we don't have to remember the (long) argument lists
+  inline void conv_vol2col_cpu(const Dtype* data, Dtype* col_buff) {
+    vol2col_cpu(data, conv_in_channels_, conv_in_height_, conv_in_width_,
+        conv_in_depth_, kernel_h_, kernel_w_, kernel_d_, pad_h_, pad_w_,
+        pad_d_, stride_h_, stride_w_, stride_d_, col_buff);
+  }
+  inline void conv_col2vol_cpu(const Dtype* col_buff, Dtype* data) {
+    col2vol_cpu(col_buff, conv_in_channels_, conv_in_height_, conv_in_width_,
+        conv_in_depth_,kernel_h_, kernel_w_, kernel_d_, pad_h_, pad_w_, 
+        pad_d_, stride_h_, stride_w_, stride_d_, data);
+  }
+// #ifndef CPU_ONLY
+//   inline void conv_im2col_gpu(const Dtype* data, Dtype* col_buff) {
+//     im2col_gpu(data, conv_in_channels_, conv_in_height_, conv_in_width_,
+//         kernel_h_, kernel_w_, pad_h_, pad_w_, stride_h_, stride_w_, col_buff);
+//   }
+//   inline void conv_col2im_gpu(const Dtype* col_buff, Dtype* data) {
+//     col2im_gpu(col_buff, conv_in_channels_, conv_in_height_, conv_in_width_,
+//         kernel_h_, kernel_w_, pad_h_, pad_w_, stride_h_, stride_w_, data);
+//   }
+// #endif
+
+  int conv_out_channels_;
+  int conv_in_channels_;
+  int conv_out_spatial_dim_;
+  int conv_in_height_;
+  int conv_in_width_;
+  int conv_in_depth_;
+  int kernel_dim_;
+  int weight_offset_;
+  int col_offset_;
+  int output_offset_;
+
+  Blob<Dtype> col_buffer_;
+  Blob<Dtype> bias_multiplier_;
+};
+
 }  // namespace caffe
 
 #endif  // CAFFE_VISION_LAYERS_HPP_
