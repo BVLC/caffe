e0c35e72f0d0ccb149ebbd2571a7d68b5286da30
Jonathan L Long <jonlong@cs.berkeley.edu>
2015-05-13 22:08:57 -0700

    remove superfluous empty destructors
    The removed definitions do nothing; these classes already have virtual
    destructors inherited from their respective base classes.

--- include/caffe/data_layers.hpp
+++ include/caffe/data_layers.hpp
@@ -29,7 +29,6 @@ template <typename Dtype>
 class BaseDataLayer : public Layer<Dtype> {
  public:
   explicit BaseDataLayer(const LayerParameter& param);
-  virtual ~BaseDataLayer() {}
   // LayerSetUp: implements common data layer setup functionality, and calls
   // DataLayerSetUp to do special data layer setup for individual layer types.
   // This method may not be overridden except by the BasePrefetchingDataLayer.
@@ -58,7 +57,6 @@ class BasePrefetchingDataLayer :
  public:
   explicit BasePrefetchingDataLayer(const LayerParameter& param)
       : BaseDataLayer<Dtype>(param) {}
-  virtual ~BasePrefetchingDataLayer() {}
   // LayerSetUp: implements common data layer setup functionality, and calls
   // DataLayerSetUp to do special data layer setup for individual layer types.
   // This method may not be overridden.
