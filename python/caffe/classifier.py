39c06a42f4c84240c72ae5af3cbc76786825de0d
hsinfu <hsinfu@cmlab.csie.ntu.edu.tw>
2015-05-16 20:22:53 +0800

    refactor oversample while cropping image

--- python/caffe/classifier.py
+++ python/caffe/classifier.py
@@ -69,23 +69,16 @@ class Classifier(caffe.Net):
         for ix, in_ in enumerate(inputs):
             input_[ix] = caffe.io.resize_image(in_, self.image_dims)
 
-        if oversample:
-            # Generate center, corner, and mirrored crops.
-            input_ = caffe.io.oversample(input_, self.crop_dims)
-        else:
-            # Take center crop.
-            center = np.array(self.image_dims) / 2.0
-            crop = np.tile(center, (1, 2))[0] + np.concatenate([
-                -self.crop_dims / 2.0,
-                self.crop_dims / 2.0
-            ])
-            input_ = input_[:, crop[0]:crop[2], crop[1]:crop[3], :]
+        # crop input image
+        input_ = caffe.io.crop_image(input_, self.crop_dims, oversample)
 
-        # Classify
+        # convert to caffe input format
         caffe_in = np.zeros(np.array(input_.shape)[[0, 3, 1, 2]],
                             dtype=np.float32)
         for ix, in_ in enumerate(input_):
             caffe_in[ix] = self.transformer.preprocess(self.inputs[0], in_)
+
+        # Classify
         out = self.forward_all(**{self.inputs[0]: caffe_in})
         predictions = out[self.outputs[0]]
 
