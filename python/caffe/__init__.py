d100766b4e09ced9e9a9df66736cf7a7b34c789f
Luke Yeager <luke.yeager@gmail.com>
2015-05-22 17:23:18 -0700

    Suppress output for python bindings
    Only allow output for errors

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -1,6 +1,19 @@
+# Suppress GLOG output for python bindings
+# unless explicitly requested in environment
+import os
+if 'GLOG_minloglevel' not in os.environ:
+    # Hide INFO and WARNING, show ERROR and FATAL
+    os.environ['GLOG_minloglevel'] = '2'
+    _unset_glog_level = True
+else:
+    _unset_glog_level = False
+
 from .pycaffe import Net, SGDSolver
 from ._caffe import set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver
 from .proto.caffe_pb2 import TRAIN, TEST
 from .classifier import Classifier
 from .detector import Detector
 from . import io
+
+if _unset_glog_level:
+    del os.environ['GLOG_minloglevel']
