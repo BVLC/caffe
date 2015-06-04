cbadb0b57627b579864fafbb6a254fab1ecf425f
Fabian Tschopp <fabian@m-tschopp.ch>
2015-06-05 00:53:08 +0200

    Added maxpool forwarding and inner product layer for OpenCL.

--- src/caffe/greentea/cl_kernels/pooling.cl
+++ src/caffe/greentea/cl_kernels/pooling.cl
@@ -6,37 +6,36 @@ __kernel void TEMPLATE(max_pool_forward,Dtype)(
     const int nthreads, __global const Dtype* bottom_data, const int num,
     const int channels, const int height, const int width,
     const int pooled_height, const int pooled_width, const int kernel_h,
-    const int kernel_w, const int ext_kernel_h, const int ext_kernel_w,
-    const int stride_h, const int stride_w, const int kstride_h,
-    const int kstride_w, const int pad_h, const int pad_w,
+    const int kernel_w, const int stride_h, const int stride_w, const int pad_h,
+    const int pad_w,
     __global Dtype* top_data,
-    __global int* mask, __global Dtype* top_mask) {
+    const int use_mask, __global int* mask, __global Dtype* top_mask) {
   for (int index = get_global_id(0); index < nthreads;
       index += get_global_size(0)) {
-    int pw = index % pooled_width;
-    int ph = (index / pooled_width) % pooled_height;
-    int c = (index / pooled_width / pooled_height) % channels;
-    int n = index / pooled_width / pooled_height / channels;
+    const int pw = index % pooled_width;
+    const int ph = (index / pooled_width) % pooled_height;
+    const int c = (index / pooled_width / pooled_height) % channels;
+    const int n = index / pooled_width / pooled_height / channels;
     int hstart = ph * stride_h - pad_h;
     int wstart = pw * stride_w - pad_w;
-    int hend = min(hstart + ext_kernel_h, height);
-    int wend = min(wstart + ext_kernel_w, width);
+    const int hend = min(hstart + kernel_h, height);
+    const int wend = min(wstart + kernel_w, width);
     hstart = max(hstart, 0);
     wstart = max(wstart, 0);
     Dtype maxval = -FLT_MAX;
     int maxidx = -1;
-    __global Dtype* bottom_data_ptr = bottom_data + bottom_data += (n * channels
-        + c) * height * width;
-    for (int h = hstart; h < hend; h += kstride_h) {
-      for (int w = wstart; w < wend; w += kstride_w) {
-        if (bottom_data_ptr[h * width + w] > maxval) {
+    __global const Dtype* bottom_slice = bottom_data
+        + (n * channels + c) * height * width;
+    for (int h = hstart; h < hend; ++h) {
+      for (int w = wstart; w < wend; ++w) {
+        if (bottom_slice[h * width + w] > maxval) {
           maxidx = h * width + w;
-          maxval = bottom_data_ptr[maxidx];
+          maxval = bottom_slice[maxidx];
         }
       }
     }
     top_data[index] = maxval;
-    if (mask) {
+    if (use_mask == 1) {
       mask[index] = maxidx;
     } else {
       top_mask[index] = maxidx;
--- src/caffe/layers/inner_product_layer.cu
+++ src/caffe/layers/inner_product_layer.cu
@@ -9,45 +9,104 @@
 
 namespace caffe {
 
-template <typename Dtype>
+template<typename Dtype>
 void InnerProductLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-    const vector<Blob<Dtype>*>& top) {
+                                           const vector<Blob<Dtype>*>& top) {
   const Dtype* bottom_data = bottom[0]->gpu_data();
   Dtype* top_data = top[0]->mutable_gpu_data();
   const Dtype* weight = this->blobs_[0]->gpu_data();
-  caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasTrans, M_, N_, K_, (Dtype)1.,
-      bottom_data, weight, (Dtype)0., top_data);
-  if (bias_term_) {
-    caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, N_, 1, (Dtype)1.,
-        bias_multiplier_.gpu_data(),
-        this->blobs_[1]->gpu_data(), (Dtype)1., top_data);
+
+  if (this->device_context_.backend() == BACKEND_CUDA) {
+#ifdef USE_CUDA
+    caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasTrans, M_, N_, K_, (Dtype) 1.,
+                          bottom_data, weight, (Dtype) 0., top_data);
+    if (bias_term_) {
+      caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, N_, 1, (Dtype) 1.,
+                            bias_multiplier_.gpu_data(),
+                            this->blobs_[1]->gpu_data(), (Dtype) 1., top_data);
+    }
+#endif // USE CUDA
+  } else {
+#ifdef USE_GREENTEA
+    greentea_gpu_gemm<Dtype>(this->device_context_.id(), CblasNoTrans,
+                             CblasTrans, M_, N_, K_, (Dtype) 1.,
+                             (cl_mem) bottom_data, 0, (cl_mem) weight, 0,
+                             (Dtype) 0., (cl_mem) top_data, 0);
+    if (bias_term_) {
+      greentea_gpu_gemm<Dtype>(this->device_context_.id(), CblasNoTrans,
+                               CblasNoTrans, M_, N_, 1, (Dtype) 1.,
+                               (cl_mem) (bias_multiplier_.gpu_data()), 0,
+                               (cl_mem) (this->blobs_[1]->gpu_data()), 0,
+                               (Dtype) 1., (cl_mem) top_data, 0);
+    }
+#endif // USE_GREENTEA
   }
 }
 
-template <typename Dtype>
-void InnerProductLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
-    const vector<bool>& propagate_down,
+template<typename Dtype>
+void InnerProductLayer<Dtype>::Backward_gpu(
+    const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down,
     const vector<Blob<Dtype>*>& bottom) {
-  if (this->param_propagate_down_[0]) {
-    const Dtype* top_diff = top[0]->gpu_diff();
-    const Dtype* bottom_data = bottom[0]->gpu_data();
-    // Gradient with respect to weight
-    caffe_gpu_gemm<Dtype>(CblasTrans, CblasNoTrans, N_, K_, M_, (Dtype)1.,
-        top_diff, bottom_data, (Dtype)1., this->blobs_[0]->mutable_gpu_diff());
-  }
-  if (bias_term_ && this->param_propagate_down_[1]) {
-    const Dtype* top_diff = top[0]->gpu_diff();
-    // Gradient with respect to bias
-    caffe_gpu_gemv<Dtype>(CblasTrans, M_, N_, (Dtype)1., top_diff,
-        bias_multiplier_.gpu_data(), (Dtype)1.,
-        this->blobs_[1]->mutable_gpu_diff());
-  }
-  if (propagate_down[0]) {
-    const Dtype* top_diff = top[0]->gpu_diff();
-    // Gradient with respect to bottom data
-    caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, K_, N_, (Dtype)1.,
-        top_diff, this->blobs_[0]->gpu_data(), (Dtype)0.,
-        bottom[0]->mutable_gpu_diff());
+
+  if (this->device_context_.backend() == BACKEND_CUDA) {
+#ifdef USE_CUDA
+    if (this->param_propagate_down_[0]) {
+      const Dtype* top_diff = top[0]->gpu_diff();
+      const Dtype* bottom_data = bottom[0]->gpu_data();
+      // Gradient with respect to weight
+      caffe_gpu_gemm<Dtype>(CblasTrans, CblasNoTrans, N_, K_, M_, (Dtype) 1.,
+                            top_diff, bottom_data, (Dtype) 1.,
+                            this->blobs_[0]->mutable_gpu_diff());
+    }
+    if (bias_term_ && this->param_propagate_down_[1]) {
+      const Dtype* top_diff = top[0]->gpu_diff();
+      // Gradient with respect to bias
+      caffe_gpu_gemv<Dtype>(CblasTrans, M_, N_, (Dtype) 1., top_diff,
+                            bias_multiplier_.gpu_data(), (Dtype) 1.,
+                            this->blobs_[1]->mutable_gpu_diff());
+    }
+    if (propagate_down[0]) {
+      const Dtype* top_diff = top[0]->gpu_diff();
+      // Gradient with respect to bottom data
+      caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, K_, N_, (Dtype) 1.,
+                            top_diff, this->blobs_[0]->gpu_data(), (Dtype) 0.,
+                            bottom[0]->mutable_gpu_diff());
+    }
+#endif // USE_CUDA
+  } else {
+#ifdef USE_GREENTEA
+    if (this->param_propagate_down_[0]) {
+      const Dtype* top_diff = top[0]->gpu_diff();
+      const Dtype* bottom_data = bottom[0]->gpu_data();
+      // Gradient with respect to weight
+      greentea_gpu_gemm<Dtype>(this->device_context_.id(), CblasTrans,
+                               CblasNoTrans, N_, K_, M_, (Dtype) 1.,
+                               (cl_mem) top_diff, 0, (cl_mem) bottom_data, 0,
+                               (Dtype) 1.,
+                               (cl_mem) (this->blobs_[0]->mutable_gpu_diff()),
+                               0);
+    }
+    if (bias_term_ && this->param_propagate_down_[1]) {
+      const Dtype* top_diff = top[0]->gpu_diff();
+      // Gradient with respect to bias
+      greentea_gpu_gemv<Dtype>(this->device_context_.id(), CblasTrans, M_, N_,
+                               (Dtype) 1., (cl_mem) top_diff, 0,
+                               (cl_mem) (bias_multiplier_.gpu_data()), 0,
+                               (Dtype) 1.,
+                               (cl_mem) (this->blobs_[1]->mutable_gpu_diff()),
+                               0);
+    }
+    if (propagate_down[0]) {
+      const Dtype* top_diff = top[0]->gpu_diff();
+      // Gradient with respect to bottom data
+      greentea_gpu_gemm<Dtype>(this->device_context_.id(), CblasNoTrans,
+                               CblasNoTrans, M_, K_, N_, (Dtype) 1.,
+                               (cl_mem) top_diff, 0,
+                               (cl_mem) (this->blobs_[0]->gpu_data()), 0,
+                               (Dtype) 0.,
+                               (cl_mem) (bottom[0]->mutable_gpu_diff()), 0);
+    }
+#endif // USE_GREENTEA
   }
 }
 
