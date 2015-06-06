98ae3b6eaed6213c23b4001ae36c44d6e8d79a2d
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-06-06 16:42:09 +0530

    Adding ls_layer.cpp LayerSetUp definition

--- src/caffe/layers/ls_layer.cpp
+++ src/caffe/layers/ls_layer.cpp
@@ -13,7 +13,18 @@ template <typename Dtype>
 void LSLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
 
-  
+  if (this->blobs_.size() > 0) {
+    LOG(INFO) << "Skipping parameter initialization";
+  } else {
+    this->blobs_.resize(1);
+    vector<int> hidden_output_shape = bottom[0]->shape();
+    vector<int> expected_output_shape = bottom[1]->shape();
+    vector<int> weight_shape;
+    weight_shape[0] = hidden_output_shape[0];
+    weight_shape[1] = expected_output_shape[1];
+    this->blobs_[0].reset(new Blob<Dtype>(weight_shape));
+    
+  } 
 }
 
 template <typename Dtype>
@@ -26,7 +37,7 @@ void LSLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
 template <typename Dtype>
 void LSLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
-  
+ 
 }
 
 
