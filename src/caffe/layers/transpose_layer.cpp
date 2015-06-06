66daaedaff35c87ba389ff8e4f44539563e3b846
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-06-06 16:54:47 +0530

    Adding Transpose Layer definitions

--- src/caffe/layers/transpose_layer.cpp
+++ src/caffe/layers/transpose_layer.cpp
@@ -13,12 +13,23 @@ template <typename Dtype>
 void TransposeLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
 
- 
+  // Check if we need to set up the weights
+  if (this->blobs_.size() > 0) {
+    LOG(INFO) << "Skipping parameter initialization";
+  } else {
+    this->blobs_.resize(2);
+    vector<int> weight_shape = this->blobs_[0].shape();
+    vector<int> transposed_weight_shape(2)
+    transposed_weight_shape[0] = weight_shape[1];
+    transposed_weight_shape[1] = weight_shape[0];
+    this->blobs_[1].reset(new Blob<Dtype>(transposed_weight_shape));
+    
+  }  
 }
 
 template <typename Dtype>
 void TransposeLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
-   
+    // Dimension verification can be added later
 }
 
 template <typename Dtype>
@@ -28,7 +39,7 @@ void TransposeLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* weight = this->blobs_[0]->cpu_data();
   Dtype* transposed_weight = this->blobs_[1]->mutable_cpu_data();
 
-  
+  // Transpose function code 
 }
 
 
