648f0a3237e18e947cf8ead6ee8504e5a4f9f1c3
Dmitriy Bolotin <bolotin.dmitriy@gmail.com>
2015-06-13 11:57:03 +0300

    Expose NesterovSolver and AdaGradSolver in pycaffe.py and __init__.py as they they are already exposed in cpp binding file.

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -1,4 +1,4 @@
-from .pycaffe import Net, SGDSolver
+from .pycaffe import Net, SGDSolver, NesterovSolver, AdaGradSolver
 from ._caffe import set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver
 from .proto.caffe_pb2 import TRAIN, TEST
 from .classifier import Classifier
