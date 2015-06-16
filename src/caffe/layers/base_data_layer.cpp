94305bb3bd165f3c8f85edc5bdc4f456a908473d
FlorisGaisser <f.gaisser@tudelft.nl>
2015-06-16 17:12:41 +0900

    Loading multiple images with ImageDataLayer
    Changed the ImageDataLayer to have more than 1 image output.
    By adding more 'top' params in the data layer in the .prototxt file while
    keeping the label last, more images can be loaded.
    This can be used to load for example siamese image pairs from a textfile as
    created by 'create_imageset' in 'examples/siamese/'.
    
    Since this required changes in the BasePrefetchingDataLayer and also other
    classes are inherited from it, for now a new class:
    BasePrefetchingMulitDataLayer has been made.
    After thorough testing, this code can be moved into the original class as it
    still supports loading only a single entity of data.

--- src/caffe/layers/base_data_layer.cpp
+++ src/caffe/layers/base_data_layer.cpp
@@ -83,7 +83,75 @@ void BasePrefetchingDataLayer<Dtype>::Forward_cpu(
 STUB_GPU_FORWARD(BasePrefetchingDataLayer, Forward);
 #endif
 
+
+
+template <typename Dtype>
+void BasePrefetchingMultiDataLayer<Dtype>::LayerSetUp(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  // the data size is of the size the top - 1 as top = data + label
+  input_data_size_ = top.size() - 1;
+  //prefetch_data_ = std::vector<Blob<Dtype> >(2);
+  for(int i = 0; i < input_data_size_; i++)
+    prefetch_data_.push_back(new Blob<Dtype>());
+  BaseDataLayer<Dtype>::LayerSetUp(bottom, top);
+  // Now, start the prefetch thread. Before calling prefetch, we make two
+  // cpu_data calls so that the prefetch thread does not accidentally make
+  // simultaneous cudaMalloc calls when the main thread is running. In some
+  // GPUs this seems to cause failures if we do not so.
+  for(int data_id = 0; data_id < input_data_size_; data_id++) {
+    this->prefetch_data_[data_id]->mutable_cpu_data();
+  }
+  if (this->output_labels_) {
+    this->prefetch_label_.mutable_cpu_data();
+  }
+  DLOG(INFO) << "Initializing prefetch";
+  this->CreatePrefetchThread();
+  DLOG(INFO) << "Prefetch initialized.";
+}
+
+template <typename Dtype>
+void BasePrefetchingMultiDataLayer<Dtype>::CreatePrefetchThread() {
+  this->data_transformer_->InitRand();
+  CHECK(StartInternalThread()) << "Thread execution failed";
+}
+
+template <typename Dtype>
+void BasePrefetchingMultiDataLayer<Dtype>::JoinPrefetchThread() {
+  CHECK(WaitForInternalThreadToExit()) << "Thread joining failed";
+}
+
+template <typename Dtype>
+void BasePrefetchingMultiDataLayer<Dtype>::Forward_cpu(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  // First, join the thread
+  JoinPrefetchThread();
+  DLOG(INFO) << "Thread joined";
+  // Reshape to loaded data.
+  for(int data_id = 0; data_id < input_data_size_; data_id++) {
+    top[data_id]->ReshapeLike(*prefetch_data_[data_id]);
+    // Copy the data
+    caffe_copy(prefetch_data_[data_id]->count(), prefetch_data_[data_id]->cpu_data(),
+             top[data_id]->mutable_cpu_data());
+    DLOG(INFO) << "Prefetch copied";
+  }
+  if (this->output_labels_) {
+    // Reshape to loaded labels.
+    top[input_data_size_]->ReshapeLike(prefetch_label_);
+    // Copy the labels.
+    caffe_copy(prefetch_label_.count(), prefetch_label_.cpu_data(),
+               top[input_data_size_]->mutable_cpu_data());
+  }
+  // Start a new prefetch thread
+  DLOG(INFO) << "CreatePrefetchThread";
+  CreatePrefetchThread();
+}
+
+#ifdef CPU_ONLY
+STUB_GPU_FORWARD(BasePrefetchingMultiDataLayer, Forward);
+#endif
+
 INSTANTIATE_CLASS(BaseDataLayer);
 INSTANTIATE_CLASS(BasePrefetchingDataLayer);
+INSTANTIATE_CLASS(BasePrefetchingMultiDataLayer);
 
 }  // namespace caffe
