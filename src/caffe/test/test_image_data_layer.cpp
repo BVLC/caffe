a29c15345bbfcc717201103b370a4d65870ddf1e
FlorisGaisser <f.gaisser@tudelft.nl>
2015-06-16 17:12:41 +0900

    Loading multiple images with ImageDataLayer
    Changed the ImageDataLayer to have more than 1 image output.
    By adding more 'top' params in the data layer in the .prototxt file while
    keeping the label last, more images can be loaded.
    This can be used to load for example siamese image pairs from a textfile as
    created by 'create_imageset' in 'examples/siamese/'.
    
    Since this required changes in the BasePrefetchingDataLayer and also other
    classes are inherited from it, for now a new class:
    BasePrefetchingMulitDataLayer has been made.
    After thorough testing, this code can be moved into the original class as it
    still supports loading only a single entity of data.

--- src/caffe/test/test_image_data_layer.cpp
+++ src/caffe/test/test_image_data_layer.cpp
@@ -23,6 +23,8 @@ class ImageDataLayerTest : public MultiDeviceTest<TypeParam> {
   ImageDataLayerTest()
       : seed_(1701),
         blob_top_data_(new Blob<Dtype>()),
+        blob_top_data_2_(new Blob<Dtype>()),
+        blob_top_data_3_(new Blob<Dtype>()),
         blob_top_label_(new Blob<Dtype>()) {}
   virtual void SetUp() {
     blob_top_vec_.push_back(blob_top_data_);
@@ -33,27 +35,73 @@ class ImageDataLayerTest : public MultiDeviceTest<TypeParam> {
     std::ofstream outfile(filename_.c_str(), std::ofstream::out);
     LOG(INFO) << "Using temporary file " << filename_;
     for (int i = 0; i < 5; ++i) {
-      outfile << EXAMPLES_SOURCE_DIR "images/cat.jpg " << i;
+      outfile << EXAMPLES_SOURCE_DIR "images/cat.jpg " << i << "\n";
     }
     outfile.close();
     // Create test input file for images of distinct sizes.
     MakeTempFilename(&filename_reshape_);
     std::ofstream reshapefile(filename_reshape_.c_str(), std::ofstream::out);
     LOG(INFO) << "Using temporary file " << filename_reshape_;
-    reshapefile << EXAMPLES_SOURCE_DIR "images/cat.jpg " << 0;
-    reshapefile << EXAMPLES_SOURCE_DIR "images/fish-bike.jpg " << 1;
+    reshapefile << EXAMPLES_SOURCE_DIR "images/cat.jpg " << 0 << "\n";
+    reshapefile << EXAMPLES_SOURCE_DIR "images/fish-bike.jpg " << 1 << "\n";
     reshapefile.close();
+
+    // Create test input files for multiple images as an input (siamese etc.).
+    MakeTempFilename(&filename_siamese_);
+    std::ofstream siamesefile(filename_siamese_.c_str(), std::ofstream::out);
+    LOG(INFO) << "Using temporary file " << filename_siamese_;
+    siamesefile << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << 1 << "\n";
+    siamesefile << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_2.jpg "
+                << 0 << "\n";
+    siamesefile << EXAMPLES_SOURCE_DIR "images/cat_2.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_2.jpg "
+                << 1 << "\n";
+    siamesefile.close();
+
+    MakeTempFilename(&filename_tripple_);
+    std::ofstream tripplefile(filename_tripple_.c_str(), std::ofstream::out);
+    LOG(INFO) << "Using temporary file " << filename_tripple_;
+    tripplefile << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_2.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_3.jpg "
+                << 0 << "\n";
+    tripplefile << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_2.jpg "
+                << 1 << "\n";
+    tripplefile << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_2.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << 2 << "\n";
+    tripplefile << EXAMPLES_SOURCE_DIR "images/cat_2.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << 3 << "\n";
+    tripplefile << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << EXAMPLES_SOURCE_DIR "images/cat_1.jpg "
+                << 4 << "\n";
+    tripplefile.close();
   }
 
   virtual ~ImageDataLayerTest() {
     delete blob_top_data_;
+    delete blob_top_data_2_;
+    delete blob_top_data_3_;
     delete blob_top_label_;
   }
 
   int seed_;
   string filename_;
   string filename_reshape_;
+  string filename_siamese_;
+  string filename_tripple_;
   Blob<Dtype>* const blob_top_data_;
+  Blob<Dtype>* const blob_top_data_2_;
+  Blob<Dtype>* const blob_top_data_3_;
   Blob<Dtype>* const blob_top_label_;
   vector<Blob<Dtype>*> blob_bottom_vec_;
   vector<Blob<Dtype>*> blob_top_vec_;
@@ -65,6 +113,8 @@ TYPED_TEST(ImageDataLayerTest, TestRead) {
   typedef typename TypeParam::Dtype Dtype;
   LayerParameter param;
   ImageDataParameter* image_data_param = param.mutable_image_data_param();
+  param.add_top("img");
+  param.add_top("label");
   image_data_param->set_batch_size(5);
   image_data_param->set_source(this->filename_.c_str());
   image_data_param->set_shuffle(false);
@@ -91,6 +141,8 @@ TYPED_TEST(ImageDataLayerTest, TestResize) {
   typedef typename TypeParam::Dtype Dtype;
   LayerParameter param;
   ImageDataParameter* image_data_param = param.mutable_image_data_param();
+  param.add_top("img");
+  param.add_top("label");
   image_data_param->set_batch_size(5);
   image_data_param->set_source(this->filename_.c_str());
   image_data_param->set_new_height(256);
@@ -119,6 +171,8 @@ TYPED_TEST(ImageDataLayerTest, TestReshape) {
   typedef typename TypeParam::Dtype Dtype;
   LayerParameter param;
   ImageDataParameter* image_data_param = param.mutable_image_data_param();
+  param.add_top("img");
+  param.add_top("label");
   image_data_param->set_batch_size(1);
   image_data_param->set_source(this->filename_reshape_.c_str());
   image_data_param->set_shuffle(false);
@@ -146,6 +200,8 @@ TYPED_TEST(ImageDataLayerTest, TestShuffle) {
   typedef typename TypeParam::Dtype Dtype;
   LayerParameter param;
   ImageDataParameter* image_data_param = param.mutable_image_data_param();
+  param.add_top("img");
+  param.add_top("label");
   image_data_param->set_batch_size(5);
   image_data_param->set_source(this->filename_.c_str());
   image_data_param->set_shuffle(true);
@@ -176,4 +232,122 @@ TYPED_TEST(ImageDataLayerTest, TestShuffle) {
   }
 }
 
+TYPED_TEST(ImageDataLayerTest, TestSiamese) {
+  typedef typename TypeParam::Dtype Dtype;
+  this->blob_top_vec_.clear();
+  this->blob_top_vec_.push_back(this->blob_top_data_);
+  this->blob_top_vec_.push_back(this->blob_top_data_2_);
+  this->blob_top_vec_.push_back(this->blob_top_label_);
+  LayerParameter param;
+  ImageDataParameter* image_data_param = param.mutable_image_data_param();
+  param.add_top("img1");
+  param.add_top("img2");
+  param.add_top("label");
+  image_data_param->set_batch_size(3);
+  image_data_param->set_source(this->filename_siamese_.c_str());
+  image_data_param->set_shuffle(false);
+  ImageDataLayer<Dtype> layer(param);
+  layer.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+  EXPECT_EQ(this->blob_top_vec_[0]->num(), 3);
+  EXPECT_EQ(this->blob_top_vec_[0]->channels(), 3);
+  EXPECT_EQ(this->blob_top_vec_[0]->height(), 360);
+  EXPECT_EQ(this->blob_top_vec_[0]->width(), 480);
+  EXPECT_EQ(this->blob_top_vec_[1]->num(), 3);
+  EXPECT_EQ(this->blob_top_vec_[1]->channels(), 3);
+  EXPECT_EQ(this->blob_top_vec_[1]->height(), 360);
+  EXPECT_EQ(this->blob_top_vec_[1]->width(), 480);
+  EXPECT_EQ(this->blob_top_label_->num(), 3);
+  EXPECT_EQ(this->blob_top_label_->channels(), 1);
+  EXPECT_EQ(this->blob_top_label_->height(), 1);
+  EXPECT_EQ(this->blob_top_label_->width(), 1);
+  // Go through the data twice
+  for (int iter = 0; iter < 2; ++iter) {
+    layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+    EXPECT_EQ(1, this->blob_top_label_->cpu_data()[0]);
+    EXPECT_EQ(0, this->blob_top_label_->cpu_data()[1]);
+    EXPECT_EQ(1, this->blob_top_label_->cpu_data()[2]);
+    int n = 0;
+    for(int c = 0; c < 3; c++) {
+      for(int h = 0; h < 360; h++) {
+        for(int w = 0; w < 480; w++) {
+          EXPECT_EQ(this->blob_top_data_->data_at(n, c, h, w),
+                    this->blob_top_data_2_->data_at(n, c, h, w));
+        }
+      }
+    }
+    n = 2;
+    for(int c = 0; c < 3; c++) {
+      for(int h = 0; h < 360; h++) {
+        for(int w = 0; w < 480; w++) {
+          EXPECT_EQ(this->blob_top_data_->data_at(n, c, h, w),
+                    this->blob_top_data_2_->data_at(n, c, h, w));
+        }
+      }
+    }
+  }
+}
+
+
+TYPED_TEST(ImageDataLayerTest, TestTripple) {
+  typedef typename TypeParam::Dtype Dtype;
+  this->blob_top_vec_.clear();
+  this->blob_top_vec_.push_back(this->blob_top_data_);
+  this->blob_top_vec_.push_back(this->blob_top_data_2_);
+  this->blob_top_vec_.push_back(this->blob_top_data_3_);
+  this->blob_top_vec_.push_back(this->blob_top_label_);
+  LayerParameter param;
+  ImageDataParameter* image_data_param = param.mutable_image_data_param();
+  param.add_top("img1");
+  param.add_top("img2");
+  param.add_top("img3");
+  param.add_top("label");
+  image_data_param->set_batch_size(5);
+  image_data_param->set_source(this->filename_tripple_.c_str());
+  image_data_param->set_shuffle(false);
+  ImageDataLayer<Dtype> layer(param);
+  layer.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+  EXPECT_EQ(this->blob_top_vec_[0]->num(), 5);
+  EXPECT_EQ(this->blob_top_vec_[0]->channels(), 3);
+  EXPECT_EQ(this->blob_top_vec_[0]->height(), 360);
+  EXPECT_EQ(this->blob_top_vec_[0]->width(), 480);
+  EXPECT_EQ(this->blob_top_vec_[1]->num(), 5);
+  EXPECT_EQ(this->blob_top_vec_[1]->channels(), 3);
+  EXPECT_EQ(this->blob_top_vec_[1]->height(), 360);
+  EXPECT_EQ(this->blob_top_vec_[1]->width(), 480);
+  EXPECT_EQ(this->blob_top_vec_[2]->num(), 5);
+  EXPECT_EQ(this->blob_top_vec_[2]->channels(), 3);
+  EXPECT_EQ(this->blob_top_vec_[2]->height(), 360);
+  EXPECT_EQ(this->blob_top_vec_[2]->width(), 480);
+  EXPECT_EQ(this->blob_top_label_->num(), 5);
+  EXPECT_EQ(this->blob_top_label_->channels(), 1);
+  EXPECT_EQ(this->blob_top_label_->height(), 1);
+  EXPECT_EQ(this->blob_top_label_->width(), 1);
+  // Go through the data twice
+  for (int iter = 0; iter < 2; ++iter) {
+    layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+    for (int i = 0; i < 5; ++i) {
+      EXPECT_EQ(i, this->blob_top_label_->cpu_data()[i]);
+    }
+    for(int n = 0; n < 5; n++) {
+      for(int c = 0; c < 3; c++) {
+        for(int h = 0; h < 360; h++) {
+          for(int w = 0; w < 480; w++) {
+            if(n == 1 || n == 4) {
+              EXPECT_EQ(this->blob_top_data_->data_at(n, c, h, w),
+                        this->blob_top_data_2_->data_at(n, c, h, w));
+            }
+            if(n == 2 || n == 4) {
+              EXPECT_EQ(this->blob_top_data_->data_at(n, c, h, w),
+                        this->blob_top_data_3_->data_at(n, c, h, w));
+            }
+            if(n == 3 || n == 4) {
+              EXPECT_EQ(this->blob_top_data_3_->data_at(n, c, h, w),
+                        this->blob_top_data_2_->data_at(n, c, h, w));
+            }
+          }
+        }
+      }
+    }
+  }
+}
 }  // namespace caffe
