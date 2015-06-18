1309f917d942b2e79811b3b5dccf125c6ae7e221
Jim <jyegerlehner@yahoo.com>
2015-06-18 07:23:46 -0500

    fix caffe time command.

--- tools/caffe.cpp
+++ tools/caffe.cpp
@@ -253,6 +253,7 @@ int time() {
     for (int i = 0; i < layers.size(); ++i) {
       timer.Start();
       layers[i]->Forward(bottom_vecs[i], top_vecs[i]);
+      Caffe::Synchronize(FLAGS_gpu);
       forward_time_per_layer[i] += timer.MicroSeconds();
     }
     forward_time += forward_timer.MicroSeconds();
@@ -261,6 +262,7 @@ int time() {
       timer.Start();
       layers[i]->Backward(top_vecs[i], bottom_need_backward[i],
                           bottom_vecs[i]);
+      Caffe::Synchronize(FLAGS_gpu);
       backward_time_per_layer[i] += timer.MicroSeconds();
     }
     backward_time += backward_timer.MicroSeconds();
