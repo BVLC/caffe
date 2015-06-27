f3a933a620b8b089a7fe29ba76ec27f5408ff68d
Tea <tea.desouza@gmail.com>
2015-06-27 11:44:56 +0800

    Separate IO dependencies
    OpenCV, LMDB, LevelDB and Snappy are made optional via switches
    (USE_OPENCV, USE_LMDB, USE_LEVELDB) available for Make and CMake
    builds. Since Snappy is a LevelDB dependency, its use is determined by
    USE_LEVELDB. HDF5 is left bundled because it is used for serializing
    weights and solverstates.

--- examples/cpp_classification/classification.cpp
+++ examples/cpp_classification/classification.cpp
@@ -1,7 +1,9 @@
 #include <caffe/caffe.hpp>
+#ifdef USE_OPENCV
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#endif  // USE_OPENCV
 #include <algorithm>
 #include <iosfwd>
 #include <memory>
@@ -9,6 +11,7 @@
 #include <utility>
 #include <vector>
 
+#ifdef USE_OPENCV
 using namespace caffe;  // NOLINT(build/namespaces)
 using std::string;
 
@@ -255,3 +258,8 @@ int main(int argc, char** argv) {
               << p.first << "\"" << std::endl;
   }
 }
+#else
+int main(int argc, char** argv) {
+  LOG(FATAL) << "This example requires OpenCV; compile with USE_OPENCV.";
+}
+#endif  // USE_OPENCV
