69d36ef34939a9a6b4de9146bed291faf08e1e4a
Chris Tensmeyer <ctensmeyer@ancestry.com>
2015-07-16 15:46:04 -0600

    Clip gradients on a per layer basis

--- include/caffe/net.hpp
+++ include/caffe/net.hpp
@@ -153,6 +153,9 @@ class Net {
     return param_names_index_;
   }
   inline const vector<int>& param_owners() const { return param_owners_; }
+  inline const vector<string>& param_display_names() const { return param_display_names_; }
+  inline const vector<pair<int, int> > param_layer_indices() const { return param_layer_indices_; }
+		
   /// @brief Input and output blob numbers
   inline int num_inputs() const { return net_input_blobs_.size(); }
   inline int num_outputs() const { return net_output_blobs_.size(); }
--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -236,6 +236,9 @@ void Solver<Dtype>::Step(int iters) {
               << result_vec[k] << loss_msg_stream.str();
         }
       }
+#if DEBUG
+        DLOG(INFO) << "Testing 1 2 3";
+#endif
     }
     ApplyUpdate();
 
@@ -464,23 +467,44 @@ void SGDSolver<Dtype>::ClipGradients() {
   const Dtype clip_gradients = this->param_.clip_gradients();
   if (clip_gradients < 0) { return; }
   const vector<shared_ptr<Blob<Dtype> > >& net_params = this->net_->params();
-  Dtype sumsq_diff = 0;
-  for (int i = 0; i < net_params.size(); ++i) {
-    if (this->net_->param_owners()[i] < 0) {
-      sumsq_diff += net_params[i]->sumsq_diff();
-    }
-  }
-  const Dtype l2norm_diff = std::sqrt(sumsq_diff);
-  if (l2norm_diff > clip_gradients) {
-    Dtype scale_factor = clip_gradients / l2norm_diff;
-    LOG(INFO) << "Gradient clipping: scaling down gradients (L2 norm "
-        << l2norm_diff << " > " << clip_gradients << ") "
-        << "by scale factor " << scale_factor;
+  if (this->param_.clip_gradients_global()) {
+    Dtype sumsq_diff = 0;
     for (int i = 0; i < net_params.size(); ++i) {
       if (this->net_->param_owners()[i] < 0) {
-        net_params[i]->scale_diff(scale_factor);
+        sumsq_diff += net_params[i]->sumsq_diff();
       }
     }
+    const Dtype l2norm_diff = std::sqrt(sumsq_diff);
+    if (l2norm_diff > clip_gradients) {
+      Dtype scale_factor = clip_gradients / l2norm_diff;
+      LOG(INFO) << "Gradient clipping: scaling down all gradients (L2 norm "
+          << l2norm_diff << " > " << clip_gradients << ") "
+          << "by scale factor " << scale_factor;
+      for (int i = 0; i < net_params.size(); ++i) {
+        if (this->net_->param_owners()[i] < 0) {
+          net_params[i]->scale_diff(scale_factor);
+        }
+      }
+    }
+  } else {
+    for (int i = 0; i < net_params.size(); ++i) {
+      if (this->net_->param_owners()[i] < 0) {
+        Dtype sumsq_diff = net_params[i]->sumsq_diff();
+        const Dtype l2norm_diff = std::sqrt(sumsq_diff);
+        const Dtype scale_factor = clip_gradients / l2norm_diff;
+
+	    const string& layer_name = this->net_->layer_names()[this->net_->param_layer_indices()[i].first];
+        const string& param_display_name = this->net_->param_display_names()[i];
+        if (l2norm_diff > clip_gradients) {
+          LOG(INFO) << "Gradient clipping " << layer_name << ", param " << param_display_name
+		      << " with gradient ("
+              << l2norm_diff << " > " << clip_gradients << ") "
+              << "by scale factor " << scale_factor;
+		  net_params[i]->scale_diff(scale_factor);
+		}
+      }
+    }
+
   }
 }
 
