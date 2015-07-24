02bae98536e5dea47511c8ced8a7b642776f0033
Wangyida <wangyida123@outlook.com>
2015-07-24 11:36:02 +0800

    ready for a review on triplet training using the 3D data, net work definition is the same as described in paper

--- examples/triplet/create_3d_triplet.sh
+++ examples/triplet/create_3d_triplet.sh
@@ -6,16 +6,16 @@ DATA=./data/linemod
 
 echo "Creating leveldb..."
 
-rm -rf ./examples/triplet/mnist_3d_train_leveldb
-rm -rf ./examples/triplet/mnist_3d_test_leveldb
+rm -rf ./examples/triplet/3d_triplet_train_leveldb
+rm -rf ./examples/triplet/3d_triplet_test_leveldb
 
 $EXAMPLES/convert_3d_triplet_data.bin \
-    $DATA/train-images-idx3-ubyte \
-    $DATA/train-labels-idx1-ubyte \
+    $DATA/binary_image_train \
+    $DATA/binary_label_train \
     ./examples/triplet/3d_triplet_train_leveldb
 $EXAMPLES/convert_3d_triplet_data.bin \
-    $DATA/test-images-idx3-ubyte \
-    $DATA/test-labels-idx1-ubyte \
+    $DATA/binary_image_test \
+    $DATA/binary_label_test \
     ./examples/triplet/3d_triplet_test_leveldb
 
 echo "Done."
