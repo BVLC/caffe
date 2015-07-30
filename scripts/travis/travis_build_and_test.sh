a131b139b87f00d1f8281788909c56718f70fe6a
lunochod <connect2robert@gmail.com>
2015-07-30 00:45:20 -0700

    add verbose to travis build

--- scripts/travis/travis_build_and_test.sh
+++ scripts/travis/travis_build_and_test.sh
@@ -4,11 +4,23 @@
 set -e
 MAKE="make --jobs=$NUM_THREADS --keep-going"
 
-if [ ! -z "$WITH_CMAKE" ]; then
-  mkdir build
-  cd build
-  cmake -DBUILD_python=ON -DCMAKE_BUILD_TYPE=Release -DCPU_ONLY=ON ..
+if $WITH_CMAKE; then
+	echo "Build using CMAKE";  
+	mkdir build
+	cd build
+	CPU_ONLY=" -DCPU_ONLY=ON"
+	if ! $WITH_CUDA; then
+		echo "Build without CUDA";
+    	CPU_ONLY=" -DCPU_ONLY=OFF"
+	fi
+	PYTHON_ARGS=""
+	if [ "$PYTHON_VERSION" = "3" ]; then
+		PYTHON_ARGS="$PYTHON_ARGS -Dpython_version=3 -DBOOST_LIBRARYDIR=$CONDA_DIR/lib/"
+	fi
+	echo "execute 'cmake -DBUILD_python=ON -DCMAKE_BUILD_TYPE=Release $CPU_ONLY $PYTHON_ARGS -DCMAKE_INCLUDE_PATH="$CONDA_DIR/include/" -DCMAKE_LIBRARY_PATH="$CONDA_DIR/lib/" ..'";
+	cmake -DBUILD_python=ON -DCMAKE_BUILD_TYPE=Release $CPU_ONLY $PYTHON_ARGS -DCMAKE_INCLUDE_PATH="$CONDA_DIR/include/" -DCMAKE_LIBRARY_PATH="$CONDA_DIR/lib/" ..
   $MAKE
+  $MAKE pytest
   if ! $WITH_CUDA; then
     $MAKE runtest
     $MAKE lint
@@ -16,8 +28,10 @@ if [ ! -z "$WITH_CMAKE" ]; then
   $MAKE clean
   cd -
 else
+	echo "Build with Makefile";
   if ! $WITH_CUDA; then
-    export CPU_ONLY=1
+		echo "Build without CUDA";
+        export CPU_ONLY=1
   fi
   $MAKE all test pycaffe warn lint || true
   if ! $WITH_CUDA; then
