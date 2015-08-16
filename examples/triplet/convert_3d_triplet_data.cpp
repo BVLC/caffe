0f4cead46061714e17834b5cb94917769a9cae69
Wangyida <wangyida123@outlook.com>
2015-08-16 21:43:45 +0800

    add RGB data training as an option in triplet training

--- examples/triplet/convert_3d_triplet_data.cpp
+++ examples/triplet/convert_3d_triplet_data.cpp
@@ -17,17 +17,30 @@ uint32_t swap_endian(uint32_t val) {
 
 void read_image(std::ifstream* image_file, std::ifstream* label_file,
         uint32_t index, uint32_t rows, uint32_t cols,
-        char* pixels, char* label_temp, signed char* label) {
-  image_file->seekg(index * rows * cols + 16);
-  image_file->read(pixels, rows * cols);
-  label_file->seekg(index * 4 + 8);
-  label_file->read(label_temp, 4);
-  for (int i = 0; i < 4; i++)
-    *(label+i) = (signed char)*(label_temp+i);
+        char* pixels, char* label_temp, signed char* label, int rgb_use) {
+  if (rgb_use == 0)
+  {
+    image_file->seekg(index * rows * cols + 16);
+    image_file->read(pixels, rows * cols);
+    label_file->seekg(index * 4 + 8);
+    label_file->read(label_temp, 4);
+    for (int i = 0; i < 4; i++)
+      *(label+i) = (signed char)*(label_temp+i);
+  }
+  else
+  {
+    image_file->seekg(3 * index * rows * cols + 16);
+    image_file->read(pixels, 3 * rows * cols);
+    label_file->seekg(index * 4 + 8);
+    label_file->read(label_temp, 4);
+    for (int i = 0; i < 4; i++)
+      *(label+i) = (signed char)*(label_temp+i);
+  }
 }
 
 void convert_dataset(const char* image_filename, const char* label_filename,
-        const char* db_filename, const char* class_number) {
+        const char* db_filename, const char* class_number, const char* rgb_use) {
+  int rgb_use1 = atoi(rgb_use);
   int class_num = atoi(class_number);
   // Open files
   std::ifstream image_file(image_filename, std::ios::in | std::ios::binary);
@@ -73,11 +86,16 @@ void convert_dataset(const char* image_filename, const char* label_filename,
   signed char* label_k = new signed char[4];
   signed char* label_l = new signed char[4];  // label for pair wise
   signed char* label_m = new signed char[4];
-  char* pixels1 = new char[rows * cols];
-  char* pixels2 = new char[rows * cols];
-  char* pixels3 = new char[rows * cols];
-  char* pixels4 = new char[rows * cols];
-  char* pixels5 = new char[rows * cols];
+  int db_size;
+  if (rgb_use1 == 0)
+    db_size = rows * cols;
+  else
+    db_size = 3 * rows * cols;
+  char* pixels1 = new char[db_size];
+  char* pixels2 = new char[db_size];
+  char* pixels3 = new char[db_size];
+  char* pixels4 = new char[db_size];
+  char* pixels5 = new char[db_size];
   const int kMaxKeyLength = 10;
   char key[kMaxKeyLength];
   std::string value;
@@ -100,15 +118,15 @@ void convert_dataset(const char* image_filename, const char* label_filename,
       int l = caffe::caffe_rng_rand() % num_items;  // pick pair wise groups
       int m = caffe::caffe_rng_rand() % num_items;
       read_image(&image_file, &label_file, i, rows, cols,  // read triplet
-        pixels1, label_temp, label_i);
+        pixels1, label_temp, label_i, rgb_use1);
       read_image(&image_file, &label_file, j, rows, cols,
-        pixels2, label_temp, label_j);
+        pixels2, label_temp, label_j, rgb_use1);
       read_image(&image_file, &label_file, k, rows, cols,
-        pixels3, label_temp, label_k);
+        pixels3, label_temp, label_k, rgb_use1);
       read_image(&image_file, &label_file, l, rows, cols,  // read pair wise
-        pixels4, label_temp, label_l);
+        pixels4, label_temp, label_l, rgb_use1);
       read_image(&image_file, &label_file, m, rows, cols,
-        pixels5, label_temp, label_m);
+        pixels5, label_temp, label_m, rgb_use1);
 
       bool pair_pass = false;
       bool triplet1_pass = false;
@@ -188,15 +206,15 @@ void convert_dataset(const char* image_filename, const char* label_filename,
 }
 
 int main(int argc, char** argv) {
-  if (argc != 5) {
+  if (argc != 6) {
     printf("This script converts the dataset to the leveldb format used\n"
            "by caffe to train a triplet network.\n"
            "Usage:\n"
            "    convert_3d_data input_image_file input_label_file "
-           "output_db_file class_number\n");
+           "output_db_file class_number rgb_use \n");
   } else {
     google::InitGoogleLogging(argv[0]);
-    convert_dataset(argv[1], argv[2], argv[3], argv[4]);
+    convert_dataset(argv[1], argv[2], argv[3], argv[4], argv[5]);
   }
   return 0;
 }
