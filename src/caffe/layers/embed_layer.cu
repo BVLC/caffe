eb2bf8daa987c2ef5c6a51739a13a342119d5be5
Fabian Tschopp <fabian@m-tschopp.ch>
2015-08-26 03:08:57 +0200

    Embed layer OpenCL preparations. Will still fail during runtest. OpenCL kernels missing.

--- src/caffe/layers/embed_layer.cu
+++ src/caffe/layers/embed_layer.cu
@@ -5,11 +5,20 @@
 #include "caffe/common_layers.hpp"
 #include "caffe/filler.hpp"
 #include "caffe/layer.hpp"
+#ifdef USE_CUDA
 #include "caffe/util/gpu_util.cuh"
+#endif  // USE_CUDA
 #include "caffe/util/math_functions.hpp"
+
+#ifdef USE_GREENTEA
+#include "caffe/greentea/greentea.hpp"
+#include "caffe/greentea/greentea_math_functions.hpp"
+#endif
+
 
 namespace caffe {
 
+#ifdef USE_CUDA
 template <typename Dtype>
 __global__ void EmbedForward(const int nthreads, const Dtype* bottom_data,
     const Dtype* weight, const int M, const int N, const int K,
@@ -40,6 +49,7 @@ __global__ void EmbedBackward(const int nthreads, const Dtype* bottom_data,
     caffe_gpu_atomic_add(top_diff[top_index], weight_diff + weight_index);
   }
 }
+#endif  // USE_CUDA
 
 template <typename Dtype>
 void EmbedLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
@@ -48,14 +58,33 @@ void EmbedLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   Dtype* top_data = top[0]->mutable_gpu_data();
   const Dtype* weight = this->blobs_[0]->gpu_data();
   const int count = top[0]->count();
-  EmbedForward<Dtype>  // NOLINT_NEXT_LINE(whitespace/operators)
-      <<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
-      count, bottom_data, weight, M_, N_, K_, top_data);
-  if (bias_term_) {
-    caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, N_, 1, Dtype(1),
-        bias_multiplier_.gpu_data(),
-        this->blobs_[1]->gpu_data(), Dtype(1), top_data);
-  }
+
+  if(this->device_context()->backend() == BACKEND_CUDA) {
+#ifdef USE_CUDA
+
+    EmbedForward<Dtype>  // NOLINT_NEXT_LINE(whitespace/operators)
+    CUDA_KERNEL(CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS)(
+        count, bottom_data, weight, M_, N_, K_, top_data);
+    if (bias_term_) {
+      caffe_gpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, M_, N_, 1, Dtype(1),
+                            bias_multiplier_.gpu_data(),
+                            this->blobs_[1]->gpu_data(), Dtype(1), top_data);
+    }
+#endif  // USE_CUDA
+    } else {
+#ifdef USE_GREENTEA
+      // TODO: Implement OpenCL kernel
+
+    if (bias_term_) {
+      greentea_gpu_gemm<Dtype>(this->device_context()->id(), CblasNoTrans,
+                               CblasNoTrans, M_, N_, 1, Dtype(1),
+                               (cl_mem) (bias_multiplier_.gpu_data()), 0,
+                               (cl_mem) (this->blobs_[1]->gpu_data()), 0,
+                               Dtype(1), (cl_mem) top_data, 0);
+    }
+
+#endif  // USE_GREENTEA
+    }
 }
 
 template <typename Dtype>
@@ -68,18 +97,43 @@ void EmbedLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
     const Dtype* top_diff = top[0]->gpu_diff();
     const Dtype* bottom_data = bottom[0]->gpu_data();
     Dtype* weight_diff = this->blobs_[0]->mutable_gpu_diff();
+
+    if(this->device_context()->backend() == BACKEND_CUDA) {
+#ifdef USE_CUDA
     EmbedBackward<Dtype>  // NOLINT_NEXT_LINE(whitespace/operators)
-        <<<CAFFE_GET_BLOCKS(top_count), CAFFE_CUDA_NUM_THREADS>>>(
+        CUDA_KERNEL(CAFFE_GET_BLOCKS(top_count), CAFFE_CUDA_NUM_THREADS)(
         top_count, bottom_data, top_diff, M_, N_, K_, weight_diff);
+#endif  // USE_CUDA
+    } else {
+#ifdef USE_GREENTEA
+      // TODO: Implement OpenCL kernel
+#endif  // USE_GREENTEA
+    }
+
   }
   if (bias_term_ && this->param_propagate_down_[1]) {
     const Dtype* top_diff = top[0]->gpu_diff();
     Dtype* bias_diff = this->blobs_[1]->mutable_gpu_diff();
+
+    if(this->device_context()->backend() == BACKEND_CUDA) {
+#ifdef USE_CUDA
     caffe_gpu_gemv<Dtype>(CblasTrans, M_, N_, Dtype(1), top_diff,
         bias_multiplier_.gpu_data(), Dtype(1), bias_diff);
+#endif  // USE_CUDA
+    } else {
+#ifdef USE_GREENTEA
+      greentea_gpu_gemv<Dtype>(this->device_context()->id(), CblasTrans, M_, N_,
+                               Dtype(1), (cl_mem) top_diff, 0,
+                               (cl_mem) (bias_multiplier_.gpu_data()), 0,
+                               Dtype(1), (cl_mem) bias_diff, 0);
+#endif  // USE_GREENTEA
+    }
+
+
   }
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(EmbedLayer);
 
 }  // namespace caffe
+
