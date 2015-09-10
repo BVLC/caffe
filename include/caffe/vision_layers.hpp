b4259a1c77f2a0357ca5409924b10dcb49955f9d
dujianyi <dujianyi@gmail.com>
2015-09-10 11:27:25 -0400

    Specify ceil/floor mode for MaxPooling

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -416,7 +416,7 @@ class PoolingLayer : public Layer<Dtype> {
   int height_, width_;
   int pooled_height_, pooled_width_;
   bool global_pooling_;
-  bool ceil_mode;
+  bool ceil_mode_;
   Blob<Dtype> rand_idx_;
   Blob<int> max_idx_;
 };
