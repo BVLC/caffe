44f09d7ea6ca29753a40e25e5a4bb64fa538c144
fabian <naibaf710@hotmail.com>
2015-10-31 20:52:55 +0100

    Removed duplicate function.

--- python/caffe/_caffe.cpp
+++ python/caffe/_caffe.cpp
@@ -293,7 +293,6 @@ BOOST_PYTHON_MODULE(_caffe) {
     .def("solve",
       static_cast<void (Solver<Dtype>::*)(const char*)>(
           &Solver<Dtype>::Solve), SolveOverloads())
-    .def("step", &Solver<Dtype>::Step)
     .def("restore", &Solver<Dtype>::Restore)
     .def("snapshot", &Solver<Dtype>::Snapshot);
 
