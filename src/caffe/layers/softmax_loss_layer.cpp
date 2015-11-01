d788f3aca411b9f37d3069827ba90cf64b12dcf7
Xindi Shang <xindi.shang@comp.nus.edu.sg>
2015-11-01 17:09:20 +0800

    added support for multi-label softmax. The softmax loss simply takes average loss value over all given labels.

--- src/caffe/layers/softmax_loss_layer.cpp
+++ src/caffe/layers/softmax_loss_layer.cpp
@@ -37,11 +37,13 @@ void SoftmaxWithLossLayer<Dtype>::Reshape(
       bottom[0]->CanonicalAxisIndex(this->layer_param_.softmax_param().axis());
   outer_num_ = bottom[0]->count(0, softmax_axis_);
   inner_num_ = bottom[0]->count(softmax_axis_ + 1);
-  CHECK_EQ(outer_num_ * inner_num_, bottom[1]->count())
-      << "Number of labels must match number of predictions; "
-      << "e.g., if softmax axis == 1 and prediction shape is (N, C, H, W), "
-      << "label count (number of labels) must be N*H*W, "
-      << "with integer values in {0, 1, ..., C-1}.";
+  CHECK_EQ(outer_num_, bottom[1]->count(0, softmax_axis_));
+  CHECK_EQ(inner_num_, bottom[1]->count(softmax_axis_ + 1));
+//  CHECK_EQ(outer_num_ * inner_num_, bottom[1]->count())
+//      << "Number of labels must match number of predictions; "
+//      << "e.g., if softmax axis == 1 and prediction shape is (N, C, H, W), "
+//      << "label count (number of labels) must be N*H*W, "
+//      << "with integer values in {0, 1, ..., C-1}.";
   if (top.size() >= 2) {
     // softmax output
     top[1]->ReshapeLike(*bottom[0]);
@@ -55,20 +57,23 @@ void SoftmaxWithLossLayer<Dtype>::Forward_cpu(
   softmax_layer_->Forward(softmax_bottom_vec_, softmax_top_vec_);
   const Dtype* prob_data = prob_.cpu_data();
   const Dtype* label = bottom[1]->cpu_data();
+  int label_num = bottom[1]->shape(softmax_axis_);
   int dim = prob_.count() / outer_num_;
   int count = 0;
   Dtype loss = 0;
   for (int i = 0; i < outer_num_; ++i) {
-    for (int j = 0; j < inner_num_; j++) {
-      const int label_value = static_cast<int>(label[i * inner_num_ + j]);
-      if (has_ignore_label_ && label_value == ignore_label_) {
-        continue;
+    for (int k = 0; k < label_num; ++k) {
+      for (int j = 0; j < inner_num_; j++) {
+        const int label_value = static_cast<int>(label[(i * label_num + k) * inner_num_ + j]);
+        if (has_ignore_label_ && label_value == ignore_label_) {
+          continue;
+        }
+        DCHECK_GE(label_value, 0);
+        DCHECK_LT(label_value, prob_.shape(softmax_axis_));
+        loss -= log(std::max(prob_data[i * dim + label_value * inner_num_ + j],
+                             Dtype(FLT_MIN)));
+        ++count;
       }
-      DCHECK_GE(label_value, 0);
-      DCHECK_LT(label_value, prob_.shape(softmax_axis_));
-      loss -= log(std::max(prob_data[i * dim + label_value * inner_num_ + j],
-                           Dtype(FLT_MIN)));
-      ++count;
     }
   }
   if (normalize_) {
@@ -93,18 +98,30 @@ void SoftmaxWithLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const Dtype* prob_data = prob_.cpu_data();
     caffe_copy(prob_.count(), prob_data, bottom_diff);
     const Dtype* label = bottom[1]->cpu_data();
+    int label_num = bottom[1]->shape(softmax_axis_);
     int dim = prob_.count() / outer_num_;
     int count = 0;
     for (int i = 0; i < outer_num_; ++i) {
       for (int j = 0; j < inner_num_; ++j) {
-        const int label_value = static_cast<int>(label[i * inner_num_ + j]);
-        if (has_ignore_label_ && label_value == ignore_label_) {
-          for (int c = 0; c < bottom[0]->shape(softmax_axis_); ++c) {
-            bottom_diff[i * dim + c * inner_num_ + j] = 0;
+        int label_cnt = 0;
+        for (int k = 0; k < label_num; ++k) {
+          const int label_value = static_cast<int>(label[(i * label_num + k) * inner_num_ + j]);
+          if (has_ignore_label_ && label_value == ignore_label_) {
+            continue;
           }
-        } else {
-          bottom_diff[i * dim + label_value * inner_num_ + j] -= 1;
-          ++count;
+          ++label_cnt;
+        }
+        for (int c = 0; c < bottom[0]->shape(softmax_axis_); ++c) {
+          bottom_diff[i * dim + c * inner_num_ + j] *= label_cnt;
+        }
+        for (int k = 0; k < label_num; ++k) {
+          const int label_value = static_cast<int>(label[(i * label_num + k) * inner_num_ + j]);
+          if (has_ignore_label_ && label_value == ignore_label_) {
+            continue;
+          } else {
+            bottom_diff[i * dim + label_value * inner_num_ + j] -= 1;
+            ++count;
+          }
         }
       }
     }
