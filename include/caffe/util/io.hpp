2415ffc13ee1d4bbee0b11338622194e7284e306
Tea <tea.desouza@gmail.com>
2015-11-25 11:43:45 +0800

    Secure temporary file creation

--- include/caffe/util/io.hpp
+++ include/caffe/util/io.hpp
@@ -2,12 +2,15 @@
 #define CAFFE_UTIL_IO_H_
 
 #include <boost/filesystem.hpp>
+#include <iomanip>
+#include <iostream>  // NOLINT(readability/streams)
 #include <string>
 
 #include "google/protobuf/message.h"
 
 #include "caffe/common.hpp"
 #include "caffe/proto/caffe.pb.h"
+#include "caffe/util/format.hpp"
 
 #ifndef CAFFE_TMP_DIR_RETRIES
 #define CAFFE_TMP_DIR_RETRIES 100
@@ -17,13 +20,6 @@ namespace caffe {
 
 using ::google::protobuf::Message;
 using ::boost::filesystem::path;
-
-inline void MakeTempFilename(string* temp_filename) {
-  temp_filename->clear();
-  const path& model = boost::filesystem::temp_directory_path()
-    /"caffe_test.%%%%%%";
-  *temp_filename = boost::filesystem::unique_path(model).string();
-}
 
 inline void MakeTempDir(string* temp_dirname) {
   temp_dirname->clear();
@@ -38,6 +34,19 @@ inline void MakeTempDir(string* temp_dirname) {
     }
   }
   LOG(FATAL) << "Failed to create a temporary directory.";
+}
+
+inline void MakeTempFilename(string* temp_filename) {
+  static path temp_files_subpath;
+  static uint64_t next_temp_file = 0;
+  temp_filename->clear();
+  if ( temp_files_subpath.empty() ) {
+    string path_string="";
+    MakeTempDir(&path_string);
+    temp_files_subpath = path_string;
+  }
+  *temp_filename =
+    (temp_files_subpath/caffe::format_int(next_temp_file++, 9)).string();
 }
 
 bool ReadProtoFromTextFile(const char* filename, Message* proto);
