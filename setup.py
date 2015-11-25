5ca8c6beb0737bdbebe92594b94f483297b1488b
Micah Chambers <micah@plangrid.com>
2015-11-24 22:21:21 -0800

    more clean up

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ import pip.download
 from pip.req import parse_requirements
 
 BASE_DIR = abspath(dirname(__file__))
-PROTO_DIR = join(BASE_DIR, 'src', 'caffe', 'proto')
+PROTO_DIR = join(BASE_DIR, 'src/caffe/proto')
 SRC_DIR = join(BASE_DIR, 'src')
 INC_DIR = join(BASE_DIR, 'include')
-SRC_GEN = [join(SRC_DIR, 'caffe', 'proto', 'caffe.pb.h'),
-           join(SRC_DIR, 'caffe', 'proto', 'caffe.pb.cc')]
+SRC_GEN = [join(SRC_DIR, 'caffe/proto/caffe.pb.h'),
+           join(SRC_DIR, 'caffe/proto/caffe.pb.cc')]
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements(join('python', 'requirements.txt'),
+install_reqs = parse_requirements('python/requirements.txt',
                                   session=pip.download.PipSession())
 
 # reqs is a list of requirement
@@ -27,32 +27,37 @@ def make_caffe_proto_module():
     """
     Creates the
     """
-    module_dir = join(BASE_DIR, 'python', 'caffe', 'proto')
-    module_file = join(module_dir, '__init__.py')
+    module_dir = join(BASE_DIR, 'python/caffe/proto')
+
     try:
         os.makedirs(module_dir)
     except OSError:
         pass
 
     # Makes an empty __init__ file for the caffe.proto module
-    with open(module_file, 'w') as f:
-        f.write('')
+    if not exists(join(module_dir, '__init__.py')):
+        with open(join(module_dir, '__init__.py'), 'w') as f:
+            f.write('')
 
-    # Converts the caffe.proto protocol buffer into a python format
-    subprocess.call(['protoc',
-                     join(PROTO_DIR, 'caffe.proto'),
-                     '--proto_path', PROTO_DIR,
-                     '--python_out', join(BASE_DIR, 'caffe', 'proto')])
+    if not exists(join(module_dir, 'caffe_pb2.py')):
+        # Converts the caffe.proto protocol buffer into a python format
+        subprocess.call(['protoc',
+                         join(PROTO_DIR, 'caffe.proto'),
+                         '--proto_path', PROTO_DIR,
+                         '--python_out', join(BASE_DIR, 'python/caffe/proto')])
 
 
 def get_sources():
     sources = []
 
     # Generates cc files from proto buffer
-    subprocess.call(['protoc',
-                     join(PROTO_DIR, 'caffe.proto'),
-                     '--proto_path', PROTO_DIR,
-                     '--cpp_out', PROTO_DIR])
+    if (not exists(join(PROTO_DIR, 'caffe.pb.cc')) or
+            not exists(join(PROTO_DIR, 'caffe.pb.h'))):
+        # Converts the caffe.proto protocol buffer into a python format
+        subprocess.call(['protoc',
+                         join(PROTO_DIR, 'caffe.proto'),
+                         '--proto_path', PROTO_DIR,
+                         '--python_out', join(BASE_DIR, 'python/caffe/proto')])
 
     for dirName, subdirList, fileList in os.walk(SRC_DIR):
         if os.path.basename(dirName) in ('test'):
@@ -73,7 +78,7 @@ def get_sources():
 make_caffe_proto_module()
 
 caffe_module = Extension(
-    join('caffe', '_caffe'),
+    'caffe/_caffe',
     define_macros = [('CPU_ONLY', '1')],
     libraries = ['cblas', 'blas', 'boost_thread', 'glog', 'gflags', 'protobuf',
                  'boost_python', 'boost_system', 'boost_filesystem', 'm',
@@ -98,9 +103,8 @@ setup(
     license = 'BSD',
     ext_modules = [caffe_module],
     package_dir = {'': 'python'},
-    packages = ['caffe', join('caffe', 'proto')],
-    scripts = [join('python/classify.py'), join('python', 'detect.py'),
-               join('python', 'draw_net.py')],
+    packages = ['caffe', 'caffe/proto'],
+    scripts = ['python/classify.py', 'python/detect.py', 'python/draw_net.py'],
     platforms = ['Linux', 'MacOS X', 'Windows'],
     long_description = ('Caffe is a deep learning framework made with '
                         'expression,  speed, and modularity in mind. It is '
