f66e0ca71ccbba920bb513d925c378cce6464e33
Evan Shelhamer <shelhamer@imaginarynumber.net>
2015-12-02 18:31:26 -0800

    collect Net inputs from Input layers
    Restore the list of net inputs for compatibility with the
    pycaffe and matcaffe interfaces and downstream C++.

--- src/caffe/net.cpp
+++ src/caffe/net.cpp
@@ -103,6 +103,12 @@ void Net<Dtype>::Init(const NetParameter& in_param) {
     int num_top = layer_param.top_size();
     for (int top_id = 0; top_id < num_top; ++top_id) {
       AppendTop(param, layer_id, top_id, &available_blobs, &blob_name_to_idx);
+      // Collect Input layer tops as Net inputs.
+      if (layer_param.type() == "Input") {
+        const int blob_id = blobs_.size() - 1;
+        net_input_blob_indices_.push_back(blob_id);
+        net_input_blobs_.push_back(blobs_[blob_id].get());
+      }
     }
     // If the layer specifies that AutoTopBlobs() -> true and the LayerParameter
     // specified fewer than the required number (as specified by
