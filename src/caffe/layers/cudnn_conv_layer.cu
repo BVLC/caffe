47228a251e79fe7f3db942de2e90229d15b25b82
Stevie James <djsteviejames@hotmail.com>
2015-12-22 06:51:12 -0800

    pod/cudnn
    added new cudnn module

--- src/caffe/layers/cudnn_conv_layer.cu
+++ src/caffe/layers/cudnn_conv_layer.cu
@@ -516,6 +516,7 @@ void CuDNNConvolutionLayer<Dtype>::Forward_gpu(
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -593,6 +594,8 @@ void CuDNNConvolutionLayer<Dtype>::Forward_gpu(
 >>>>>>> BVLC/master
 =======
 >>>>>>> BVLC/master
+=======
+>>>>>>> BVLC/master
 #if CUDNN_VERSION_MIN(4, 0, 0)
         CUDNN_CHECK(cudnnAddTensor(handle_[g],
               cudnn::dataType<Dtype>::one,
@@ -600,6 +603,7 @@ void CuDNNConvolutionLayer<Dtype>::Forward_gpu(
               cudnn::dataType<Dtype>::one,
               top_descs_[i], top_data + top_offset_ * g));
 #else
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -624,11 +628,14 @@ void CuDNNConvolutionLayer<Dtype>::Forward_gpu(
 >>>>>>> BVLC/master
 =======
 >>>>>>> BVLC/master
+=======
+>>>>>>> BVLC/master
         CUDNN_CHECK(cudnnAddTensor(handle_[g], CUDNN_ADD_SAME_C,
               cudnn::dataType<Dtype>::one,
               bias_desc_, bias_data + bias_offset_ * g,
               cudnn::dataType<Dtype>::one,
               top_descs_[i], top_data + top_offset_ * g));
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -743,6 +750,9 @@ void CuDNNConvolutionLayer<Dtype>::Forward_gpu(
 =======
 >>>>>>> pod-caffe-pod.hpp-merge
 >>>>>>> pod/device/blob.hpp
+=======
+#endif
+>>>>>>> BVLC/master
 =======
 #endif
 >>>>>>> BVLC/master
