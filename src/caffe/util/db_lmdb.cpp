016c460bcfc35f35e7ef1baa3c33846c70f1caca
Wei Liu <lwbiosoft@gmail.com>
2016-01-15 13:30:31 -0800

    add LabelMap and tools for create DB to store AnnotatedDatum

--- src/caffe/util/db_lmdb.cpp
+++ src/caffe/util/db_lmdb.cpp
@@ -13,7 +13,7 @@ void LMDB::Open(const string& source, Mode mode) {
   MDB_CHECK(mdb_env_create(&mdb_env_));
   MDB_CHECK(mdb_env_set_mapsize(mdb_env_, LMDB_MAP_SIZE));
   if (mode == NEW) {
-    CHECK_EQ(mkdir(source.c_str(), 0744), 0) << "mkdir " << source << "failed";
+    CHECK_EQ(mkdir(source.c_str(), 0744), 0) << "mkdir " << source << " failed";
   }
   int flags = 0;
   if (mode == READ) {
