bef4098ea488dcd0c0df80c20159238a6c701c0b
Ishant Mrinal Haloi <n3011@users.noreply.github.com>
2016-01-15 11:31:35 +0800

    Update io.py

--- python/caffe/io.py
+++ python/caffe/io.py
@@ -256,10 +256,10 @@ class Transformer:
                 raise ValueError('Mean shape invalid')
             if ms != self.inputs[in_][1:]:
                 print(self.inputs[in_])
-    		 in_shape = self.inputs[in_][1:]
-    		 m_min, m_max = mean.min(), mean.max()
-    		 normal_mean = (mean - m_min) / (m_max - m_min)
-    		 mean = resize_image(normal_mean.transpose((1,2,0)),
+    		    in_shape = self.inputs[in_][1:]
+    		    m_min, m_max = mean.min(), mean.max()
+    		    normal_mean = (mean - m_min) / (m_max - m_min)
+    		    mean = resize_image(normal_mean.transpose((1,2,0)),
                         in_shape[1:]).transpose((2,0,1)) * \
                         (m_max - m_min) + m_min
                 #raise ValueError('Mean shape incompatible with input shape.')
