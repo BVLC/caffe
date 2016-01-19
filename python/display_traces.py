262697ee8424dc27b0c25d9a365053f226904acb
Calvin Seward <calvin.seward@zalando.de>
2016-01-19 14:46:54 +0100

    updated python script to also plot test and training errors

--- python/display_traces.py
+++ python/display_traces.py
@@ -37,11 +37,19 @@ def main(argv):
     plot_activation_trace(trace_digest, fig=fig3)
     fig3.suptitle("activation traces")
     
+    fig4 = plt.figure()
+    plot_test_score(trace_digest, fig=fig4)
+    plot_train_loss(trace_digest, fig=fig4, smoothing_window=10)
+    fig4.suptitle("test score")
+    
     plt.show()
     
 def plot_activation_trace(trace_digest, fig=None, blobs=[]):
     '''
     Plot the activations from a proto buffer
+    @param trace_digest protobuffer created by parsing the trace file
+    @param fig Optional figure that the traces should be plotted to
+    @param blobs If set, plot only the activation traces of these blobs
     '''
     if fig==None:
         fig = plt.figure()
@@ -74,10 +82,17 @@ def plot_activation_trace(trace_digest, fig=None, blobs=[]):
             ax.plot(x, y[:,j])
         ax.set_title(at.blob_name)
     
-def plot_weight_trace(trace_digest, fig=None, return_traces=False, traces={}):
+def plot_weight_trace(trace_digest, fig=None, return_traces=False, traces=set([])):
     return plot_trace(trace_digest.weight_trace_point, fig, return_traces, traces)
     
-def plot_diff_trace(trace_digest, fig=None, return_traces=False, traces={}):
+def plot_diff_trace(trace_digest, fig=None, return_traces=False, traces=set([])):
+    '''
+    Plot the trace of the diffs through training time
+    @param trace_digest protobuffer created by parsing the trace file
+    @param fig Optional figure that the traces should be plotted to
+    @param return_traces If set to true, values of the traces are returned
+    @param traces If set, plot only traces of layers in this set, otherwise everything
+    '''
     return plot_trace(trace_digest.diff_trace_point, fig, return_traces, traces)
 
 def plot_trace(weight_trace_digest, fig, return_traces, traces):
@@ -110,7 +125,7 @@ def plot_trace(weight_trace_digest, fig, return_traces, traces):
     
     x=np.sort(x.keys());
 
-    if traces=={}:
+    if len(traces) == 0:
         traces = layers.keys()
     else:
         traces = set(layers.keys()).intersection(traces)
@@ -127,6 +142,78 @@ def plot_trace(weight_trace_digest, fig, return_traces, traces):
 
     if return_traces:
         return layers, x
+
+def plot_test_loss(trace_digest, fig=None, target=None, return_traces=False):
+    x_test,y_test = {},{}
+
+    for tp in trace_digest.test_loss_trace_point:
+        if target is None or tp.test_net_id in target:
+            if tp.test_net_id in x_test:
+                x_test[tp.test_net_id].append(tp.iter)
+                y_test[tp.test_net_id].append(tp.test_loss)
+            else:
+                x_test[tp.test_net_id] = [tp.iter]
+                y_test[tp.test_net_id] = [tp.test_loss]
+    
+    for test_name in x_test.keys():
+        if fig == None:
+            plt.plot(x_test[test_name],y_test[test_name], label='test net id: ' + str(test_name))
+            plt.legend()
+        else:
+            ax = fig.add_subplot(111)
+            ax.plot(x_test[test_name],y_test[test_name], label='test net id: ' + str(test_name))
+            ax.legend()
+    if return_traces:
+        return x_test,y_test
+        
+def plot_test_score(trace_digest, fig=None, target=None, return_traces=False):
+    x_test,y_test = {},{}
+
+    for tp in trace_digest.test_score_trace_point:
+        if target is None or tp.test_net_id in target:
+            id_string = 'test net id, score name: '
+            id_string += str(tp.test_net_id) + ' ' + str(tp.score_name)
+            if id_string in x_test:
+                x_test[id_string].append(tp.iter)
+                y_test[id_string].append(tp.mean_score)
+            else:
+                x_test[id_string] = [tp.iter]
+                y_test[id_string] = [tp.mean_score]
+    if fig:
+        ax = fig.add_subplot(111)
+
+    for test_name in x_test.keys():
+        if fig == None:
+            plt.plot(x_test[test_name],y_test[test_name], label=test_name)
+            plt.legend()
+        else:
+            ax.plot(x_test[test_name],y_test[test_name], label=test_name)
+            ax.legend()
+    if return_traces:
+        return x_test,y_test
+
+def plot_train_loss(trace_digest, fig=None, return_traces=False, smoothing_window=1):
+    assert smoothing_window >= 0
+    x_test,y_test = [], []
+    moving_window = np.zeros(smoothing_window, dtype=float)
+    for i, tp in enumerate(trace_digest.train_trace_point):
+        x_test.append(tp.iter)
+        moving_window[i % smoothing_window] = tp.train_loss
+        if i >= smoothing_window:
+            y_test.append(np.mean(moving_window))
+        else:
+            y_test.append(np.sum(moving_window) / (i+1))
+    if fig:
+        ax = fig.add_subplot(111)
+
+    if fig == None:
+        plt.plot(x_test, y_test, label='training loss')
+        plt.legend()
+    else:
+        ax.plot(x_test,y_test, label='training loss')
+        ax.legend()
+    if return_traces:
+        return x_test,y_test
 
 if __name__ == '__main__':
     main(sys.argv)
