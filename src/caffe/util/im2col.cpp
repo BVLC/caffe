2324c06dd271f53c5566914cbd0d53f417c370bc
Mariusz Moczala <mariusz.moczala@intel.com>
2016-01-21 14:27:14 +0100

    im2col update

--- src/caffe/util/im2col.cpp
+++ src/caffe/util/im2col.cpp
@@ -5,66 +5,45 @@
 
 namespace caffe {
 
-template <typename Dtype>
-void im2col_cpu(const Dtype *data_im, int channels, int input_h, int input_w,
-    int kernel_h, int kernel_w, int pad_h, int pad_w,
-    int stride_h, int stride_w, int dilation_h, int dilation_w,
-    Dtype *data_col) {
-  const int output_h = (input_h + 2 * pad_h -
-    (dilation_h * (kernel_h - 1) + 1)) / stride_h + 1;
-  const int output_w = (input_w + 2 * pad_w -
-    (dilation_w * (kernel_w - 1) + 1)) / stride_w + 1;
-  const int channel_size = input_h * input_w;
-  for (; channels--; data_im += channel_size) {
-    for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
-      for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) {
-        int input_row = -pad_h + kernel_row * dilation_h;
-        for (int output_rows = output_h; output_rows; output_rows--) {
-          if (((unsigned) input_row) >= ((unsigned) input_h)) {
-            for (int output_cols = output_w; output_cols; output_cols--)
-              *(data_col++) = 0;
-          } else {
-            int input_col = -pad_w + kernel_col * dilation_w;
-            for (int output_col = output_w; output_col; output_col--) {
-              if (((unsigned) input_col) < ((unsigned) input_w))
-                *(data_col++) = data_im[input_row * input_w + input_col];
-              else
-                *(data_col++) = 0;
-              input_col += stride_w;
-            }
-          }
-          input_row += stride_h;
-        }
-      }
-    }
-  }
+// Function uses casting from int to unsigned to compare if value of
+// parameter a is greater or equal to zero and lower than value of
+// parameter b. The b parameter is of type signed and is always positive,
+// therefore its value is always lower than 0x800... where casting
+// negative value of a parameter converts it to value higher than 0x800...
+// The casting allows to use one condition instead of two.
+inline bool is_a_ge_zero_and_a_lt_b(int a, int b) {
+  return static_cast<unsigned>(a) < static_cast<unsigned>(b);
 }
 
 template <typename Dtype>
-void col2im_cpu(
-    const Dtype *data_col, int channels, int input_h, int input_w,
-    int kernel_h, int kernel_w, int pad_h, int pad_w,
-    int stride_h, int stride_w, int dilation_h, int dilation_w,
-    Dtype *data_im) {
-  const int output_h = (input_h + 2 * pad_h -
+void im2col_cpu(const Dtype* data_im, const int channels,
+    const int height, const int width, const int kernel_h, const int kernel_w,
+    const int pad_h, const int pad_w,
+    const int stride_h, const int stride_w,
+    const int dilation_h, const int dilation_w,
+    Dtype* data_col) {
+  const int output_h = (height + 2 * pad_h -
     (dilation_h * (kernel_h - 1) + 1)) / stride_h + 1;
-  const int output_w = (input_w + 2 * pad_w -
+  const int output_w = (width + 2 * pad_w -
     (dilation_w * (kernel_w - 1) + 1)) / stride_w + 1;
-  const int channel_size = input_h * input_w;
-  caffe_set(channels * input_h * input_w, Dtype(0), data_im);
-  for (; channels--; data_im += channel_size) {
+  const int channel_size = height * width;
+  for (int channel = channels; channel--; data_im += channel_size) {
     for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
       for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) {
         int input_row = -pad_h + kernel_row * dilation_h;
         for (int output_rows = output_h; output_rows; output_rows--) {
-          if (((unsigned) input_row) >= ((unsigned) input_h)) {
-            data_col += output_w;
+          if (!is_a_ge_zero_and_a_lt_b(input_row, height)) {
+            for (int output_cols = output_w; output_cols; output_cols--) {
+              *(data_col++) = 0;
+            }
           } else {
             int input_col = -pad_w + kernel_col * dilation_w;
             for (int output_col = output_w; output_col; output_col--) {
-              if (((unsigned) input_col) < ((unsigned) input_w))
-                data_im[input_row * input_w + input_col] += *data_col;
-              data_col++;
+              if (is_a_ge_zero_and_a_lt_b(input_col, width)) {
+                *(data_col++) = data_im[input_row * width + input_col];
+              } else {
+                *(data_col++) = 0;
+              }
               input_col += stride_w;
             }
           }
@@ -76,22 +55,16 @@ void col2im_cpu(
 }
 
 // Explicit instantiation
-template void im2col_cpu<float>(const float* data_im, int channels,
-    int input_h, int input_w, int kernel_h, int kernel_w,
-    int pad_h, int pad_w, int stride_h, int stride_w,
-    int dilation_h, int dilation_w, float* data_col);
-template void im2col_cpu<double>(const double* data_im, int channels,
-    int input_h, int input_w, int kernel_h, int kernel_w,
-    int pad_h, int pad_w, int stride_h, int stride_w,
-    int dilation_h, int dilation_w, double* data_col);
-template void col2im_cpu<float>(const float* data_col, int channels,
-    int input_h, int input_w, int kernel_h, int kernel_w,
-    int pad_h, int pad_w, int stride_h, int stride_w,
-    int dilation_h, int dilation_w, float* data_im);
-template void col2im_cpu<double>(const double* data_col, int channels,
-    int input_h, int input_w, int kernel_h, int kernel_w,
-    int pad_h, int pad_w, int stride_h, int stride_w,
-    int dilation_h, int dilation_w, double* data_im);
+template void im2col_cpu<float>(const float* data_im, const int channels,
+    const int height, const int width, const int kernel_h, const int kernel_w,
+    const int pad_h, const int pad_w, const int stride_h,
+    const int stride_w, const int dilation_h, const int dilation_w,
+    float* data_col);
+template void im2col_cpu<double>(const double* data_im, const int channels,
+    const int height, const int width, const int kernel_h, const int kernel_w,
+    const int pad_h, const int pad_w, const int stride_h,
+    const int stride_w, const int dilation_h, const int dilation_w,
+    double* data_col);
 
 template <typename Dtype>
 inline void im2col_nd_core_cpu(const Dtype* data_input, const bool im2col,
@@ -174,16 +147,6 @@ void im2col_nd_cpu(const Dtype* data_im, const int num_spatial_axes,
                   kernel_shape, pad, stride, dilation, data_col);
 }
 
-template <typename Dtype>
-void col2im_nd_cpu(const Dtype* data_col, const int num_spatial_axes,
-    const int* im_shape, const int* col_shape,
-    const int* kernel_shape, const int* pad, const int* stride,
-    const int* dilation, Dtype* data_im) {
-  const bool kIm2Col = false;
-  im2col_nd_core_cpu(data_col, kIm2Col, num_spatial_axes, im_shape, col_shape,
-                     kernel_shape, pad, stride, dilation, data_im);
-}
-
 // Explicit instantiation
 template void im2col_nd_cpu<float>(const float* data_im,
     const int num_spatial_axes,
@@ -195,6 +158,67 @@ template void im2col_nd_cpu<double>(const double* data_im,
     const int* im_shape, const int* col_shape,
     const int* kernel_shape, const int* pad, const int* stride,
     const int* dilation, double* data_col);
+
+template <typename Dtype>
+void col2im_cpu(const Dtype* data_col, const int channels,
+    const int height, const int width, const int kernel_h, const int kernel_w,
+    const int pad_h, const int pad_w,
+    const int stride_h, const int stride_w,
+    const int dilation_h, const int dilation_w,
+    Dtype* data_im) {
+  caffe_set(height * width * channels, Dtype(0), data_im);
+  const int output_h = (height + 2 * pad_h -
+    (dilation_h * (kernel_h - 1) + 1)) / stride_h + 1;
+  const int output_w = (width + 2 * pad_w -
+    (dilation_w * (kernel_w - 1) + 1)) / stride_w + 1;
+  const int channel_size = height * width;
+  for (int channel = channels; channel--; data_im += channel_size) {
+    for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
+      for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) {
+        int input_row = -pad_h + kernel_row * dilation_h;
+        for (int output_rows = output_h; output_rows; output_rows--) {
+          if (!is_a_ge_zero_and_a_lt_b(input_row, height)) {
+            data_col += output_w;
+          } else {
+            int input_col = -pad_w + kernel_col * dilation_w;
+            for (int output_col = output_w; output_col; output_col--) {
+              if (is_a_ge_zero_and_a_lt_b(input_col, width)) {
+                data_im[input_row * width + input_col] += *data_col;
+              }
+              data_col++;
+              input_col += stride_w;
+            }
+          }
+          input_row += stride_h;
+        }
+      }
+    }
+  }
+}
+
+// Explicit instantiation
+template void col2im_cpu<float>(const float* data_col, const int channels,
+    const int height, const int width, const int kernel_h, const int kernel_w,
+    const int pad_h, const int pad_w, const int stride_h,
+    const int stride_w, const int dilation_h, const int dilation_w,
+    float* data_im);
+template void col2im_cpu<double>(const double* data_col, const int channels,
+    const int height, const int width, const int kernel_h, const int kernel_w,
+    const int pad_h, const int pad_w, const int stride_h,
+    const int stride_w, const int dilation_h, const int dilation_w,
+    double* data_im);
+
+template <typename Dtype>
+void col2im_nd_cpu(const Dtype* data_col, const int num_spatial_axes,
+    const int* im_shape, const int* col_shape,
+    const int* kernel_shape, const int* pad, const int* stride,
+    const int* dilation, Dtype* data_im) {
+  const bool kIm2Col = false;
+  im2col_nd_core_cpu(data_col, kIm2Col, num_spatial_axes, im_shape, col_shape,
+                     kernel_shape, pad, stride, dilation, data_im);
+}
+
+// Explicit instantiation
 template void col2im_nd_cpu<float>(const float* data_col,
     const int num_spatial_axes,
     const int* im_shape, const int* col_shape,
@@ -205,5 +229,6 @@ template void col2im_nd_cpu<double>(const double* data_col,
     const int* im_shape, const int* col_shape,
     const int* kernel_shape, const int* pad, const int* stride,
     const int* dilation, double* data_im);
+
 
 }  // namespace caffe
