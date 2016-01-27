BVLC/caffe

commit c11e04dd9a7fc148ec1806ac1f0885a4bfd8664c

fix lint error

src/caffe/util/io.cpp
@@ -104,7 +104,8 @@ cv::Mat ReadImageToCVMat(const string& filename, const int height,
     if (scale_factor == 1) {
       cv_img = cv_img_origin;
     } else {
-      cv::resize(cv_img_origin, cv_img, cv::Size(0, 0), scale_factor, scale_factor);
+      cv::resize(cv_img_origin, cv_img, cv::Size(0, 0),
+                 scale_factor, scale_factor);
     }
   } else if (height > 0 && width > 0) {
     cv::resize(cv_img_origin, cv_img, cv::Size(width, height));
@@ -112,6 +113,11 @@ cv::Mat ReadImageToCVMat(const string& filename, const int height,
     cv_img = cv_img_origin;
   }
   return cv_img;
+}
+
+cv::Mat ReadImageToCVMat(const string& filename, const int height,
+    const int width, const int min_dim, const int max_dim) {
+  return ReadImageToCVMat(filename, height, width, min_dim, max_dim, true);
 }
 
 cv::Mat ReadImageToCVMat(const string& filename,
@@ -150,7 +156,7 @@ static bool matchExt(const std::string & fn,
 bool ReadImageToDatum(const string& filename, const int label,
     const int height, const int width, const int min_dim, const int max_dim,
     const bool is_color, const std::string & encoding, Datum* datum) {
-  cv::Mat cv_img = ReadImageToCVMat(filename, height, width, min_dim, max_dim, is_color);
+  cv::Mat cv_img = ReadImageToCVMat(filename, height, width, min_dim, max_dim);
   if (cv_img.data) {
     if (encoding.size()) {
       if ( (cv_img.channels() == 3) == is_color && !height && !width &&

include/caffe/util/io.hpp
@@ -235,6 +235,9 @@ inline bool MapLabelToName(const LabelMap& map,
 cv::Mat ReadImageToCVMat(const string& filename, const int height,
     const int width, const int min_dim, const int max_dim, const bool is_color);
 
+cv::Mat ReadImageToCVMat(const string& filename, const int height,
+    const int width, const int min_dim, const int max_dim);
+
 cv::Mat ReadImageToCVMat(const string& filename,
     const int height, const int width, const bool is_color);
 

commit 05f918576ea6e927ced89d864c99e9229c954d87

fix a bug in io.cpp introduced in c11e04d

src/caffe/util/io.cpp
@@ -156,7 +156,8 @@ static bool matchExt(const std::string & fn,
 bool ReadImageToDatum(const string& filename, const int label,
     const int height, const int width, const int min_dim, const int max_dim,
     const bool is_color, const std::string & encoding, Datum* datum) {
-  cv::Mat cv_img = ReadImageToCVMat(filename, height, width, min_dim, max_dim);
+  cv::Mat cv_img = ReadImageToCVMat(filename, height, width, min_dim, max_dim,
+                                    is_color);
   if (cv_img.data) {
     if (encoding.size()) {
       if ( (cv_img.channels() == 3) == is_color && !height && !width &&
@@ -216,8 +217,9 @@ bool ReadRichImageToAnnotatedDatum(const string& filename,
     const string& encoding, const AnnotatedDatum_AnnotationType type,
     const std::map<string, int>& name_to_label, AnnotatedDatum* anno_datum) {
   // Read image to datum.
-  bool status = ReadImageToDatum(filename, -1, height, width, min_dim, max_dim,
-                              is_color, encoding, anno_datum->mutable_datum());
+  bool status = ReadImageToDatum(filename, -1, height, width,
+                                 min_dim, max_dim, is_color, encoding,
+                                 anno_datum->mutable_datum());
   if (status == false) {
     return status;
   }