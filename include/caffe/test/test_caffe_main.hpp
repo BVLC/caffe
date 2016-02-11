9972a979b04f3610e9c851cfaf000676e4f405b7
Pavle Josipovic <pajosi@microsoft.com>
2016-02-11 18:03:10 +0100

    Add support for windows build
    Necessary changes to enable widndows build:
    Visual studio project files.
    Minor code chnages.
    3rd party dependencies are resolved through NuGet.

--- include/caffe/test/test_caffe_main.hpp
+++ include/caffe/test/test_caffe_main.hpp
@@ -10,6 +10,7 @@
 #include <cstdlib>
 
 #include "caffe/common.hpp"
+#include "caffe/util/io.hpp"
 
 using std::cout;
 using std::endl;
@@ -35,7 +36,8 @@ class MultiDeviceTest : public ::testing::Test {
   MultiDeviceTest() {
     Caffe::set_mode(TypeParam::device);
   }
-  virtual ~MultiDeviceTest() {}
+  // Caffe tests may create some temporary files, here we will do the cleanup.
+  virtual ~MultiDeviceTest() { RemoveCaffeTempDir(); }
 };
 
 typedef ::testing::Types<float, double> TestDtypes;
