f5e2d55c513e391c95b6ad30ee67a36a7247cea4
Pavle Josipovic <pajosi@microsoft.com>
2016-02-26 17:51:17 +0100

    Tweak default build options
    Make compiling Maxwell and Kepler cuda arch default.
    Support extrating cuDNN files to CUDA install folder.
    Copy caffe dependancies to python output folder.
    Adjust README.md

--- windows/scripts/BinplaceCudaDependencies.cmd
+++ windows/scripts/BinplaceCudaDependencies.cmd
@@ -14,8 +14,13 @@ if %IS_CPU_ONLY_BUILD% == true (
     copy /y "%CUDA_TOOLKIT_BIN_DIR%\curand*.dll" "%OUTPUT_DIR%"
 
     if %USE_CUDNN% == true (
-        echo BinplaceCudaDependencies : Copy cunn*.dll to output.
-        copy /y "%CUDNN_PATH%\cuda\bin\cudnn*.dll" "%OUTPUT_DIR%"
+        echo BinplaceCudaDependencies : Copy cudnn*.dll to output.
+
+        if [%CUDNN_PATH%] == [] (
+            copy /y "%CUDA_TOOLKIT_BIN_DIR%\cudnn*.dll" "%OUTPUT_DIR%"
+        ) else (
+            copy /y "%CUDNN_PATH%\cuda\bin\cudnn*.dll" "%OUTPUT_DIR%"
+        )
     ) else (
         echo BinplaceCudaDependencies : cuDNN isn't enabled.
     )
