57b4e734ef821ee885d2498a31b49bd952b85d5a
Evan Lezar <evan.lezar@zalando.de>
2016-02-26 15:06:48 +0100

    Apply clangformat

--- src/caffe/layers/key_pooling_layer.cpp
+++ src/caffe/layers/key_pooling_layer.cpp
@@ -12,18 +12,16 @@ using std::max;
 
 template <typename Dtype>
 void KeyPoolingLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
+                                        const vector<Blob<Dtype>*>& top) {
   CHECK_EQ(bottom[0]->shape(0), bottom[1]->shape(0))
       << "The number of keys must be equal to the size of the batch";
 
   pooling_layer_.LayerSetUp(bottom, top);
-
 }
 
 template <typename Dtype>
 void KeyPoolingLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
-
+                                     const vector<Blob<Dtype>*>& top) {
   int num_keys = bottom[1]->shape(0);
   has_keys_.clear();
   key_start_.clear();
@@ -50,18 +48,18 @@ void KeyPoolingLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
         j++;
         key_end_.push_back(i);
 
-        largest_key_set_ = std::max(key_end_[j-1] - key_start_[j-1], largest_key_set_);
+        largest_key_set_ =
+            std::max(key_end_[j - 1] - key_start_[j - 1], largest_key_set_);
 
         current_key = keys[i];
         has_keys_.push_back(current_key);
         key_start_.push_back(i);
-
       }
     }
     key_end_.push_back(num_keys);
 
-
-    largest_key_set_ = std::max(key_end_[num_keys-1] - key_start_[num_keys-1], largest_key_set_);
+    largest_key_set_ = std::max(
+        key_end_[num_keys - 1] - key_start_[num_keys - 1], largest_key_set_);
   }
 
   CHECK_LE(has_keys_.size(), num_keys);
@@ -77,14 +75,11 @@ void KeyPoolingLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   if (top.size() > 2) {
     top[2]->Reshape(has_keys_.size(), 1, 1, 1);
   }
-
 }
 
 template <typename Dtype>
 void KeyPoolingLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
-
-
+                                         const vector<Blob<Dtype>*>& top) {
   for (int i = 0; i < has_keys_.size(); ++i) {
     // Create a local copy of the blobs for the top and the bottom
     Blob<Dtype> key_bottom;
@@ -99,18 +94,19 @@ void KeyPoolingLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     bottom_shape[0] = key_end_[i] - key_start_[i];
     key_bottom.Reshape(bottom_shape);
 
-    const Dtype *bottom_data = bottom[0]->cpu_data();
+    const Dtype* bottom_data = bottom[0]->cpu_data();
     caffe_copy(key_bottom.count(),
                &bottom_data[bottom[0]->offset(key_start_[i])],
                key_bottom.mutable_cpu_data());
 
+    // Perform pooling on this key.
     pooling_layer_.Forward(pooling_bottoms, pooling_tops);
 
     key_top_mask.Reshape(key_top.shape());
+    Dtype* key_pool = key_top.mutable_cpu_data();
+    Dtype* key_mask = key_top_mask.mutable_cpu_data();
 
-    Dtype *key_pool = key_top.mutable_cpu_data();
-    Dtype *key_mask = key_top_mask.mutable_cpu_data();
-    caffe_set(key_top.count(), Dtype(key_start_[i]), key_mask);
+    caffe_set(key_mask.count(), Dtype(key_start_[i]), key_mask);
     for (int j = 1; j < key_top.shape(0); ++j) {
       int j_offset = key_top.offset(j);
       for (int k = 0; k < key_top.count(1); ++k) {
@@ -134,12 +130,12 @@ void KeyPoolingLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   if (top.size() > 2) {
     caffe_copy(has_keys_.size(), &has_keys_[0], top[2]->mutable_cpu_data());
   }
-
 }
 
 template <typename Dtype>
 void KeyPoolingLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+                                          const vector<bool>& propagate_down,
+                                          const vector<Blob<Dtype>*>& bottom) {
   pooling_layer_.Backward(bottom, propagate_down, top);
 }
 
