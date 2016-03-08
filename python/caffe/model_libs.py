cbef98eddc834d82523465cb14d6a6c4bb1c9c16
Wei Liu <lwbiosoft@gmail.com>
2016-03-07 19:02:22 -0800

    enhance model_libs

--- python/caffe/model_libs.py
+++ python/caffe/model_libs.py
@@ -18,14 +18,18 @@ def ConvBNLayer(net, from_layer, out_layer, use_bn, use_relu, num_output, kernel
     kwargs = {
         'param': [dict(lr_mult=1, decay_mult=1)],
         'weight_filler': dict(type='gaussian', std=0.01),
-        'bias_term': False
+        'bias_term': False,
+        }
+    # parameters for batchnorm layer.
+    bn_kwargs = {
+        'param': [dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0)],
         }
     # parameters for scale bias layer after batchnorm.
     sb_kwargs = {
         'bias_term': True,
-        'param': [dict(lr_mult=1.00001)],
+        'param': [dict(lr_mult=1.00001, decay_mult=0), dict(lr_mult=1.00001, decay_mult=0)],
         'filler': dict(type='constant', value=1),
-        'bias_filler': dict(type='constant', value=0.001)
+        'bias_filler': dict(type='constant', value=0.001),
         }
   else:
     kwargs = {
@@ -38,7 +42,7 @@ def ConvBNLayer(net, from_layer, out_layer, use_bn, use_relu, num_output, kernel
   net[conv_name] = L.Convolution(net[from_layer], num_output=num_output, kernel_size=kernel_size, pad=pad, stride=stride, **kwargs)
   if use_bn:
     bn_name = '{}{}'.format(bn_prefix, out_layer)
-    net[bn_name] = L.BatchNorm(net[conv_name], in_place=True)
+    net[bn_name] = L.BatchNorm(net[conv_name], in_place=True, **bn_kwargs)
     sb_name = '{}{}'.format(scale_prefix, out_layer)
     net[sb_name] = L.Scale(net[conv_name], in_place=True, **sb_kwargs)
   if use_relu:
@@ -114,7 +118,8 @@ def CreateAnnotatedDataLayer(source, batch_size=32, backend=P.Data.LMDB,
         return data
 
 
-def VGGNetBody(net, from_layer, need_fc=True, fully_conv=False, reduced=False, freeze_layers=[]):
+def VGGNetBody(net, from_layer, need_fc=True, fully_conv=False, reduced=False,
+        dilated=False, nopool=False, dropout=True, freeze_layers=[]):
     kwargs = {
             'param': [dict(lr_mult=1, decay_mult=1), dict(lr_mult=2, decay_mult=0)],
             'weight_filler': dict(type='xavier'),
@@ -127,34 +132,54 @@ def VGGNetBody(net, from_layer, need_fc=True, fully_conv=False, reduced=False, f
     net.conv1_2 = L.Convolution(net.relu1_1, num_output=64, pad=1, kernel_size=3, **kwargs)
     net.relu1_2 = L.ReLU(net.conv1_2, in_place=True)
 
-    net.pool1 = L.Pooling(net.relu1_2, pool=P.Pooling.MAX, kernel_size=2, stride=2)
+    if nopool:
+        name = 'conv1_3'
+        net[name] = L.Convolution(net.relu1_2, num_output=64, pad=1, kernel_size=3, stride=2, **kwargs)
+    else:
+        name = 'pool1'
+        net.pool1 = L.Pooling(net.relu1_2, pool=P.Pooling.MAX, kernel_size=2, stride=2)
 
-    net.conv2_1 = L.Convolution(net.pool1, num_output=128, pad=1, kernel_size=3, **kwargs)
+    net.conv2_1 = L.Convolution(net[name], num_output=128, pad=1, kernel_size=3, **kwargs)
     net.relu2_1 = L.ReLU(net.conv2_1, in_place=True)
     net.conv2_2 = L.Convolution(net.relu2_1, num_output=128, pad=1, kernel_size=3, **kwargs)
     net.relu2_2 = L.ReLU(net.conv2_2, in_place=True)
 
-    net.pool2 = L.Pooling(net.relu2_2, pool=P.Pooling.MAX, kernel_size=2, stride=2)
+    if nopool:
+        name = 'conv2_3'
+        net[name] = L.Convolution(net.relu2_2, num_output=128, pad=1, kernel_size=3, stride=2, **kwargs)
+    else:
+        name = 'pool2'
+        net[name] = L.Pooling(net.relu2_2, pool=P.Pooling.MAX, kernel_size=2, stride=2)
 
-    net.conv3_1 = L.Convolution(net.pool2, num_output=256, pad=1, kernel_size=3, **kwargs)
+    net.conv3_1 = L.Convolution(net[name], num_output=256, pad=1, kernel_size=3, **kwargs)
     net.relu3_1 = L.ReLU(net.conv3_1, in_place=True)
     net.conv3_2 = L.Convolution(net.relu3_1, num_output=256, pad=1, kernel_size=3, **kwargs)
     net.relu3_2 = L.ReLU(net.conv3_2, in_place=True)
     net.conv3_3 = L.Convolution(net.relu3_2, num_output=256, pad=1, kernel_size=3, **kwargs)
     net.relu3_3 = L.ReLU(net.conv3_3, in_place=True)
 
-    net.pool3 = L.Pooling(net.relu3_3, pool=P.Pooling.MAX, kernel_size=2, stride=2)
+    if nopool:
+        name = 'conv3_4'
+        net[name] = L.Convolution(net.relu3_3, num_output=256, pad=1, kernel_size=3, stride=2, **kwargs)
+    else:
+        name = 'pool3'
+        net[name] = L.Pooling(net.relu3_3, pool=P.Pooling.MAX, kernel_size=2, stride=2)
 
-    net.conv4_1 = L.Convolution(net.pool3, num_output=512, pad=1, kernel_size=3, **kwargs)
+    net.conv4_1 = L.Convolution(net[name], num_output=512, pad=1, kernel_size=3, **kwargs)
     net.relu4_1 = L.ReLU(net.conv4_1, in_place=True)
     net.conv4_2 = L.Convolution(net.relu4_1, num_output=512, pad=1, kernel_size=3, **kwargs)
     net.relu4_2 = L.ReLU(net.conv4_2, in_place=True)
     net.conv4_3 = L.Convolution(net.relu4_2, num_output=512, pad=1, kernel_size=3, **kwargs)
     net.relu4_3 = L.ReLU(net.conv4_3, in_place=True)
 
-    net.pool4 = L.Pooling(net.relu4_3, pool=P.Pooling.MAX, kernel_size=2, stride=2)
+    if nopool:
+        name = 'conv4_4'
+        net[name] = L.Convolution(net.relu4_3, num_output=512, pad=1, kernel_size=3, stride=2, **kwargs)
+    else:
+        name = 'pool4'
+        net[name] = L.Pooling(net.relu4_3, pool=P.Pooling.MAX, kernel_size=2, stride=2)
 
-    net.conv5_1 = L.Convolution(net.pool4, num_output=512, pad=1, kernel_size=3, **kwargs)
+    net.conv5_1 = L.Convolution(net[name], num_output=512, pad=1, kernel_size=3, **kwargs)
     net.relu5_1 = L.ReLU(net.conv5_1, in_place=True)
     net.conv5_2 = L.Convolution(net.relu5_1, num_output=512, pad=1, kernel_size=3, **kwargs)
     net.relu5_2 = L.ReLU(net.conv5_2, in_place=True)
@@ -162,28 +187,53 @@ def VGGNetBody(net, from_layer, need_fc=True, fully_conv=False, reduced=False, f
     net.relu5_3 = L.ReLU(net.conv5_3, in_place=True)
 
     if need_fc:
-        if fully_conv:
-            if reduced:
-                net.pool5 = L.Pooling(net.relu5_3, pool=P.Pooling.MAX, kernel_size=3, pad=1, stride=1)
-                net.fc6 = L.Convolution(net.pool5, num_output=1024, pad=6, kernel_size=3, dilation=6, **kwargs)
+        if dilated:
+            if nopool:
+                name = 'conv5_4'
+                net[name] = L.Convolution(net.relu5_3, num_output=512, pad=1, kernel_size=3, stride=1, **kwargs)
             else:
-                net.pool5 = L.Pooling(net.relu5_3, pool=P.Pooling.MAX, kernel_size=2, stride=2)
-                net.fc6 = L.Convolution(net.pool5, num_output=4096, pad=3, kernel_size=7, **kwargs)
+                name = 'pool5'
+                net[name] = L.Pooling(net.relu5_3, pool=P.Pooling.MAX, pad=1, kernel_size=3, stride=1)
+        else:
+            if nopool:
+                name = 'conv5_4'
+                net[name] = L.Convolution(net.relu5_3, num_output=512, pad=1, kernel_size=3, stride=2, **kwargs)
+            else:
+                name = 'pool5'
+                net[name] = L.Pooling(net.relu5_3, pool=P.Pooling.MAX, kernel_size=2, stride=2)
+
+        if fully_conv:
+            if dilated:
+                if reduced:
+                    net.fc6 = L.Convolution(net[name], num_output=1024, pad=6, kernel_size=3, dilation=6, **kwargs)
+                else:
+                    net.fc6 = L.Convolution(net[name], num_output=4096, pad=6, kernel_size=7, dilation=2, **kwargs)
+            else:
+                if reduced:
+                    net.fc6 = L.Convolution(net[name], num_output=1024, pad=3, kernel_size=3, dilation=3, **kwargs)
+                else:
+                    net.fc6 = L.Convolution(net[name], num_output=4096, pad=3, kernel_size=7, **kwargs)
+
             net.relu6 = L.ReLU(net.fc6, in_place=True)
-            net.drop6 = L.Dropout(net.relu6, dropout_ratio=0.5, in_place=True)
+            if dropout:
+                net.drop6 = L.Dropout(net.relu6, dropout_ratio=0.5, in_place=True)
+
             if reduced:
                 net.fc7 = L.Convolution(net.relu6, num_output=1024, kernel_size=1, **kwargs)
             else:
                 net.fc7 = L.Convolution(net.relu6, num_output=4096, kernel_size=1, **kwargs)
             net.relu7 = L.ReLU(net.fc7, in_place=True)
-            net.drop7 = L.Dropout(net.relu7, dropout_ratio=0.5, in_place=True)
+            if dropout:
+                net.drop7 = L.Dropout(net.relu7, dropout_ratio=0.5, in_place=True)
         else:
             net.fc6 = L.InnerProduct(net.pool5, num_output=4096)
             net.relu6 = L.ReLU(net.fc6, in_place=True)
-            net.drop6 = L.Dropout(net.relu6, dropout_ratio=0.5, in_place=True)
+            if dropout:
+                net.drop6 = L.Dropout(net.relu6, dropout_ratio=0.5, in_place=True)
             net.fc7 = L.InnerProduct(net.relu6, num_output=4096)
             net.relu7 = L.ReLU(net.fc7, in_place=True)
-            net.drop7 = L.Dropout(net.relu7, dropout_ratio=0.5, in_place=True)
+            if dropout:
+                net.drop7 = L.Dropout(net.relu7, dropout_ratio=0.5, in_place=True)
 
     # Update freeze layers.
     kwargs['param'] = [dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0)]
@@ -232,8 +282,10 @@ def ResNet152Body(net, from_layer, use_pool5=True):
 
 
 def CreateMultiBoxHead(net, data_layer="data", num_classes=[], from_layers=[],
-        normalizations=[], use_batchnorm=True, min_sizes=[], max_sizes=[], prior_variance = 0.1,
-        aspect_ratios=[], share_location=True, flip=True, clip=True, inter_layer_depth=0):
+        use_objectness=False, normalizations=[], use_batchnorm=True,
+        min_sizes=[], max_sizes=[], prior_variance = [0.1],
+        aspect_ratios=[], share_location=True, flip=True, clip=True,
+        inter_layer_depth=0, kernel_size=1, pad=0):
     assert num_classes, "must provide num_classes"
     assert num_classes > 0, "num_classes must be positive number"
     if normalizations:
@@ -248,6 +300,7 @@ def CreateMultiBoxHead(net, data_layer="data", num_classes=[], from_layers=[],
     priorbox_layers = []
     loc_layers = []
     conf_layers = []
+    objectness_layers = []
     for i in range(0, num):
         from_layer = from_layers[i]
 
@@ -256,14 +309,14 @@ def CreateMultiBoxHead(net, data_layer="data", num_classes=[], from_layers=[],
             if normalizations[i] != -1:
                 norm_name = "{}_norm".format(from_layer)
                 net[norm_name] = L.Normalize(net[from_layer], scale_filler=dict(type="constant", value=normalizations[i]),
-                    across_spatial=False, channel_shared=False, fix_scale=False)
+                    across_spatial=False, channel_shared=False)
                 from_layer = norm_name
 
         # Add intermediate layers.
         if inter_layer_depth > 0:
             inter_name = "{}_inter".format(from_layer)
             ConvBNLayer(net, from_layer, inter_name, use_bn=use_batchnorm, use_relu=True,
-                num_output=inter_layer_depth, kernel_size=1, pad=0, stride=1)
+                num_output=inter_layer_depth, kernel_size=3, pad=1, stride=1)
             from_layer = inter_name
 
         # Estimate number of priors per location given provided parameters.
@@ -272,7 +325,7 @@ def CreateMultiBoxHead(net, data_layer="data", num_classes=[], from_layers=[],
             aspect_ratio = aspect_ratios[i]
             if type(aspect_ratio) is not list:
                 aspect_ratio = [aspect_ratio]
-        if max_sizes:
+        if max_sizes and max_sizes[i]:
             num_priors_per_location = 2 + len(aspect_ratio)
         else:
             num_priors_per_location = 1 + len(aspect_ratio)
@@ -285,18 +338,18 @@ def CreateMultiBoxHead(net, data_layer="data", num_classes=[], from_layers=[],
         if not share_location:
             num_loc_output *= num_classes
         ConvBNLayer(net, from_layer, name, use_bn=use_batchnorm, use_relu=False,
-            num_output=num_loc_output, kernel_size=1, pad=0, stride=1)
+            num_output=num_loc_output, kernel_size=kernel_size, pad=pad, stride=1)
         permute_name = "{}_perm".format(name)
         net[permute_name] = L.Permute(net[name], order=[0, 2, 3, 1])
         flatten_name = "{}_flat".format(name)
         net[flatten_name] = L.Flatten(net[permute_name], axis=1)
         loc_layers.append(net[flatten_name])
 
-        # Create location prediction layer.
+        # Create confidence prediction layer.
         name = "{}_mbox_conf".format(from_layer)
         num_conf_output = num_priors_per_location * num_classes;
         ConvBNLayer(net, from_layer, name, use_bn=use_batchnorm, use_relu=False,
-            num_output=num_conf_output, kernel_size=1, pad=0, stride=1)
+            num_output=num_conf_output, kernel_size=kernel_size, pad=pad, stride=1)
         permute_name = "{}_perm".format(name)
         net[permute_name] = L.Permute(net[name], order=[0, 2, 3, 1])
         flatten_name = "{}_flat".format(name)
@@ -305,13 +358,33 @@ def CreateMultiBoxHead(net, data_layer="data", num_classes=[], from_layers=[],
 
         # Create prior generation layer.
         name = "{}_mbox_priorbox".format(from_layer)
-        if max_sizes:
-            net[name] = L.PriorBox(net[from_layer], net[data_layer], min_size=min_sizes[i], max_size=max_sizes[i],
-                aspect_ratio=aspect_ratio, flip=flip, clip=clip, variance=prior_variance)
+        if max_sizes and max_sizes[i]:
+            if aspect_ratio:
+                net[name] = L.PriorBox(net[from_layer], net[data_layer], min_size=min_sizes[i], max_size=max_sizes[i],
+                    aspect_ratio=aspect_ratio, flip=flip, clip=clip, variance=prior_variance)
+            else:
+                net[name] = L.PriorBox(net[from_layer], net[data_layer], min_size=min_sizes[i], max_size=max_sizes[i],
+                    clip=clip, variance=prior_variance)
         else:
-            net[name] = L.PriorBox(net[from_layer], net[data_layer], min_size=min_sizes[i],
-                aspect_ratio=aspect_ratio, flip=flip, clip=clip, variance=prior_variance)
+            if aspect_ratio:
+                net[name] = L.PriorBox(net[from_layer], net[data_layer], min_size=min_sizes[i],
+                    aspect_ratio=aspect_ratio, flip=flip, clip=clip, variance=prior_variance)
+            else:
+                net[name] = L.PriorBox(net[from_layer], net[data_layer], min_size=min_sizes[i],
+                    clip=clip, variance=prior_variance)
         priorbox_layers.append(net[name])
+
+        # Create objectness prediction layer.
+        if use_objectness:
+            name = "{}_mbox_objectness".format(from_layer)
+            num_obj_output = num_priors_per_location * 2;
+            ConvBNLayer(net, from_layer, name, use_bn=use_batchnorm, use_relu=False,
+                num_output=num_obj_output, kernel_size=kernel_size, pad=pad, stride=1)
+            permute_name = "{}_perm".format(name)
+            net[permute_name] = L.Permute(net[name], order=[0, 2, 3, 1])
+            flatten_name = "{}_flat".format(name)
+            net[flatten_name] = L.Flatten(net[permute_name], axis=1)
+            objectness_layers.append(net[flatten_name])
 
     # Concatenate priorbox, loc, and conf layers.
     mbox_layers = []
@@ -324,5 +397,9 @@ def CreateMultiBoxHead(net, data_layer="data", num_classes=[], from_layers=[],
     name = "mbox_priorbox"
     net[name] = L.Concat(*priorbox_layers, axis=2)
     mbox_layers.append(net[name])
+    if use_objectness:
+        name = "mbox_objectness"
+        net[name] = L.Concat(*objectness_layers, axis=1)
+        mbox_layers.append(net[name])
 
     return mbox_layers
