c14b17c67421b9885d958c48eb3461710ef98b02
Wei Liu <lwbiosoft@gmail.com>
2016-03-09 13:51:13 -0800

    keep label when crop datum

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -340,6 +340,7 @@ void DataTransformer<Dtype>::CropImage(const Datum& datum,
     CropImage(cv_img, bbox, &crop_img);
     // Save the image into datum.
     EncodeCVMatToDatum(crop_img, "jpg", crop_datum);
+    crop_datum->set_label(datum.label());
     return;
 #else
     LOG(FATAL) << "Encoded datum requires OpenCV; compile with USE_OPENCV.";
@@ -368,6 +369,7 @@ void DataTransformer<Dtype>::CropImage(const Datum& datum,
   crop_datum->set_channels(datum_channels);
   crop_datum->set_height(height);
   crop_datum->set_width(width);
+  crop_datum->set_label(datum.label());
   crop_datum->clear_data();
   crop_datum->clear_float_data();
   crop_datum->set_encoded(false);
