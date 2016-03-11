aa26c1baf88c160f14a8e0b7111e496a00e254ee
dongzhuoyao <929109550@qq.com>
2016-03-11 19:29:18 +0800

    mnist get data script bug fix

--- data/mnist/get_mnist.sh
+++ data/mnist/get_mnist.sh
@@ -9,7 +9,7 @@ echo "Downloading..."
 for fname in train-images-idx3-ubyte train-labels-idx1-ubyte t10k-images-idx3-ubyte t10k-labels-idx1-ubyte
 do
     if [ ! -e $fname ]; then
-        wget --no-check-certificate http://yann.lecun.com/exdb/mnist/${fname}.gz
+        wget --no-check-certificate http://yann.lecun.com/exdb/mnist/${fname}.gz -O ${fname}.gz
         gunzip ${fname}.gz
     fi
 done
