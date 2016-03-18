e08e7548d206ae6a07b0c92f9e405e33f9f43902
Dmytro Mishkin <ducha.aiki@gmail.com>
2016-03-18 16:55:39 +0200

    Fixed tabs, added in-place mode
    when variance is computed before non-linearity if in-place

--- tools/extra/LSUV.py
+++ tools/extra/LSUV.py
@@ -9,7 +9,8 @@ caffe_root_dir=os.path.dirname(os.path.realpath(__file__))
 caffe_root_dir+='/../../python'
 sys.path.insert(0, caffe_root_dir)
 import caffe
-
+# Orthonorm init code is taked from Lasagne
+# https://github.com/Lasagne/Lasagne/blob/master/lasagne/init.py
 def svd_orthonormal(shape):
     if len(shape) < 2:
         raise RuntimeError("Only shapes of length 2 or more are supported.")
@@ -27,7 +28,8 @@ if __name__ == '__main__':
     init_mode =  str(sys.argv[3])
     margin = 0.02;
     max_iter = 20;
-
+    needed_variance = 1.0
+    var_before_relu_if_inplace=True
     mode_check=False;  
     if init_mode == 'Orthonormal':
         mode_check=True
@@ -42,13 +44,14 @@ if __name__ == '__main__':
     solver = caffe.SGDSolver(solver_path)
     if os.path.isfile(init_path):
         print("Loading")
-        solver.net.copy_from(init_path)
-
-
+        try:
+            solver.net.copy_from(init_path)
+        except:
+            print('Failed to load weights from ', init_path) 
 
     for k,v in solver.net.params.iteritems():
         try:
-            print(k, v[0].data.shape,v[1].data.shape)
+            print(k, v[0].data.shape)
         except:
             print('Error with layer',k, 'skipping it')
             continue
@@ -61,17 +64,23 @@ if __name__ == '__main__':
         else:
             weights=solver.net.params[k][0].data[:]
         if 'LSUV' in init_mode:
-            solver.net.forward()
+            if var_before_relu_if_inplace:
+                solver.net.forward(end=k)
+            else:
+                solver.net.forward()
             v = solver.net.blobs[k];
             var1=np.var(v.data[:]);
             mean1 = np.mean(v.data[:]);
             print(k,'var = ', var1,'mean = ', mean1)
             sys.stdout.flush()
             iter_num = 0;
-            while (abs(1.0 - var1) > margin):
+            while (abs(needed_variance - var1) > margin):
                 weights=solver.net.params[k][0].data[:]
                 solver.net.params[k][0].data[:] = weights / sqrt(var1);
-                solver.net.forward()
+                if var_before_relu_if_inplace:
+                    solver.net.forward(end=k)
+                else:
+                    solver.net.forward()
                 v = solver.net.blobs[k];
                 var1=np.var(v.data[:]);
                 mean1 = np.mean(v.data[:]);
@@ -82,10 +91,10 @@ if __name__ == '__main__':
                     print('Could not converge in ', iter_num, ' iterations, go to next layer')
                     break; 
     print("Initialization finished!")
-    for k,v in solver.net.params.iteritems():
-        vv = solver.net.blobs[k];
+    solver.net.forward()
+    for k,v in solver.net.blobs.iteritems():
         try:
-            print(k,vv.data[:].shape, ' var = ', np.var(vv.data[:]), ' mean = ', np.mean(vv.data[:]))
+            print(k,v.data[:].shape, ' var = ', np.var(v.data[:]), ' mean = ', np.mean(v.data[:]))
         except:
             print('Cannot proceed layer',k,'skiping')
             
