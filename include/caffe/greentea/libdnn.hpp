3794cbf5c9cc6956f9db66ef510b1f97eeb970b1
fabian <naibaf710@hotmail.com>
2016-04-25 21:22:08 +0200

    LibDNN initial release.

---
+++ include/caffe/greentea/libdnn.hpp
@@ -0,0 +1,135 @@
+#ifndef CAFFE_GREENTEA_LIBDNN_HPP_
+#define CAFFE_GREENTEA_LIBDNN_HPP_
+#ifdef USE_GREENTEA
+#include <string>
+#include <vector>
+#include "caffe/greentea/greentea.hpp"
+#include "viennacl/backend/opencl.hpp"
+#include "viennacl/ocl/backend.hpp"
+#include "viennacl/ocl/context.hpp"
+#include "viennacl/ocl/device.hpp"
+#include "viennacl/ocl/platform.hpp"
+
+namespace caffe {
+
+typedef enum {
+    // Stack the batch update into one GEMM block
+    // (deterministic, 1 kernel call)
+    LIBDNN_CONVOLUTION_WG_ALGO_DIRECT        = 0,
+    // Use multiple GEMM blocks in parallel and update weights atomically
+    // (non deterministic, 1 kernel call, not supported on all devices)
+    LIBDNN_CONVOLUTION_WG_ALGO_ATOMIC        = 1,
+    // Use multiple GEMM blocks and an intermediate buffer
+    // reduce weight updates
+    // (deterministic, >= 2 kernel calls)
+    LIBDNN_CONVOLUTION_WG_ALGO_REDUCTION     = 2
+} libdnnConvolutionWeightAlgo_t;
+
+struct libdnn_config {
+  libdnn_config() :
+    in_shape(3, 1),
+    out_shape(3, 1),
+    kernel(1, 1),
+    pad(1, 0),
+    stride(1, 1),
+    dilation(1, 0)
+  {}
+  device* dev_ptr = nullptr;
+  std::vector<int_tp> in_shape;
+  std::vector<int_tp> out_shape;
+  std::vector<int_tp> kernel;
+  std::vector<int_tp> pad;
+  std::vector<int_tp> stride;
+  std::vector<int_tp> dilation;
+  int_tp group = 1;
+  bool bias_term = false;
+  bool fast_unsafe_math = false;
+  bool weights_backward = true;
+  bool bias_backward = true;
+  libdnnConvolutionWeightAlgo_t wgalgo = LIBDNN_CONVOLUTION_WG_ALGO_DIRECT;
+};
+
+
+template<typename Dtype>
+class libdnn_conv {
+ public:
+  explicit libdnn_conv(libdnn_config config);
+  void forward(cl_mem bottom_data, cl_mem weight, cl_mem bias,
+               cl_mem top_data, int_tp batch_size);
+  void backward(bool prop_down_data,
+                cl_mem top_data, cl_mem top_diff,
+                cl_mem weight, cl_mem weight_diff,
+                cl_mem bias, cl_mem bias_diff,
+                cl_mem bottom_data, cl_mem bottom_diff,
+                int_tp batch_size);
+
+ protected:
+  void generate_kernels();
+  void compile_kernel();
+  std::string generate_header();
+  std::string generate_common_defs();
+  std::string generate_fw_defs();
+  std::string generate_bw_defs();
+  std::string generate_wg_defs();
+  std::string generate_fw_kernels(std::string name);
+  std::string generate_bw_kernels(std::string name);
+  std::string generate_wg_kernels(std::string name);
+  viennacl::ocl::program compile_kernels(viennacl::ocl::context *ctx);
+  template<class T>
+  void add_def(std::stringstream& ss, const char* name, T value);  // NOLINT
+  template<class T>
+  void add_def(std::stringstream& ss, const std::string name, T value);  // NOLINT
+
+ private:
+  device* dev_ptr_;
+  viennacl::ocl::program program_;
+  std::string kernel_;
+
+  // Forward GEMM sizes
+  int_tp M_FW_;
+  int_tp MG_FW_;
+  int_tp N_FW_;
+  int_tp K_FW_;
+  int_tp KG_FW_;
+
+  // Backward GEMM sizes
+  int_tp M_BW_;
+  int_tp MG_BW_;
+  int_tp N_BW_;
+  int_tp K_BW_;
+  int_tp KG_BW_;
+
+  // Weight GEMM sizes
+  int_tp M_WG_;
+  int_tp MG_WG_;
+  int_tp N_WG_;
+  int_tp NG_WG_;
+  int_tp K_WG_;
+
+  // Convolution parameters
+  int_tp num_axes_;
+  int_tp fmaps_in_;
+  int_tp fmaps_out_;
+  int_tp group_;
+
+  std::vector<int_tp> pad_;
+  std::vector<int_tp> stride_;
+  std::vector<int_tp> dilation_;
+  std::vector<int_tp> kernel_shape_;
+  std::vector<int_tp> im_in_shape_;
+  std::vector<int_tp> im_out_shape_;
+
+  // Compile and method flags
+  bool weights_backward_;
+  bool bias_backward_;
+  bool fast_unsafe_math_;
+  bool bias_term_;
+  bool skip_range_check_;
+  Dtype bias_multiplier_;
+  libdnnConvolutionWeightAlgo_t wgalgo_;
+};
+
+}  // namespace caffe
+
+#endif  // USE_GREENTEA
+#endif /* CAFFE_GREENTEA_LIBDNN_HPP_ */
---
+++ src/caffe/layers/libdnn_conv_layer.cpp
@@ -0,0 +1,122 @@
+#ifdef USE_GREENTEA
+#include <algorithm>
+#include <vector>
+
+#include "caffe/layers/libdnn_conv_layer.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void LibDNNConvolutionLayer<Dtype>::LayerSetUp(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  ConvolutionLayer<Dtype>::LayerSetUp(bottom, top);
+  this->use_colbuffer_ = false;
+
+
+  Reshape(bottom, top);
+}
+
+template <typename Dtype>
+void LibDNNConvolutionLayer<Dtype>::Reshape(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+
+  this->use_colbuffer_ = false;
+
+  ConvolutionLayer<Dtype>::Reshape(bottom, top);
+
+  if (libdnn_.get() == nullptr) {
+    int_tp* kernel_shape_data = this->kernel_shape_.mutable_cpu_data();
+    int_tp* pad_data = this->pad_.mutable_cpu_data();
+    int_tp* stride_data = this->stride_.mutable_cpu_data();
+    int_tp* dilation_data = this->dilation_.mutable_cpu_data();
+
+    std::vector<int_tp> kernel_vec;
+    std::vector<int_tp> pad_vec;
+    std::vector<int_tp> stride_vec;
+    std::vector<int_tp> dilation_vec;
+
+    for (int_tp i = 0; i < this->num_spatial_axes_; ++i) {
+        kernel_vec.push_back(kernel_shape_data[i]);
+        pad_vec.push_back(pad_data[i]);
+        stride_vec.push_back(stride_data[i]);
+        dilation_vec.push_back(dilation_data[i]);
+    }
+
+    libdnn_config config;
+    config.dev_ptr = this->device_;
+    config.in_shape = bottom[0]->shape();
+    config.out_shape = top[0]->shape();
+    config.kernel = kernel_vec;
+    config.pad = pad_vec;
+    config.stride = stride_vec;
+    config.dilation = dilation_vec;
+    config.group = this->group_;
+    config.bias_term = this->bias_term_;
+    config.fast_unsafe_math = false;
+    config.weights_backward = this->param_propagate_down_[0];
+    config.bias_backward = this->param_propagate_down_[1];
+
+    libdnn_conv<Dtype>* libdnn = new libdnn_conv<Dtype>(config);
+
+    libdnn_.reset(libdnn);
+  }
+}
+
+template <typename Dtype>
+LibDNNConvolutionLayer<Dtype>::~LibDNNConvolutionLayer() {
+}
+
+template <typename Dtype>
+void LibDNNConvolutionLayer<Dtype>::Forward_gpu(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+
+  const Dtype* weight = this->blobs_[0]->gpu_data();
+  const Dtype* bias = nullptr;
+  if (this->bias_term_) {
+     bias = this->blobs_[1]->gpu_data();
+  }
+
+  for (int_tp i = 0; i < bottom.size(); ++i) {
+    const Dtype* bottom_data = bottom[i]->gpu_data();
+    Dtype* top_data = top[i]->mutable_gpu_data();
+    libdnn_.get()->forward((cl_mem) bottom_data, (cl_mem) weight, (cl_mem) bias,
+                           (cl_mem) top_data, bottom[i]->shape()[0]);
+  }
+}
+
+template <typename Dtype>
+void LibDNNConvolutionLayer<Dtype>::Backward_gpu(
+    const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down,
+    const vector<Blob<Dtype>*>& bottom) {
+
+  const Dtype* weight = this->blobs_[0]->gpu_data();
+  const Dtype* bias = nullptr;
+  Dtype* weight_diff = this->blobs_[0]->mutable_gpu_diff();
+  Dtype* bias_diff = nullptr;
+  if (this->bias_term_) {
+     bias = this->blobs_[1]->gpu_data();
+     bias_diff = this->blobs_[1]->mutable_gpu_diff();
+  }
+
+  for (int_tp i = 0; i < top.size(); ++i) {
+    const Dtype* top_data = top[i]->gpu_data();
+    const Dtype* top_diff = top[i]->gpu_diff();
+    const Dtype* bottom_data = bottom[i]->gpu_data();
+    Dtype* bottom_diff = bottom[i]->mutable_gpu_diff();
+    libdnn_.get()->backward(propagate_down[i],
+                            (cl_mem) top_data, (cl_mem) top_diff,
+                            (cl_mem) weight, (cl_mem) weight_diff,
+                            (cl_mem) bias, (cl_mem) bias_diff,
+                            (cl_mem) bottom_data, (cl_mem) bottom_diff,
+                          bottom[i]->shape()[0]);
+  }
+}
+
+
+
+INSTANTIATE_CLASS(LibDNNConvolutionLayer);
+
+
+}   // namespace caffe
+#endif
