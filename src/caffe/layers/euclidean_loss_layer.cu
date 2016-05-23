0dcb68ab7051d73472e29a6c580db0ee920f9d65
Christian Payer <christian.payer@icg.tugraz.at>
2016-05-23 14:08:59 +0200

    remove some calls of Blob::LegacyShape() to support 3D

--- src/caffe/layers/euclidean_loss_layer.cu
+++ src/caffe/layers/euclidean_loss_layer.cu
@@ -16,7 +16,7 @@ void EuclideanLossLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       diff_.mutable_gpu_data());
   Dtype dot;
   caffe_gpu_dot(count, diff_.gpu_data(), diff_.gpu_data(), &dot);
-  Dtype loss = dot / bottom[0]->num() / Dtype(2);
+  Dtype loss = dot / bottom[0]->shape(0) / Dtype(2);
   top[0]->mutable_cpu_data()[0] = loss;
 }
 
@@ -26,7 +26,7 @@ void EuclideanLossLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
   for (int i = 0; i < 2; ++i) {
     if (propagate_down[i]) {
       const Dtype sign = (i == 0) ? 1 : -1;
-      const Dtype alpha = sign * top[0]->cpu_diff()[0] / bottom[i]->num();
+      const Dtype alpha = sign * top[0]->cpu_diff()[0] / bottom[i]->shape(0);
       caffe_gpu_axpby(
           bottom[i]->count(),              // count
           alpha,                              // alpha
