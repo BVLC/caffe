4d60f43986bbc1c2a719f0aa2332147316c25e86
Chuck Cho <chuck.cho@dextro.co>
2016-06-02 14:35:14 -0400

    Fixing a typo

--- tools/extract_features.cpp
+++ tools/extract_features.cpp
@@ -130,7 +130,7 @@ int feature_extraction_pipeline(int argc, char** argv) {
     txns.push_back(txn);
   }
 
-  LOG(ERROR)<< "Extacting Features";
+  LOG(ERROR)<< "Extracting Features";
 
   Datum datum;
   std::vector<int> image_indices(num_features, 0);
