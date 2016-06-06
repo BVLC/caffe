8044fca614f9f6b5d797264c05ef434e13d8db39
Denis Tome <D.Tome@cs.ucl.ac.uk>
2016-06-06 18:06:51 +0100

    script to read weights size in the architecture

---
+++ models/cpm_architecture/readModelWeights.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  6 16:58:57 2016
+
+@author: denitome
+"""
+
+import caffe
+import os
+
+caffe.set_mode_cpu()
+caffe.set_device(1)
+def_file = '/home/denitome/MATLAB/convolutional-pose-machines-release/model/_trained_MPI/pose_deploy_centerMap.prototxt'
+model_file = '/home/denitome/MATLAB/convolutional-pose-machines-release/model/_trained_MPI/pose_iter_985000_addLEEDS.caffemodel'
+if ((not os.path.isfile(def_file)) or (not os.path.isfile(model_file))):
+    print 'ERROR'
+
+net1 = caffe.Net(def_file, model_file , caffe.TEST)
+layer_names1 = list(net1._layer_names)
+
+def_file = '/home/denitome/Libraries/caffe_cpm/models/cpm_architecture/prototxt/pose_deploy.prototxt'
+if (not os.path.isfile(def_file)):
+    print 'ERROR' 
+
+net2 = caffe.Net(def_file, caffe.TEST)
+layer_names2 = list(net2._layer_names)
+
+print '\n\nORIGINAL NETWORK\n'
+for i in range(1,len(layer_names1)):
+    name = layer_names1[i]
+    if not('conv' in name):
+        continue
+    layers = net1.params[layer_names1[i]][0];
+    print 'layer %s -> size: (%d, %d, %d)' % (name, layers.width, layers.height, layers.channels)
+
+print '\n\nNEW NETWORK\n'
+for i in range(1,len(layer_names2)):
+    name = layer_names2[i]
+    if not('conv' in name):
+        continue
+    layers = net2.params[layer_names2[i]][0];
+    print 'layer %s -> size: (%d, %d, %d)' % (name, layers.width, layers.height, layers.channels)
+    
+print '\n\nBOTH\n'
+for i in range(1,len(layer_names2)):
+    name = layer_names2[i]
+    if not('conv' in name):
+        continue
+    layers1 = net1.params[layer_names1[i]][0];
+    layers2 = net2.params[layer_names2[i]][0];
+    print 'layer %s -> size: (%d, %d, %d, %d) -> (%d, %d, %d, %d)' % (name, layers1.width, layers1.height, layers1.channels, layers1.num, layers2.width, layers2.height, layers2.channels, layers2.num)
