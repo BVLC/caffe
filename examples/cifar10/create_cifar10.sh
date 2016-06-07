75a5140b5e49e11ad9ab492eec79dc28d2b668a0
Nishidha Panpaliya <nishidha@us.ibm.com>
2016-06-07 08:50:18 -0500

    Corrected cifar10 examples' scripts to use correct paths

--- examples/cifar10/create_cifar10.sh
+++ examples/cifar10/create_cifar10.sh
@@ -5,15 +5,26 @@ EXAMPLE=examples/cifar10
 DATA=data/cifar10
 DBTYPE=lmdb
 
+# Check if CAFFE_ROOT is set
+if [ -z ${CAFFE_ROOT+x} ]; 
+# if unset
+then
+  EXAMPLES_BIN=./build/$EXAMPLE
+  TOOLS_BIN=./build/tools
+else
+  EXAMPLES_BIN=$CAFFE_BIN
+  TOOLS_BIN=$CAFFE_BIN
+fi  
+
 echo "Creating $DBTYPE..."
 
 rm -rf $EXAMPLE/cifar10_train_$DBTYPE $EXAMPLE/cifar10_test_$DBTYPE
 
-./build/examples/cifar10/convert_cifar_data.bin $DATA $EXAMPLE $DBTYPE
+$EXAMPLES_BIN/convert_cifar_data.bin $DATA $EXAMPLE $DBTYPE
 
 echo "Computing image mean..."
 
-./build/tools/compute_image_mean -backend=$DBTYPE \
+$TOOLS_BIN/compute_image_mean -backend=$DBTYPE \
   $EXAMPLE/cifar10_train_$DBTYPE $EXAMPLE/mean.binaryproto
 
 echo "Done."
--- examples/cifar10/train_quick.sh
+++ examples/cifar10/train_quick.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
+# Check if CAFFE_BIN is unset
+if [ -z ${CAFFE_BIN+x} ];
+then
 TOOLS=./build/tools
+else
+TOOLS=$CAFFE_BIN
+fi
 
 $TOOLS/caffe train \
   --solver=examples/cifar10/cifar10_quick_solver.prototxt
