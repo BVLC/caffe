6c900bf723e4ea50a9adf26ab7719b2439469f27
Bartosz Kalinczuk <bartosz.kalinczuk@intel.com>
2016-06-27 13:48:44 +0200

    multinode: removed unused or obsolete code

--- include/caffe/multinode/multinode.hpp
+++ include/caffe/multinode/multinode.hpp
@@ -4,11 +4,7 @@
 #include "caffe/internode/configuration.hpp"
 #include "caffe/internode/mpiutil.hpp"
 #include "caffe/multinode/DataServer.hpp"
-#include "caffe/multinode/ModelServer.hpp"
-#include "caffe/multinode/Relay.hpp"
 #include "caffe/multinode/SynchronousNode.hpp"
-#include "caffe/multinode/SynchronousParamClient.hpp"
-#include "caffe/multinode/SynchronousParamServer.hpp"
 #include "caffe/MultiSolver.hpp"
 
 #endif  // CAFFE_MULTINODE_HPP_
