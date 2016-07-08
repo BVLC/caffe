fa7bab7e71e0d145a033d69e9c67d0adf5166126
Daylen Yang <services@daylenyang.com>
2016-07-07 23:54:18 -0700

    Add BDD license to compact bilinear layers

--- include/caffe/layers/compact_bilinear_layer.hpp
+++ include/caffe/layers/compact_bilinear_layer.hpp
@@ -1,3 +1,29 @@
+/*
+Copyright (c) 2016. The Regents of the University of California (Regents). All
+Rights Reserved. Permission to use, copy, modify, and distribute this software
+and its documentation for educational, research, not-for-profit, and commercial
+purposes (such rights not subject to transfer), without fee, and without a
+signed licensing agreement, is hereby granted, provided that the above copyright
+notice, this paragraph and the following two paragraphs appear in all copies,
+modifications, and distributions. Contact The Office of Technology Licensing,
+UC Berkeley, 2150 Shattuck Avenue, Suite 510, Berkeley, CA 94720-1620,
+(510) 643-7201, for commercial licensing opportunities.
+
+Yang Gao, University of California, Berkeley.
+
+
+IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE
+USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+
+REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS
+PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+*/
+
 #ifndef CAFFE_COMPACT_BILINEAR_LAYER_HPP_
 #define CAFFE_COMPACT_BILINEAR_LAYER_HPP_
 
