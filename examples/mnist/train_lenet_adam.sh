93d321227f0681165b126d9ca47b211f5d2c1909
Luke Yeager <luke.yeager@gmail.com>
2016-07-13 15:58:29 -0700

    Add "set -e" and $@ to example scripts

--- examples/mnist/train_lenet_adam.sh
+++ examples/mnist/train_lenet_adam.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
+set -e
 
-./build/tools/caffe train --solver=examples/mnist/lenet_solver_adam.prototxt
+./build/tools/caffe train --solver=examples/mnist/lenet_solver_adam.prototxt $@
