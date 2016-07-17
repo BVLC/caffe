227f48bb56cbdaaec784036cbd86b426d52eeedb
Wei Liu <lwbiosoft@gmail.com>
2016-07-17 07:09:21 -0700

    change prior size in scripts

--- examples/ssd/ssd_pascal_webcam.py
+++ examples/ssd/ssd_pascal_webcam.py
@@ -200,8 +200,17 @@ min_dim = 300
 # conv8_2 ==> 3 x 3
 # conv9_2 ==> 1 x 1
 mbox_source_layers = ['conv4_3', 'fc7', 'conv6_2', 'conv7_2', 'conv8_2', 'conv9_2']
-min_sizes = [32, 64, 112, 160, 216, 272]
-max_sizes = [64, 112, 160, 216, 272, 320]
+# in percent %
+min_ratio = 20
+max_ratio = 90
+step = int(math.floor((max_ratio - min_ratio) / (len(mbox_source_layers) - 2)))
+min_sizes = []
+max_sizes = []
+for ratio in xrange(min_ratio, max_ratio + 1, step):
+  min_sizes.append(min_dim * ratio / 100.)
+  max_sizes.append(min_dim * (ratio + step) / 100.)
+min_sizes = [min_dim * 10 / 100.] + min_sizes
+max_sizes = [min_dim * 20 / 100.] + max_sizes
 steps = [8, 16, 32, 64, 100, 300]
 aspect_ratios = [[2], [2, 3], [2, 3], [2, 3], [2], [2]]
 # L2 normalize conv4_3.
