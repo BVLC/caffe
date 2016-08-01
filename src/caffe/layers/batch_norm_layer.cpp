66d73988d047d496d5a9f0d0abf5af6bf824ea97
Oh233 <macshqi@gmail.com>
2016-08-01 19:17:38 +0800

    Fix test issues

--- src/caffe/layers/batch_norm_layer.cpp
+++ src/caffe/layers/batch_norm_layer.cpp
@@ -48,9 +48,7 @@ void BatchNormLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   mean_.Reshape(sz);
   variance_.Reshape(sz);
   temp_.ReshapeLike(*bottom[0]);
-  if (use_global_stats_) {
-    x_norm_.ReshapeLike(*bottom[0]);
-  }
+  x_norm_.ReshapeLike(*bottom[0]);
   sz[0]=bottom[0]->shape(0);
   batch_sum_multiplier_.Reshape(sz);
 
