7e695c8ed12e5087f27e939844ddeb0493c1f017
max argus <argus.max@gmail.com>
2016-08-05 23:56:30 +0000

    [halide] made HalideLayer call wrapper instead of halide objects directly.

--- include/caffe/layers/halide_layer.hpp
+++ include/caffe/layers/halide_layer.hpp
@@ -6,31 +6,40 @@
 #include <vector>
 
 #include "caffe/layer.hpp"
-#include "caffe/layers/halide_layer.hpp"
 #include "caffe/util/halide.hpp"
 
 
 
 namespace caffe {
 
-/*
-class ExternClass {
-protected:
-    double side_length_;
-public:
-    HalideExtern() {}
-    ~HalideExtern() {}
+template< typename Dtype>
+class DLLInterface {
+ public:
+  virtual ~DLLInterface() {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) = 0;
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) = 0;
 
-    //void set_side_length(double side_length) {
-    //    side_length_ = side_length;
-    //}
-    virtual void Forward_gpu();
-    virtual void Backward_gpu();
+  virtual inline int ExactNumBottomBlobs() = 0;
+  virtual inline int ExactNumTopBlobs() = 0;
+
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) = 0;
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) = 0;
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down,
+      const vector<Blob<Dtype>*>& bottom) = 0;
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down,
+      const vector<Blob<Dtype>*>& bottom) = 0;
 };
-// the types of the class factories
-typedef HalideExtern* create_t();
-typedef void destroy_t(HalideExtern*);
-*/
+template class DLLInterface<float>;
+template class DLLInterface<double>;
+
+
+
 
 template <typename Dtype>
 class HalideLayer : public Layer<Dtype> {
@@ -38,16 +47,9 @@ class HalideLayer : public Layer<Dtype> {
   explicit HalideLayer(const LayerParameter& param)
       : Layer<Dtype>(param),
         param_(param.halide_param()),
-        hv_Forward_cpu(NULL),
-        hv_Backward_cpu(NULL),
-        hv_Forward_gpu(NULL),
-        hv_Backward_gpu(NULL)
-      /*,
         create_ext(NULL),
         destroy_ext(NULL),
-        inst_ext(NULL)
-     */
-     {}
+        inst_ext(NULL) {}
   virtual ~HalideLayer();
 
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
@@ -70,29 +72,24 @@ class HalideLayer : public Layer<Dtype> {
 
   HalideParameter param_;
 
-  std::vector< std::pair<int, int> > caffe_from_halide_;
 
  private:
+  void* halide_lib_handle;
+
+
+  // the types of the class factories
+  typedef DLLInterface<Dtype>* create_t();
+  typedef void destroy_t(DLLInterface<Dtype>*);
+
   // Creator and destructors of external instance
-  /*
   create_t* create_ext;
   destroy_t* destroy_ext;
-  HalideExtern* inst_ext;
-  */
-
-  typedef void (*hvfunc_t)(void* argv);
-
-  hvfunc_t hv_Forward_cpu;
-  hvfunc_t hv_Backward_cpu;
-  hvfunc_t hv_Forward_gpu;
-  hvfunc_t hv_Backward_gpu;
+  DLLInterface<Dtype>* inst_ext;
 
   buffer_t bottom_buf_;
   buffer_t bottom_diff_buf_;
   buffer_t top_buf_;
   buffer_t top_diff_buf_;
-
-  void* halide_lib_handle;
 };
 
 }  // namespace caffe
--- src/caffe/layers/halide_layer.cpp
+++ src/caffe/layers/halide_layer.cpp
@@ -17,116 +17,36 @@ namespace caffe {
 template <typename Dtype>
 void HalideLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-  CHECK_GT(param_.library_size(), 0) << "Must have at least one library";
-  string library(param_.library(0));
+  string library(param_.library());
 
   LOG(INFO) << library;
 
   halide_lib_handle = dlopen(library.c_str(), RTLD_LAZY);
   if (halide_lib_handle == NULL) {
     LOG(INFO) << "Failed to open dynamic library: " << library;
-    LOG(INFO) << "with error:"<< dlerror();
+    LOG(INFO) << "with error: "<< dlerror();
+    LOG(INFO) << "ProTip: Check that libHalide.so file is in LD_LIBRARY_PATH";
+    LOG(FATAL) << "Halide layer failed to open dynamic library";
   }
   // reset errors
   dlerror();
   const char* dlsym_error;
 
-  // Get the metadata
-
-  // Get the halide metadata names
-  vector<string> halide_names;
-  vector<int> halide_kinds;
-  halide_filter_metadata_t* md = reinterpret_cast<halide_filter_metadata_t*>(
-        dlsym(halide_lib_handle, "plip_metadata"));
-  CHECK_DL;
-
-  int num_arguments = md->num_arguments;
-  for (int i = 0; i < num_arguments; i++) {
-    halide_names.push_back(md->arguments[i].name);
-    halide_kinds.push_back(md->arguments[i].kind);
-  }
-
-  // Go over halide names
-  caffe_from_halide_.clear();
-  for (int i = 0; i < halide_names.size(); i++) {
-    int kind = halide_kinds[i];
-
-    if (kind == halide_argument_kind_input_buffer) {
-      int bottom_size = bottom.size();
-      if (bottom_size == 1) {
-        // Ignore bottom names
-        caffe_from_halide_.push_back(std::pair<int, int>(0, 0));
-      } else {
-        // Look through bottom names
-        CHECK_EQ(param_.bottom_blob_names_size(), bottom_size);
-
-        string halide_name = halide_names[i];
-        bool found = false;
-        for (int j = 0; j < bottom_size; j++) {
-          if (halide_name == param_.bottom_blob_names(j)) {
-            caffe_from_halide_.push_back(std::pair<int, int>(0, j));
-            found = true;
-            break;
-          }
-        }
-        CHECK(found) << "Halide input param " << halide_name
-                     << " not found in proto bottom_blob_names";
-      }
-    } else if (kind == halide_argument_kind_output_buffer) {
-      int top_shape = top.size();
-      if (top_shape == 1) {
-        // Ignore bottom names
-        caffe_from_halide_.push_back(std::pair<int, int>(1, 0));
-      } else {
-        // Look through bottom names
-        CHECK_EQ(param_.top_blob_names_size(), top_shape);
-
-        string halide_name = halide_names[i];
-        bool found = false;
-        for (int j = 0; j < top_shape; j++) {
-          if (halide_name == param_.top_blob_names(j)) {
-            caffe_from_halide_.push_back(std::pair<int, int>(1, j));
-            found = true;
-            break;
-          }
-        }
-        CHECK(found) << "Halide output param " << halide_name
-                     << " not found in proto top_blob_names";
-      }
-    } else {
-        LOG(FATAL) << "Invalid halid argument kind only accepting"
-                   << "halide_argument_kind_input_buffer and"
-                   << "halide_argument_kind_output_buffer";
-    }
-  }
-
-  // Go over functions
-  // int num_functions = param_.function_name_size();
-  // CHECK_EQ(param_.library_size(), num_functions )
-  //    << "function_name, library size missmatch";
-
-  // CHECK_EQ(param_.function_type_size(), num_functions )
-  //    << "function_name, function_type missmatch";
-
-  hv_Forward_gpu = (hvfunc_t) dlsym(halide_lib_handle, "plip_argv");
-  CHECK_DL
-
-  /*
   // load the symbols
-  create_ext = (create_t*) dlsym(halide_lib_handle, "create");
+  create_ext = reinterpret_cast<create_t*>( dlsym(halide_lib_handle, "create"));
   CHECK_DL
-  destroy_ext = (destroy_t*) dlsym(halide_lib_handle, "destroy");
+
+  destroy_ext = reinterpret_cast<destroy_t*>( dlsym(halide_lib_handle, "destroy") );
   CHECK_DL
 
   // create an instance of the class
   inst_ext = create_ext();
-  */
 }
 
 template <typename Dtype>
 HalideLayer<Dtype>::~HalideLayer() {
   // destroy the class
-  // destroy_ext(inst_ext);
+  destroy_ext(inst_ext);
 
   // unload the halide library
   dlclose(halide_lib_handle);
@@ -135,11 +55,22 @@ HalideLayer<Dtype>::~HalideLayer() {
 template <typename Dtype>
 void HalideLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-  // XXX update this for stride > 1
-  // XXX shape checks
-  CHECK_EQ(bottom[0]->shape(0), bottom[0]->shape(1));
-  top[0]->ReshapeLike(*bottom[0]);
+  inst_ext->Reshape(bottom, top);
 }
+
+template <typename Dtype>
+void HalideLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  // Now for what we have all been waiting for.
+  inst_ext->Forward_gpu(bottom, top);
+}
+
+template <typename Dtype>
+void HalideLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  inst_ext->Backward_gpu(bottom, propagate_down, top);
+}
+
 
 template <typename Dtype>
 void HalideLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
@@ -153,56 +84,6 @@ void HalideLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
   NOT_IMPLEMENTED;
 }
 
-
-template <typename Dtype>
-void HalideLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
-  std::vector< buffer_t > halide_buffers;
-
-  int n = caffe_from_halide_.size();
-  void* args[n];
-
-  for (int i = 0; i < n; i++) {
-    int blob_source = caffe_from_halide_[i].first;
-    int blob_index = caffe_from_halide_[i].second;
-    if (blob_source == 0) {
-      halide_buffers.push_back(HalideWrapBlob(*bottom[blob_index]));
-      args[i] = &(halide_buffers.back());
-    } else if (blob_source == 1) {
-      halide_buffers.push_back(HalideWrapBlob(*top[blob_index]));
-      args[i] = &(halide_buffers.back());
-    } else {
-      LOG(FATAL) << "Wrong blob_source value";
-    }
-  }
-
-  // Now for what we have all been waiting for.
-  hv_Forward_gpu(args);
-
-  for (int i = 0; i < n; i++) {
-    int blob_source = caffe_from_halide_[i].first;
-    int blob_index = caffe_from_halide_[i].second;
-    if (blob_source == 0) {
-      HalideSyncBlob(
-            *(reinterpret_cast<buffer_t*>(args[i])), bottom[blob_index]);
-    } else if (blob_source == 1) {
-      HalideSyncBlob(
-            *(reinterpret_cast<buffer_t*>(args[i])), top[blob_index]);
-    } else {
-      LOG(FATAL) << "Wrong blob_source value";
-    }
-  }
-}
-
-template <typename Dtype>
-void HalideLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-  bottom_diff_buf_ = HalideWrapBlob(*bottom[0], false);
-  top_diff_buf_ = HalideWrapBlob(*top[0], false);
-  // plip(&top_diff_buf_, &bottom_diff_buf_);
-  HalideSyncBlob(top_diff_buf_, top[0], false);
-  HalideSyncBlob(bottom_diff_buf_, bottom[0], false);
-}
 
 #ifdef CPU_ONLY
 // STUB_GPU(HalideLayer);
