d9f9daa192383c687f1dd9f5b2120860cdba7c0b
Que, Kevin <kevin.que@intel.com>
2016-08-18 06:25:26 +0800

    fix lint error in async_reader

--- include/caffe/multi_node/async_reader.hpp
+++ include/caffe/multi_node/async_reader.hpp
@@ -12,19 +12,19 @@
 #include "caffe/util/blocking_queue.hpp"
 #include "caffe/util/db.hpp"
 
+#include "boost/thread/mutex.hpp"
 #include "boost/thread/once.hpp"
 #include "boost/thread/thread.hpp"
-#include "boost/thread/mutex.hpp"
 
 
 namespace caffe {
 
-class ReaderThread : public InternalThread 
-{
-public:
-  explicit ReaderThread(const LayerParameter& param, BlockingQueue<Datum*> *free, BlockingQueue<Datum*> * full) 
-    : param_(param)
-  {
+class ReaderThread : public InternalThread {
+ public:
+  explicit ReaderThread(const LayerParameter& param,
+                        BlockingQueue<Datum*> *free,
+                        BlockingQueue<Datum*> * full)
+      : param_(param) {
     free_ = free;
     full_ = full;
 
@@ -35,10 +35,10 @@ public:
     StopInternalThread();
   }
 
-protected:
+ protected:
   void InternalThreadEntry();
-  
-protected:
+
+ protected:
   LayerParameter param_;
   BlockingQueue<Datum*> *free_;
   BlockingQueue<Datum*> * full_;
@@ -47,11 +47,10 @@ DISABLE_COPY_AND_ASSIGN(ReaderThread);
 };
 
 class AsyncReader {
-
-public:
+ public:
   virtual ~AsyncReader() {
     Datum* datum = NULL;
-    
+
     for (int i = 0; i < free_queue_.size(); i++) {
       while (free_queue_[i]->try_pop(&datum)) {
         delete datum;
@@ -65,57 +64,51 @@ public:
     }
   }
 
-  static AsyncReader* Instance()
-  {
+  static AsyncReader* Instance() {
     boost::call_once(flag_once_, InitReader);
-    
+
     return instance_;
   }
 
 
-public:
+ public:
   int RegisterLayer(const LayerParameter& param);
   BlockingQueue<Datum*> *GetFree(int i) { return free_queue_[i]; }
   BlockingQueue<Datum*> *GetFull(int i) { return full_queue_[i]; }
 
-protected:
+ protected:
   int AddBlockQueue(const LayerParameter& param);
 
-protected:
-  static void InitReader()
-  {
+ protected:
+  static void InitReader() {
     instance_ = new AsyncReader();
   }
-  
-  static inline string source_key(const LayerParameter& param) 
-  {
+
+  static inline string source_key(const LayerParameter& param) {
     return param.name() + ":" + param.data_param().source();
   }
 
-protected:
+ protected:
   vector<BlockingQueue<Datum*> *> free_queue_;
   vector<BlockingQueue<Datum*> *> full_queue_;
-  
-  //for the reader threads
+
+  // for the reader threads
   vector<shared_ptr<ReaderThread> > thread_arr_;
 
   map<const string, int> index_map_;
   boost::mutex register_mutex_;
 
-protected:
+ protected:
   static AsyncReader *instance_;
   static boost::once_flag flag_once_;
 
-
-private:
+ private:
   AsyncReader() {}
 
 DISABLE_COPY_AND_ASSIGN(AsyncReader);
-
 };
 
-
-} //namespace caffe
+}  // end namespace caffe
 
 
 #endif
--- src/caffe/multi_node/async_reader.cpp
+++ src/caffe/multi_node/async_reader.cpp
@@ -1,4 +1,6 @@
 
+#include <map>
+#include <string>
 
 #include "caffe/multi_node/async_reader.hpp"
 
@@ -8,13 +10,12 @@ boost::once_flag AsyncReader::flag_once_;
 
 AsyncReader *AsyncReader::instance_ = NULL;
 
-int AsyncReader::RegisterLayer(const LayerParameter& param)
-{
+int AsyncReader::RegisterLayer(const LayerParameter& param) {
   boost::mutex::scoped_lock lock(register_mutex_);
-  
+
   string key = source_key(param);
   map<const string, int>::iterator iter = index_map_.find(key);
-  
+
   int index = -1;
 
   if (iter == index_map_.end()) {
@@ -23,11 +24,11 @@ int AsyncReader::RegisterLayer(const LayerParameter& param)
   } else {
     index = iter->second;
   }
-  
+
   BlockingQueue<Datum*> *p = free_queue_[index];
   int sz = param.data_param().prefetch() * param.data_param().batch_size();
 
-  //increase the buffer queue
+  // increase the buffer queue
   for (int i = 0; i < sz; i++) {
     p->push(new Datum());
   }
@@ -36,27 +37,26 @@ int AsyncReader::RegisterLayer(const LayerParameter& param)
 }
 
 
-int AsyncReader::AddBlockQueue(const LayerParameter& param)
-{
+int AsyncReader::AddBlockQueue(const LayerParameter& param) {
   BlockingQueue<Datum*> *p = new BlockingQueue<Datum*>();
 
   free_queue_.push_back(p);
   full_queue_.push_back(new BlockingQueue<Datum*>());
 
   CHECK_EQ(free_queue_.size(), full_queue_.size());
-  
+
   int index = free_queue_.size() - 1;
 
-  //start a new reader thread
-  thread_arr_.push_back(shared_ptr<ReaderThread>(new ReaderThread( param,
-                                      free_queue_[index], full_queue_[index] )));
+  // start a new reader thread
+  thread_arr_.push_back(shared_ptr<ReaderThread>(new ReaderThread(param,
+                                                        free_queue_[index],
+                                                        full_queue_[index])));
 
   return index;
 }
 
 
-void ReaderThread::InternalThreadEntry()
-{
+void ReaderThread::InternalThreadEntry() {
   shared_ptr<db::DB> db(db::GetDB(param_.data_param().backend()));
   db->Open(param_.data_param().source(), db::READ);
   shared_ptr<db::Cursor> cursor(db->NewCursor());
@@ -75,6 +75,6 @@ void ReaderThread::InternalThreadEntry()
   }
 }
 
-}
+}  // end namespace caffe
 
 
