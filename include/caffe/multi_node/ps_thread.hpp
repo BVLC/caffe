72c3470cb46fdcdb0da8b8e4cb788285ab69b43c
Que, Kevin <kevin.que@intel.com>
2016-08-18 08:43:47 +0800

    remove lint error in ps_thread

--- include/caffe/multi_node/ps_thread.hpp
+++ include/caffe/multi_node/ps_thread.hpp
@@ -3,9 +3,14 @@
 #ifndef MULTI_NODE_PS_THREAD_H_
 #define MULTI_NODE_PS_THREAD_H_
 
+#include <map>
+#include <string>
+#include <vector>
+
+#include "caffe/multi_node/node_env.hpp"
 #include "caffe/multi_node/worker_thread.hpp"
 #include "caffe/sgd_solvers.hpp"
-#include "caffe/multi_node/node_env.hpp"
+
 
 namespace caffe {
 /**
@@ -14,9 +19,8 @@ namespace caffe {
 * and update parameters using SGD solver
 */
 template <typename Dtype>
-class PSThread : public WorkerThread<Dtype>
-{
-public:
+class PSThread : public WorkerThread<Dtype> {
+ public:
   PSThread() {
     ps_solver_ = NULL;
     iter_ = 0;
@@ -27,7 +31,7 @@ public:
 
   virtual void Run();
 
-protected:
+ protected:
   void SendUpdates(int layer_id);
 
   int GetBatchSize(shared_ptr<Net<Dtype> > net) {
@@ -36,7 +40,7 @@ protected:
 
     return out_blobs[0]->shape(0);
   }
-  
+
   /// update a layer
   void UpdateLayer(int layer_id);
 
@@ -46,32 +50,36 @@ protected:
   /// register a new client to PS
   void RegisterNode(shared_ptr<Msg> m);
 
-  /// BSP stype, return true if we are ready to broadcast the parameter to clients
+  /// BSP stype
+  // return true if we are ready to broadcast the parameter to clients
   void UpdateParam(shared_ptr<Msg> m);
 
-  void SendParam(shared_ptr<Net<Dtype> > net, const vector<string>& layer_names, int dst, int clock);
+  void SendParam(shared_ptr<Net<Dtype> > net,
+                 const vector<string>& layer_names,
+                 int dst, int clock);
 
-  virtual Solver<Dtype> *CreateSolver(const Solver<Dtype> *root_solver, const SolverParameter& solver_param) {
+  virtual Solver<Dtype> *CreateSolver(const Solver<Dtype> *root_solver,
+                                      const SolverParameter& solver_param) {
     Caffe::set_root_solver(false);
     SGDSolver<Dtype> *ps_root = (SGDSolver<Dtype> *)root_solver;
 
     return new SGDSolver<Dtype>(solver_param, ps_root);
   }
 
-protected:
+ protected:
   SGDSolver<Dtype> *ps_solver_;
-  
+
   int iter_;
 
   // map from client id to its array index
   map<int, int> client_idx_map_;
 
-  // zmq id of each client 
+  // zmq id of each client
   vector<int> client_ids_;
 
   // store the messages from clients
   vector<vector<shared_ptr<Msg> > > client_msgs_;
-  
+
   // number of gradients updated
   int updated_layers_;
 
@@ -84,12 +92,12 @@ protected:
   // allowed staleness of PS
   // -1 means doesn't check staleness
   int staleness_;
-  
+
   // the number of conv. workers
   int num_workers_;
 };
 
-} //end caffe
+}  // end namespace caffe
 
 #endif
 
--- src/caffe/multi_node/ps_thread.cpp
+++ src/caffe/multi_node/ps_thread.cpp
@@ -1,15 +1,20 @@
 
-#include "caffe/multi_node/ps_thread.hpp"
-#include "caffe/multi_node/param_helper.hpp"
 #include <boost/make_shared.hpp>
+
+#include <algorithm>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "caffe/multi_node/param_helper.hpp"
+#include "caffe/multi_node/ps_thread.hpp"
 
 namespace caffe {
 
 template <typename Dtype>
-void PSThread<Dtype>::UpdateParam(shared_ptr<Msg> m)
-{
+void PSThread<Dtype>::UpdateParam(shared_ptr<Msg> m) {
   map<int, int>::iterator map_iter = client_idx_map_.find(m->src());
-  
+
   if (map_iter == client_idx_map_.end()) {
     LOG(WARNING) << "update from unregistered node id: " << m->src();
     return;
@@ -25,17 +30,16 @@ void PSThread<Dtype>::UpdateParam(shared_ptr<Msg> m)
   if ((msg_clocks_[layer_id][client_idx] + 1) != m->clock()) {
     LOG(INFO) << "unmatched clock between ps and client " << m->src();
   }
-  
+
   // update clock
   msg_clocks_[layer_id][client_idx] = m->clock();
   SendUpdates(layer_id);
 }
 
-
 template <typename Dtype>
-void PSThread<Dtype>::SendParam(shared_ptr<Net<Dtype> > net, const vector<string>& layer_names,
-                                int dst, int clock)
-{
+void PSThread<Dtype>::SendParam(shared_ptr<Net<Dtype> > net,
+                                const vector<string>& layer_names,
+                                int dst, int clock) {
   shared_ptr<Msg> r(new Msg());
   r->set_type(PUT_PARAM);
   r->set_dst(dst);
@@ -44,13 +48,12 @@ void PSThread<Dtype>::SendParam(shared_ptr<Net<Dtype> > net, const vector<string
 
   // shared_ptr<Net<Dtype> > net = ps_solver_->net();
   ParamHelper<Dtype>::CopyParamDataToMsg(net, layer_names, r);
-  
+
   this->SendMsg(r);
 }
 
 template <typename Dtype>
-void PSThread<Dtype>::UpdateLayer(int layer_id)
-{
+void PSThread<Dtype>::UpdateLayer(int layer_id) {
   CHECK(this->IsLearnable(layer_id));
 
   const vector<int>& learnable_indices = this->GetLearnableIndices(layer_id);
@@ -60,8 +63,7 @@ void PSThread<Dtype>::UpdateLayer(int layer_id)
 }
 
 template <typename Dtype>
-void PSThread<Dtype>::BroadcastLayer(int layer_id)
-{
+void PSThread<Dtype>::BroadcastLayer(int layer_id) {
   vector<string> layer_names;
   layer_names.push_back(ps_solver_->net()->layer_names()[layer_id]);
 
@@ -71,13 +73,12 @@ void PSThread<Dtype>::BroadcastLayer(int layer_id)
 }
 
 template <typename Dtype>
-void PSThread<Dtype>::SendUpdates(int layer_id)
-{
+void PSThread<Dtype>::SendUpdates(int layer_id) {
   CHECK_EQ(staleness_, 0) << "only support sync mode";
-  
+
   const vector<int>& clock_vec = msg_clocks_[layer_id];
   int clock_bound = *std::min_element(clock_vec.begin(), clock_vec.end());
-  
+
   vector<shared_ptr<Msg> > *pmsg_vec = &client_msgs_[layer_id];
   int num_updates = 0;
 
@@ -89,14 +90,16 @@ void PSThread<Dtype>::SendUpdates(int layer_id)
       num_updates++;
     }
   }
-  
+
   if (num_updates > 0) {
-    ParamHelper<Dtype>::ScalDiff(ps_solver_->net(), (Dtype)(1.0 / (Dtype)num_updates), layer_id);
+    ParamHelper<Dtype>::ScalDiff(ps_solver_->net(),
+                                 (Dtype)(1.0 / (Dtype)num_updates),
+                                 layer_id);
     UpdateLayer(layer_id);
     BroadcastLayer(layer_id);
     updated_layers_++;
   }
-  
+
   if (updated_layers_ >= num_learnable_layers_) {
     #if 0
     LOG(INFO) << "iter: " << iter_;
@@ -113,10 +116,9 @@ void PSThread<Dtype>::SendUpdates(int layer_id)
 }
 
 template <typename Dtype>
-void PSThread<Dtype>::RegisterNode(shared_ptr<Msg> m)
-{
+void PSThread<Dtype>::RegisterNode(shared_ptr<Msg> m) {
   LOG(INFO) << "registering node: " << m->src();
-  
+
   int client_idx = client_ids_.size();
 
   int node_id = m->src();
@@ -124,19 +126,19 @@ void PSThread<Dtype>::RegisterNode(shared_ptr<Msg> m)
   client_idx_map_[node_id] = client_idx;
 
   // wait for all the conv. clients
-  LOG(INFO) << "total workers: " << num_workers_ << ", current workers: " << client_ids_.size();
+  LOG(INFO) << "total workers: " << num_workers_
+            << ", current workers: " << client_ids_.size();
   if (client_ids_.size() >= num_workers_) {
     shared_ptr<Net<Dtype> > ps_net = ps_solver_->net();
     for (int i = 0; i < client_ids_.size(); i++) {
-      // send the expected clock to clients 
+      // send the expected clock to clients
       SendParam(ps_net, ps_net->layer_names(), client_ids_[i], iter_);
     }
   }
 }
 
 template <typename Dtype>
-void PSThread<Dtype>::Run()
-{
+void PSThread<Dtype>::Run() {
   Caffe::set_root_solver(true);
   // use root solver as ps solver
   ps_solver_ = (SGDSolver<Dtype> *)NodeEnv::Instance()->GetRootSolver();
@@ -168,14 +170,15 @@ void PSThread<Dtype>::Run()
     } else if (m->type() == GET_PARAM) {
       SendParam(ps_net, ps_net->layer_names(), m->src(), iter_);
     } else {
-      LOG(ERROR) << "Cannot deal with message type: " << m->type() 
-        << " from: " << m->src();
+      LOG(ERROR) << "Cannot deal with message type: " << m->type()
+                 << " from: " << m->src();
     }
   }
 }
 
 INSTANTIATE_CLASS(PSThread);
-} //end caffe
+
+}  // end namespace caffe
 
 
 
