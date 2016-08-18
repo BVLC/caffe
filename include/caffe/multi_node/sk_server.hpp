19d085d55b36450f89cc47109d6e8a6e6ee7b454
Que, Kevin <kevin.que@intel.com>
2016-08-19 02:28:11 +0800

    sk server lint

--- include/caffe/multi_node/sk_server.hpp
+++ include/caffe/multi_node/sk_server.hpp
@@ -3,33 +3,34 @@
 #ifndef MULTI_NODE_SK_SERVER_H
 #define MULTI_NODE_SK_SERVER_H
 
+#include <string>
+
 #include "caffe/multi_node/msg.hpp"
 #include "caffe/multi_node/sk_sock.hpp"
 
 namespace caffe {
 /*
-*SkServer wrapps ZMQ router socket
-*ZMQ attaches an ID in a message sent by a dealer socket
+* SkServer wrapps ZMQ router socket
+* ZMQ attaches an ID in a message sent by a dealer socket
 * to router socket, we intentionally remove the header
 * and hide the details to high lever classes
 */
 class SkServer : public SkSock {
-
-public:
-  SkServer(); 
+ public:
+  SkServer();
 
   virtual ~SkServer();
 
   virtual int Connect(string addr);
-  
+
   /// removes the ID message added by ZMQ layer
   virtual shared_ptr<Msg> RecvMsg(bool blocked);
-  
+
   /// attaches an ID message for ZMQ
   virtual int SendMsg(shared_ptr<Msg> msg);
 };
 
-} //end caffe
+}  // end namespace caffe
 
 #endif
 
--- src/caffe/multi_node/sk_server.cpp
+++ src/caffe/multi_node/sk_server.cpp
@@ -1,31 +1,24 @@
 
+#include <string>
+
 #include "caffe/multi_node/sk_server.hpp"
 
 namespace caffe {
 
-//TODO: remove the default ID
+// TODO: remove the default ID
 SkServer::SkServer()
-  : SkSock(ZMQ_ROUTER, REQ_SERVER_ID)
-{
-
-}
+  : SkSock(ZMQ_ROUTER, REQ_SERVER_ID) { }
 
 
-SkServer::~SkServer()
-{
-
-}
+SkServer::~SkServer() { }
 
 
-int SkServer::Connect(string addr)
-{
+int SkServer::Connect(string addr) {
   LOG(ERROR) << "Cannot connect to " << addr << " in router";
   exit(-1);
 }
 
-
-shared_ptr<Msg> SkServer::RecvMsg(bool blocked)
-{
+shared_ptr<Msg> SkServer::RecvMsg(bool blocked) {
   int src = 0;
   int r = 0;
 
@@ -33,44 +26,44 @@ shared_ptr<Msg> SkServer::RecvMsg(bool blocked)
 
   zmq_msg_t zmsg;
   zmq_msg_init(&zmsg);
-  
-  if ( blocked ) {
-    r = zmq_msg_recv (&zmsg, sock_, 0);
+
+  if (blocked) {
+    r = zmq_msg_recv(&zmsg, sock_, 0);
   } else {
-    r = zmq_msg_recv (&zmsg, sock_, ZMQ_DONTWAIT);
-    
-    //return a null message
+    r = zmq_msg_recv(&zmsg, sock_, ZMQ_DONTWAIT);
+
+    // return a null message
     if (r < 0) {
       m.reset();
-      return m; 
+      return m;
     }
   }
 
-  //deal with src, it is automatically added by zmq
-  int size = zmq_msg_size (&zmsg);
-  CHECK( size == sizeof(int) ) << "Please init zmq socket client id to a integer";
-  memcpy( &src, zmq_msg_data(&zmsg), sizeof(int) );
+  // deal with src, it is automatically added by zmq
+  int size = zmq_msg_size(&zmsg);
+  CHECK(size == sizeof(int))
+                << "Please init zmq socket client id to a integer";
+  memcpy(&src, zmq_msg_data(&zmsg), sizeof(int));  // NOLINT
 
   zmq_msg_close(&zmsg);
-  
-  //Make sure we have payloads
+
+  // Make sure we have payloads
   CHECK(zmq_msg_more(&zmsg));
-  
+
   return SkSock::RecvMsg(blocked);
 }
 
 
-int SkServer::SendMsg(shared_ptr<Msg> msg)
-{
-  //attaching dst
+int SkServer::SendMsg(shared_ptr<Msg> msg) {
+  // attaching dst
   zmq_msg_t header_dst;
   zmq_msg_init_size (&header_dst, sizeof(int));
   int dst = msg->dst();
-  memcpy (zmq_msg_data (&header_dst), &dst, sizeof(int));
-  zmq_msg_send (&header_dst, sock_, ZMQ_SNDMORE);
+  memcpy(zmq_msg_data (&header_dst), &dst, sizeof(int));  // NOLINT
+  zmq_msg_send(&header_dst, sock_, ZMQ_SNDMORE);
 
   return SkSock::SendMsg(msg);
 }
 
-} //end caffe
+}  // end namespace caffe
 
