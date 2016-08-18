7dbaad8879c451f7c592b82cfaf8b38d4af9b922
Que, Kevin <kevin.que@intel.com>
2016-08-19 02:56:08 +0800

    sk_sock lint

--- include/caffe/multi_node/sk_sock.hpp
+++ include/caffe/multi_node/sk_sock.hpp
@@ -3,51 +3,52 @@
 #ifndef MULTI_NODE_SK_SOCK_H
 #define MULTI_NODE_SK_SOCK_H
 
-#include "caffe/multi_node/msg.hpp"
 #include <boost/thread.hpp>
+
+#include <string>
+
+#include "caffe/multi_node/msg.hpp"
 
 namespace caffe {
 
 class SkSock {
-
-public:
+ public:
   SkSock() {
     id_ = INVALID_ID;
   }
 
   SkSock(int type, int id) {
-    InitZmq(); 
-    sock_ = zmq_socket (zmq_ctx_, type);
+    InitZmq();
+    sock_ = zmq_socket(zmq_ctx_, type);
     id_ = id;
     sk_type_ = type;
   }
-  
-  SkSock(int type) {
-    InitZmq(); 
+
+  explicit SkSock(int type) {
+    InitZmq();
     sk_type_ = type;
-    sock_ = zmq_socket (zmq_ctx_, type);
+    sock_ = zmq_socket(zmq_ctx_, type);
   }
 
   virtual ~SkSock() {
     if (NULL != sock_) {
-        zmq_close(sock_);
-    }
-    
-    boost::mutex::scoped_lock lock(zmq_init_mutex_);
-    LOG(INFO) << "deinitialize zmq ctx cnt: " << inited_cnt_;
-    
-    inited_cnt_--;
-    if (0 == inited_cnt_) {
-        LOG(INFO) << "destroying zmq context";
-        zmq_ctx_destroy(zmq_ctx_);
+      zmq_close(sock_);
     }
 
+    boost::mutex::scoped_lock lock(zmq_init_mutex_);
+    LOG(INFO) << "deinitialize zmq ctx cnt: " << inited_cnt_;
+
+    inited_cnt_--;
+    if (0 == inited_cnt_) {
+      LOG(INFO) << "destroying zmq context";
+      zmq_ctx_destroy(zmq_ctx_);
+    }
   }
-  
+
   virtual int Bind(const string& addr);
 
   virtual int Connect(const string& addr);
-  
+
   virtual shared_ptr<Msg> RecvMsg(bool blocked);
 
   virtual int SendMsg(shared_ptr<Msg> msg);
@@ -60,16 +61,16 @@ public:
 
     return 0;
   }
-  
+
   /// return the native zmq socket
   void *GetSock() {
     return sock_;
   }
 
-protected:
+ protected:
   int SendHeader(shared_ptr<Msg> msg);
 
-protected:
+ protected:
   // the native zmq socket
   void *sock_;
   // the zmq address of this socket
@@ -78,19 +79,19 @@ protected:
   void InitZmq(void);
   static void *zmq_ctx_;
 
-protected:
+ protected:
   // an unique id allocated by an global id server
   int id_;
   // zmq socket type
   int sk_type_;
 
-private:
+ private:
   // a zmq context should be inited only once
   static boost::mutex zmq_init_mutex_;
   static int inited_cnt_;
 };
 
-} //end caffe
+}  // end namespace caffe
 
 #endif
 
--- src/caffe/multi_node/sk_sock.cpp
+++ src/caffe/multi_node/sk_sock.cpp
@@ -1,4 +1,5 @@
 
+#include <string>
 
 #include "caffe/multi_node/sk_sock.hpp"
 
@@ -8,8 +9,7 @@ void *SkSock::zmq_ctx_ = NULL;
 boost::mutex SkSock::zmq_init_mutex_;
 int SkSock::inited_cnt_ = 0;
 
-void SkSock::InitZmq(void)
-{
+void SkSock::InitZmq(void) {
   boost::mutex::scoped_lock lock(zmq_init_mutex_);
 
   if (0 == inited_cnt_) {
@@ -20,34 +20,32 @@ void SkSock::InitZmq(void)
 }
 
 
-inline int SkSock::SendHeader(shared_ptr<Msg> msg)
-{
-  //ataching meta data, including type, src, dst etc.
+inline int SkSock::SendHeader(shared_ptr<Msg> msg) {
+  // ataching meta data, including type, src, dst etc.
   string header_str;
   msg->SerializeHeader(&header_str);
 
   zmq_msg_t header_msg;
-  zmq_msg_init_size (&header_msg, header_str.length());
+  zmq_msg_init_size(&header_msg, header_str.length());
 
-  memcpy (zmq_msg_data (&header_msg), header_str.data(), header_str.length());
-  zmq_msg_send (&header_msg, sock_, ZMQ_SNDMORE);
-  
+  memcpy(zmq_msg_data (&header_msg), header_str.data(), header_str.length()); // NOLINT
+  zmq_msg_send(&header_msg, sock_, ZMQ_SNDMORE);
+
   return 0;
 }
 
 
-int SkSock::SendMsg(shared_ptr<Msg> msg)
-{
+int SkSock::SendMsg(shared_ptr<Msg> msg) {
   if (sk_type_ == ZMQ_DEALER) {
     CHECK_GT(msg->src(), 0) << "Message source must be set for dealer socket";
   }
-  
+
   if (msg->ZmsgCnt() <= 0) {
     LOG(WARNING) << "Cannot send message with length " << msg->ZmsgCnt();
 
     return -1;
   }
-  
+
   SendHeader(msg);
 
   for (int i = 0; i < msg->ZmsgCnt() - 1; i++) {
@@ -60,8 +58,7 @@ int SkSock::SendMsg(shared_ptr<Msg> msg)
 }
 
 
-shared_ptr<Msg> SkSock::RecvMsg(bool blocked)
-{
+shared_ptr<Msg> SkSock::RecvMsg(bool blocked) {
   shared_ptr<Msg> m(new Msg());
 
   int r = 0;
@@ -69,38 +66,39 @@ shared_ptr<Msg> SkSock::RecvMsg(bool blocked)
   zmq_msg_t header_msg;
   zmq_msg_init(&header_msg);
   if ( blocked ) {
-    r = zmq_msg_recv (&header_msg, sock_, 0);
+    r = zmq_msg_recv(&header_msg, sock_, 0);
   } else {
-    r = zmq_msg_recv (&header_msg, sock_, ZMQ_DONTWAIT);
+    r = zmq_msg_recv(&header_msg, sock_, ZMQ_DONTWAIT);
 
     if (r < 0) {
-        m.reset();
-        return m;
+      m.reset();
+      return m;
     }
   }
-  
-  string header_str( string( (char *) zmq_msg_data(&header_msg), zmq_msg_size (&header_msg) ) );
+
+  string header_str(string(reinterpret_cast<char *>(zmq_msg_data(&header_msg)),
+                    zmq_msg_size(&header_msg)));
 
   m->ParseHeader(header_str);
   zmq_msg_close(&header_msg);
-  
-  CHECK(zmq_msg_more (&header_msg));
+
+  CHECK(zmq_msg_more(&header_msg));
 
   for (int i = 0; ; i++) {
     zmq_msg_t *zmsg = new zmq_msg_t;
     zmq_msg_init(zmsg);
-    
-    if ( blocked ) {
-      r = zmq_msg_recv (zmsg, sock_, 0);
+
+    if (blocked) {
+      r = zmq_msg_recv(zmsg, sock_, 0);
     } else {
-      r = zmq_msg_recv (zmsg, sock_, ZMQ_DONTWAIT);    
+      r = zmq_msg_recv(zmsg, sock_, ZMQ_DONTWAIT);
     }
 
-    CHECK( r >= 0 );
+    CHECK_GE(r, 0);
 
     m->AppendZmsg(zmsg);
-        
-    if (! zmq_msg_more(zmsg)) {
+
+    if (!zmq_msg_more(zmsg)) {
       break;
     }
   }
@@ -108,13 +106,12 @@ shared_ptr<Msg> SkSock::RecvMsg(bool blocked)
   return m;
 }
 
-int SkSock::Bind(const string& addr)
-{
+int SkSock::Bind(const string& addr) {
   CHECK(sock_ != NULL) << "cannot bind null sock";
 
   int rc = 0;
   rc = zmq_bind(sock_, addr.c_str());
-  CHECK( 0 == rc) << "failed to bind zmq sock to " << addr;
+  CHECK_EQ(0, rc) << "failed to bind zmq sock to " << addr;
 
   addr_ = addr;
 
@@ -122,9 +119,8 @@ int SkSock::Bind(const string& addr)
 }
 
 
-int SkSock::Connect(const string& addr)
-{
-  CHECK (sock_ != NULL) << "cannot connect to NULL sock";
+int SkSock::Connect(const string& addr) {
+  CHECK(sock_ != NULL) << "cannot connect to NULL sock";
 
   zmq_connect(sock_, addr.c_str());
   addr_ = addr;
@@ -132,5 +128,5 @@ int SkSock::Connect(const string& addr)
   return 0;
 }
 
-}  //end caffe
+}  // end namespace caffe
 
