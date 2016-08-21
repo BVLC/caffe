1c49d9d72953c1905dffc46025f2d8a764f5ef36
Que, Kevin <kevin.que@intel.com>
2016-08-22 03:04:56 +0800

    lint fc_node

--- include/caffe/multi_node/fc_node.hpp
+++ include/caffe/multi_node/fc_node.hpp
@@ -2,22 +2,23 @@
 #ifndef MULTI_NODE_FC_NODE_H_
 #define MULTI_NODE_FC_NODE_H_
 
-#include "caffe/multi_node/msg_hub.hpp"
+#include <boost/lexical_cast.hpp>
+
+#include <string>
+#include <vector>
+
 #include "caffe/multi_node/fc_thread.hpp"
+#include "caffe/multi_node/msg_hub.hpp"
 #include "caffe/sgd_solvers.hpp"
-#include "boost/lexical_cast.hpp"
 
 namespace caffe {
 
 template <typename Dtype>
-class FcNode : public MsgHub<Dtype>
-{
-
-public:
-  FcNode (int nthreads, int nworkers, int omp_param_threads = 0) 
+class FcNode : public MsgHub<Dtype> {
+ public:
+  FcNode(int nthreads, int nworkers, int omp_param_threads = 0)
       : MsgHub<Dtype>(nthreads, nworkers),
-      work_loads_(nworkers, 0)
-  {
+      work_loads_(nworkers, 0) {
     node_id_ = NodeEnv::Instance()->ID();
 
     string pub_addr = NodeEnv::Instance()->pub_addr();
@@ -30,7 +31,7 @@ public:
 
     sock_back_.reset(new SkServer());
     sock_back_->Bind(back_addr);
-    
+
     LOG(INFO) << "Bind Router to " << back_addr;
 
     num_next_hops_ = 0;
@@ -40,27 +41,27 @@ public:
     omp_param_threads_ = omp_param_threads;
   }
 
-
-public:
+ public:
   virtual int Init();
 
   virtual int RouteMsg();
 
-protected:
+ protected:
   virtual int SetUpPoll();
-  
+
   /// send out the msg processed by threads
   virtual int SendOutMsg(shared_ptr<Msg> m);
 
   virtual void ProcessFwdMsg(shared_ptr<Msg> m);
-  
-  /// Set up the connections and init the routing table 
+
+  /// Set up the connections and init the routing table
   int InitRoute();
 
   inline int num_inputs() { return input_blob_name_map_.size(); }
-  
+
   bool is_input_blob(const string& blob_name) {
-    unordered_map<string, bool>::iterator iter = input_blob_name_map_.find(blob_name);
+    unordered_map<string, bool>::iterator iter =
+                                 input_blob_name_map_.find(blob_name);
     if (iter == input_blob_name_map_.end()) {
       return false;
     } else {
@@ -69,24 +70,24 @@ protected:
   }
 
   void PrepareInputData(shared_ptr<Msg> m);
-  
+
   // schedule the message to worker threads
   int ScheduleMsg(shared_ptr<Msg> m);
 
-protected:
+ protected:
   // broadcast blobs to downstream nodes
   shared_ptr<SkSock> sock_pub_;
 
   /// a ROUTER socket to received the packets from downstream nodes
   shared_ptr<SkSock> sock_back_;
-  
+
   /// back sock index in the poll table
   int back_sock_index_;
   int num_next_hops_;
 
   vector<int> prev_ids_;
   int num_prev_hops_;
-  
+
   /// use hash map as a routing table
   /// map from node id to the sock index
   unordered_map<int, shared_ptr<SkSock> > node_to_sock_;
@@ -96,77 +97,74 @@ protected:
 
   /// number of conv. clients a thread handles
   vector<int> work_loads_;
-  
+
   /// the dealer socks used to connect upstream nodes
   vector<shared_ptr<SkSock> > vec_dealer_;
-  
+
   /// NodeID, fetched from ID server
   int node_id_;
-  
+
+  typedef unordered_map<int64_t, shared_ptr<vector<shared_ptr<Msg> > > >
+                                                                      MsgMap;
+
   // map msg id to a vector of partial message
-  unordered_map<int64_t, shared_ptr<vector<shared_ptr<Msg> > > > msg_id_to_buf_;
- 
+  MsgMap msg_id_to_buf_;
+
   // use hash map to store message
   unordered_map<int64_t, shared_ptr<Msg> > id_to_msg_;
-  
+
   unordered_map<string, bool> input_blob_name_map_;
 
   //
   int param_thread_index_;
-  
+
   // number of omp threads for param thread
   int omp_param_threads_;
 };
 
 
-//Interface to the conv. clients
+// Interface to the conv. clients
 template <typename Dtype>
-class FcGateway : public FcNode<Dtype>
-{
-public:
+class FcGateway : public FcNode<Dtype> {
+ public:
   FcGateway(int nthreads, int omp_param_threads = 0)
-      : FcNode<Dtype>(nthreads, nthreads - 1, omp_param_threads)
-  {
-    /////
-  }
+      : FcNode<Dtype>(nthreads, nthreads - 1, omp_param_threads) { }
 
-public:
+ public:
   virtual int SendOutMsg(shared_ptr<Msg> m);
-  
+
   virtual int RouteMsg() {
     return FcNode<Dtype>::RouteMsg();
   }
 
-protected:
+ protected:
   virtual int SetUpPoll();
 };
 
 template <typename Dtype>
-class FcClient : public FcNode<Dtype>
-{
-public:
+class FcClient : public FcNode<Dtype> {
+ public:
   FcClient(int nthreads, int omp_param_threads = 0)
-      : FcNode<Dtype>(nthreads, nthreads - 1, omp_param_threads)
-  {
+      : FcNode<Dtype>(nthreads, nthreads - 1, omp_param_threads) {
     sub_sock_index_ = nthreads + 1;
   }
 
-public:
+ public:
   virtual int Init();
   virtual int RouteMsg();
 
-protected:
+ protected:
   virtual int SetUpPoll();
 
-protected:
-  //receive broadcast message from upstream nodes
+ protected:
+  // receive broadcast message from upstream nodes
   vector<shared_ptr<SkSock> > vec_sub_sock_;
-  
+
   //
   int sub_sock_index_;
 };
 
-} //end caffe
+}  // end namespace caffe
 
 #endif
 
--- src/caffe/multi_node/fc_node.cpp
+++ src/caffe/multi_node/fc_node.cpp
@@ -1,4 +1,6 @@
 
+#include <string>
+#include <vector>
 
 #include "caffe/multi_node/fc_node.hpp"
 #include "caffe/multi_node/param_helper.hpp"
@@ -7,8 +9,7 @@
 namespace caffe {
 
 template <typename Dtype>
-int FcNode<Dtype>::SetUpPoll()
-{
+int FcNode<Dtype>::SetUpPoll() {
   if (this->poll_items_ == NULL) {
     this->poll_items_ = new zmq_pollitem_t[this->num_poll_items_];
   }
@@ -17,17 +18,16 @@ int FcNode<Dtype>::SetUpPoll()
   this->poll_items_[back_sock_index_].events = ZMQ_POLLIN;
   this->poll_items_[back_sock_index_].fd = 0;
   this->poll_items_[back_sock_index_].revents = 0;
-  
+
   return MsgHub<Dtype>::SetUpPoll();
 }
 
 
 template <typename Dtype>
-int FcNode<Dtype>::ScheduleMsg(shared_ptr<Msg> m)
-{
+int FcNode<Dtype>::ScheduleMsg(shared_ptr<Msg> m) {
   int src = m->src();
   unordered_map<int, int>::iterator iter = src_to_thread_.find(src);
-  
+
   int qidx = -1;
   if (iter == src_to_thread_.end()) {
     // find a thread with the smallest work loads
@@ -44,7 +44,7 @@ int FcNode<Dtype>::ScheduleMsg(shared_ptr<Msg> m)
   } else {
     qidx = iter->second;
   }
-  
+
   // put the message to the thread
   this->Enqueue(qidx, m);
 
@@ -53,10 +53,9 @@ int FcNode<Dtype>::ScheduleMsg(shared_ptr<Msg> m)
 
 
 template <typename Dtype>
-int FcNode<Dtype>::Init()
-{
+int FcNode<Dtype>::Init() {
   const vector<string>& next = NodeEnv::Instance()->bcast_addrs();
-  
+
   for (int i = 0; i < this->nworkers_; i++) {
     // if it doesn't broadcast, it means it has loss layer
     if (next.size() > 0) {
@@ -65,30 +64,30 @@ int FcNode<Dtype>::Init()
       this->threads_[i].reset(new FcLossThread<Dtype>());
     }
   }
-  
+
   // the last slot for param thread
   this->threads_[param_thread_index_].reset(new FcParamThread<Dtype>());
   this->threads_[param_thread_index_]->SetOMPThreads(omp_param_threads_);
-  
+
   // wait for the downstream nodes to connect
   for (int i = 0; i < next.size(); i++) {
     shared_ptr<Msg> m = sock_back_->RecvMsg(true);
 
-    CHECK (m->type() == PING);
-    
-    string node((char *)m->ZmsgData(0), m->ZmsgSize(0));
+    CHECK(m->type() == PING);
+
+    string node(reinterpret_cast<char *>(m->ZmsgData(0)), m->ZmsgSize(0));
     LOG(INFO) << "Accepted Connection from: " << node;
   }
-  
+
   num_next_hops_ = next.size();
 
   prev_ids_ = NodeEnv::Instance()->prev_node_ids();
   num_prev_hops_ = prev_ids_.size();
-  
+
   const SolverParameter& param = NodeEnv::Instance()->SolverParam();
   // set up solvers
   SGDSolver<Dtype> *pfc0 = new SGDSolver<Dtype>(param);
-  
+
   // init input blobs
   shared_ptr<Net<Dtype> > net = pfc0->net();
   for (int i = 0; i < net->num_inputs(); i++) {
@@ -99,31 +98,29 @@ int FcNode<Dtype>::Init()
 
   // clear root net's diff
   net->ClearParamDiffs();
-  ParamHelper<Dtype>::CopyParamDataFromMsg(net, NodeEnv::Instance()->model_server_msg());
+  ParamHelper<Dtype>::CopyParamDataFromMsg(net,
+                        NodeEnv::Instance()->model_server_msg());
   NodeEnv::Instance()->PushFreeSolver(pfc0);
 
   // init parameter buffer
-  #if 1
   const vector<Blob<Dtype>*>& root_params = net->learnable_params();
   ParamBuf<Dtype> *pbuf = FcWorker<Dtype>::GetParamBuf();
-  
+
   pbuf->InitParamBuf(root_params);
   vector<Blob<Dtype>*> *pparam = pbuf->FindFreeParam();
   // share the root params to param buffer
   for (int i = 0; i < pparam->size(); i++) {
     pparam->at(i)->ShareData(*root_params[i]);
   }
-  
+
   pbuf->ReplaceParam(pparam);
-  #endif
-  
+
   // init the threads
   return this->StartThreads();
 }
 
 template <typename Dtype>
-void FcNode<Dtype>::PrepareInputData(shared_ptr<Msg> m)
-{
+void FcNode<Dtype>::PrepareInputData(shared_ptr<Msg> m) {
   /// check we got what we need
   for (int i = 0; i < m->num_blobs(); i++) {
     const BlobInfo& blob_info = m->blob_info(i);
@@ -131,11 +128,12 @@ void FcNode<Dtype>::PrepareInputData(shared_ptr<Msg> m)
 
     CHECK(is_input_blob(blob_name)) << "fatal: unknown blob: " << blob_name;
   }
-  
+
   if (num_inputs() > m->num_blobs()) {
     // merge inputs together
     int64_t msg_id = m->msg_id();
-    unordered_map<int64_t, shared_ptr<Msg> >::iterator iter = id_to_msg_.find(msg_id);
+    unordered_map<int64_t, shared_ptr<Msg> >::iterator iter
+                                              = id_to_msg_.find(msg_id);
 
     if (iter == id_to_msg_.end()) {
       id_to_msg_[msg_id] = m;
@@ -143,7 +141,7 @@ void FcNode<Dtype>::PrepareInputData(shared_ptr<Msg> m)
       // move the blobs in m to the buffer
       iter->second->MergeMsg(m);
     }
-    
+
     // double check the hashed msg
     iter = id_to_msg_.find(msg_id);
     CHECK(iter != id_to_msg_.end()) << "cannot find msg id: " << msg_id;
@@ -157,7 +155,6 @@ void FcNode<Dtype>::PrepareInputData(shared_ptr<Msg> m)
       // wait for more messages
       return;
     }
-    
   } else {
     // do nothing for we've got all the inputs
     this->ScheduleMsg(m);
@@ -165,12 +162,10 @@ void FcNode<Dtype>::PrepareInputData(shared_ptr<Msg> m)
 }
 
 template <typename Dtype>
-void FcNode<Dtype>::ProcessFwdMsg(shared_ptr<Msg> m)
-{
+void FcNode<Dtype>::ProcessFwdMsg(shared_ptr<Msg> m) {
   if (m->is_partial()) {
-    unordered_map<int64_t, shared_ptr<vector<shared_ptr<Msg> > > >::iterator iter = 
-                          msg_id_to_buf_.find(m->msg_id()); 
-    
+    MsgMap::iterator iter = msg_id_to_buf_.find(m->msg_id());
+
     shared_ptr<vector<shared_ptr<Msg> > > pvec;
     if (iter == msg_id_to_buf_.end()) {
       pvec.reset(new vector<shared_ptr<Msg> >());
@@ -178,7 +173,7 @@ void FcNode<Dtype>::ProcessFwdMsg(shared_ptr<Msg> m)
     } else {
       pvec = iter->second;
     }
-    
+
     pvec->push_back(m);
     iter = msg_id_to_buf_.find(m->msg_id());
 
@@ -205,14 +200,13 @@ void FcNode<Dtype>::ProcessFwdMsg(shared_ptr<Msg> m)
 }
 
 template <typename Dtype>
-int FcNode<Dtype>::RouteMsg()
-{
+int FcNode<Dtype>::RouteMsg() {
   // Got messages from the work threads:
   for (int i = 0; i < this->nworkers_; i++) {
     //
     if (this->poll_items_[i].revents & ZMQ_POLLIN) {
       shared_ptr<Msg> m = this->sockp_arr_[i]->RecvMsg(true);
-      
+
       // route the msg to root thread for updating parameter
       if (m->dst() == ROOT_THREAD_ID) {
         this->Enqueue(param_thread_index_, m);
@@ -225,7 +219,7 @@ int FcNode<Dtype>::RouteMsg()
   // Paramter update thread
   if (this->poll_items_[param_thread_index_].revents & ZMQ_POLLIN) {
     shared_ptr<Msg> m = this->sockp_arr_[param_thread_index_]->RecvMsg(true);
-    
+
     if (m->dst() == WORKER_BCAST) {
       // broadcast the message to all the workers
       for (int i = 0; i < this->nworkers_; i++) {
@@ -240,13 +234,13 @@ int FcNode<Dtype>::RouteMsg()
 
   if (this->poll_items_[back_sock_index_].revents & ZMQ_POLLIN) {
     shared_ptr<Msg> m = sock_back_->RecvMsg(true);
-    
+
     // adding a unique id for each incoming message
     if (m->msg_id() <= 0) {
       int64_t msg_id = m->src();
       msg_id <<= 32;
       msg_id |= (int64_t)m->conv_id();
-    
+
       m->set_msg_id(msg_id);
     }
 
@@ -261,26 +255,27 @@ int FcNode<Dtype>::RouteMsg()
       m->PrintHeader();
     }
   }
-  
+
   return 0;
 }
 
 
 // send out the message which has been processed by the threads
 template <typename Dtype>
-int FcNode<Dtype>::SendOutMsg(shared_ptr<Msg> m)
-{
+int FcNode<Dtype>::SendOutMsg(shared_ptr<Msg> m) {
   // broadcast address, send the message to hub sock to broadcast
   if (m->dst() < 0) {
     sock_pub_->SendMsg(m);
 
     return 0;
   }
-  
-  // looking up the route table
-  unordered_map<int, shared_ptr<SkSock> >::iterator iter = node_to_sock_.find(m->dst());
 
-  CHECK( iter != node_to_sock_.end() ) << "Cannot find routes for id: " << m->dst();
+  // looking up the route table
+  unordered_map<int, shared_ptr<SkSock> >::iterator iter =
+                                            node_to_sock_.find(m->dst());
+
+  CHECK(iter != node_to_sock_.end()) << "Cannot find routes for id: "
+                                       << m->dst();
 
   iter->second->SendMsg(m);
 
@@ -288,21 +283,20 @@ int FcNode<Dtype>::SendOutMsg(shared_ptr<Msg> m)
 }
 
 template <typename Dtype>
-int FcNode<Dtype>::InitRoute()
-{
+int FcNode<Dtype>::InitRoute() {
   const vector<string>& prev_addrs = NodeEnv::Instance()->prev_router_addrs();
   const vector<int>& prev_ids = NodeEnv::Instance()->prev_node_ids();
-  
+
   for (int i = 0; i < prev_addrs.size(); i++) {
     // connect ROUTER node
     shared_ptr<SkSock> dealer(new SkSock(ZMQ_DEALER));
     dealer->SetId(node_id_);
     dealer->Connect(prev_addrs[i]);
-    
+
     // Unique CHECK
     CHECK(node_to_sock_.find(prev_ids[i]) == node_to_sock_.end());
     node_to_sock_[prev_ids[i]] = dealer;
-    
+
     // send notification to upstream node
     shared_ptr<Msg> m(new Msg());
     m->set_src(node_id_);
@@ -318,17 +312,16 @@ int FcNode<Dtype>::InitRoute()
 }
 
 template <typename Dtype>
-int FcClient<Dtype>::Init()
-{
-  //Connect Upstream ROUTER & PUB nodes
+int FcClient<Dtype>::Init() {
+  // Connect Upstream ROUTER & PUB nodes
   const vector<string>& sub_addrs = NodeEnv::Instance()->sub_addrs();
 
   for (int i = 0; i < sub_addrs.size(); i++) {
-    //connect PUB node
+    // connect PUB node
     shared_ptr<SkSock> sub(new SkSock(ZMQ_SUB));
     sub->Connect(sub_addrs[i]);
     zmq_setsockopt(sub->GetSock(), ZMQ_SUBSCRIBE, "", 0);
-    
+
     vec_sub_sock_.push_back(sub);
   }
 
@@ -336,17 +329,16 @@ int FcClient<Dtype>::Init()
   FcNode<Dtype>::Init();
 
   LOG(INFO) << "FcClient successfully initialized.";
-  
+
   return 0;
 }
 
 template <typename Dtype>
-int FcClient<Dtype>::SetUpPoll()
-{
+int FcClient<Dtype>::SetUpPoll() {
   this->num_poll_items_ = this->nthreads_ + 1 + vec_sub_sock_.size();
   this->poll_items_ = new zmq_pollitem_t[this->num_poll_items_];
 
-  //adding the subscribers to the polling items
+  // adding the subscribers to the polling items
   int poll_index = sub_sock_index_;
   for (int i = 0; i < vec_sub_sock_.size(); i++, poll_index++) {
     this->poll_items_[poll_index].socket = vec_sub_sock_[i]->GetSock();
@@ -359,13 +351,12 @@ int FcClient<Dtype>::SetUpPoll()
 }
 
 template <typename Dtype>
-int FcClient<Dtype>::RouteMsg()
-{
+int FcClient<Dtype>::RouteMsg() {
   int poll_index = sub_sock_index_;
   for (int i = 0; i < vec_sub_sock_.size(); i++, poll_index++) {
     if (this->poll_items_[poll_index].revents & ZMQ_POLLIN) {
       shared_ptr<Msg> m = vec_sub_sock_[i]->RecvMsg(true);
-      
+
       this->ProcessFwdMsg(m);
     }
   }
@@ -374,19 +365,17 @@ int FcClient<Dtype>::RouteMsg()
 }
 
 template <typename Dtype>
-int FcGateway<Dtype>::SetUpPoll()
-{
-  //backward sock for downstream nodes
+int FcGateway<Dtype>::SetUpPoll() {
+  // backward sock for downstream nodes
   this->num_poll_items_ = this->nthreads_ + 1;
   this->poll_items_ = new zmq_pollitem_t[this->num_poll_items_];
- 
+
   return FcNode<Dtype>::SetUpPoll();
 }
 
 
 template <typename Dtype>
-int FcGateway<Dtype>::SendOutMsg(shared_ptr<Msg> m)
-{
+int FcGateway<Dtype>::SendOutMsg(shared_ptr<Msg> m) {
   // send the backward message to the conv clients
   if (m->type() == BACKWARD) {
     this->sock_back_->SendMsg(m);
@@ -401,5 +390,5 @@ INSTANTIATE_CLASS(FcGateway);
 INSTANTIATE_CLASS(FcNode);
 INSTANTIATE_CLASS(FcClient);
 
-} // end caffe
+}  // end namespace caffe
 
