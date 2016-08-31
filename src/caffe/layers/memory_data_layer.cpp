6ca16882b9b301c6134aee901e2d9ebaea78b618
Fabian Tschopp <fabian@m-tschopp.ch>
2016-08-31 21:32:47 +0200

    MemoryData-Layer make label optional.

--- src/caffe/layers/memory_data_layer.cpp
+++ src/caffe/layers/memory_data_layer.cpp
@@ -13,6 +13,8 @@ void MemoryDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
                                             const vector<Blob<Dtype>*>& top) {
   MemoryDataParameter mem_param = this->layer_param_.memory_data_param();
 
+  has_label_ = false;
+
   // Old 4D (2D spatial) parameters
   shape_.clear();
   shape_.push_back(mem_param.batch_size());
@@ -28,23 +30,26 @@ void MemoryDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
     }
   }
 
-  // Labels have shape batch_size, 1, 1, ..., 1
-  label_shape_.push_back(shape_[0]);
-  size_ = 1;
-  // All sizes except the batch index
-  for (int_tp i = 1; i < shape_.size(); ++i) {
-    size_ *= shape_[i];
-    label_shape_.push_back(1);
-  }
-
   top[0]->Reshape(shape_);
-  top[1]->Reshape(label_shape_);
   added_data_.Reshape(shape_);
-  added_label_.Reshape(label_shape_);
   data_ = NULL;
-  labels_ = NULL;
   added_data_.cpu_data();
-  added_label_.cpu_data();
+
+  if (top.size() == 2) {
+    has_label_ = true;
+    // Labels have shape batch_size, 1, 1, ..., 1
+    label_shape_.push_back(shape_[0]);
+    size_ = 1;
+    // All sizes except the batch index
+    for (int_tp i = 1; i < shape_.size(); ++i) {
+      size_ *= shape_[i];
+      label_shape_.push_back(1);
+    }
+    top[1]->Reshape(label_shape_);
+    added_label_.Reshape(label_shape_);
+    labels_ = NULL;
+    added_label_.cpu_data();
+  }
 }
 
 template<typename Dtype>
@@ -58,18 +63,23 @@ void MemoryDataLayer<Dtype>::AddDatumVector(const vector<Datum>& datum_vector) {
   vector<int_tp> added_shape = shape_;
   added_shape[0] = num;
   added_data_.Reshape(added_shape);
-  vector<int_tp> added_label_shape = label_shape_;
-  added_label_shape[0] = num;
-  added_label_.Reshape(added_label_shape);
+
   // Apply data transformations (mirror, scale, crop...)
   this->data_transformer_->Transform(datum_vector, &added_data_);
-  // Copy Labels
-  Dtype* top_label = added_label_.mutable_cpu_data();
-  for (int_tp item_id = 0; item_id < num; ++item_id) {
-    top_label[item_id] = datum_vector[item_id].label();
+  Dtype* top_data = added_data_.mutable_cpu_data();
+  Dtype* top_label = nullptr;
+
+  if (has_label_) {
+    vector<int_tp> added_label_shape = label_shape_;
+    added_label_shape[0] = num;
+    added_label_.Reshape(added_label_shape);
+    // Copy Labels
+    top_label = added_label_.mutable_cpu_data();
+    for (int_tp item_id = 0; item_id < num; ++item_id) {
+      top_label[item_id] = datum_vector[item_id].label();
+    }
   }
   // num_images == batch_size_
-  Dtype* top_data = added_data_.mutable_cpu_data();
   Reset(top_data, top_label, num);
   has_new_data_ = true;
 }
@@ -87,18 +97,23 @@ void MemoryDataLayer<Dtype>::AddMatVector(const vector<cv::Mat>& mat_vector,
   vector<int_tp> added_shape = shape_;
   added_shape[0] = num;
   added_data_.Reshape(added_shape);
-  vector<int_tp> added_label_shape = label_shape_;
-  added_label_shape[0] = num;
-  added_label_.Reshape(added_label_shape);
+
   // Apply data transformations (mirror, scale, crop...)
   this->data_transformer_->Transform(mat_vector, &added_data_);
-  // Copy Labels
-  Dtype* top_label = added_label_.mutable_cpu_data();
-  for (int_tp item_id = 0; item_id < num; ++item_id) {
-    top_label[item_id] = labels[item_id];
+  Dtype* top_data = added_data_.mutable_cpu_data();
+  Dtype* top_label = nullptr;
+
+  if (has_label_) {
+    vector<int_tp> added_label_shape = label_shape_;
+    added_label_shape[0] = num;
+    added_label_.Reshape(added_label_shape);
+    // Copy Labels
+    Dtype* top_label = added_label_.mutable_cpu_data();
+    for (int_tp item_id = 0; item_id < num; ++item_id) {
+      top_label[item_id] = labels[item_id];
+    }
   }
   // num_images == batch_size_
-  Dtype* top_data = added_data_.mutable_cpu_data();
   Reset(top_data, top_label, num);
   has_new_data_ = true;
 }
@@ -107,7 +122,9 @@ void MemoryDataLayer<Dtype>::AddMatVector(const vector<cv::Mat>& mat_vector,
 template<typename Dtype>
 void MemoryDataLayer<Dtype>::Reset(Dtype* data, Dtype* labels, int_tp n) {
   CHECK(data);
-  CHECK(labels);
+  if (has_label_) {
+    CHECK(labels);
+  }
   CHECK_EQ(n % shape_[0], 0)<< "n must be a multiple of batch size";
   // Warn with transformation parameters since a memory array is meant to
   // be generic and no transformations are done with Reset().
@@ -125,9 +142,11 @@ void MemoryDataLayer<Dtype>::set_batch_size(int_tp new_size) {
   CHECK(!has_new_data_) <<
   "Can't change batch_size until current data has been consumed.";
   shape_[0] = new_size;
-  label_shape_[0] = new_size;
   added_data_.Reshape(shape_);
-  added_label_.Reshape(label_shape_);
+  if (has_label_) {
+    label_shape_[0] = new_size;
+    added_label_.Reshape(label_shape_);
+  }
 }
 
 template<typename Dtype>
@@ -135,9 +154,11 @@ void MemoryDataLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
                                          const vector<Blob<Dtype>*>& top) {
   CHECK(data_) << "MemoryDataLayer needs to be initialized by calling Reset";
   top[0]->Reshape(shape_);
-  top[1]->Reshape(label_shape_);
   top[0]->set_cpu_data(data_ + pos_ * size_);
-  top[1]->set_cpu_data(labels_ + pos_);
+  if (top.size() == 2 && has_label_) {
+    top[1]->Reshape(label_shape_);
+    top[1]->set_cpu_data(labels_ + pos_);
+  }
   pos_ = (pos_ + shape_[0]) % n_;
   if (pos_ == 0) {
     has_new_data_ = false;
