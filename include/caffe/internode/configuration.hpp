6d7387f6392d960308b0078640279b4919b382ae
Artur Wojcik <artur.wojcik@intel.com>
2016-09-22 11:12:18 +0200

    move Deamon class to header file
    Signed-off-by: Artur Wojcik <artur.wojcik@intel.com>

--- include/caffe/internode/configuration.hpp
+++ include/caffe/internode/configuration.hpp
@@ -1,25 +1,74 @@
 #ifndef CAFFE_INTERNODE_CONFIGURATION_H_
 #define CAFFE_INTERNODE_CONFIGURATION_H_
 
-#include <boost/shared_ptr.hpp>
 #include <string>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/make_shared.hpp>
+
 #include "communication.hpp"
 
 namespace caffe {
 namespace internode {
 
-class Daemon;
-
 typedef boost::function<void()> TimerCallback;
 
-boost::shared_ptr<Daemon> create_communication_daemon();
-void poll_one(boost::shared_ptr<Daemon>);
-void run_one(boost::shared_ptr<Daemon>);
-void create_timer(
-  boost::shared_ptr<Daemon>,
-  uint64_t duration_microseconds,
-  TimerCallback callback,
-  bool repeat);
+class Daemon {
+  typedef boost::asio::deadline_timer Timer;
+  typedef boost::posix_time::microseconds Microseconds;
+
+  void expired(const boost::system::error_code& error,
+               TimerCallback callback,
+               shared_ptr<Timer> timer,
+               bool repeat,
+               Microseconds duration) {
+    if (!error) callback();
+    if (repeat) {
+      timer->expires_at(timer->expires_at() + duration);
+      timer->async_wait(
+        boost::bind(&Daemon::expired, this, _1, callback, timer, repeat,
+          duration));
+    }
+  }
+
+ public:
+  boost::asio::io_service io_service;
+
+  void create_timer(uint64_t duration, TimerCallback callback, bool repeat) {
+    shared_ptr<Timer> timer(new Timer(io_service, Microseconds(duration)));
+    timer->async_wait(
+      boost::bind(&Daemon::expired, this, _1, callback, timer, repeat,
+        Microseconds(duration)));
+  }
+};
+
+inline void
+run_one(shared_ptr<Daemon> daemon) {
+  daemon->io_service.run_one();
+}
+
+inline void
+poll_one(shared_ptr<Daemon> daemon) {
+  daemon->io_service.poll_one();
+}
+
+inline boost::asio::io_service&
+get_io_service(boost::shared_ptr<Daemon> daemon) {
+  return daemon->io_service;
+}
+
+inline void
+create_timer(boost::shared_ptr<Daemon> daemon,
+                  uint64_t duration_microseconds,
+                  TimerCallback callback,
+                  bool repeat) {
+  daemon->create_timer(duration_microseconds, callback, repeat);
+}
+
+inline boost::shared_ptr<Daemon>
+create_communication_daemon() {
+  return boost::make_shared<Daemon>();
+}
 
 boost::shared_ptr<Waypoint> configure_client(
     boost::shared_ptr<Daemon> communication_daemon,
--- src/caffe/internode/configuration.cpp
+++ src/caffe/internode/configuration.cpp
@@ -1,68 +1,16 @@
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/make_shared.hpp>
-#include <boost/tuple/tuple.hpp>
-#include <boost/unordered_map.hpp>
+/* */
+
 #include <string>
 #include <vector>
+#include <boost/tuple/tuple.hpp>
+#include <boost/unordered_map.hpp>
+
 #include "caffe/internode/communication.hpp"
 #include "caffe/internode/configuration.hpp"
 #include "caffe/internode/mpi_configuration.hpp"
 
 namespace caffe {
 namespace internode {
-
-class Daemon {
-  typedef boost::asio::deadline_timer Timer;
-  typedef boost::posix_time::microseconds Microseconds;
-
-  void expired(const boost::system::error_code& error,
-               TimerCallback callback,
-               shared_ptr<Timer> timer,
-               bool repeat,
-               Microseconds duration) {
-    if (!error) callback();
-    if (repeat) {
-      timer->expires_at(timer->expires_at() + duration);
-      timer->async_wait(
-        boost::bind(&Daemon::expired, this, _1, callback, timer, repeat,
-          duration));
-    }
-  }
-
- public:
-  boost::asio::io_service io_service;
-
-  void create_timer(uint64_t duration, TimerCallback callback, bool repeat) {
-    shared_ptr<Timer> timer(new Timer(io_service, Microseconds(duration)));
-    timer->async_wait(
-      boost::bind(&Daemon::expired, this, _1, callback, timer, repeat,
-        Microseconds(duration)));
-  }
-};
-
-void run_one(shared_ptr<Daemon> daemon) {
-  daemon->io_service.run_one();
-}
-
-void poll_one(shared_ptr<Daemon> daemon) {
-  daemon->io_service.poll_one();
-}
-
-boost::asio::io_service& get_io_service(boost::shared_ptr<Daemon> daemon) {
-  return daemon->io_service;
-}
-
-void create_timer(boost::shared_ptr<Daemon> daemon,
-                  uint64_t duration_microseconds,
-                  TimerCallback callback,
-                  bool repeat) {
-  daemon->create_timer(duration_microseconds, callback, repeat);
-}
-
-boost::shared_ptr<Daemon> create_communication_daemon() {
-  return boost::make_shared<Daemon>();
-}
 
 struct Protocol {
   enum Type {
