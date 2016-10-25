f1c66088b8776b2a4f1b7b55bb1969f5c94d8f15
Adnan Chaudhry <adnan_chaudhry@gatech.edu>
2016-10-25 19:43:49 -0400

    Find the network generating maximum spearman correlation

--- compute_LCC_SROCC.m
+++ compute_LCC_SROCC.m
@@ -7,11 +7,15 @@ clear;
 
 % number of trained CNN's being tested
 nNets = 10;
+
 % Create an output file
 out = fopen('outputs/out.txt', 'wt');
 if(out == -1)
     error('unable to open output file for writing');
 end
+
+maxCorr = 0;
+maxCorrNetId = -1;
 
 % Compute LCC and SROCC for the outputs of all nets
 for i = 1 : nNets
@@ -26,10 +30,18 @@ for i = 1 : nNets
     gTruth = fscanf(gTruthFile, '%f');
     fclose(gTruthFile);
 
-    % Compute LCC and SROCC for the output
+    % Compute LCC and SROCC for the output and record the maximum Spearman
+    % correlation coefficient obtained and the corresponding network ID
+    sCorr = corr(output, gTruth, 'type', 'Spearman');
+    if(sCorr > maxCorr)
+        maxCorr = sCorr;
+        maxCorrNetId = i;
+    end
     fprintf(out, '%d LCC = %f\n', i, corr(output, gTruth, 'type', 'Pearson'));
-    fprintf(out, '%d SROCC = %f\n', i, corr(output, gTruth, 'type', 'Spearman'));
+    fprintf(out, '%d SROCC = %f\n', i, sCorr);
 
 end
 
+fprintf(out, 'Network with highest Spearman Correlation is No. %d, corr value = %f\n', maxCorrNetId, maxCorr);
 fclose(out);
+disp(['Network with highest Spearman Correlation is No. ' num2str(maxCorrNetId) ', corr value = ' num2str(maxCorr)]);
