26849275ef16bdedb23ce2af84ea3bc0a0843f2e
SHEKOLDA <graaaand@mail.ru>
2016-11-11 16:43:25 +0300

    Add files via upload

--- python/caffe/_caffe.cpp
+++ python/caffe/_caffe.cpp
@@ -3,6 +3,7 @@
 // Produce deprecation warnings (needs to come before arrayobject.h inclusion).
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 
+
 #include <boost/make_shared.hpp>
 #include <boost/python.hpp>
 #include <boost/python/exception_translator.hpp>
@@ -40,6 +41,32 @@
     bp::register_ptr_to_python<shared_ptr<PTR > >(); \
   } \
 } while (0)
+
+#if defined(_MSC_VER) && (_MSC_FULL_VER >= 190024210)
+// Workaround for VS 2015 Update 3 which breaks boost python
+// See: http://stackoverflow.com/questions/38261530/unresolved-external-symbols-since-visual-studio-2015-update-3-boost-python-link
+// and https://msdn.microsoft.com/vs-knownissues/vs2015-update3
+#define BP_GET_POINTER(cls, dtype) \
+namespace boost { \
+template <> \
+caffe::cls<dtype> const volatile * \
+get_pointer<class caffe::cls<dtype> const volatile >( \
+  class caffe::cls<dtype> const volatile *c) { \
+    return c; \
+} \
+}
+
+BP_GET_POINTER(Net, float);
+BP_GET_POINTER(Layer, float);
+BP_GET_POINTER(Solver, float);
+BP_GET_POINTER(SGDSolver, float);
+BP_GET_POINTER(NesterovSolver, float);
+BP_GET_POINTER(AdaGradSolver, float);
+BP_GET_POINTER(RMSPropSolver, float);
+BP_GET_POINTER(AdaDeltaSolver, float);
+BP_GET_POINTER(AdamSolver, float);
+
+#endif
 
 namespace bp = boost::python;
 
@@ -275,7 +302,7 @@ struct NdarrayCallPolicies : public bp::default_call_policies {
     void* data = PyArray_DATA(reinterpret_cast<PyArrayObject*>(result));
     Py_DECREF(result);
     const int_tp num_axes = blob->num_axes();
-    vector<npy_long> dims(blob->shape().begin(), blob->shape().end());
+    vector<npy_intp> dims(blob->shape().begin(), blob->shape().end());
     PyObject *arr_obj = PyArray_SimpleNewFromData(num_axes, dims.data(),
                                                   NPY_FLOAT32, data);
     // SetBaseObject steals a ref, so we need to INCREF.
