9a37607b5ef41b1f79cda81eb6f0255eec634fa6
SHEKOLDA <graaaand@mail.ru>
2016-11-17 15:57:03 +0300

    Delete appveyor_build_and_test.cmd

--- scripts/appveyor/appveyor_build_and_test.cmd
+++
@@ -1,7 +0,0 @@
-if "%WITH_CMAKE%" == "1" (
-    echo "Building with CMake"
-    call %~dp0appveyor_cmake_build_and_test.cmd
-) else (
-    echo "Building with Visual Studio"
-    call %~dp0appveyor_vs_build_and_test.cmd
-)
