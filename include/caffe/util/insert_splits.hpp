6175a95e49c494cf570dc11d553cdf7f71203d3a
SHEKOLDA <graaaand@mail.ru>
2016-11-17 16:06:08 +0300

    Add files via upload

--- include/caffe/util/insert_splits.hpp
+++ include/caffe/util/insert_splits.hpp
@@ -12,14 +12,14 @@ namespace caffe {
 void InsertSplits(const NetParameter& param, NetParameter* param_split);
 
 void ConfigureSplitLayer(const string& layer_name, const string& blob_name,
-    const int blob_idx, const int split_count, const float loss_weight,
+    const int_tp blob_idx, const int_tp split_count, const float loss_weight,
     LayerParameter* split_layer_param);
 
 string SplitLayerName(const string& layer_name, const string& blob_name,
-    const int blob_idx);
+    const int_tp blob_idx);
 
 string SplitBlobName(const string& layer_name, const string& blob_name,
-    const int blob_idx, const int split_idx);
+    const int_tp blob_idx, const int_tp split_idx);
 
 }  // namespace caffe
 
