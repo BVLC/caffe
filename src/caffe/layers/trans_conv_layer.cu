fea596ac5e3afb48ab5a9f57c07e9b5caba52dd1
Hongyang Gao <hongyang.gao@wsu.edu>
2016-11-19 12:35:57 -0800

    remove log

--- src/caffe/layers/trans_conv_layer.cu
+++ src/caffe/layers/trans_conv_layer.cu
@@ -95,7 +95,7 @@ void TransformerConvolutionLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>&
   Dtype weights[8 * count];
   get_trans_weights(weights, weight, param);
   Dtype* curWeight;
-  CaffeMallocHost((void**) &curWeight, count, &(this->cpu_malloc_use_cuda_));
+  CaffeMallocHost((void**) &curWeight, count, true);
   for (int i = 0; i < bottom.size(); ++i) {
     const Dtype* bottom_data = bottom[i]->gpu_data();
     Dtype* top_data = top[i]->mutable_gpu_data();
