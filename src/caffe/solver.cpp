6ec7f3d08e736cbe3ceb11751a4caf72ecbeb3c2
Evan Shelhamer <shelhamer@imaginarynumber.net>
2016-11-21 09:35:57 -0800

    solver: check and set type to reconcile class and proto
    the solver checks its proto type (SolverParameter.type) on
    instantiation:
    
    - if the proto type is unspecified it's set according to the class type
      `Solver::type()`
    - if the proto type and class type conflict, the solver dies loudly
    
    this helps avoid accidental instantiation of a different solver type
    than intended when the solver def and class differ. guaranteed type
    information in the SolverParameter will simplify multi-solver
    coordination too.

--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -38,7 +38,19 @@ Solver<Dtype>::Solver(const string& param_file, const Solver* root_solver)
       requested_early_exit_(false) {
   SolverParameter param;
   ReadSolverParamsFromTextFileOrDie(param_file, &param);
+  CheckType(&param);
   Init(param);
+}
+
+template <typename Dtype>
+void Solver<Dtype>::CheckType(SolverParameter* param) {
+  // Harmonize solver class type with configured type to avoid confusion.
+  if (param->has_type()) {
+    CHECK_EQ(param->type(), this->type())
+        << "Solver type must agree with instantiated solver class.";
+  } else {
+    param->set_type(this->type());
+  }
 }
 
 template <typename Dtype>
