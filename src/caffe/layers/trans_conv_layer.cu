52f70c4b1b5ee4ea2260f34bdb0399d2b3e4258e
Hongyang Gao <hongyang.gao@wsu.edu>
2016-11-21 16:49:19 -0800

    change cu to cuda loop

--- src/caffe/layers/trans_conv_layer.cu
+++ src/caffe/layers/trans_conv_layer.cu
@@ -12,20 +12,21 @@ void TransformerConvolutionLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>&
   TransformerConvParameter param = this->layer_param_.trans_conv_param();
   Dtype* weight = this->blobs_[0]->mutable_gpu_data();
   Dtype weights[8 * count];
+  int nthreads = this->num_ * 8;
+  int n, j;
   get_trans_weights(weights, weight, param);
   for (int i = 0; i < bottom.size(); ++i) {
     const Dtype* bottom_data = bottom[i]->gpu_data();
     Dtype* top_data = top[i]->mutable_gpu_data();
-    for (int n = 0; n < this->num_; ++n) {
-      for (int j = 0; j < 8; ++j){
-        //caffe_gpu_memcpy(count, weights+j*count, (Dtype*) curWeight);
-        caffe_copy(count, weights+j*count, weight);
-        this->forward_gpu_gemm(bottom_data + n * this->bottom_dim_, weight,
-            top_data + (n*8+j) * this->top_dim_);
-        if (this->bias_term_) {
-          const Dtype* bias = this->blobs_[1]->gpu_data();
-          this->forward_gpu_bias(top_data + (n*8+j) * this->top_dim_, bias);
-        } 
+    CUDA_KERNEL_LOOP(index, nthreads) {
+      n = index / 8;
+      j = index % 8;
+      caffe_copy(count, weights+j*count, weight);
+      this->forward_gpu_gemm(bottom_data + n * this->bottom_dim_, weight,
+          top_data + (n*8+j) * this->top_dim_);
+      if (this->bias_term_) {
+        const Dtype* bias = this->blobs_[1]->gpu_data();
+        this->forward_gpu_bias(top_data + (n*8+j) * this->top_dim_, bias);
       }
     }
   }
@@ -47,6 +48,9 @@ void TransformerConvolutionLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>
   Dtype bottom_diff_temp[this->bottom_dim_];
   get_trans_weights(weights, weight, param);
   get_trans_weights(weight_diffs, weight_diff, param);
+  int nthreads = this->num_ * 8;
+  int n, j
+  int pre_n = -1;
   for (int i = 0; i < top.size(); ++i) {
     const Dtype* top_diff = top[i]->gpu_diff();
     const Dtype* bottom_data = bottom[i]->gpu_data();
@@ -54,36 +58,41 @@ void TransformerConvolutionLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>
     // Bias gradient, if necessary.
     if (this->bias_term_ && this->param_propagate_down_[1]) {
       Dtype* bias_diff = this->blobs_[1]->mutable_gpu_diff();
-      for (int n = 0; n < this->num_; ++n) {
-        for (int j = 0; j < 8; ++j){
-          this->backward_gpu_bias(bias_diff, top_diff + (n*8+j) * this->top_dim_);
-        }
+      CUDA_KERNEL_LOOP(index, nthreads) {
+        n = index / 8;
+        j = index % 8;
+        this->backward_gpu_bias(bias_diff, top_diff + (n*8+j) * this->top_dim_);
       }
     }
     if (this->param_propagate_down_[0] || propagate_down[i]) {
-      for (int n = 0; n < this->num_; ++n) {
-        caffe_set(this->bottom_dim_, (Dtype)0.0, bottom_diff_temp);
-        for (int j = 0; j < 8; ++j){
-          // gradient w.r.t. weight. Note that we will accumulate diffs.
-          if (this->param_propagate_down_[0]) {
-            caffe_set(count, (Dtype) 0.0, diff_temp);
-            caffe_copy(count, diff_temp, weight_diff);
-            this->weight_gpu_gemm(bottom_data + n * this->bottom_dim_,
-                top_diff + (n*8+j) * this->top_dim_, weight_diff);
-            caffe_copy(count, weight_diff, diff_temp);
-            caffe_add(count, diff_temp, weight_diffs+j*count, weight_diffs+j*count);
-          }
-          // gradient w.r.t. bottom data, if necessary.
-          if (propagate_down[i]) {
-            caffe_copy(count, weights+j*count, weight);
-            this->backward_gpu_gemm(top_diff + (n*8+j) * this->top_dim_, weight,
-                bottom_diff + n * this->bottom_dim_);
-            caffe_copy(this->bottom_dim_, bottom_diff + n * this->bottom_dim_, bottom_temp);
-            caffe_add(this->bottom_dim_, bottom_temp, bottom_diff_temp, bottom_diff_temp);
-          }
+      CUDA_KERNEL_LOOP(index, nthreads) {
+        n = index / 8;
+        j = index % 8;
+        if (pre_n != n){
+          caffe_set(this->bottom_dim_, (Dtype)0.0, bottom_diff_temp);
         }
-        caffe_copy(this->bottom_dim_, bottom_diff_temp, bottom_diff + n * this->bottom_dim_);
+        if (this->param_propagate_down_[0]) {
+          caffe_set(count, (Dtype) 0.0, diff_temp);
+          caffe_copy(count, diff_temp, weight_diff);
+          this->weight_gpu_gemm(bottom_data + n * this->bottom_dim_,
+              top_diff + (n*8+j) * this->top_dim_, weight_diff);
+          caffe_copy(count, weight_diff, diff_temp);
+          caffe_add(count, diff_temp, weight_diffs+j*count, weight_diffs+j*count);
+        }
+        // gradient w.r.t. bottom data, if necessary.
+        if (propagate_down[i]) {
+          caffe_copy(count, weights+j*count, weight);
+          this->backward_gpu_gemm(top_diff + (n*8+j) * this->top_dim_, weight,
+              bottom_diff + n * this->bottom_dim_);
+          caffe_copy(this->bottom_dim_, bottom_diff + n * this->bottom_dim_, bottom_temp);
+          caffe_add(this->bottom_dim_, bottom_temp, bottom_diff_temp, bottom_diff_temp);
+        }
+        if (pre_n != n && pre_n >= 0){
+          caffe_copy(this->bottom_dim_, bottom_diff_temp, bottom_diff + n * this->bottom_dim_);
+          pre_n = n;
+        }
       }
+      caffe_copy(this->bottom_dim_, bottom_diff_temp, bottom_diff + n * this->bottom_dim_);
     }
   }
   caffe_copy(count, weights, weight);
