db6643232cc95ba79f2a21ad98ef15725ee576d6
Zhou Mo <cdluminate@gmail.com>
2016-11-27 09:13:42 +0000

    fix many typos by using codespell

--- src/caffe/layers/crop_layer.cu
+++ src/caffe/layers/crop_layer.cu
@@ -39,7 +39,7 @@ void CropLayer<Dtype>::crop_copy_gpu(const vector<Blob<Dtype>*>& bottom,
                 src_data, dest_data, is_forward);
     }
   } else {
-    // We are at the last two dimensions, which are stored continously in memory
+    // We are at the last two dimensions, which are stored continuously in memory
     // With (N,C,H,W)
     //      (0,1,2,3) cur_dim   -> H
     //                cur_dim+1 -> W
