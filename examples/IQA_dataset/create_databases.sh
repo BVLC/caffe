2e16b3ce53ba59dcdc921767d4b83e188e109aa1
Adnan Chaudhry <adnan_chaudhry@gatech.edu>
2016-11-27 21:09:02 -0500

    Modify script so that its parallel instances can be launched

--- examples/IQA_dataset/create_databases.sh
+++ examples/IQA_dataset/create_databases.sh
@@ -2,13 +2,14 @@
 # Create databases for IQA CNN training and validation
 # The first argument is the number of train-val databases to generate
 # The second argument is the distortion type
-NITERS=$1
-DISTORTION_TYPE=$2
+ITER_START=$1
+ITER_END=$2
+DISTORTION_TYPE=$3
 DATABASE_DIR=examples/IQA_dataset/$DISTORTION_TYPE
 if [ ! -d $DATABASE_DIR ]; then
   mkdir $DATABASE_DIR
 fi
-for i in $(seq 1 1 $NITERS)
+for i in $(seq $ITER_START 1 $ITER_END)
 do
    examples/IQA_dataset/create_db.sh "$i" "$DISTORTION_TYPE"
 done
