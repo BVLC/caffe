068358c0a032d40f650f0f5c98f077e7b67e357a
Dale <sunskn@163.com>
2016-12-05 22:01:59 +0800

    fix typo

--- include/caffe/filler.hpp
+++ include/caffe/filler.hpp
@@ -148,8 +148,9 @@ class XavierFiller : public Filler<Dtype> {
   virtual void Fill(Blob<Dtype>* blob) {
     CHECK(blob->count());
     int fan_in = blob->count() / blob->shape(0);
-    //Compatible for both Convolution and InnerProduct layer
-    int fan_out = (blob->num_axes() == 2) ? blob->shape(0) : blob->count() / blob->shape(1);
+    // Compatible for both Convolution and InnerProduct layer
+    int fan_out = (blob->num_axes() == 2) ? blob->shape(0) : 
+	    blob->count() / blob->shape(1);
     Dtype n = fan_in;  // default to fan_in
     if (this->filler_param_.variance_norm() ==
         FillerParameter_VarianceNorm_AVERAGE) {
@@ -191,8 +192,9 @@ class MSRAFiller : public Filler<Dtype> {
   virtual void Fill(Blob<Dtype>* blob) {
     CHECK(blob->count());
     int fan_in = blob->count() / blob->shape(0);
-    //Compatible for both Convolution and InnerProduct layer
-    int fan_out = (blob->num_axes() == 2) ? blob->shape(0) : blob->count() / blob->shape(1);
+    // Compatible for both Convolution and InnerProduct layer
+    int fan_out = (blob->num_axes() == 2) ? blob->shape(0) : 
+	    blob->count() / blob->shape(1);
     Dtype n = fan_in;  // default to fan_in
     if (this->filler_param_.variance_norm() ==
         FillerParameter_VarianceNorm_AVERAGE) {
