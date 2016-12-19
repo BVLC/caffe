b2ec31f73b28be79b3adf46995145b75aea4fef9
Artur Wojcik <artur.wojcik@intel.com>
2016-12-19 14:35:29 +0100

    fix compilation issue
    Signed-off-by: Artur Wojcik <artur.wojcik@intel.com>

--- src/caffe/layers/batch_norm_layer.cpp
+++ src/caffe/layers/batch_norm_layer.cpp
@@ -35,6 +35,8 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <mpi.h>
+
 #include <algorithm>
 #include <functional>
 #include <vector>
