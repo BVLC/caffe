22efa36aa3c6d06791e89fadc0358a484a97ad1b
Patrick Follmann <follmann@mvtec.com>
2016-12-29 14:37:21 +0100

    Add GPU sqrt functions

--- include/caffe/util/math_functions.hpp
+++ include/caffe/util/math_functions.hpp
@@ -214,6 +214,9 @@ void caffe_gpu_log(const int n, const Dtype* a, Dtype* y);
 template <typename Dtype>
 void caffe_gpu_powx(const int n, const Dtype* a, const Dtype b, Dtype* y);
 
+template <typename Dtype>
+void caffe_gpu_sqrt(const int n, const Dtype* a, Dtype* y);
+
 // caffe_gpu_rng_uniform with two arguments generates integers in the range
 // [0, UINT_MAX].
 void caffe_gpu_rng_uniform(const int n, unsigned int* r);
