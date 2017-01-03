c333e3022786322fac810cdd85bc87af7931049d
Tomasz Patejko <tomasz.patejko@intel.com>
2017-01-03 12:04:34 +0100

    Changes related to ReadImageToCVMat. Works

--- include/caffe/util/io.hpp
+++ include/caffe/util/io.hpp
@@ -266,12 +266,6 @@ inline bool MapLabelToDisplayName(const LabelMap& map,
 }
 
 #ifdef USE_OPENCV
-cv::Mat ReadImageToCVMat(const string& filename, const int height,
-    const int width, const int min_dim, const int max_dim, const bool is_color);
-
-cv::Mat ReadImageToCVMat(const string& filename, const int height,
-    const int width, const int min_dim, const int max_dim);
-
 cv::Mat ReadImageToCVMat(const string& filename,
     const int height, const int width, const bool is_color);
 
