2a98f3d4da78a32236b8c91ef299ce8e49524ea5
Tomasz Patejko <tomasz.patejko@intel.com>
2017-01-11 11:36:43 +0100

    caffe test enabled for detection. Additional switch --detection was added

--- tools/caffe.cpp
+++ tools/caffe.cpp
@@ -47,6 +47,7 @@ namespace bp = boost::python;
 #include <map>
 #include <string>
 #include <vector>
+#include <utility>
 
 #include "boost/algorithm/string.hpp"
 #include "boost/make_shared.hpp"
@@ -55,6 +56,8 @@ namespace bp = boost::python;
 #include "caffe/multinode/multinode.hpp"
 #include "caffe/training_utils.hpp"
 #include "caffe/util/signal_handler.h"
+
+#include "caffe/util/bbox_util.hpp"
 
 #ifdef USE_MLSL
 #include "caffe/multinode/MlslSync.hpp"
@@ -111,6 +114,9 @@ DEFINE_bool(forward_only, false,
     "Optional; Execute only forward pass");
 DEFINE_string(engine, "",
     "Optional; Engine sequence in format: engine:subengine_1,subengine_2,...");
+DEFINE_bool(detection, false,
+    "Optional; Type of evaluation when model is tested. Options are: classification (default), detection");
+
 
 // A simple registry for caffe commands.
 typedef int (*BrewFunction)();
@@ -377,10 +383,99 @@ int data_server() {
 }
 RegisterBrewFunction(data_server);
 
+int test_detection(Net<float>& caffe_net)
+{
+  std::map<int, std::map<int, std::vector<std::pair<float, int> > > > all_true_pos;
+  std::map<int, std::map<int, std::vector<std::pair<float, int> > > > all_false_pos;
+  std::map<int, std::map<int, int> > all_num_pos;
+
+  for (int i = 0; i < FLAGS_iterations; ++i) {
+    float iter_loss;
+    const vector<Blob<float>*>& result = caffe_net.Forward(&iter_loss);
+
+    for (int j = 0; j < result.size(); ++j) {
+      const float* result_vec = result[j]->cpu_data();
+      
+      int num_det = result[j]->height();
+      for (int k = 0; k < num_det; ++k) {
+        int item_id = static_cast<int>(result_vec[k * 5]);
+        int label = static_cast<int>(result_vec[k * 5 + 1]);
+        if (item_id == -1) {
+          // Special row of storing number of positives for a label.
+          if (all_num_pos[j].find(label) == all_num_pos[j].end()) {
+            all_num_pos[j][label] = static_cast<int>(result_vec[k * 5 + 2]);
+          } else {
+            all_num_pos[j][label] += static_cast<int>(result_vec[k * 5 + 2]);
+          }
+        } else {
+          // Normal row storing detection status.
+          float score = result_vec[k * 5 + 2];
+          int tp = static_cast<int>(result_vec[k * 5 + 3]);
+          int fp = static_cast<int>(result_vec[k * 5 + 4]);
+          if (tp == 0 && fp == 0) {
+            // Ignore such case. It happens when a detection bbox is matched to
+            // a difficult gt bbox and we don't evaluate on difficult gt bbox.
+            continue;
+          }
+          all_true_pos[j][label].push_back(std::make_pair(score, tp));
+          all_false_pos[j][label].push_back(std::make_pair(score, fp));
+        }
+      }
+    }
+  }
+  
+  for (int i = 0; i < all_true_pos.size(); ++i) {
+    if (all_true_pos.find(i) == all_true_pos.end()) {
+      LOG(FATAL) << "Missing output_blob true_pos: " << i;
+    }
+    const std::map<int, std::vector<std::pair<float, int> > >& true_pos =
+        all_true_pos.find(i)->second;
+    if (all_false_pos.find(i) == all_false_pos.end()) {
+      LOG(FATAL) << "Missing output_blob false_pos: " << i;
+    }
+    const std::map<int, std::vector<std::pair<float, int> > >& false_pos =
+        all_false_pos.find(i)->second;
+    if (all_num_pos.find(i) == all_num_pos.end()) {
+      LOG(FATAL) << "Missing output_blob num_pos: " << i;
+    }
+    const std::map<int, int>& num_pos = all_num_pos.find(i)->second;
+    std::map<int, float> APs;
+    float mAP = 0.;
+    // Sort true_pos and false_pos with descend scores.
+    for (std::map<int, int>::const_iterator it = num_pos.begin();
+         it != num_pos.end(); ++it) {
+      int label = it->first;
+      int label_num_pos = it->second;
+      if (true_pos.find(label) == true_pos.end()) {
+        LOG(WARNING) << "Missing true_pos for label: " << label;
+        continue;
+      }
+      const std::vector<std::pair<float, int> >& label_true_pos =
+          true_pos.find(label)->second;
+      if (false_pos.find(label) == false_pos.end()) {
+        LOG(WARNING) << "Missing false_pos for label: " << label;
+        continue;
+      }
+      const std::vector<std::pair<float, int> >& label_false_pos =
+          false_pos.find(label)->second;
+      std::vector<float> prec, rec;
+      caffe::ComputeAP(label_true_pos, label_num_pos, label_false_pos,
+                "11point", &prec, &rec, &(APs[label]));
+      mAP += APs[label];
+    }
+    mAP /= num_pos.size();
+    const int output_blob_index = caffe_net.output_blob_indices()[i];
+    const string& output_name = caffe_net.blob_names()[output_blob_index];
+    LOG(INFO) << "    Test net output #" << i << ": " << output_name << " = "
+              << mAP;
+  }
+  
+  return 0;
+}
+
 // Test: score a model.
 int test() {
-  CHECK_GT(FLAGS_model.size(), 0) << "Need a model definition to score.";
-  CHECK_GT(FLAGS_weights.size(), 0) << "Need model weights to score.";
+  
   vector<string> stages = get_stages_from_flags(FLAGS_stage);
 
   // Set device id and mode
@@ -404,6 +499,11 @@ int test() {
                        FLAGS_engine);
   caffe_net.CopyTrainedLayersFrom(FLAGS_weights);
   LOG(INFO) << "Running for " << FLAGS_iterations << " iterations.";
+  
+  if (FLAGS_detection) {
+    test_detection(caffe_net);
+    return 0;
+  }
 
   vector<int> test_score_output_id;
   vector<float> test_score;
