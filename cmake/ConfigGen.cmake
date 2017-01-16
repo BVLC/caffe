56e89483f78f9c352721cda66924c20523c3d20c
sfraczek <sylwester.fraczek@intel.com>
2017-01-16 19:19:27 +0100

    Performance Monitoring flag for cmake fixed

--- cmake/ConfigGen.cmake
+++ cmake/ConfigGen.cmake
@@ -113,10 +113,6 @@ function(caffe_generate_export_configs)
     list(APPEND DEFINITIONS -DUSE_CUDNN)
   endif()
 
-  if(PERFORMANCE_MONITORING)
-    list(APPEND Caffe_DEFINITIONS -DPERFORMANCE_MONITORING)
-  endif()
-
   if(BLAS STREQUAL "MKL" OR BLAS STREQUAL "mkl")
     list(APPEND Caffe_DEFINITIONS -DUSE_MKL)
   endif()
