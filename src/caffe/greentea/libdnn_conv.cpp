35ed6ba96f27bd4fd77c0e1ab75ce226c05b08f7
Fabian Tschopp <fabian@m-tschopp.ch>
2017-01-19 04:27:08 +0100

    Fix LibDNN memory flush bug.

--- src/caffe/greentea/libdnn_conv.cpp
+++ src/caffe/greentea/libdnn_conv.cpp
@@ -1729,19 +1729,6 @@ void LibDNNConv<Dtype>::Backward(bool prop_down_data, bool prop_down_weights,
     LibDNN<Dtype>::SetMemory(bottom_diff, ims, 0, (Dtype) 0);
   }
 
-  if (prop_down_weights && wgalgo_ == LIBDNN_CONVOLUTION_WG_ALGO_ATOMIC) {
-    int_tp wms = fmaps_in_ * fmaps_out_;
-    for (int_tp i = 0; i < kernel_shape_.size(); ++i) {
-      wms *= kernel_shape_[i];
-    }
-    LibDNN<Dtype>::SetMemory(bottom_diff, wms, 0, (Dtype) 0);
-  }
-
-  if (bias_term_ && prop_down_weights &&
-      wgalgo_ == LIBDNN_CONVOLUTION_WG_ALGO_ATOMIC) {
-    LibDNN<Dtype>::SetMemory(bias_diff, fmaps_out_, 0, (Dtype) 0);
-  }
-
 #ifdef USE_GREENTEA
   if (LibDNN<Dtype>::dev_ptr_->backend() == BACKEND_OpenCL) {
     // Backprop w.r.t. data
