2ba99ef81f87355930891b1b3f14f183a3055806
Matt Hill <matthill@openalpr.com>
2017-01-30 16:28:26 -0500

    Applied Euclidean Loss ignore code from caffe PR 3677

--- src/caffe/layers/euclidean_loss_layer.cu
+++ src/caffe/layers/euclidean_loss_layer.cu
@@ -6,6 +6,18 @@
 namespace caffe {
 
 template <typename Dtype>
+__global__ void EuclideanLossForwardGPU(const int n,
+          const Dtype* label_data_, Dtype* diff__data_,
+          const int ignore_label_) {
+  CUDA_KERNEL_LOOP(index, n) {
+    const int label_value = static_cast<int>(label_data_[index]);
+    if (label_value == ignore_label_) {
+      diff__data_[index] = 0;
+    }
+  }
+}
+
+template <typename Dtype>
 void EuclideanLossLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
   int count = bottom[0]->count();
@@ -14,6 +26,13 @@ void EuclideanLossLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       bottom[0]->gpu_data(),
       bottom[1]->gpu_data(),
       diff_.mutable_gpu_data());
+  if (has_ignore_label_) {
+    // NOLINT_NEXT_LINE(whitespace/operators)
+    EuclideanLossForwardGPU<Dtype><<<CAFFE_GET_BLOCKS(count),
+        CAFFE_CUDA_NUM_THREADS>>>(count, bottom[1]->gpu_data(),
+                                  diff_.mutable_gpu_data(),
+                                  ignore_label_);
+  }
   Dtype dot;
   caffe_gpu_dot(count, diff_.gpu_data(), diff_.gpu_data(), &dot);
   Dtype loss = dot / bottom[0]->num() / Dtype(2);
