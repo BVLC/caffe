6e4b89aff5178a5b8afc8a354446306255b3b990
sfraczek <sylwester.fraczek@intel.com>
2017-02-01 14:26:45 +0100

    data readded to gitignore and ssdvars comment made more clear

--- examples/ssd/ssdvars.sh
+++ examples/ssd/ssdvars.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 export CAFFE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../..
 export PYTHONPATH=$CAFFE_ROOT/"python"
-# WHERE OU WISH TO STORE THE IMAGE DATASET AND LMDB AND SCORING RESULTS
-export DATAPATH="/home/sfraczek/disk/sfraczek/ssd"
+
+# datapath is where you wish to store generated lmdb
+# also in $DATAPATH/data directory you should have unpacked i
+# VOCdevkit and/or coco directories. see more in ./data/coco/README.md
+# this variable is used in create_list and create_data scripts only.
+export DATAPATH=""
+
 echo CAFFE_ROOT is $CAFFE_ROOT
 echo PYTHONPATH is $PYTHONPATH
-echo DATAPATH is $DATAPATH
+echo DATAPATH   is $DATAPATH
