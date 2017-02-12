d6bb9ff7ccece289058ecac2e6a84f554e03640a
Daewoo-PC\Daewoo <ttagu99@gmail.com>
2017-02-12 14:46:04 +0900

    remove unused variable

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -442,15 +442,12 @@ template<typename Dtype>
 void DataTransformer<Dtype>::CopyBlob(Blob<Dtype>* source_blob, Blob<Dtype>* target_blob)
 {
   const int input_num = source_blob->num();
-  const int input_channels = source_blob->channels();
   const int input_height = source_blob->height();
   const int input_width = source_blob->width();
 
-  const int num = target_blob->num();
   const int channels = target_blob->channels();
   const int height = target_blob->height();
   const int width = target_blob->width();
-  const int size = target_blob->count();
   Dtype* input_data = source_blob->mutable_cpu_data();
   Dtype* target_data = target_blob->mutable_cpu_data();
   for (int n = 0; n < input_num; ++n) {
