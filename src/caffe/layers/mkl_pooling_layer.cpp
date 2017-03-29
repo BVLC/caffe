25a684caf38e8518c86a4c9b9b927d5679b32d6a
Yu, Chong <chong.yu@intel.com>
2017-03-29 19:54:32 +0800

    In the previous, IntelCaffe had to zero diff for the bottom blob in the MKL Pooling layer because of the bug in MKL. This is no longer necessary.

--- src/caffe/layers/mkl_pooling_layer.cpp
+++ src/caffe/layers/mkl_pooling_layer.cpp
@@ -435,8 +435,6 @@ void MKLPoolingLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
   } else {
     pooling_res[dnnResourceDiffSrc] = bottom[0]->mutable_cpu_diff();
   }
-  caffe_set(bottom[0]->count(), Dtype(0),
-          reinterpret_cast<Dtype *>(pooling_res[dnnResourceDiffSrc]));
 
   PERFORMANCE_EVENT_ID_INIT(perf_id_bw_, PERFORMANCE_MKL_NAME("BW"));
   PERFORMANCE_MEASUREMENT_BEGIN();
