a32114e6b2e098e2fdef47e397542b105eb58b66
Will Crichton <wcrichto@stanford.edu>
2017-03-31 11:22:22 -0400

    Fixed memory leaks in cudnn conv and relu

--- src/caffe/layers/cudnn_conv_layer.cpp
+++ src/caffe/layers/cudnn_conv_layer.cpp
@@ -252,6 +252,7 @@ CuDNNConvolutionLayer<Dtype>::~CuDNNConvolutionLayer() {
   }
 
   cudaFree(workspaceData);
+  delete [] workspace;
   delete [] stream_;
   delete [] handle_;
   delete [] fwd_algo_;
