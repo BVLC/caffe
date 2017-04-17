51e5a8a4e49e6f782550b9e0b6e9b575a697cd46
Bruno Bowden <github@brunobowden.com>
2017-04-17 16:15:52 -0700

    Shape mismatch CHECK logging improvements
    - More logs when shapes mismatch
    - Manual reviewed all matches of “grep -r CHECK.*shape src”
    - Follow JUnit convention of:
    ```
      CHECK(expected == actual)
          << “expected: “ << expected
          << “ vs. actual: ” << actual;
    ```
    
    Layers:
    - Recurrent
    - BaseConvolutionLayer
    - Slice (improves logging for incorrect parameter)
    
    CHECK_EQ
    - Would prefer to use CHECK_EQ(expected, actual):
    https://github.com/google/glog/blob/master/src/glog/stl_logging.h.in#L36
    - However this requires include of stl_logging.h which introduces a
    build warning. I believe this would work if caffe was updated to the
    latest version of glog but that's a bigger change.

--- src/caffe/layers/slice_layer.cpp
+++ src/caffe/layers/slice_layer.cpp
@@ -41,7 +41,9 @@ void SliceLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   int count = 0;
   if (slice_point_.size() != 0) {
     CHECK_EQ(slice_point_.size(), top.size() - 1);
-    CHECK_LE(top.size(), bottom_slice_axis);
+    CHECK_LE(top.size(), bottom_slice_axis)
+        << "slice axis: " << slice_axis_
+        << ", bottom[0] shape: " << bottom[0]->shape_string();
     int prev = 0;
     vector<int> slices;
     for (int i = 0; i < slice_point_.size(); ++i) {
