1e3275a6396b75797be18b95f91c23b94979c9a6
Glue Crow <yacwu@cs.nctu.edu.tw>
2017-04-26 04:47:55 +0800

    Change the library links according to https://software.intel.com/en-us/articles/intel-mkl-link-line-advisor

--- cmake/Modules/FindMKL.cmake
+++ cmake/Modules/FindMKL.cmake
@@ -22,8 +22,7 @@ caffe_option(MKL_MULTI_THREADED  "Use multi-threading"   ON IF NOT MKL_USE_SINGL
 if(WIN32)
   set(ProgramFilesx86 "ProgramFiles(x86)")
   set(INTEL_ROOT $ENV{${ProgramFilesx86}}/IntelSWTools/compilers_and_libraries/windows CACHE PATH "Folder contains intel libs")
-endif()
-if(UNIX)
+else()
   set(INTEL_ROOT "/opt/intel" CACHE PATH "Folder contains intel libs")
 endif()
 find_path(MKL_ROOT include/mkl.h PATHS $ENV{MKLROOT} ${INTEL_ROOT}/mkl
@@ -68,8 +67,16 @@ foreach (__lib ${__mkl_libs})
   set(__mkl_lib "mkl_${__lib}")
   string(TOUPPER ${__mkl_lib} __mkl_lib_upper)
 
-  if(MKL_USE_STATIC_LIBS)
-    set(__mkl_lib "lib${__mkl_lib}.a")
+  if(NOT MKL_USE_SINGLE_DYNAMIC_LIBRARY)
+    if(MKL_USE_STATIC_LIBS)
+      if(NOT WIN32)
+        set(__mkl_lib "lib${__mkl_lib}.a")
+      endif()
+    else()
+      if(WIN32)
+        set(__mkl_lib "${__mkl_lib}_dll")
+      endif()
+    endif()
   endif()
 
   find_library(${__mkl_lib_upper}_LIBRARY
@@ -84,17 +91,8 @@ foreach (__lib ${__mkl_libs})
 endforeach()
 
 
-if(NOT MKL_USE_SINGLE_DYNAMIC_LIBRARY)
-  if (MKL_USE_STATIC_LIBS)
-    set(__iomp5_libs iomp5 libiomp5mt.lib)
-  else()
-    set(__iomp5_libs iomp5 libiomp5md.lib)
-  endif()
-
-  if(WIN32)
-    find_path(INTEL_INCLUDE_DIR omp.h PATHS ${INTEL_ROOT} PATH_SUFFIXES include)
-    list(APPEND __looked_for INTEL_INCLUDE_DIR)
-  endif()
+if(NOT MKL_USE_SINGLE_DYNAMIC_LIBRARY AND MKL_MULTI_THREADED)
+  set(__iomp5_libs iomp5 libiomp5md)
 
   find_library(MKL_RTL_LIBRARY ${__iomp5_libs}
      PATHS ${INTEL_RTL_ROOT} ${INTEL_ROOT}/compiler ${MKL_ROOT}/.. ${MKL_ROOT}/../compiler
