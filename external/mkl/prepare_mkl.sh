24e64f4a57e9fc2e20cd069fb72943a6d5612ab9
Shen, Haihao <haihao.shen@intel.com>
2017-05-23 09:17:22 +0800

    Use MKL 0425 link in Caffe github

--- external/mkl/prepare_mkl.sh
+++ external/mkl/prepare_mkl.sh
@@ -77,9 +77,9 @@ OMP=0
 VERSION_MATCH=20170425
 ARCHIVE_BASENAME=mklml_lnx_2018.0.20170425.tgz
 MKL_CONTENT_DIR=`echo $ARCHIVE_BASENAME | rev | cut -d "." -f 2- | rev`
-GITHUB_RELEASE_TAG=v0.7
+GITHUB_RELEASE_TAG=1.0.0
 
-MKLURL="https://github.com/01org/mkl-dnn/releases/download/$GITHUB_RELEASE_TAG/$ARCHIVE_BASENAME"
+MKLURL="https://github.com/intel/caffe/releases/download/$GITHUB_RELEASE_TAG/$ARCHIVE_BASENAME"
 # there are diffrent MKL lib to be used for GCC and for ICC
 reg='^[0-9]+$'
 VERSION_LINE=`GetVersionName $MKLROOT`
