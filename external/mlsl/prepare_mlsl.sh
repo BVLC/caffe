178f530b39336b4e38c1ba80cf29406a6e76de04
Yu, Chong <chong.yu@intel.com>
2017-06-21 01:03:24 +0800

    Add the shell script to support the automatic downloading and installation of MLSL.

---
+++ external/mlsl/prepare_mlsl.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+# set -ex
+# 
+# All modification made by Intel Corporation: © 2016 Intel Corporation
+# 
+# This script is used to prepare the Intel® Machine Learning Scaling Library
+#
+FindLibrary()
+{
+# Find all the instances of the MKL libraries present in Caffe
+  MLSL_LIBS=`find $1 -name libmlsl.so`
+  #echo "[Debug][FindLibrary function] MLSL_LIBS: $MLSL_LIBS"
+
+  LOCALMLSL=$MLSL_LIBS
+  #echo "[Debug][FindLibrary function] LOCALMLSL: $LOCALMLSL"
+}
+
+GetVersionName()
+{
+VERSION_LINE=0
+if [ $1 ]; then
+  RAW_VERSION_LINE=`echo $1 | rev | cut -d "_" -f -1 | rev`
+  VERSION_LINE=`echo $RAW_VERSION_LINE | sed 's/\.//g'`
+fi
+if [ -z $VERSION_LINE ]; then
+  VERSION_LINE=0
+fi
+echo $VERSION_LINE  # Return Version Line
+}
+
+# MLSL
+DST=`dirname $0`
+#echo "[Debug] dirname: $0"
+#echo "[Debug] DST value: $DST"
+ABS_DST=`readlink -f $DST`
+#echo "[Debug] ABS_DST value: $ABS_DST"
+VERSION_MATCH=20170014
+ARCHIVE_BASENAME=l_mlsl_p_2017.0.014.tgz
+ARCHIVE_INSTALL_FOLDERNAME=l_mlsl_p_2017.0.014
+MLSL_CONTENT_DIR=`echo $ARCHIVE_BASENAME | rev | cut -d "." -f 2- | rev`
+#echo "[Debug] MLSL_CONTENT_DIR value: $MLSL_CONTENT_DIR"
+GITHUB_RELEASE_TAG=v2017-Preview
+
+MLSLURL="https://github.com/01org/MLSL/releases/download/$GITHUB_RELEASE_TAG/$ARCHIVE_BASENAME"
+#echo "[Debug] MLSLROOT value: $MLSLROOT"
+VERSION_LINE=`GetVersionName $MLSLROOT`
+#echo "[Debug] VERSION_LINE value: $VERSION_LINE"
+# Check if MLSLROOT is set if positive then set one will be used..
+if [ -z $MLSLROOT ] || [ $VERSION_LINE -lt $VERSION_MATCH ]; then
+  # ..if MLSLROOT is not set then check if we have MLSL unpacked and installed in proper version
+  FindLibrary $DST
+  #echo "[Debug] LOCALMLSL value inside if: $LOCALMLSL"
+  if [ -z $LOCALMLSL ]; then
+    echo "No MLSL unpacked and installed in proper version"
+  else
+    echo "Some verison of MLSL is unpacked and installed"
+    MLSL_PREVIOUS_CONTENT_DIR=`echo $LOCALMLSL | rev | cut -d "/" -f 4- | cut -d "/" -f -1 | rev`
+    VERSION_LINE=`GetVersionName $DST/$MLSL_PREVIOUS_CONTENT_DIR`
+  fi
+  #echo "[Debug] VERSION_LINE value inside if: $VERSION_LINE"
+  if [ $VERSION_LINE -lt $VERSION_MATCH ] ; then
+    #...If it is not then downloaded, unpacked and installed
+    wget --no-check-certificate -P $DST $MLSLURL -O $DST/$ARCHIVE_BASENAME
+    tar -xzf $DST/$ARCHIVE_BASENAME -C $DST
+    #echo "[Debug] PWD value: $PWD"
+    #install.sh did not support the relative path as the parameter
+    bash $DST/install.sh -s -d $ABS_DST/$ARCHIVE_INSTALL_FOLDERNAME
+  fi
+  FindLibrary $DST
+  #echo "[Debug] LOCALMLSL value: $LOCALMLSL"
+  #echo "[Debug] PWD value: $PWD"
+  MLSLROOT=$PWD/`echo $LOCALMLSL | sed -e 's/intel64.*$//'`
+  #echo "[Debug] MLSLROOT value: $MLSLROOT"
+fi
+
+if [ -z $LOCALMLSL ] ; then
+# LOCALMLSL is not set, when MLSLROOT was set manually and it should point to MLSL in correct version
+  FindLibrary $MLSLROOT
+fi
+    
+LIBRARIES=`basename $LOCALMLSL | sed -e 's/^.*lib//' | sed -e 's/\.so.*$//'`
+#echo "[Debug] LIBRARIES value: $LIBRARIES"
+
+# return value to calling script (Makefile,cmake)
+echo $MLSLROOT $LIBRARIES
