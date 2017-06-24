889f569c34aeb4956f4e68e6cb116aa1f032f157
kvmanohar22 <kvmanohar22@gmail.com>
2017-06-24 11:14:28 +0530

    Add more test cases

--- src/caffe/test/test_bbox_data_layer.cpp
+++ src/caffe/test/test_bbox_data_layer.cpp
@@ -39,6 +39,25 @@ class BboxDataLayerTest : public MultiDeviceTest<TypeParam> {
               << std::endl;
     }
     outfile.close();
+    // Multiple objects in an image
+    MakeTempFilename(&filename_multiobj_);
+    std::ofstream multifile(filename_multiobj_.c_str(), std::ofstream::out);
+    LOG(INFO) << "Using temporary file " << filename_multiobj_;
+    multifile << EXAMPLES_SOURCE_DIR "images/fish-bike.jpg" << " "
+              << EXAMPLES_SOURCE_DIR "annotations/fish-bike.txt"
+              << std::endl;
+    multifile.close();
+    // Multi image and multi boxes
+    MakeTempFilename(&filename_multi_multi);
+    std::ofstream multi_multi(filename_multi_multi.c_str(), std::ofstream::out);
+    LOG(INFO) << "Using temporary file " << filename_multi_multi;
+    multi_multi << EXAMPLES_SOURCE_DIR "images/cat.jpg" << " "
+                << EXAMPLES_SOURCE_DIR "annotations/cat.txt"
+                << std::endl
+                << EXAMPLES_SOURCE_DIR "images/fish-bike.jpg" << " "
+                << EXAMPLES_SOURCE_DIR "annotations/fish-bike.txt"
+                << std::endl;
+    multi_multi.close();
   }
 
   virtual ~BboxDataLayerTest() {
@@ -48,6 +67,8 @@ class BboxDataLayerTest : public MultiDeviceTest<TypeParam> {
 
   int seed_;
   string filename_;
+  string filename_multiobj_;
+  string filename_multi_multi;
   Blob<Dtype>* const blob_top_data_;
   Blob<Dtype>* const blob_top_label_;
   vector<Blob<Dtype>*> blob_bottom_vec_;
@@ -74,7 +95,6 @@ TYPED_TEST(BboxDataLayerTest, TestRead) {
   EXPECT_EQ(this->blob_top_label_->height(), 1);
   EXPECT_EQ(this->blob_top_label_->width(), 1);
 
-  // Iterate through the data
   const int arr0[] = {0, 0, 0, 0};
   const vector<int> num_objs(arr0, arr0 + sizeof(arr0) / sizeof(arr0[0]));
   const int arr1[] = {1, 0, 0, 0};
@@ -86,7 +106,12 @@ TYPED_TEST(BboxDataLayerTest, TestRead) {
   const int arr4[] = {4, 0, 0, 0};
   const vector<int> ymax(arr4, arr4 + sizeof(arr4) / sizeof(arr4[0]));
   const int arr5[] = {5, 0, 0, 0};
-  const vector<int> class_idx(arr5, arr5 + sizeof(arr5) / sizeof(arr5[0]));
+  const vector<int> class_idx1(arr5, arr5 + sizeof(arr5) / sizeof(arr5[0]));
+  const int arr6[] = {6, 0, 0, 0};
+  const vector<int> num_objs2(arr6, arr6 + sizeof(arr6) / sizeof(arr6[0]));
+  const int arr7[] = {7, 0, 0, 0};
+  const vector<int> xmin2(arr7, arr7 + sizeof(arr7) / sizeof(arr7[0]));
+  // Iterate through the data
   for (int iter = 0; iter < 2; ++iter) {
     layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
     EXPECT_EQ(this->blob_top_label_->count(), 30);
@@ -96,8 +121,82 @@ TYPED_TEST(BboxDataLayerTest, TestRead) {
     EXPECT_EQ(this->blob_top_label_->data_at(ymin), 56);
     EXPECT_EQ(this->blob_top_label_->data_at(xmax), 278);
     EXPECT_EQ(this->blob_top_label_->data_at(ymax), 411);
-    EXPECT_EQ(this->blob_top_label_->data_at(class_idx), 0);
+    EXPECT_EQ(this->blob_top_label_->data_at(class_idx1), 0);
+    EXPECT_EQ(this->blob_top_label_->data_at(num_objs2), 1);
+    EXPECT_EQ(this->blob_top_label_->data_at(xmin2), 23);
   }
+}
+
+TYPED_TEST(BboxDataLayerTest, TestMulti) {
+    typedef typename TypeParam::Dtype Dtype;
+    LayerParameter param;
+    BboxDataParameter* bbox_data_param = param.mutable_bbox_data_param();
+    bbox_data_param->set_batch_size(1);
+    bbox_data_param->set_source(this->filename_multiobj_.c_str());
+    bbox_data_param->set_shuffle(false);
+    BboxDataLayer<Dtype> layer(param);
+    layer.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+    layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+    EXPECT_EQ(this->blob_top_data_->num(), 1);
+    EXPECT_EQ(this->blob_top_data_->channels(), 3);
+    EXPECT_EQ(this->blob_top_data_->height(), 323);
+    EXPECT_EQ(this->blob_top_data_->width(), 481);
+    EXPECT_EQ(this->blob_top_label_->num(), 11);
+    EXPECT_EQ(this->blob_top_label_->channels(), 1);
+    EXPECT_EQ(this->blob_top_label_->width(), 1);
+    EXPECT_EQ(this->blob_top_label_->width(), 1);
+    // bbox related annotations
+    EXPECT_EQ(this->blob_top_label_->count(), 11);
+    EXPECT_EQ(this->blob_top_label_->data_at(0, 0, 0, 0), 2);
+    EXPECT_EQ(this->blob_top_label_->data_at(1, 0, 0, 0), 22);
+    EXPECT_EQ(this->blob_top_label_->data_at(2, 0, 0, 0), 45);
+    EXPECT_EQ(this->blob_top_label_->data_at(3, 0, 0, 0), 153);
+    EXPECT_EQ(this->blob_top_label_->data_at(4, 0, 0, 0), 221);
+    EXPECT_EQ(this->blob_top_label_->data_at(5, 0, 0, 0), 21);
+    EXPECT_EQ(this->blob_top_label_->data_at(6, 0, 0, 0), 32);
+    EXPECT_EQ(this->blob_top_label_->data_at(7, 0, 0, 0), 155);
+    EXPECT_EQ(this->blob_top_label_->data_at(8, 0, 0, 0), 421);
+    EXPECT_EQ(this->blob_top_label_->data_at(9, 0, 0, 0), 301);
+    EXPECT_EQ(this->blob_top_label_->data_at(10, 0, 0, 0), 22);
+}
+
+TYPED_TEST(BboxDataLayerTest, TestMultiMulti) {
+    typedef typename TypeParam::Dtype Dtype;
+    LayerParameter param;
+    BboxDataParameter* bbox_data_param = param.mutable_bbox_data_param();
+    bbox_data_param->set_batch_size(2);
+    bbox_data_param->set_source(this->filename_multi_multi.c_str());
+    bbox_data_param->set_shuffle(false);
+    bbox_data_param->set_new_height(416);
+    bbox_data_param->set_new_width(416);
+    BboxDataLayer<Dtype> layer(param);
+    layer.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+    layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+
+    EXPECT_EQ(this->blob_top_data_->num(), 2);
+    EXPECT_EQ(this->blob_top_data_->channels(), 3);
+    EXPECT_EQ(this->blob_top_data_->height(), 416);
+    EXPECT_EQ(this->blob_top_data_->width(), 416);
+    EXPECT_EQ(this->blob_top_label_->count(), 17);
+    // cat.jpg
+    EXPECT_EQ(this->blob_top_label_->data_at(0, 0, 0, 0), 1);
+    EXPECT_EQ(this->blob_top_label_->data_at(1, 0, 0, 0), 23);
+    EXPECT_EQ(this->blob_top_label_->data_at(2, 0, 0, 0), 56);
+    EXPECT_EQ(this->blob_top_label_->data_at(3, 0, 0, 0), 278);
+    EXPECT_EQ(this->blob_top_label_->data_at(4, 0, 0, 0), 411);
+    EXPECT_EQ(this->blob_top_label_->data_at(5, 0, 0, 0), 0);
+    // fish-bike.jpg
+    EXPECT_EQ(this->blob_top_label_->data_at(6, 0, 0, 0), 2);
+    EXPECT_EQ(this->blob_top_label_->data_at(7, 0, 0, 0), 22);
+    EXPECT_EQ(this->blob_top_label_->data_at(8, 0, 0, 0), 45);
+    EXPECT_EQ(this->blob_top_label_->data_at(9, 0, 0, 0), 153);
+    EXPECT_EQ(this->blob_top_label_->data_at(10, 0, 0, 0), 221);
+    EXPECT_EQ(this->blob_top_label_->data_at(11, 0, 0, 0), 21);
+    EXPECT_EQ(this->blob_top_label_->data_at(12, 0, 0, 0), 32);
+    EXPECT_EQ(this->blob_top_label_->data_at(13, 0, 0, 0), 155);
+    EXPECT_EQ(this->blob_top_label_->data_at(14, 0, 0, 0), 421);
+    EXPECT_EQ(this->blob_top_label_->data_at(15, 0, 0, 0), 301);
+    EXPECT_EQ(this->blob_top_label_->data_at(16, 0, 0, 0), 22);
 }
 
 }  // namespace caffe
