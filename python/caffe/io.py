42d2d34b2bc593e1b2b364e844aca7b14db9e9fa
Geunsik Lim <geunsik.lim@samsung.com>
2017-06-29 13:12:53 +0900

    Fix: mean shape in compatible with input shape
    This commit is to fix issue #5718.
    
    * reference:
    1. https://groups.google.com/forum/#!topic/caffe-users/nBpWJCcJoCU
    2. https://stackoverflow.com/questions/28692209/using-gpu-despite-setting-cpu-only-yielding-unexpected-keyword-argument
    
    Signed-off-by: Geunsik Lim <geunsik.lim@samsung.com>

--- python/caffe/io.py
+++ python/caffe/io.py
@@ -256,7 +256,12 @@ class Transformer:
             if len(ms) != 3:
                 raise ValueError('Mean shape invalid')
             if ms != self.inputs[in_][1:]:
-                raise ValueError('Mean shape incompatible with input shape.')
+                in_shape = self.inputs[in_][1:]
+                m_min, m_max = mean.min(), mean.max()
+                normal_mean = (mean - m_min) / (m_max - m_min)
+                mean = resize_image(normal_mean.transpose((1,2,0)),
+                        in_shape[1:]).transpose((2,0,1)) * \
+                        (m_max - m_min) + m_min
         self.mean[in_] = mean
 
     def set_input_scale(self, in_, scale):
