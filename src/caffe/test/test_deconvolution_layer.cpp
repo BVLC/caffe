720755161506253252d7917eb47312e50c952bd4
Bo Wang <david.b.wang@gmail.com>
2017-09-14 15:03:21 -0700

    Fix format

--- src/caffe/test/test_deconvolution_layer.cpp
+++ src/caffe/test/test_deconvolution_layer.cpp
@@ -5,8 +5,8 @@
 #include "caffe/blob.hpp"
 #include "caffe/common.hpp"
 #include "caffe/filler.hpp"
-#include "caffe/layers/deconv_layer.hpp"
 #include "caffe/layers/cudnn_deconv_layer.hpp"
+#include "caffe/layers/deconv_layer.hpp"
 
 #include "caffe/test/test_caffe_main.hpp"
 #include "caffe/test/test_gradient_check_util.hpp"
