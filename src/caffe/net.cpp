331a127d6c973a5b3055503d69af25447574e0fc
fzou1 <feng.zou@intel.com>
2017-09-22 23:09:16 +0800

    add check of input layer and support MLSL in google test (contributed by Shiryaev, Mikhail)

--- src/caffe/net.cpp
+++ src/caffe/net.cpp
@@ -284,7 +284,8 @@ void Net<Dtype>::Init(const NetParameter& in_param) {
             batch_size = layer_param.memory_data_param().batch_size();
         else if (!layer_param.type().compare("WindowData"))
             batch_size = layer_param.window_data_param().batch_size();
-        else if (!layer_param.type().compare("Input"))
+        else if (!layer_param.type().compare("Input")
+            && layer_param.input_param().shape(0).dim().size())
             batch_size = layer_param.input_param().shape(0).dim(0);
 
         if (caffe::TRAIN == param.state().phase()) {
