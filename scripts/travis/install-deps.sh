949ab5bf8658dda2bb8939407417adb6adac3e62
Icyblade Dai <icyblade.py@gmail.com>
2017-10-11 15:29:21 +0800

    add supports for cuDNN v7

--- scripts/travis/install-deps.sh
+++ scripts/travis/install-deps.sh
@@ -106,7 +106,7 @@ if $WITH_CUDA ; then
   ln -s /usr/local/cuda-$CUDA_VERSION /usr/local/cuda
 
   if $WITH_CUDNN ; then
-    apt-get install -y --no-install-recommends libcudnn6-dev
+    apt-get install -y --no-install-recommends libcudnn7-dev
   fi
 fi
 
