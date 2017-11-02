53daefa87b79774711ccbba5704a91af8e40ab31
Brian Lee <lkm1321@gmail.com>
2017-11-02 17:37:16 +1100

    Follow base fork dependency

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -106,7 +106,7 @@ endif()
 
 # ---[ BLAS
 if(NOT APPLE)
-  set(BLAS "Open" CACHE STRING "Selected BLAS library")
+  set(BLAS "Atlas" CACHE STRING "Selected BLAS library")
   set_property(CACHE BLAS PROPERTY STRINGS "Atlas;Open;MKL")
 
   if(BLAS STREQUAL "Atlas" OR BLAS STREQUAL "atlas")
