d74bcdfc9b5dfa393595b5aca6e80a427d0c524a
cyy <cyyever@outlook.com>
2017-12-27 18:35:27 +0800

    刪除無用代碼

--- src/caffe/util/bbox_util.cpp
+++ src/caffe/util/bbox_util.cpp
@@ -466,25 +466,6 @@ void GetTopKScoreIndex(const vector<float>& scores, const vector<int>& indices,
   }
 }
 
-void GetMaxScoreIndex(const vector<float>& scores, const float threshold,
-      const int top_k, vector<pair<float, int> >* score_index_vec) {
-  // Generate index score pairs.
-  for (int i = 0; i < scores.size(); ++i) {
-    if (scores[i] > threshold) {
-      score_index_vec->push_back(std::make_pair(scores[i], i));
-    }
-  }
-
-  // Sort the score pair according to the scores in descending order
-  std::stable_sort(score_index_vec->begin(), score_index_vec->end(),
-                   SortScorePairDescend<int>);
-
-  // Keep top_k scores if needed.
-  if (top_k > -1 && top_k < score_index_vec->size()) {
-    score_index_vec->resize(top_k);
-  }
-}
-
 template <typename Dtype>
 void GetMaxScoreIndex(const Dtype* scores, const int num, const float threshold,
       const int top_k, vector<pair<Dtype, int> >* score_index_vec) {
@@ -517,43 +498,6 @@ template
 void GetMaxScoreIndex(const double* scores, const int num,
       const float threshold, const int top_k,
       vector<pair<double, int> >* score_index_vec);
-
-void ApplyNMSFast(const vector<NormalizedBBox>& bboxes,
-      const vector<float>& scores, const float score_threshold,
-      const float nms_threshold, const float eta, const int top_k,
-      vector<int>* indices) {
-  // Sanity check.
-  CHECK_EQ(bboxes.size(), scores.size())
-      << "bboxes and scores have different size.";
-
-  // Get top_k scores (with corresponding indices).
-  vector<pair<float, int> > score_index_vec;
-  GetMaxScoreIndex(scores, score_threshold, top_k, &score_index_vec);
-
-  // Do nms.
-  float adaptive_threshold = nms_threshold;
-  indices->clear();
-  while (score_index_vec.size() != 0) {
-    const int idx = score_index_vec.front().second;
-    bool keep = true;
-    for (int k = 0; k < indices->size(); ++k) {
-      if (keep) {
-        const int kept_idx = (*indices)[k];
-        float overlap = JaccardOverlap(bboxes[idx], bboxes[kept_idx]);
-        keep = overlap <= adaptive_threshold;
-      } else {
-        break;
-      }
-    }
-    if (keep) {
-      indices->push_back(idx);
-    }
-    score_index_vec.erase(score_index_vec.begin());
-    if (keep && eta < 1 && adaptive_threshold > 0.5) {
-      adaptive_threshold *= eta;
-    }
-  }
-}
 
 template <typename Dtype>
 void ApplyNMSFast(const Dtype* bboxes, const Dtype* scores, const int num,
