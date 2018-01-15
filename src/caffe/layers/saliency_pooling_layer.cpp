bfac36fb122fd6d03f49bc6c6319e61952813e38
Abraham Montoya Obeso <montoyaobeso@gmail.com>
2018-01-15 19:16:28 +0100

    Add Saliency Pooling Layer (Working as MaxPoolingLayer)

---
+++ src/caffe/layers/saliency_pooling_layer.cpp
@@ -0,0 +1,214 @@
+#include <algorithm>
+#include <cfloat>
+#include <vector>
+
+#include "caffe/layers/saliency_pooling_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+
+using std::min;
+using std::max;
+
+template <typename Dtype>
+void SaliencyPoolingLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  PoolingParameter pool_param = this->layer_param_.pooling_param();
+  if (pool_param.global_pooling()) {
+    CHECK(!(pool_param.has_kernel_size() ||
+      pool_param.has_kernel_h() || pool_param.has_kernel_w()))
+      << "With Global_pooling: true Filter size cannot specified";
+  } else {
+    CHECK(!pool_param.has_kernel_size() !=
+      !(pool_param.has_kernel_h() && pool_param.has_kernel_w()))
+      << "Filter size is kernel_size OR kernel_h and kernel_w; not both";
+    CHECK(pool_param.has_kernel_size() ||
+      (pool_param.has_kernel_h() && pool_param.has_kernel_w()))
+      << "For non-square filters both kernel_h and kernel_w are required.";
+  }
+  CHECK((!pool_param.has_pad() && pool_param.has_pad_h()
+      && pool_param.has_pad_w())
+      || (!pool_param.has_pad_h() && !pool_param.has_pad_w()))
+      << "pad is pad OR pad_h and pad_w are required.";
+  CHECK((!pool_param.has_stride() && pool_param.has_stride_h()
+      && pool_param.has_stride_w())
+      || (!pool_param.has_stride_h() && !pool_param.has_stride_w()))
+      << "Stride is stride OR stride_h and stride_w are required.";
+  global_pooling_ = pool_param.global_pooling();
+  if (global_pooling_) {
+    kernel_h_ = bottom[0]->height();
+    kernel_w_ = bottom[0]->width();
+  } else {
+    if (pool_param.has_kernel_size()) {
+      kernel_h_ = kernel_w_ = pool_param.kernel_size();
+    } else {
+      kernel_h_ = pool_param.kernel_h();
+      kernel_w_ = pool_param.kernel_w();
+    }
+  }
+  CHECK_GT(kernel_h_, 0) << "Filter dimensions cannot be zero.";
+  CHECK_GT(kernel_w_, 0) << "Filter dimensions cannot be zero.";
+  if (!pool_param.has_pad_h()) {
+    pad_h_ = pad_w_ = pool_param.pad();
+  } else {
+    pad_h_ = pool_param.pad_h();
+    pad_w_ = pool_param.pad_w();
+  }
+  if (!pool_param.has_stride_h()) {
+    stride_h_ = stride_w_ = pool_param.stride();
+  } else {
+    stride_h_ = pool_param.stride_h();
+    stride_w_ = pool_param.stride_w();
+  }
+}
+
+template <typename Dtype>
+void SaliencyPoolingLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  CHECK_EQ(4, bottom[0]->num_axes()) << "Input must have 4 axes, "
+      << "corresponding to (num, channels, height, width)";
+  channels_ = bottom[0]->channels();
+  height_ = bottom[0]->height();
+  width_ = bottom[0]->width();
+  if (global_pooling_) {
+    kernel_h_ = bottom[0]->height();
+    kernel_w_ = bottom[0]->width();
+  }
+  pooled_height_ = static_cast<int>(ceil(static_cast<float>(
+      height_ + 2 * pad_h_ - kernel_h_) / stride_h_)) + 1;
+  pooled_width_ = static_cast<int>(ceil(static_cast<float>(
+      width_ + 2 * pad_w_ - kernel_w_) / stride_w_)) + 1;
+  if (pad_h_ || pad_w_) {
+    // If we have padding, ensure that the last pooling starts strictly
+    // inside the image (instead of at the padding); otherwise clip the last.
+    if ((pooled_height_ - 1) * stride_h_ >= height_ + pad_h_) {
+      --pooled_height_;
+    }
+    if ((pooled_width_ - 1) * stride_w_ >= width_ + pad_w_) {
+      --pooled_width_;
+    }
+    CHECK_LT((pooled_height_ - 1) * stride_h_, height_ + pad_h_);
+    CHECK_LT((pooled_width_ - 1) * stride_w_, width_ + pad_w_);
+  }
+  top[0]->Reshape(bottom[0]->num(), channels_, pooled_height_,
+      pooled_width_);
+  if (top.size() > 1) {
+    top[1]->ReshapeLike(*top[0]);
+  }
+  // If max pooling, we will initialize the vector index part.
+    max_idx_.Reshape(bottom[0]->num(), channels_, pooled_height_,
+        pooled_width_);
+  }
+
+// TODO(Yangqing): Is there a faster way to do pooling in the channel-first
+// case?
+template <typename Dtype>
+void SaliencyPoolingLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  Dtype* top_data = top[0]->mutable_cpu_data();
+  const int top_count = top[0]->count();
+  // We'll output the mask to top[1] if it's of size >1.
+  const bool use_top_mask = top.size() > 1;
+  int* mask = NULL;  // suppress warnings about uninitalized variables
+  Dtype* top_mask = NULL;
+  // Different pooling methods. We explicitly do the switch outside the for
+  // loop to save time, although this results in more code.
+  // Initialize
+  if (use_top_mask) {
+    top_mask = top[1]->mutable_cpu_data();
+    caffe_set(top_count, Dtype(-1), top_mask);
+  } else {
+    mask = max_idx_.mutable_cpu_data();
+    caffe_set(top_count, -1, mask);
+  }
+  caffe_set(top_count, Dtype(-FLT_MAX), top_data);
+  // The main loop
+  for (int n = 0; n < bottom[0]->num(); ++n) {
+    for (int c = 0; c < channels_; ++c) {
+      for (int ph = 0; ph < pooled_height_; ++ph) {
+        for (int pw = 0; pw < pooled_width_; ++pw) {
+          int hstart = ph * stride_h_ - pad_h_;
+          int wstart = pw * stride_w_ - pad_w_;
+          int hend = min(hstart + kernel_h_, height_);
+          int wend = min(wstart + kernel_w_, width_);
+          hstart = max(hstart, 0);
+          wstart = max(wstart, 0);
+          const int pool_index = ph * pooled_width_ + pw;
+          for (int h = hstart; h < hend; ++h) {
+            for (int w = wstart; w < wend; ++w) {
+              const int index = h * width_ + w;
+              if (bottom_data[index] > top_data[pool_index]) {
+                top_data[pool_index] = bottom_data[index];
+                if (use_top_mask) {
+                  top_mask[pool_index] = static_cast<Dtype>(index);
+                } else {
+                  mask[pool_index] = index;
+                }
+              }
+            }
+          }
+        }
+      }
+      // compute offset
+      bottom_data += bottom[0]->offset(0, 1);
+      top_data += top[0]->offset(0, 1);
+      if (use_top_mask) {
+        top_mask += top[0]->offset(0, 1);
+      } else {
+        mask += top[0]->offset(0, 1);
+      }
+    }
+  }
+}
+
+template <typename Dtype>
+void SaliencyPoolingLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  if (!propagate_down[0]) {
+    return;
+  }
+  const Dtype* top_diff = top[0]->cpu_diff();
+  Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+  // Different pooling methods. We explicitly do the switch outside the for
+  // loop to save time, although this results in more codes.
+  caffe_set(bottom[0]->count(), Dtype(0), bottom_diff);
+  // We'll output the mask to top[1] if it's of size >1.
+  const bool use_top_mask = top.size() > 1;
+  const int* mask = NULL;  // suppress warnings about uninitialized variables
+  const Dtype* top_mask = NULL;
+  // The main loop
+  if (use_top_mask) {
+    top_mask = top[1]->cpu_data();
+  } else {
+    mask = max_idx_.cpu_data();
+  }
+  for (int n = 0; n < top[0]->num(); ++n) {
+    for (int c = 0; c < channels_; ++c) {
+      for (int ph = 0; ph < pooled_height_; ++ph) {
+        for (int pw = 0; pw < pooled_width_; ++pw) {
+          const int index = ph * pooled_width_ + pw;
+          const int bottom_index =
+              use_top_mask ? top_mask[index] : mask[index];
+          bottom_diff[bottom_index] += top_diff[index];
+        }
+      }
+      bottom_diff += bottom[0]->offset(0, 1);
+      top_diff += top[0]->offset(0, 1);
+      if (use_top_mask) {
+        top_mask += top[0]->offset(0, 1);
+      } else {
+        mask += top[0]->offset(0, 1);
+      }
+    }
+  }
+}
+
+
+#ifdef CPU_ONLY
+STUB_GPU(SaliencyPoolingLayer);
+#endif
+
+INSTANTIATE_CLASS(SaliencyPoolingLayer);
+
+}  // namespace caffe
---
+++ src/caffe/layers/saliency_pooling_layer.cu
@@ -0,0 +1,169 @@
+#include <algorithm>
+#include <cfloat>
+#include <vector>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+
+#include "caffe/layers/saliency_pooling_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+__global__ void SalPoolForward(const int nthreads,
+    const Dtype* const image_data, const Dtype* const saliency_data,
+    const int num, const int channels, const int height, const int width,
+    const int pooled_height, const int pooled_width, const int kernel_h,
+    const int kernel_w, const int stride_h, const int stride_w, const int pad_h,
+    const int pad_w, Dtype* const top_image_data, int* mask, Dtype* top_mask) {
+  CUDA_KERNEL_LOOP(index, nthreads) {
+    const int pw = index % pooled_width;
+    const int ph = (index / pooled_width) % pooled_height;
+    const int c = (index / pooled_width / pooled_height) % channels;
+    const int n = index / pooled_width / pooled_height / channels;
+    int hstart = ph * stride_h - pad_h;
+    int wstart = pw * stride_w - pad_w;
+    const int hend = min(hstart + kernel_h, height);
+    const int wend = min(wstart + kernel_w, width);
+    hstart = max(hstart, 0);
+    wstart = max(wstart, 0);
+    Dtype maxval = -FLT_MAX;
+    int maxidx = -1;
+    const Dtype* const bottom_slice = image_data + (n * channels + c) * height * width;
+
+    for (int h = hstart; h < hend; ++h) {
+      for (int w = wstart; w < wend; ++w) {
+        if (bottom_slice[h * width + w] > maxval) {
+          maxidx = h * width + w;
+          maxval = bottom_slice[maxidx];
+        }
+      }
+    }
+    top_image_data[index] = maxval;
+    if (mask) {
+      mask[index] = maxidx;
+    } else {
+      top_mask[index] = maxidx;
+    }
+  }
+}
+
+template <typename Dtype>
+void SaliencyPoolingLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  const vector<Blob<Dtype>*>& top){
+    const Dtype* image_data = bottom[0]->gpu_data();
+    const Dtype* saliency_data = bottom[1]->gpu_data();
+    Dtype* top_image_data = top[0]->mutable_gpu_data();
+    Dtype* top_saliency_data = top[1]->mutable_gpu_data();
+    int count = top[0]->count();
+    // We'll output the mask to top[1] if it's of size >1.
+    const bool use_top_mask = top.size() > 1;
+    int* mask = NULL;
+    Dtype* top_mask = NULL;
+
+    printf("Bottom[0]: Image\n");
+    printf("Num: \t%d\n", bottom[0]->num());
+    printf("Channels: \t%d\n", bottom[0]->channels());
+    printf("Height: \t%d\n", bottom[0]->height());
+    printf("Width: \t%d\n", bottom[0]->width());
+
+    printf("\nBottom[1]: Saliency Map\n");
+    printf("Num: \t%d\n", bottom[1]->num());
+    printf("Channels: \t%d\n", bottom[1]->channels());
+    printf("Height: \t%d\n", bottom[1]->height());
+    printf("Width: \t%d\n", bottom[1]->width());
+
+    if (use_top_mask) {
+      top_mask = top[1]->mutable_gpu_data();
+    } else {
+      mask = max_idx_.mutable_gpu_data();
+    }
+    // NOLINT_NEXT_LINE(whitespace/operators)
+    SalPoolForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+        count, image_data, saliency_data, bottom[0]->num(), channels_,
+        height_, width_, pooled_height_, pooled_width_, kernel_h_,
+        kernel_w_, stride_h_, stride_w_, pad_h_, pad_w_, top_image_data,
+        mask, top_mask);
+    CUDA_POST_KERNEL_CHECK;
+  }
+
+
+template <typename Dtype>
+__global__ void SalPoolBackward(const int nthreads, const Dtype* const top_diff,
+    const int* const mask, const Dtype* const top_mask, const int num,
+    const int channels, const int height, const int width,
+    const int pooled_height, const int pooled_width, const int kernel_h,
+    const int kernel_w, const int stride_h, const int stride_w, const int pad_h,
+    const int pad_w, Dtype* const bottom_diff) {
+  CUDA_KERNEL_LOOP(index, nthreads) {
+    // find out the local index
+    // find out the local offset
+    const int w = index % width;
+    const int h = (index / width) % height;
+    const int c = (index / width / height) % channels;
+    const int n = index / width / height / channels;
+    const int phstart =
+         (h + pad_h < kernel_h) ? 0 : (h + pad_h - kernel_h) / stride_h + 1;
+    const int phend = min((h + pad_h) / stride_h + 1, pooled_height);
+    const int pwstart =
+         (w + pad_w < kernel_w) ? 0 : (w + pad_w - kernel_w) / stride_w + 1;
+    const int pwend = min((w + pad_w) / stride_w + 1, pooled_width);
+    Dtype gradient = 0;
+    const int offset = (n * channels + c) * pooled_height * pooled_width;
+    const Dtype* const top_diff_slice = top_diff + offset;
+    if (mask) {
+      const int* const mask_slice = mask + offset;
+      for (int ph = phstart; ph < phend; ++ph) {
+        for (int pw = pwstart; pw < pwend; ++pw) {
+          if (mask_slice[ph * pooled_width + pw] == h * width + w) {
+            gradient += top_diff_slice[ph * pooled_width + pw];
+          }
+        }
+      }
+    } else {
+      const Dtype* const top_mask_slice = top_mask + offset;
+      for (int ph = phstart; ph < phend; ++ph) {
+        for (int pw = pwstart; pw < pwend; ++pw) {
+          if (top_mask_slice[ph * pooled_width + pw] == h * width + w) {
+            gradient += top_diff_slice[ph * pooled_width + pw];
+          }
+        }
+      }
+    }
+    bottom_diff[index] = gradient;
+  }
+}
+
+template <typename Dtype>
+void SaliencyPoolingLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  if (!propagate_down[0]) {
+    return;
+  }
+  const Dtype* top_diff = top[0]->gpu_diff();
+  Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
+  const int count = bottom[0]->count();
+  caffe_gpu_set(count, Dtype(0.), bottom_diff);
+  // We'll output the mask to top[1] if it's of size >1.
+  const bool use_top_mask = top.size() > 1;
+  const int* mask = NULL;
+  const Dtype* top_mask = NULL;
+
+  if (use_top_mask) {
+    top_mask = top[1]->gpu_data();
+  } else {
+    mask = max_idx_.gpu_data();
+  }
+  // NOLINT_NEXT_LINE(whitespace/operators)
+  SalPoolBackward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+      count, top_diff, mask, top_mask, top[0]->num(), channels_,
+      height_, width_, pooled_height_, pooled_width_,
+      kernel_h_, kernel_w_, stride_h_, stride_w_, pad_h_, pad_w_,
+      bottom_diff);
+  CUDA_POST_KERNEL_CHECK;
+}
+
+INSTANTIATE_LAYER_GPU_FUNCS(SaliencyPoolingLayer);
+
+}  // namespace caffe
