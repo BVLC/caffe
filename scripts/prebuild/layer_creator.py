507cbcad765241463216dca301f1e8a8d5e2370f
Fabian Tschopp <fabian@m-tschopp.ch>
2018-02-04 04:40:16 +0100

    Python 3 compatibility.

--- scripts/prebuild/layer_creator.py
+++ scripts/prebuild/layer_creator.py
@@ -19,35 +19,35 @@ header.write('namespace caffe {\n')
 header.write('inline shared_ptr<LayerBase> CreateLayer(LayerParameter layer_param) {\n')
 header.write('switch(layer_param.bottom_data_type()) {\n')
 
-var_types = proto_types.keys()
+var_types = list(proto_types.keys())
 
 for i in range(0, len(var_types)):
-    if var_types[i] in variant_enable_flags.keys():
+    if var_types[i] in list(variant_enable_flags.keys()):
         header.write('#if defined(' + variant_enable_flags[var_types[i]] + ')\n')
     header.write('case ' + proto_types[var_types[i]] + ': {\n')
     header.write('switch(layer_param.compute_data_type()) {\n')
     for j in range(0, len(var_types)):
-        if var_types[j] in variant_enable_flags.keys():
+        if var_types[j] in list(variant_enable_flags.keys()):
             header.write('#if defined(' + variant_enable_flags[var_types[j]] + ')\n')
         header.write('case ' + proto_types[var_types[j]] + ': {\n')
         header.write('switch(layer_param.top_data_type()) {\n')
         for k in range(0, len(var_types)):
-            if var_types[k] in variant_enable_flags.keys():
+            if var_types[k] in list(variant_enable_flags.keys()):
                 header.write('#if defined(' + variant_enable_flags[var_types[k]] + ')\n')
             header.write('case ' + proto_types[var_types[k]] + ': {\n')
             header.write('return LayerRegistry<' + var_types[i]  + ', ' + var_types[j] + ', ' + var_types[k] + '>::CreateLayer(layer_param);\n')
             header.write('}\n')  # case
-            if var_types[k] in variant_enable_flags.keys():
+            if var_types[k] in list(variant_enable_flags.keys()):
                 header.write('#endif\n')
         header.write('default: { LOG(FATAL) << "Data types not enabled or supported"; }\n')
         header.write('}\n')  # switch
         header.write('}\n')  # case
-        if var_types[j] in variant_enable_flags.keys():
+        if var_types[j] in list(variant_enable_flags.keys()):
             header.write('#endif\n')
     header.write('default: { LOG(FATAL) << "Data types not enabled or supported"; }\n')
     header.write('}\n')  # switch
     header.write('}\n')  # case
-    if var_types[i] in variant_enable_flags.keys():
+    if var_types[i] in list(variant_enable_flags.keys()):
         header.write('#endif\n')
 header.write('default: { LOG(FATAL) << "Data types not enabled or supported"; }\n')
 header.write('}\n') # switch
--- scripts/prebuild/quantizer_creator.py
+++ scripts/prebuild/quantizer_creator.py
@@ -18,25 +18,25 @@ header.write('namespace caffe {\n')
 header.write('inline shared_ptr<QuantizerBase> CreateQuantizer(QuantizerParameter quant_param) {\n')
 header.write('switch (quant_param.input_data_type()) {\n')
 
-var_types = proto_types.keys()
+var_types = list(proto_types.keys())
 
 for i in range(0, len(var_types)):
-    if var_types[i] in variant_enable_flags.keys():
+    if var_types[i] in list(variant_enable_flags.keys()):
         header.write('#if defined(' + variant_enable_flags[var_types[i]] + ')\n')
     header.write('case ' + proto_types[var_types[i]] + ': {\n')
     header.write('switch (quant_param.output_data_type()) {\n')
     for j in range(0, len(var_types)):
-        if var_types[j] in variant_enable_flags.keys():
+        if var_types[j] in list(variant_enable_flags.keys()):
             header.write('#if defined(' + variant_enable_flags[var_types[j]] + ')\n')
         header.write('case ' + proto_types[var_types[j]] + ': {\n')
         header.write('return make_shared<Quantizer<' + var_types[i]  + ', ' + var_types[j] + '> >(quant_param);\n')
         header.write('}\n')  # case
-        if var_types[j] in variant_enable_flags.keys():
+        if var_types[j] in list(variant_enable_flags.keys()):
             header.write('#endif\n')
     header.write('default: { LOG(FATAL) << "Data types not enabled or supported"; }\n')
     header.write('}\n')  # switch
     header.write('}\n')  # case
-    if var_types[i] in variant_enable_flags.keys():
+    if var_types[i] in list(variant_enable_flags.keys()):
         header.write('#endif\n')
 header.write('default: { LOG(FATAL) << "Data types not enabled or supported"; }\n')
 header.write('}\n')  # switch
