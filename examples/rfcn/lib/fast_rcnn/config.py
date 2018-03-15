b5e6707cdba85682b38ca2f0086c4f6e033d800b
Feng Tian <feng.tian@intel.com>
2018-03-15 09:41:41 +0800

    fix wrong prototxt file path

--- examples/rfcn/lib/fast_rcnn/config.py
+++ examples/rfcn/lib/fast_rcnn/config.py
@@ -208,7 +208,7 @@ __C.ROOT_DIR = osp.abspath(osp.join(osp.dirname(__file__), '..', '..'))
 __C.DATA_DIR = osp.abspath(osp.join(__C.ROOT_DIR, 'data'))
 
 # Model directory
-__C.MODELS_DIR = osp.abspath(osp.join(__C.ROOT_DIR, '..', '..', 'models', 'intel_optimized_models', 'faster-rcnn', 'pascal_voc'))
+__C.MODELS_DIR = osp.abspath(osp.join(__C.ROOT_DIR, '..', '..', 'models', 'intel_optimized_models', 'rfcn', 'pascal_voc'))
 
 # Name (or path to) the matlab executable
 __C.MATLAB = 'matlab'
