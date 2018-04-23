ae76772ae27e29459f00ec42babaee104f0da9c2
twmht <qrnnis2623891@gmail.com>
2018-04-23 15:33:06 +0800

    fix when adding the extra top blob in softmax_with_loss

--- src/caffe/layers/softmax_loss_layer.cpp
+++ src/caffe/layers/softmax_loss_layer.cpp
@@ -13,6 +13,7 @@ void SoftmaxWithLossLayer<Dtype>::LayerSetUp(
   LossLayer<Dtype>::LayerSetUp(bottom, top);
   LayerParameter softmax_param(this->layer_param_);
   softmax_param.set_type("Softmax");
+  softmax_param.clear_loss_weight();
   softmax_layer_ = LayerRegistry<Dtype>::CreateLayer(softmax_param);
   softmax_bottom_vec_.clear();
   softmax_bottom_vec_.push_back(bottom[0]);
