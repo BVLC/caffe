4f9e1edda9afeddea41174cf951de82470dbdd42
Feng Tian <feng.tian@intel.com>
2018-05-22 16:56:02 +0800

    update mklml version to the one recommended by mkldnn 0.14

--- external/mkl/prepare_mkl.sh
+++ external/mkl/prepare_mkl.sh
@@ -74,11 +74,11 @@ echo $VERSION_LINE  # Return Version Line
 # MKL
 DST=`dirname $0`
 OMP=0 
-VERSION_MATCH=20171227
-ARCHIVE_BASENAME=mklml_lnx_2018.0.1.20171227.tgz
+VERSION_MATCH=20180406
+ARCHIVE_BASENAME=mklml_lnx_2018.0.3.20180406.tgz
 MKL_CONTENT_DIR=`echo $ARCHIVE_BASENAME | rev | cut -d "." -f 2- | rev`
 
-MKLURL="https://github.com/01org/mkl-dnn/releases/download/v0.12/$ARCHIVE_BASENAME"
+MKLURL="https://github.com/intel/mkl-dnn/releases/download/v0.14/$ARCHIVE_BASENAME"
 # there are diffrent MKL lib to be used for GCC and for ICC
 reg='^[0-9]+$'
 VERSION_LINE=`GetVersionName $MKLROOT`
