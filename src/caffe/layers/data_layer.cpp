c5890958285267f81ee868db3a8c0786170bc332
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2018-09-06 22:04:10 +0200

    add optional bottom to data_layer that is only mirrored and rotated but not noised nor distorted

--- src/caffe/layers/data_layer.cpp
+++ src/caffe/layers/data_layer.cpp
@@ -34,12 +34,22 @@ void DataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
   // Use data_transformer to infer the expected blob shape from datum.
   vector<int> top_shape = this->data_transformer_->InferBlobShape(datum);
   this->transformed_data_.Reshape(top_shape);
+  if (this->untransformed_top_)
+    this->untransformed_data_.Reshape(top_shape);
+
   // Reshape top[0] and prefetch_data according to the batch_size.
   top_shape[0] = batch_size;
   top[0]->Reshape(top_shape);
   for (int i = 0; i < this->PREFETCH_COUNT; ++i) {
     this->prefetch_[i].data_.Reshape(top_shape);
   }
+  if (this->untransformed_top_)
+    {
+      for (int i = 0; i < this->PREFETCH_COUNT; ++i) {
+        this->prefetch_untransformed_[i].data_.Reshape(top_shape);
+      }
+    }
+
   LOG(INFO) << "output data size: " << top[0]->num() << ","
       << top[0]->channels() << "," << top[0]->height() << ","
       << top[0]->width();
@@ -50,8 +60,20 @@ void DataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
     for (int i = 0; i < this->PREFETCH_COUNT; ++i) {
       this->prefetch_[i].label_.Reshape(label_shape);
     }
+    if (this->untransformed_top_)
+      for (int i = 0; i < this->PREFETCH_COUNT; ++i) {
+        this->prefetch_untransformed_[i].label_.Reshape(label_shape);
+      }
+
   }
+
+  if (this->untransformed_top_)
+    {
+      top[2]->Reshape(top_shape);
+    }
 }
+
+
 
 // This function is called on prefetch thread
 template<typename Dtype>
@@ -63,7 +85,7 @@ void DataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
   CPUTimer timer;
   CHECK(batch->data_.count());
   CHECK(this->transformed_data_.count());
-  
+
   // Reshape according to the first datum of each batch
   // on single input batches allows for inputs of varying dimension.
   const int batch_size = this->layer_param_.data_param().batch_size();
@@ -71,12 +93,15 @@ void DataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
   // Use data_transformer to infer the expected blob shape from datum.
   vector<int> top_shape = this->data_transformer_->InferBlobShape(datum);
   this->transformed_data_.Reshape(top_shape);
+
   // Reshape batch according to the batch_size.
   top_shape[0] = batch_size;
   batch->data_.Reshape(top_shape);
 
   Dtype* top_data = batch->data_.mutable_cpu_data();
   Dtype* top_label = NULL;  // suppress warnings about uninitialized variables
+
+
 
   if (this->output_labels_) {
     top_label = batch->label_.mutable_cpu_data();
@@ -96,7 +121,6 @@ void DataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
       top_label[item_id] = datum.label();
     }
     trans_time += timer.MicroSeconds();
-    
     reader_.free().push(const_cast<Datum*>(&datum));
   }
   timer.Stop();
@@ -105,6 +129,88 @@ void DataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
   DLOG(INFO) << "     Read time: " << read_time / 1000 << " ms.";
   DLOG(INFO) << "Transform time: " << trans_time / 1000 << " ms.";
 }
+
+
+// This function is called on prefetch thread
+template<typename Dtype>
+void DataLayer<Dtype>::load_batch_and_untransformed_batch(Batch<Dtype>* batch, Batch<Dtype>* batch_untransformed) {
+  CPUTimer batch_timer;
+  batch_timer.Start();
+  double read_time = 0;
+  double trans_time = 0;
+  CPUTimer timer;
+  CHECK(batch->data_.count());
+  CHECK(this->transformed_data_.count());
+  if (this->untransformed_top_)
+    {
+      CHECK(batch_untransformed->data_.count());
+      CHECK(this->untransformed_data_.count());
+    }
+
+  // Reshape according to the first datum of each batch
+  // on single input batches allows for inputs of varying dimension.
+  const int batch_size = this->layer_param_.data_param().batch_size();
+  Datum& datum = *(reader_.full().peek());
+  // Use data_transformer to infer the expected blob shape from datum.
+  vector<int> top_shape = this->data_transformer_->InferBlobShape(datum);
+  this->transformed_data_.Reshape(top_shape);
+  if (this->untransformed_top_)
+    this->untransformed_data_.Reshape(top_shape);
+
+  // Reshape batch according to the batch_size.
+  top_shape[0] = batch_size;
+  batch->data_.Reshape(top_shape);
+  if (this->untransformed_top_)
+    batch_untransformed->data_.Reshape(top_shape);
+
+  Dtype* top_data = batch->data_.mutable_cpu_data();
+  Dtype* top_label = NULL;  // suppress warnings about uninitialized variables
+
+
+  Dtype* untransformed_top_data;
+  Dtype* untransformed_top_label = NULL;  // suppress warnings about uninitialized variables
+  if (this->untransformed_top_)
+    untransformed_top_data = batch_untransformed->data_.mutable_cpu_data();
+
+
+  if (this->output_labels_) {
+    top_label = batch->label_.mutable_cpu_data();
+    if (this->untransformed_top_)
+      untransformed_top_label = batch_untransformed->label_.mutable_cpu_data();
+  }
+  for (int item_id = 0; item_id < batch_size; ++item_id) {
+    timer.Start();
+    // get a datum
+    Datum& datum = *(reader_.full().pop("Waiting for data"));
+    read_time += timer.MicroSeconds();
+    timer.Start();
+    // Apply data transformations (mirror, scale, crop...)
+    int offset = batch->data_.offset(item_id);
+    this->transformed_data_.set_cpu_data(top_data + offset);
+    this->data_transformer_->Transform(datum, &(this->transformed_data_));
+    if (this->untransformed_top_)
+      {
+        this->untransformed_data_.set_cpu_data(untransformed_top_data + offset);
+        // apply mirror bu not noise neither distort neither rotate
+        this->data_transformer_->Transform(datum, &(this->untransformed_data_),true, false);
+      }
+    // Copy label.
+    if (this->output_labels_) {
+      top_label[item_id] = datum.label();
+      if (this->untransformed_top_)
+        untransformed_top_label[item_id] = datum.label();
+    }
+    trans_time += timer.MicroSeconds();
+
+    reader_.free().push(const_cast<Datum*>(&datum));
+  }
+  timer.Stop();
+  batch_timer.Stop();
+  DLOG(INFO) << "Prefetch batch: " << batch_timer.MilliSeconds() << " ms.";
+  DLOG(INFO) << "     Read time: " << read_time / 1000 << " ms.";
+  DLOG(INFO) << "Transform time: " << trans_time / 1000 << " ms.";
+}
+
 
 INSTANTIATE_CLASS(DataLayer);
 REGISTER_LAYER_CLASS(Data);
