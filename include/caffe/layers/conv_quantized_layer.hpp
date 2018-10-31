9fc289e435ac41b8429503c4a2cff4f2e571815d
Andrew Anderson <aanderso@tcd.ie>
2018-10-31 16:09:04 +0000

    Add computation of output saliencies

--- include/caffe/layers/conv_quantized_layer.hpp
+++ include/caffe/layers/conv_quantized_layer.hpp
@@ -72,6 +72,9 @@ class ConvolutionQuantizedLayer : public BaseConvolutionLayer<Dtype> {
   /// @brief The spatial dimensions of the weights_masked_.
   vector<int> weights_quantized_shape_;
 
+  /// @brief The spatial dimensions of the centroids_.
+  vector<int> centroids_shape_;
+
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
