5ccc35b5ff45f84f1e64528019c97211e4d48f0e
Andrew Anderson <aanderso@tcd.ie>
2018-10-31 12:07:45 +0000

    Update quantized layer: call it quantized layer. Add options to caffe.proto to control quantization

--- src/caffe/util/upgrade_proto.cpp
+++ src/caffe/util/upgrade_proto.cpp
@@ -893,8 +893,8 @@ const char* UpgradeV1LayerType(const V1LayerParameter_LayerType type) {
     return "Convolution";
   case V1LayerParameter_LayerType_CONVOLUTIONMASKED:
     return "ConvolutionMasked";
-  case V1LayerParameter_LayerType_CONVOLUTIONCLUSTERED:
-    return "ConvolutionClustered";
+  case V1LayerParameter_LayerType_CONVOLUTIONQUANTIZED:
+    return "ConvolutionQuantized";
   case V1LayerParameter_LayerType_DECONVOLUTION:
     return "Deconvolution";
   case V1LayerParameter_LayerType_DATA:
