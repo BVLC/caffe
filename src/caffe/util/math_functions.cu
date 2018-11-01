ab2d953da7df9521e1b1c881023aceff7cc59c96
Andrew Anderson <aanderso@tcd.ie>
2018-11-01 16:31:31 +0000

    I hate templates

--- src/caffe/util/math_functions.cu
+++ src/caffe/util/math_functions.cu
@@ -1,4 +1,4 @@
-#include <math_functions.h>  // CUDA's, not caffe's, for fabs, signbit
+#include <cuda_runtime_api.h>  // CUDA's, not caffe's, for fabs, signbit
 #include <thrust/device_vector.h>
 #include <thrust/functional.h>  // thrust::plus
 #include <thrust/reduce.h>
