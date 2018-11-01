d85adc36917a107acff257f1289f3854efe7ba4c
Andrew Anderson <aanderso@tcd.ie>
2018-11-01 16:08:23 +0000

    Add GPU implementation of quantization

--- src/caffe/util/math_functions.cu
+++ src/caffe/util/math_functions.cu
@@ -388,14 +388,14 @@ void caffe_gpu_powx<double>(const int N, const double* a,
 }
 
 template <>
-__global__ void round_kernel(const int n, const float* a, float* y) {
+__global__ void round_kernel<float>(const int n, const float* a, float* y) {
   CUDA_KERNEL_LOOP(index, n) {
     y[index] = (Dtype) __float2int_rn(a[index]);
   }
 }
 
 template <>
-__global__ void round_kernel(const int n, const double* a, double* y) {
+__global__ void round_kernel<double>(const int n, const double* a, double* y) {
   CUDA_KERNEL_LOOP(index, n) {
     y[index] = (Dtype) __double2ll_rn(a[index]);
   }
@@ -416,14 +416,14 @@ void caffe_gpu_round<double>(const int N, const double* a, double* y) {
 }
 
 template <>
-__global__ void and_kernel(const int n, const unsigned m, const float* a, float* y) {
+__global__ void and_kernel<float, unsigned long>(const int n, const unsigned m, const float* a, float* y) {
   CUDA_KERNEL_LOOP(index, n) {
     y[index] = __int_as_float(m & __float_as_int(a[index]));
   }
 }
 
 template <>
-__global__ void and_kernel(const int n, const unsigned long long m, const double* a, double* y) {
+__global__ void and_kernel<double, unsigned long long>(const int n, const unsigned long long m, const double* a, double* y) {
   CUDA_KERNEL_LOOP(index, n) {
     y[index] = __longlong_as_double(m & __double_as_longlong(a[index]));
   }
@@ -433,14 +433,14 @@ template <>
 void caffe_gpu_and<float, unsigned long>(const int N, const std::bitset<8*sizeof(float)> m,
   const float* a, float* y) {
   // NOLINT_NEXT_LINE(whitespace/operators)
-  and_kernel<float><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(N, m.to_ulong(), a, y);
+  and_kernel<float, unsigned long><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(N, m.to_ulong(), a, y);
 }
 
 template <>
 void caffe_gpu_and<double, unsigned long long>(const int N, const std::bitset<8*sizeof(double)> m,
   const double* a, double* y) {
   // NOLINT_NEXT_LINE(whitespace/operators)
-  and_kernel<double><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(N, m.to_ullong(), a, y);
+  and_kernel<double, unsigned long long><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(N, m.to_ullong(), a, y);
 }
 
 template <typename Dtype>
