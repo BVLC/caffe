8663516260724b5a6df3386f6a1902de1076508a
Jinhang Choi <cepiross@gmail.com>
2019-02-20 14:56:00 -0500

    Have to use old-style version macro. OpenCV2.x use the following version pattern: <epoch>.<major>.<minor>.<revision> (e.g. 2.4.13.6)

--- src/caffe/test/test_io.cpp
+++ src/caffe/test/test_io.cpp
@@ -3,7 +3,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/highgui/highgui_c.h>
 #include <opencv2/imgproc/imgproc.hpp>
-#if CV_VERSION_MAJOR >= 4
+#if CV_MAJOR_VERSION >= 4
 #include <opencv2/imgcodecs/legacy/constants_c.h>
 #endif 
 
