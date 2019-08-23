3101248ed43cb754f12038f56924e5c3732e3cf3
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2019-08-23 10:52:01 +0200

    adamW

--- src/caffe/solvers/sgd_solver.cpp
+++ src/caffe/solvers/sgd_solver.cpp
@@ -231,6 +231,8 @@ void SGDSolver<Dtype>::Regularize(int param_id) {
             local_decay,
             temp_[param_id]->cpu_data(),
             net_params[param_id]->mutable_cpu_diff());
+      } else if (regularization_type == "decoupled") {
+        // do nothing, decoupled weight decay will be perfomer later
       } else {
         LOG(FATAL) << "Unknown regularization type: " << regularization_type;
       }
@@ -254,6 +256,8 @@ void SGDSolver<Dtype>::Regularize(int param_id) {
             local_decay,
             temp_[param_id]->gpu_data(),
             net_params[param_id]->mutable_gpu_diff());
+      } else if (regularization_type == "decoupled") {
+        // do nothing, decoupled weight decay will be applyed later
       } else {
         LOG(FATAL) << "Unknown regularization type: " << regularization_type;
       }
