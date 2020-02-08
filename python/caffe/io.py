f638034a9854fa867538d6989b058f0eb36a9d06
nhs000 <33925625+nhs000@users.noreply.github.com>
2020-02-08 17:26:09 +0700

    update `as_grey` argument of `skimage.io.imread` to `as_gray`
    due to https://github.com/scikit-image/scikit-image/commit/4feccd002729653a5784569414fbc2b8e313fcaa

--- python/caffe/io.py
+++ python/caffe/io.py
@@ -299,7 +299,7 @@ def load_image(filename, color=True):
         of size (H x W x 3) in RGB or
         of size (H x W x 1) in grayscale.
     """
-    img = skimage.img_as_float(skimage.io.imread(filename, as_grey=not color)).astype(np.float32)
+    img = skimage.img_as_float(skimage.io.imread(filename, as_gray=not color)).astype(np.float32)
     if img.ndim == 2:
         img = img[:, :, np.newaxis]
         if color:
